5ce92c0a191183c41c048eb508034f1df73eba42
rybex <tomek.rybka@gmail.com>
2015-04-01 19:10:22 +0200

    Added new errors

--- rails_event_store/lib/rails_event_store/errors.rb
+++ rails_event_store/lib/rails_event_store/errors.rb
@@ -1,4 +1,6 @@
 module RailsEventStore
   WrongExpectedEventVersion  = Class.new(StandardError)
   IncorrectStreamData        = Class.new(StandardError)
+  EventCannotBeSaved         = Class.new(StandardError)
+  EventNotFound              = Class.new(StandardError)
 end
