4d8993f6e3e2420a87479e1306219836fd37d6db
rybex <tomek.rybka@gmail.com>
2015-04-01 19:12:49 +0200

    Added calls to newly created services to client facade

--- lib/rails_event_store/client.rb
+++ lib/rails_event_store/client.rb
@@ -9,20 +9,20 @@ module RailsEventStore
       #TODO
     end
 
-    def read_events_forward
-      #TODO
+    def read_events_forward(stream_name, start, count)
+      Actions::ReadEventsBatch.new(event_repository).call(stream_name, start, count, :forward)
     end
 
-    def read_events_backward
-      #TODO
+    def read_events_backward(stream_name, start, count)
+      Actions::ReadEventsBatch.new(event_repository).call(stream_name, start, count, :backward)
     end
 
-    def read_all_events_forward
-      #TODO
+    def read_all_events_forward(stream_name)
+      Actions::ReadAllEvents.new(event_repository).call(stream_name, :forward)
     end
 
-    def read_all_events_backward
-      #TODO
+    def read_all_events_backward(stream_name)
+      Actions::ReadAllEvents.new(event_repository).call(stream_name, :backward)
     end
 
     private
