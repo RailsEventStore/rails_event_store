d65e95b03dd37efe35dbb76b8f708875d2fd8bfb
rybex <tomek.rybka@gmail.com>
2015-04-08 09:53:21 +0200

    We have our logotype

--- lib/rails_event_store.rb
+++ lib/rails_event_store.rb
@@ -1,10 +1,16 @@
+if defined?(Rails)
+  require 'rails_event_store/models/event'
+  require 'rails_event_store/generators/migrate_generator'
+  require 'rails_event_store/generators/templates/migration_template'
+end
 require 'rails_event_store/version'
-require 'rails_event_store/models/event' if defined?(Rails)
 require 'rails_event_store/models/event_entity'
 require 'rails_event_store/repositories/repository'
 require 'rails_event_store/repositories/event_repository'
 require 'rails_event_store/actions/append_event_to_stream'
 require 'rails_event_store/actions/read_all_events'
 require 'rails_event_store/actions/read_events_batch'
+require 'rails_event_store/actions/delete_stream_events'
+require 'rails_event_store/actions/read_all_strams'
 require 'rails_event_store/errors'
 require 'rails_event_store/client'
