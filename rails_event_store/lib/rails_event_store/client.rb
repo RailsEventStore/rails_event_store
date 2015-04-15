b9b62c626238326bb613f897de8ece78c34b80f9
rybex <tomek.rybka@gmail.com>
2015-04-15 14:56:04 +0200

    Finished refactoring. Now we are using method publish_event for global events and stream events

--- rails_event_store/lib/rails_event_store/client.rb
+++ rails_event_store/lib/rails_event_store/client.rb
@@ -6,18 +6,13 @@ module RailsEventStore
       @observers = []
     end
 
-    def append_to_stream(event_data, stream_name="all", expected_version = nil)
+    def publish_event(event_data, stream_name="all", expected_version = nil)
       event = Actions::AppendEventToStream.new(@repository).call(stream_name, event_data, expected_version)
       notify_observers(event)
     end
 
     def delete_stream(stream_name)
       Actions::DeleteStreamEvents.new(@repository).call(stream_name)
-    end
-
-    def publish_event(event_data, expected_version = nil)
-      event = Actions::AppendEventToStream.new(@repository).call("global", event_data, expected_version)
-      notify_observers(event)
     end
 
     def read_events(stream_name, start, count)
