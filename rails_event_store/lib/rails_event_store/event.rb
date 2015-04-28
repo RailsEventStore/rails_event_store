889fb6fb0f54d8fc3205d16d4c22d58deb627ef9
rybex <tomek.rybka@gmail.com>
2015-04-28 11:29:09 +0200

    Using demodularization during creation of default event_type. Created test for event model

--- rails_event_store/lib/rails_event_store/event.rb
+++ rails_event_store/lib/rails_event_store/event.rb
@@ -5,17 +5,14 @@ module RailsEventStore
 
     def initialize(event_data)
       @event_type = event_data.fetch(:event_type, event_name)
-      @event_id = event_data.fetch(:event_id, generate_id).to_s
-      @metadata = event_data.fetch(:metadata, nil)
-      @data = event_data.fetch(:data, nil)
+      @event_id   = event_data.fetch(:event_id, generate_id).to_s
+      @metadata   = event_data.fetch(:metadata, nil)
+      @data       = event_data.fetch(:data, nil)
     end
-
     attr_reader :event_type, :event_id, :metadata, :data
 
     def validate!
-      [event_type, event_id, data].each do |attribute|
-        raise IncorrectStreamData if is_invalid?(attribute)
-      end
+      raise IncorrectStreamData if data.nil? || data.empty?
     end
 
     def to_h
@@ -29,16 +26,12 @@ module RailsEventStore
 
     private
 
-    def is_invalid?(attribute)
-      attribute.nil? || attribute.empty?
-    end
-
     def generate_id
       SecureRandom.uuid
     end
 
     def event_name
-      self.class.name
+      self.class.name.demodulize
     end
   end
 end
