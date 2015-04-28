31f58b2d80383f473f44de850f2b28ca855c06fa
rybex <tomek.rybka@gmail.com>
2015-04-28 11:29:09 +0200

    Using demodularization during creation of default event_type. Created test for event model

---
+++ spec/event_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+module Test
+  class TestCreated < RailsEventStore::Event
+  end
+end
+
+module RailsEventStore
+  describe Event do
+
+    specify 'for empty data it initializes instance with default values' do
+      event = Test::TestCreated.new({})
+      expect(event.event_type).to eq  'TestCreated'
+      expect(event.event_id).to_not   be_nil
+      expect(event.metadata).to       be_nil
+      expect(event.data).to           be_nil
+    end
+
+    specify 'for empty events data it throws validation exception' do
+      event = Test::TestCreated.new({})
+      expect { event.validate! }.to raise_error(IncorrectStreamData)
+    end
+
+    specify 'convert to hash' do
+      event_data = {
+          data: { data: 'sample' },
+          event_id: 'b2d506fd-409d-4ec7-b02f-c6d2295c7edd',
+          event_type: 'OrderCreated',
+          metadata: { meta: 'test'}
+      }
+      event = Test::TestCreated.new(event_data)
+      expect(event.to_h).to eq event_data
+    end
+  end
+end
