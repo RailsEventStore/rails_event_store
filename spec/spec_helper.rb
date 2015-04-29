c75ddbc063c15737fb5c481821bf8642b73d38a9
rybex <tomek.rybka@gmail.com>
2015-04-29 11:12:51 +0200

    We shoul load this only on CI because locally we dont have climate reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
-require 'codeclimate-test-reporter'
-CodeClimate::TestReporter.start
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rails_event_store'
