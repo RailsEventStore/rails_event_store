e784f327da7e41c8bcd69ace47bcb0817f46eae8
Rafał Łasocha <me+dev@swistak35.com>
2015-04-30 19:43:09 +0200

    Add an argument to EventHandler

--- rails_event_store/lib/rails_event_store/event_handlers/event_handler.rb
+++ rails_event_store/lib/rails_event_store/event_handlers/event_handler.rb
@@ -1,7 +1,7 @@
 module RailsEventStore
   module EventHandlers
     class EventHandler
-      def handle_event
+      def handle_event(event)
       end
     end
   end
--- rails_event_store/spec/event_handlers/event_handler_spec.rb
+++ rails_event_store/spec/event_handlers/event_handler_spec.rb
@@ -6,5 +6,10 @@ module RailsEventStore::EventHandlers
       event_handler = EventHandler.new
       expect(event_handler).to respond_to(:handle_event)
     end
+
+    specify "#handle_event should accept one argument" do
+      event_handler = EventHandler.new
+      event_handler.handle_event("foo")
+    end
   end
 end
