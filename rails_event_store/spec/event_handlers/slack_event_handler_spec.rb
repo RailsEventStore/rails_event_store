f235be031dc30195843fc9524a1da6c0a8edf229
Rafał Łasocha <me+dev@swistak35.com>
2015-04-30 20:50:23 +0200

    Added code sending request to webhook_url in SlackEventHandler

--- rails_event_store/spec/event_handlers/slack_event_handler_spec.rb
+++ rails_event_store/spec/event_handlers/slack_event_handler_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 module RailsEventStore
   describe EventHandlers::SlackEventHandler do
-    let(:webhook_url) { "http://hooks.slack.com/example" }
+    let(:webhook_url) { "https://hooks.slack.com/example" }
     let(:repository) { EventInMemoryRepository.new }
     let(:client) { Client.new(repository) }
 
@@ -11,12 +11,16 @@ module RailsEventStore
       expect(slack_event_handler).to respond_to(:handle_event)
     end
 
-    specify "should be able to subscribe" do
+    specify "should send request to slack" do
+      stub_request(:any, webhook_url)
+
       slack_event_handler = EventHandlers::SlackEventHandler.new(webhook_url)
 
       client.subscribe(slack_event_handler)
       event = OrderCreated.new({data: 'sample'})
       client.publish_event(event)
+
+      expect(a_request(:post, webhook_url)).to have_been_made
     end
   end
 end
