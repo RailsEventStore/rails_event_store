54a801533a2c04647b91c3d3054320aa99680eee
Rafał Łasocha <me+dev@swistak35.com>
2015-05-07 17:09:24 +0200

    Added error when model is not valid

--- rails_event_store/lib/rails_event_store/command.rb
+++ rails_event_store/lib/rails_event_store/command.rb
@@ -1,8 +1,11 @@
 module RailsEventStore
   class Command
+    ValidationError = Class.new(StandardError)
+
     include ActiveModel::Validations
 
     def validate!
+      raise ValidationError.new unless valid?
     end
   end
 end
