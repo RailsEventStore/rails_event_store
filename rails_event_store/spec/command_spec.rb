12d48d3295441c8eb7e8080e678a3752f008fd5b
Rafał Łasocha <me+dev@swistak35.com>
2015-05-07 17:03:20 +0200

    Added validations to Command class

--- rails_event_store/spec/command_spec.rb
+++ rails_event_store/spec/command_spec.rb
@@ -1,9 +1,19 @@
 require 'spec_helper.rb'
 
+module Commands
+  class CreateOrder < RailsEventStore::Command
+    attr_accessor :order_id
+
+    validates :order_id, presence: true
+  end
+end
+
 module RailsEventStore
   describe Client do
-    it "is available in namespace" do
-      RailsEventStore::Command.new
+    it "should have validations" do
+      cmd = Commands::CreateOrder.new
+      cmd.order_id = 5
+      expect(cmd.valid?).to eq(true)
     end
   end
 end
