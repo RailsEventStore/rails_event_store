4e942e049d54a634a8f52581b24b120304a88a79
rybex <tomek.rybka@gmail.com>
2015-05-25 17:27:48 +0200

    Moved event class from RES

---
+++ ruby_event_store/lib/ruby_event_store/event.rb
@@ -0,0 +1,37 @@
+require 'securerandom'
+
+module RubyEventStore
+  class Event
+
+    def initialize(event_data={})
+      @event_type = event_data.fetch(:event_type, event_name)
+      @event_id   = event_data.fetch(:event_id, generate_id).to_s
+      @metadata   = event_data.fetch(:metadata, {})
+      @data       = event_data.fetch(:data, {})
+    end
+    attr_reader :event_type, :event_id, :metadata, :data
+
+    def to_h
+      {
+          event_type: event_type,
+          event_id:   event_id,
+          metadata:   metadata.merge!(publish_time),
+          data:       data
+      }
+    end
+
+    private
+
+    def publish_time
+      { published_at: Time.now.utc }
+    end
+
+    def generate_id
+      SecureRandom.uuid
+    end
+
+    def event_name
+      self.class.name
+    end
+  end
+end
