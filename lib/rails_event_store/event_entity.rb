35198d8e61c2321efa12993f9c78cd6c8c020ca5
Pierre-Louis Gottfrois <pierrelouis.gottfrois@gmail.com>
2015-07-21 17:55:57 +0200

    fix: allow event entity to call methods on data

--- lib/rails_event_store/event_entity.rb
+++ lib/rails_event_store/event_entity.rb
@@ -6,5 +6,17 @@ module RailsEventStore
 
     attr_accessor :id, :stream, :event_type, :event_id,
                   :metadata, :data, :created_at
+
+    def method_missing(method_name, *args, &block)
+      data.send(method_name, *args, &block)
+    end
+
+    def respond_to_missing?(method_name, include_private = false)
+      data.respond_to?(method_name)
+    end
+
+    def data
+      @_data ||= OpenStruct.new(@data)
+    end
   end
 end
