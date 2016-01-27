01867736fe08ddcf18ba68c87bd0e69f2b2d0e80
Mirosław Pragłowski <m@praglowski.com>
2016-01-27 18:15:29 +0100

    Minor code refactor

--- rails_event_store/spec/matchers/event_store_matcher.rb
+++ rails_event_store/spec/matchers/event_store_matcher.rb
@@ -2,7 +2,7 @@ RSpec::Matchers.define :be_event do |expected|
   match do |actual|
     expect(actual.event_type).to eq expected[:event_type]
     expect(actual.data).to eq expected[:data]
-    expect(actual.metadata).to eq expected[:metadata] if expected[:metadata]
-    if expected.key? :event_id then expect(actual.event_id).to eq expected[:event_id] else true end
+    expect(actual.metadata).to eq expected[:metadata] if expected.key?(:metadata)
+    expect(actual.event_id).to eq expected[:event_id] if expected.key?(:event_id)
   end
 end
