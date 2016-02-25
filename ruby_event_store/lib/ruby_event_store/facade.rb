b8c828070df6f7b7d8ddd3e4f8eeedc20714db7f
Mirosław Pragłowski <m@praglowski.com>
2016-02-25 22:47:40 +0100

    Methods, methods everywhere... and each 1 liners (almost)
    Having private method that are one liners just to call them from
    public methods is a additional friction for me. Reducing friction
    by removing private methods.

--- ruby_event_store/lib/ruby_event_store/facade.rb
+++ ruby_event_store/lib/ruby_event_store/facade.rb
@@ -7,23 +7,24 @@ module RubyEventStore
     attr_reader :repository
 
     def publish_event(event_data, stream_name = GLOBAL_STREAM, expected_version = nil)
-      append_event_to_stream(event_data, stream_name, expected_version)
+      event = Actions::AppendEventToStream.new(repository).call(stream_name, event_data, expected_version)
+      event_broker.notify_subscribers(event)
     end
 
     def delete_stream(stream_name)
-      delete_stream_events(stream_name)
+      Actions::DeleteStreamEvents.new(repository).call(stream_name)
     end
 
     def read_events(stream_name, start, count)
-      read_events_batch(stream_name, start, count)
+      Actions::ReadEventsBatch.new(repository).call(stream_name, start, count)
     end
 
     def read_all_events(stream_name)
-      read_all_stream_events(stream_name)
+      Actions::ReadAllEvents.new(repository).call(stream_name)
     end
 
     def read_all_streams
-      read_all_events_from_store
+      Actions::ReadAllStreams.new(repository).call
     end
 
     def subscribe(subscriber, event_types)
@@ -35,27 +36,6 @@ module RubyEventStore
     end
 
     private
-
-    def append_event_to_stream(event_data, stream_name, expected_version)
-      event = Actions::AppendEventToStream.new(repository).call(stream_name, event_data, expected_version)
-      event_broker.notify_subscribers(event)
-    end
-
-    def delete_stream_events(stream_name)
-      Actions::DeleteStreamEvents.new(repository).call(stream_name)
-    end
-
-    def read_events_batch(stream_name, start, count)
-      Actions::ReadEventsBatch.new(repository).call(stream_name, start, count)
-    end
-
-    def read_all_stream_events(stream_name)
-      Actions::ReadAllEvents.new(repository).call(stream_name)
-    end
-
-    def read_all_events_from_store
-      Actions::ReadAllStreams.new(repository).call
-    end
 
     def event_broker
       @event_broker ||= PubSub::Broker.new
