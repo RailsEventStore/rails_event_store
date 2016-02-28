f0293e49f931c0077d95d52fa051082dda209bb9
Mirosław Pragłowski <m@praglowski.com>
2016-02-29 00:00:13 +0100

    No more group by in reading from all streams
    And that required change of expectations

--- rails_event_store/spec/client_spec.rb
+++ rails_event_store/spec/client_spec.rb
@@ -10,18 +10,17 @@ module RailsEventStore
 
     specify 'read_all_streams' do
       client = Client.new
-      expect(client.read_all_streams).to eq({})
+      expect(client.read_all_streams).to eq([])
 
       OrderPlaced = Class.new(RailsEventStore::Event)
-      client.publish_event(OrderPlaced.new)
-      client.publish_event(OrderPlaced.new, "stream-1")
-      client.publish_event(OrderPlaced.new, "stream-2")
-      client.publish_event(OrderPlaced.new, "stream-2")
+      client.publish_event(OrderPlaced.new(order_id: 1))
+      client.publish_event(OrderPlaced.new(order_id: 2), "stream-1")
+      client.publish_event(OrderPlaced.new(order_id: 3), "stream-2")
+      client.publish_event(OrderPlaced.new(order_id: 4), "stream-2")
 
       actuals = client.read_all_streams
-      expect(actuals["all"].count).to eq 1
-      expect(actuals["stream-1"].count).to eq 1
-      expect(actuals["stream-2"].count).to eq 2
+      expect(actuals.count).to eq 4
+      expect(actuals.map(&:order_id)).to eq [1,2,3,4]
     end
   end
 end
