dcd7609f9727a8115cc9feca8ed141ef3a1c08f3
Mirosław Pragłowski <m@praglowski.com>
2016-02-28 23:53:04 +0100

    EventRepository methods & implementation as required by RubyEventStore 0.3.0

--- lib/rails_event_store/repositories/event_repository.rb
+++ lib/rails_event_store/repositories/event_repository.rb
@@ -7,35 +7,81 @@ module RailsEventStore
       end
       attr_reader :adapter
 
-      def find(condition)
-        build_event_entity(adapter.where(condition).first)
-      end
-
-      def create(data)
+      def create(event, stream_name)
+        data = event.to_h.merge!(stream: stream_name)
         build_event_entity(adapter.create(data))
-      rescue ActiveRecord::RecordNotUnique
-        raise EventCannotBeSaved
       end
 
-      def delete(condition)
+      def delete_stream(stream_name)
+        condition = {stream: stream_name}
         adapter.destroy_all condition
         nil
       end
 
-      def get_all_events
-        adapter.order('id ASC').order('stream').all.map(&method(:build_event_entity))
+      def has_event?(event_id)
+        adapter.exists?(event_id: event_id)
       end
 
       def last_stream_event(stream_name)
         build_event_entity(adapter.where(stream: stream_name).last)
       end
 
-      def load_all_events_forward(stream_name)
-        adapter.where(stream: stream_name).order('id ASC').map(&method(:build_event_entity))
+      def get_all_events
+        adapter.order('id ASC').order('stream').all.map(&method(:build_event_entity))
       end
 
-      def load_events_batch(stream_name, start_point, count)
-        adapter.where('id >= ? AND stream = ?', start_point, stream_name).limit(count).map(&method(:build_event_entity))
+      def read_events_forward(stream_name, start_event_id, count)
+        stream = adapter.where(stream: stream_name)
+        if start_event_id
+          starting_event = adapter.find_by(event_id: start_event_id)
+          stream = stream.where('id > ?', starting_event.id)
+        end
+
+        stream.order('id ASC').limit(count)
+          .map(&method(:build_event_entity))
+      end
+
+      def read_events_backward(stream_name, start_event_id, count)
+        stream = adapter.where(stream: stream_name)
+        if start_event_id
+          starting_event = adapter.find_by(event_id: start_event_id)
+          stream = stream.where('id < ?', starting_event.id)
+        end
+
+        stream.order('id DESC').limit(count)
+          .map(&method(:build_event_entity))
+      end
+
+      def read_stream_events_forward(stream_name)
+        adapter.where(stream: stream_name).order('id ASC')
+          .map(&method(:build_event_entity))
+      end
+
+      def read_stream_events_backward(stream_name)
+        adapter.where(stream: stream_name).order('id DESC')
+          .map(&method(:build_event_entity))
+      end
+
+      def read_all_streams_forward(start_event_id, count)
+        stream = adapter
+        if start_event_id
+          starting_event = adapter.find_by(event_id: start_event_id)
+          stream = stream.where('id > ?', starting_event.id)
+        end
+
+        stream.order('id ASC').limit(count)
+          .map(&method(:build_event_entity))
+      end
+
+      def read_all_streams_backward(start_event_id, count)
+        stream = adapter
+        if start_event_id
+          starting_event = adapter.find_by(event_id: start_event_id)
+          stream = stream.where('id < ?', starting_event.id)
+        end
+
+        stream.order('id DESC').limit(count)
+          .map(&method(:build_event_entity))
       end
 
       private
@@ -45,7 +91,6 @@ module RailsEventStore
         ::RailsEventStore::Event.new(record.data.merge(
           event_type: record.event_type,
           event_id:   record.event_id,
-          stream:     record.stream,
           metadata:   record.metadata))
       end
 
