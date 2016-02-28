2a396825e8e31818adc37c674de323117f1052a2
Mirosław Pragłowski <m@praglowski.com>
2016-02-28 17:00:35 +0100

    Move code from actions to Facade class
    Actions are here just a simle wrappers around repository, no need to add this abstraction layer here.

--- ruby_event_store/lib/ruby_event_store/facade.rb
+++ ruby_event_store/lib/ruby_event_store/facade.rb
@@ -7,24 +7,29 @@ module RubyEventStore
     attr_reader :repository
 
     def publish_event(event_data, stream_name = GLOBAL_STREAM, expected_version = nil)
-      event = Actions::AppendEventToStream.new(repository).call(stream_name, event_data, expected_version)
+      raise WrongExpectedEventVersion if version_incorrect?(stream_name, expected_version)
+      event = repository.create(event_data.to_h.merge!(stream: stream_name))
       event_broker.notify_subscribers(event)
     end
 
     def delete_stream(stream_name)
-      Actions::DeleteStreamEvents.new(repository).call(stream_name)
+      raise IncorrectStreamData if stream_name.nil? || stream_name.empty?
+      repository.delete({stream: stream_name})
     end
 
     def read_events(stream_name, start, count)
-      Actions::ReadEventsBatch.new(repository).call(stream_name, start, count)
+      raise IncorrectStreamData if stream_name.nil? || stream_name.empty?
+      event = find_event(start)
+      repository.load_events_batch(stream_name, event.id, count)
     end
 
     def read_all_events(stream_name)
-      Actions::ReadAllEvents.new(repository).call(stream_name)
+      raise IncorrectStreamData if stream_name.nil? || stream_name.empty?
+      repository.load_all_events_forward(stream_name)
     end
 
     def read_all_streams
-      Actions::ReadAllStreams.new(repository).call
+      repository.get_all_events
     end
 
     def subscribe(subscriber, event_types)
@@ -39,6 +44,22 @@ module RubyEventStore
 
     def event_broker
       @event_broker ||= PubSub::Broker.new
+    end
+
+    def version_incorrect?(stream_name, expected_version)
+      unless expected_version.nil?
+        find_last_event_version(stream_name) != expected_version
+      end
+    end
+
+    def find_last_event_version(stream_name)
+      repository.last_stream_event(stream_name).event_id
+    end
+
+    def find_event(start)
+      event = repository.find({event_id: start})
+      raise EventNotFound if event.nil?
+      event
     end
   end
 end
