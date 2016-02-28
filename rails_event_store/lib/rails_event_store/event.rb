148532fdd985e666265a8d45c8b120ca67fd572e
Mirosław Pragłowski <m@praglowski.com>
2016-02-28 23:52:34 +0100

    Moved to RubyEventStore::Event

--- rails_event_store/lib/rails_event_store/event.rb
+++ rails_event_store/lib/rails_event_store/event.rb
@@ -1,19 +1,3 @@
 module RailsEventStore
-  class Event < RubyEventStore::Event
-    def initialize(**args)
-      attributes = args.except(:event_type, :event_id, :metadata)
-      singleton_class = (class << self; self; end)
-      attributes.each do |key, value|
-        singleton_class.send(:define_method, key) { value }
-      end
-
-      event_data = {
-        event_type: args[:event_type] || self.class.name,
-        data:       attributes,
-      }
-      event_data[:event_id] = args[:event_id] if args.key?(:event_id)
-      event_data[:metadata] = args[:metadata] if args.key?(:metadata)
-      super(event_data)
-    end
-  end
+  Event = RubyEventStore::Event
 end
