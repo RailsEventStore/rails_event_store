00573bfacd285f6fc489e89b2ac454382b37769f
Mirosław Pragłowski <m@praglowski.com>
2016-03-02 03:56:38 +0100

    Simpler implementation of InMemoryRepository

--- ruby_event_store/lib/ruby_event_store/in_memory_repository.rb
+++ ruby_event_store/lib/ruby_event_store/in_memory_repository.rb
@@ -2,24 +2,27 @@ require 'ostruct'
 
 module RubyEventStore
   class InMemoryRepository
-
     def initialize
-      reset!
+      @all = Array.new
+      @streams = Hash.new
     end
-    attr_reader :db
 
     def create(event, stream_name)
-      model = {index: db.length, stream: stream_name, event: event}
-      db.push(model)
+      stream = streams[stream_name] || Array.new
+      all.push(event)
+      stream.push(event)
+      streams[stream_name] = stream
       event
     end
 
     def delete_stream(stream_name)
-      db.reject! { |item| item.fetch(:stream) == stream_name }
+      removed = (streams[stream_name] || Array.new).map(&:event_id)
+      @streams.reject!{|stream| stream == stream_name}
+      @all.reject!{|ev| removed.include?(ev.event_id)}
     end
 
     def has_event?(event_id)
-      db.any?{ |item| item.fetch(:event).event_id == event_id }
+      all.any?{ |item| item.event_id.eql?(event_id) }
     end
 
     def last_stream_event(stream_name)
@@ -28,17 +31,16 @@ module RubyEventStore
 
     def read_events_forward(stream_name, start_event_id, count)
       source = read_stream_events_forward(stream_name)
-      read_batch(source, start_event_id, count, ->(a,b) { a > b })
+      read_batch(source, start_event_id, count)
     end
 
     def read_events_backward(stream_name, start_event_id, count)
       source = read_stream_events_backward(stream_name)
-      read_batch(source, start_event_id, count, ->(a,b) { a < b })
+      read_batch(source, start_event_id, count)
     end
 
     def read_stream_events_forward(stream_name)
-      db.select { |item| item.fetch(:stream) == stream_name }
-        .map{ |item| item.fetch(:event) }
+      streams[stream_name] || Array.new
     end
 
     def read_stream_events_backward(stream_name)
@@ -46,31 +48,29 @@ module RubyEventStore
     end
 
     def read_all_streams_forward(start_event_id, count)
-      read_batch(db.map{ |item| item.fetch(:event) }, start_event_id, count, ->(a,b) { a > b })
+      read_batch(all, start_event_id, count)
     end
 
     def read_all_streams_backward(start_event_id, count)
-      read_batch(db.map{ |item| item.fetch(:event) }.reverse, start_event_id, count, ->(a,b) { a < b })
-    end
-
-    def reset!
-      @db = Array.new
+      read_batch(all.reverse, start_event_id, count)
     end
 
     private
-    def read_batch(source, start_event_id, count, comparision)
+    attr_accessor :streams, :all
+
+    def read_batch(source, start_event_id, count)
       response = Array.new
-      start_index = index_of(start_event_id) unless start_event_id.instance_of?(Symbol)
+      start_index = index_of(source, start_event_id) unless start_event_id.instance_of?(Symbol)
       source.each do |event|
-        if (start_event_id == :head || comparision.(index_of(event.event_id), start_index)) && response.length < count
+        if (start_event_id.eql?(:head) || index_of(source, event.event_id) > start_index) && response.length < count
           response.push(event)
         end
       end
       response
     end
 
-    def index_of(event_id)
-      db.select { |item| item.fetch(:event).event_id == event_id }.first.fetch(:index)
+    def index_of(source, event_id)
+      source.index{ |item| item.event_id.eql?(event_id) }
     end
   end
 end
