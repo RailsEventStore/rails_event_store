8c98d9154bd6276be0451bfb68c772e7fe7c229b
Mirosław Pragłowski <m@praglowski.com>
2016-03-21 12:40:11 +0100

    Hide `ruby_event_store` internals by adding classes in RailsEventStore module

--- lib/rails_event_store.rb
+++ lib/rails_event_store.rb
@@ -1,10 +1,15 @@
 require 'ruby_event_store'
 
+module RailsEventStore
+  Event               = RubyEventStore::Event
+  InMemoryRepository  = RubyEventStore::InMemoryRepository
+  EventBroker         = RubyEventStore::PubSub::Broker
+end
+
 require 'rails_event_store/models/event'
 require 'rails_event_store/generators/migrate_generator'
 require 'rails_event_store/generators/templates/migration_template'
 require 'rails_event_store/version'
-require 'rails_event_store/event'
 require 'rails_event_store/repositories/event_repository'
 require 'rails_event_store/event_handlers/slack_event_handler'
 require 'rails_event_store/errors'
--- lib/rails_event_store/event.rb
+++
@@ -1,3 +0,0 @@
-module RailsEventStore
-  Event = RubyEventStore::Event
-end
