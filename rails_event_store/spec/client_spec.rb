9f1a3d2be2436f358f9be5ad9a47123d5e9822dd
Michal Lomnicki <michal.lomnicki@gmail.com>
2016-04-26 21:12:42 +0200

    Allow to read events backward
    Just delegated methods responsible for reading events backward
    to the repository.

--- rails_event_store/spec/client_spec.rb
+++ rails_event_store/spec/client_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 module RailsEventStore
+  TestEvent = Class.new(RailsEventStore::Event)
+
   describe Client do
 
     specify 'initialize proper adapter type' do
@@ -13,19 +15,74 @@ module RailsEventStore
       expect(client.__send__("event_broker")).to be_a EventBroker
     end
 
-    specify 'read_all_streams' do
+    specify 'read events forward' do
       client = Client.new
-      expect(client.read_all_streams).to eq([])
+      client.publish_event(TestEvent.new(event_id: '1'), 'stream')
+      client.publish_event(TestEvent.new(event_id: '2'), 'stream')
+      client.publish_event(TestEvent.new(event_id: '3'), 'stream')
+      client.publish_event(TestEvent.new(event_id: '4'), 'other_stream')
+      client.publish_event(TestEvent.new(event_id: '5'), 'stream')
 
-      OrderPlaced = Class.new(RailsEventStore::Event)
-      client.publish_event(OrderPlaced.new(order_id: 1))
-      client.publish_event(OrderPlaced.new(order_id: 2), "stream-1")
-      client.publish_event(OrderPlaced.new(order_id: 3), "stream-2")
-      client.publish_event(OrderPlaced.new(order_id: 4), "stream-2")
+      expect(client.read_events_forward('stream').map(&:event_id)).to eq(['1', '2', '3', '5'])
+      expect(client.read_events_forward('stream', :head).map(&:event_id)).to eq(['1', '2', '3', '5'])
+      expect(client.read_events_forward('stream', '1').map(&:event_id)).to eq(['2', '3', '5'])
+      expect(client.read_events_forward('stream', '1', 2).map(&:event_id)).to eq(['2', '3'])
+    end
 
-      actuals = client.read_all_streams
-      expect(actuals.count).to eq 4
-      expect(actuals.map(&:order_id)).to eq [1,2,3,4]
+    specify 'read events backward' do
+      client = Client.new
+      client.publish_event(TestEvent.new(event_id: '1'), 'stream')
+      client.publish_event(TestEvent.new(event_id: '2'), 'stream')
+      client.publish_event(TestEvent.new(event_id: '3'), 'stream')
+      client.publish_event(TestEvent.new(event_id: '4'), 'other_stream')
+      client.publish_event(TestEvent.new(event_id: '5'), 'stream')
+
+      expect(client.read_events_backward('stream').map(&:event_id)).to eq(['5', '3', '2', '1'])
+      expect(client.read_events_backward('stream', :head).map(&:event_id)).to eq(['5', '3', '2', '1'])
+      expect(client.read_events_backward('stream', '3').map(&:event_id)).to eq(['2', '1'])
+      expect(client.read_events_backward('stream', '3', 1).map(&:event_id)).to eq(['2'])
+    end
+
+    specify 'read all stream events forward' do
+      client = Client.new
+      client.publish_event(TestEvent.new(event_id: '1'), 'stream')
+      client.publish_event(TestEvent.new(event_id: '2'), 'other_stream')
+      client.publish_event(TestEvent.new(event_id: '3'), 'stream')
+
+      expect(client.read_stream_events_forward('stream').map(&:event_id)).to eq(['1', '3'])
+    end
+
+    specify 'read all stream events backward' do
+      client = Client.new
+      client.publish_event(TestEvent.new(event_id: '1'), 'stream')
+      client.publish_event(TestEvent.new(event_id: '2'), 'other_stream')
+      client.publish_event(TestEvent.new(event_id: '3'), 'stream')
+
+      expect(client.read_stream_events_backward('stream').map(&:event_id)).to eq(['3', '1'])
+    end
+
+    specify 'read all streams forward' do
+      client = Client.new
+      client.publish_event(TestEvent.new(event_id: '1'))
+      client.publish_event(TestEvent.new(event_id: '2'), 'stream-1')
+      client.publish_event(TestEvent.new(event_id: '3'), 'stream-2')
+      client.publish_event(TestEvent.new(event_id: '4'), 'stream-2')
+
+      expect(client.read_all_streams_forward.map(&:event_id)).to eq(['1', '2', '3', '4'])
+      expect(client.read_all_streams_forward(:head, 3).map(&:event_id)).to eq(['1', '2', '3'])
+      expect(client.read_all_streams_forward('2', 2).map(&:event_id)).to eq(['3', '4'])
+    end
+
+    specify 'read all streams backward' do
+      client = Client.new
+      client.publish_event(TestEvent.new(event_id: '1'))
+      client.publish_event(TestEvent.new(event_id: '2'), 'stream-1')
+      client.publish_event(TestEvent.new(event_id: '3'), 'stream-2')
+      client.publish_event(TestEvent.new(event_id: '4'), 'stream-2')
+
+      expect(client.read_all_streams_backward.map(&:event_id)).to eq(['4', '3', '2', '1'])
+      expect(client.read_all_streams_backward(:head, 3).map(&:event_id)).to eq(['4', '3', '2'])
+      expect(client.read_all_streams_backward('4', 2).map(&:event_id)).to eq(['3', '2'])
     end
   end
 end
