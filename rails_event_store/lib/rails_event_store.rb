31e50a2ad0657cbe9597bdec027751cac2f66893
Mirosław Pragłowski <m@praglowski.com>
2016-06-21 09:33:54 +0200

    Remove SlackEventHandler
    This is not a good fit to RailsEventStore.

--- rails_event_store/lib/rails_event_store.rb
+++ rails_event_store/lib/rails_event_store.rb
@@ -9,6 +9,5 @@ module RailsEventStore
 end
 
 require 'rails_event_store/version'
-require 'rails_event_store/event_handlers/slack_event_handler'
 require 'rails_event_store/client'
 require 'rails_event_store/constants'
