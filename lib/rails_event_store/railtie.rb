f5dec7792b5e1eb1886ff9dd08fe7f96c592861e
Paweł Pacana <pawel.pacana@gmail.com>
2016-06-28 23:44:27 +0200

    Allow customization of recorded metadata from request.
    To achieve this, set following callable (anything that responds to #call):
    
    Rails.application.configure do
      config.rails_event_store = {
        request_metadata: ->(env) do
          { some_key: env['valid_rack_env_key'] }
        end
      }
    end
    
    By Rack specification, you have following keys to use:
    http://www.rubydoc.info/github/rack/rack/file/SPEC
    
    Other may be added by the middleware used (i.e. https://github.com/hassox/warden).

--- lib/rails_event_store/railtie.rb
+++ lib/rails_event_store/railtie.rb
@@ -3,7 +3,28 @@ require 'rails_event_store/middleware'
 module RailsEventStore
   class Railtie < ::Rails::Railtie
     initializer 'rails_event_store.middleware' do |rails|
-      rails.middleware.use ::RailsEventStore::Middleware
+      rails.middleware.use(::RailsEventStore::Middleware, RailsConfig.new(rails.config).request_metadata)
+    end
+
+    class RailsConfig
+      def initialize(config)
+        @config = config
+      end
+
+      def request_metadata
+        return default_request_metadata unless @config.respond_to?(:rails_event_store)
+        @config.rails_event_store.fetch(:request_metadata, default_request_metadata)
+      end
+
+      private
+      def default_request_metadata
+        ->(env) do
+          request = ActionDispatch::Request.new(env)
+          { remote_ip:  request.remote_ip,
+            request_id: request.uuid,
+          }
+        end
+      end
     end
   end
 end
