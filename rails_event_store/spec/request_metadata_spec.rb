d196a81080843a7e99a982769bcfad2611ee04ed
Paweł Pacana <pawel.pacana@gmail.com>
2016-06-28 16:56:25 +0200

    Failing test case illustrating intent.
    We want to record Rails request details (i.e. IP address) so that it can
    be a help in audit log usecase. Such data can be recorded in metadata as
    not something directly related with particular event but still useful to
    track and associate.

---
+++ rails_event_store/spec/request_metadata_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+require 'action_controller/railtie'
+require 'rack/test'
+require 'securerandom'
+
+
+class TestRails
+  include Rack::Test::Methods
+
+  attr_reader :app
+
+  def initialize
+    @app = Class.new(::Rails::Application)
+  end
+
+  def call(action)
+    app.config.secret_key_base = SecureRandom.hex
+    app.config.eager_load = false
+    app.initialize!
+    app.routes.draw { root(to: ->(env) { action.(); [200, {}, ['']] }) }
+    app.default_url_options = { host: 'example.com' }
+    get('/')
+  end
+end
+
+module RailsEventStore
+  DummyEvent = Class.new(RailsEventStore::Event)
+
+  RSpec.describe 'request details in event metadata' do
+    specify do
+      event_store = Client.new
+
+      TestRails.new.(->(env) { event_store.publish_event(DummyEvent.new) })
+
+      expect(event_store.read_all_events(GLOBAL_STREAM))
+        .to_not(be_empty)
+      event_store.read_all_events(GLOBAL_STREAM)
+        .map  { |event|    event.metadata }
+        .each { |metadata| expect(metadata).to(include(remote_ip: '127.0.0.1')) }
+    end
+  end
+end
