0c4e12cd609fab0fed47b6f0c52f76d48022f2c2
Paweł Pacana <pawel.pacana@gmail.com>
2016-06-29 23:09:28 +0200

    Move metadata enrichment to event initializer.
    * it's not only publish_event but also append_to_stream interested in
      enriched metadata
    * consistent with timestamp also enriched in initializer

--- lib/rails_event_store.rb
+++ lib/rails_event_store.rb
@@ -2,7 +2,6 @@ require 'ruby_event_store'
 require 'rails_event_store_active_record'
 
 module RailsEventStore
-  Event               = RubyEventStore::Event
   InMemoryRepository  = RubyEventStore::InMemoryRepository
   EventBroker         = RubyEventStore::PubSub::Broker
   Projection          = RubyEventStore::Projection
@@ -11,4 +10,5 @@ end
 require 'rails_event_store/version'
 require 'rails_event_store/client'
 require 'rails_event_store/constants'
+require 'rails_event_store/event'
 require 'rails_event_store/railtie' if defined?(Rails::Railtie)
