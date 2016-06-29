10f94b3a1fc9752befae4ba6738391af2799a665
Mirosław Pragłowski <m@praglowski.com>
2016-06-29 09:24:53 +0200

    In-memory sqlite3 with schema load over prebaked filesystem blob.
    * safe for concurrency (i.e. running mutant) as each connection binds to
      new database instance and there's no lock
    * no need for transaction wrap, clean state for each test by loading schema

--- rails_event_store_active_record/spec/spec_helper.rb
+++ rails_event_store_active_record/spec/spec_helper.rb
@@ -8,14 +8,22 @@ require 'rails_event_store_active_record'
 
 RSpec.configure do |config|
   config.around(:each) do |example|
-    RailsEventStoreActiveRecord::Event.establish_connection(
-      :adapter => "sqlite3",
-      :database => "spec/test.sqlite3",
-      :timeout=> 10000
-    )
-    RailsEventStoreActiveRecord::Event.transaction do
-      example.run
-      raise ActiveRecord::Rollback
+    ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
+    ActiveRecord::Schema.define do
+      self.verbose = false
+      create_table(:event_store_events) do |t|
+        t.string      :stream,      null: false
+        t.string      :event_type,  null: false
+        t.string      :event_id,    null: false
+        t.text        :metadata
+        t.text        :data,        null: false
+        t.datetime    :created_at,  null: false
+      end
+      add_index :event_store_events, :stream
+      add_index :event_store_events, :created_at
+      add_index :event_store_events, :event_type
+      add_index :event_store_events, :event_id, unique: true
     end
+    example.run
   end
 end
