ba88a865f5d758ce5cff38590eb5d600cd99ac6a
Mirosław Pragłowski <m@praglowski.com>
2016-07-11 23:41:42 +0200

    Rebuild Event class - no more magic, data & metadata are just structs
    This is a breaking change. Now instead of `event.some_attribute` you
    will need to use `event.data.some_attribute`. This will allow to
    avoid name clashes(especially the one with `event_id`)

--- ruby_event_store/lib/ruby_event_store/event.rb
+++ ruby_event_store/lib/ruby_event_store/event.rb
@@ -1,34 +1,31 @@
 require 'securerandom'
+require 'closed_struct'
 
 module RubyEventStore
   class Event
-
-    def initialize(event_id: SecureRandom.uuid, metadata: {}, **data)
-      data.each do |key, value|
-        singleton_class.__send__(:define_method, key) { value }
-      end
-
+    def initialize(event_id: SecureRandom.uuid, metadata: {}, data: {})
       @event_id = event_id.to_s
-      @metadata = metadata
-      @data     = data
-      @metadata[:timestamp] ||= Time.now.utc
+      @metadata = ClosedStruct.new(metadata)
+      @data     = ClosedStruct.new(data)
     end
     attr_reader :event_id, :metadata, :data
 
     def to_h
       {
           event_id:   event_id,
-          metadata:   metadata,
-          data:       data
+          metadata:   metadata.to_h,
+          data:       data.to_h
       }
     end
 
     def timestamp
-      metadata[:timestamp]
+      metadata.timestamp rescue nil
     end
 
     def ==(other_event)
-      other_event.instance_of?(self.class) && other_event.to_h.eql?(to_h)
+      other_event.instance_of?(self.class) &&
+        other_event.event_id.eql?(event_id) &&
+        other_event.data.eql?(data)
     end
 
     alias_method :eql?, :==
