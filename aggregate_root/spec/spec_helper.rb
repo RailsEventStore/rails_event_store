4cc209e4fd7b1d4c43f0afc6537338e90c58a65a
Mirosław Pragłowski <m@praglowski.com>
2016-08-11 22:42:20 +0200

    Refactor tests to have 100% mutant free coverage

--- aggregate_root/spec/spec_helper.rb
+++ aggregate_root/spec/spec_helper.rb
@@ -5,3 +5,62 @@ end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'aggregate_root'
+require 'ruby_event_store'
+
+RSpec.configure do |spec|
+  spec.before(:each) do
+    AggregateRoot.configure do |config|
+      config.default_event_store = nil
+    end
+  end
+end
+
+module Orders
+  module Events
+    OrderCreated = Class.new(RubyEventStore::Event)
+  end
+end
+
+class Order
+  include AggregateRoot::Base
+
+  def initialize(id = generate_uuid)
+    self.id = id
+    @status = :draft
+  end
+
+  private
+  attr_accessor :status
+
+  def apply_orders_events_order_created(event)
+    @status = :created
+  end
+end
+
+class CustomOrderApplyStrategy
+  def call(aggregate, event)
+    {
+      Orders::Events::OrderCreated => aggregate.method(:custom_order_processor),
+    }.fetch(event.class, ->(ev) {}).call(event)
+  end
+end
+
+class OrderWithCustomStrategy
+  include AggregateRoot::Base
+
+  def initialize(id = generate_uuid)
+    self.id = id
+    @status = :draft
+  end
+
+  def apply_strategy
+    @apply_strategy ||= CustomOrderApplyStrategy.new
+  end
+
+  private
+  attr_accessor :status, :other_value
+
+  def custom_order_processor(event)
+    @status = :created
+  end
+end
