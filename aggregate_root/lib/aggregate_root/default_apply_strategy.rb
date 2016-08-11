0fc7f02c7e46393ab3b700d5ef3135141cf53699
Mirosław Pragłowski <m@praglowski.com>
2016-08-11 22:42:48 +0200

    No need for symbols here

--- aggregate_root/lib/aggregate_root/default_apply_strategy.rb
+++ aggregate_root/lib/aggregate_root/default_apply_strategy.rb
@@ -2,7 +2,7 @@ module AggregateRoot
   class DefaultApplyStrategy
     def call(aggregate, event)
       event_name_processed = event.class.to_s.underscore.gsub('/','_')
-      aggregate.method("apply_#{event_name_processed}".to_sym).call(event)
+      aggregate.method("apply_#{event_name_processed}").call(event)
     end
   end
 end
