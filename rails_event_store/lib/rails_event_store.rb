ade1e9586430676540f1c1a0933c9e3a73ddfa0d
Mirosław Pragłowski <m@praglowski.com>
2016-11-15 17:39:31 +0100

    No need for RailsEventStore::Repository
    Module attribute is the same kind of evil thing ;)

--- rails_event_store/lib/rails_event_store.rb
+++ rails_event_store/lib/rails_event_store.rb
@@ -1,5 +1,15 @@
+require 'active_support/core_ext/class/attribute_accessors'
 require 'rails_event_store_active_record'
 require 'rails_event_store/all'
 
+module RailsEventStore
+  mattr_reader :event_repository
+
+  def self.event_repository=(event_repository)
+    raise ArgumentError unless event_repository
+    @@event_repository = event_repository
+  end
+end
+
 # Use active record by default
-RailsEventStore::Repository.adapter = RailsEventStoreActiveRecord::EventRepository.new
+RailsEventStore.event_repository = RailsEventStoreActiveRecord::EventRepository.new
