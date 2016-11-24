728ebf0127beeea12e49d2f9d7f6727d6606c35f
Pierre-Louis Gottfrois <pierrelouis.gottfrois@gmail.com>
2016-11-24 16:31:34 +0100

    Fixes where is initialized event repository

--- lib/rails_event_store.rb
+++ lib/rails_event_store.rb
@@ -1,15 +1,5 @@
-require 'active_support/core_ext/class/attribute_accessors'
 require 'rails_event_store_active_record'
 require 'rails_event_store/all'
-
-module RailsEventStore
-  mattr_reader :event_repository
-
-  def self.event_repository=(event_repository)
-    raise ArgumentError unless event_repository
-    @@event_repository = event_repository
-  end
-end
 
 # Use active record by default
 RailsEventStore.event_repository = RailsEventStoreActiveRecord::EventRepository.new
--- lib/rails_event_store/all.rb
+++ lib/rails_event_store/all.rb
@@ -1,3 +1,4 @@
+require 'active_support/core_ext/class/attribute_accessors'
 require 'ruby_event_store'
 require 'rails_event_store/client'
 require 'rails_event_store/version'
@@ -17,4 +18,11 @@ module RailsEventStore
   InvalidPageSize           = RubyEventStore::InvalidPageSize
   GLOBAL_STREAM             = RubyEventStore::GLOBAL_STREAM
   PAGE_SIZE                 = RubyEventStore::PAGE_SIZE
+
+  mattr_reader :event_repository
+
+  def self.event_repository=(event_repository)
+    raise ArgumentError unless event_repository
+    @@event_repository = event_repository
+  end
 end
