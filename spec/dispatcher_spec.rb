b47ff95e947ab8a3f45d16b5d0a47c203192808a
Paweł Pacana <pawel.pacana@gmail.com>
2017-08-21 21:59:20 +0200

    InstrumentedDispatcher -> Dispatcher
    There's no other dispatcher in RailsEventStore namespace actually to
    name this one with specialized name.

--- spec/dispatcher_spec.rb
+++ spec/dispatcher_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'ruby_event_store/spec/dispatcher_lint'
 
 module RailsEventStore
-  RSpec.describe InstrumentedDispatcher do
-    it_behaves_like :dispatcher, InstrumentedDispatcher.new
+  RSpec.describe Dispatcher do
+    it_behaves_like :dispatcher, Dispatcher.new
   end
 end
