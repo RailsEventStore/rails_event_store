97e5f63068b3b3c95cb1622faa93f10144890088
Paweł Pacana <pawel.pacana@gmail.com>
2017-08-21 21:39:21 +0200

    Split instrumentation related spec to different file.
    Still maintaining "describe" with class name so that mutant can match
    specs to changed code.

--- rails_event_store/spec/instrumented_dispatcher_spec.rb
+++ rails_event_store/spec/instrumented_dispatcher_spec.rb
@@ -2,30 +2,7 @@ require 'spec_helper'
 require 'ruby_event_store/spec/dispatcher_lint'
 
 module RailsEventStore
-  DummyEvent = Class.new(RailsEventStore::Event)
-
   RSpec.describe InstrumentedDispatcher do
     it_behaves_like :dispatcher, InstrumentedDispatcher.new
-
-    specify do
-      client  = Client.new(event_broker: EventBroker.new(dispatcher: InstrumentedDispatcher.new))
-      event   = DummyEvent.new
-      handler = Proc.new { }
-      client.subscribe(handler, [DummyEvent])
-
-      notifications = []
-      callback = ->(name, started, finished, unique_id, data) do
-        notifications << unique_id
-
-        expect(data[:subscriber]).to eq(handler)
-        expect(data[:event]).to      eq(event)
-      end
-
-      ActiveSupport::Notifications.subscribed(callback, "dispatch.rails_event_store") do
-        client.publish_event(event)
-      end
-
-      expect(notifications.size).to eq(1)
-    end
   end
 end
