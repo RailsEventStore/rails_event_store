c596102a29447039e95d54d94fce16eceab2943e
Paweł Pacana <pawel.pacana@gmail.com>
2017-08-24 13:58:01 +0200

    Slightly different spec for migration generator.
    - no assertion on full content as it is not parametrized, just check if
      reminds on any migration
    - swap mocking for asserting of result

--- rails_event_store_active_record/spec/migration_generator_spec.rb
+++ rails_event_store_active_record/spec/migration_generator_spec.rb
@@ -1,13 +1,19 @@
 require 'spec_helper'
+require 'pp'
+require 'fakefs'
 
 module RailsEventStoreActiveRecord
   describe MigrationGenerator do
     specify do
-      generator = MigrationGenerator.new
-      expect(Time).to receive(:now).and_return(Time.new(2016,8,9,22,22,22))
-      expect(generator).to receive(:template).with(
-        "migration_template.rb", "db/migrate/20160809222222_create_event_store_events.rb")
-      generator.create_migration
+      FakeFS do
+        FakeFS::FileSystem.clone(File.expand_path('../../', __FILE__))
+
+        generator = MigrationGenerator.new
+        allow(Time).to receive(:now).and_return(Time.new(2016,8,9,22,22,22))
+        generator.create_migration
+
+        expect(File.read("db/migrate/20160809222222_create_event_store_events.rb")).to match("ActiveRecord::Migration")
+      end
     end
   end
 end
