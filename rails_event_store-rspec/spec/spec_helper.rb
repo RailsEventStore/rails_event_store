4a3fb764e5ba8142391fbdb0781865f7c5010447
Szymon Fiedler <fidelski@gmail.com>
2017-09-01 13:43:33 +0200

    Tooling for tests

--- rails_event_store-rspec/spec/spec_helper.rb
+++ rails_event_store-rspec/spec/spec_helper.rb
@@ -2,11 +2,12 @@ require "bundler/setup"
 require "rails_event_store/rspec"
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
-  # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
+
+  config.order = :random
+  Kernel.srand config.seed
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
