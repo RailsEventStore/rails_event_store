ed33ffd890ba75b6d9020b1a09135b4edec50d80
Mirosław Pragłowski <m@praglowski.com>
2017-09-01 18:30:16 +0200

    ActiveJobDispatcher specification defined as tests

--- rails_event_store/spec/active_job_dispatcher_spec.rb
+++ rails_event_store/spec/active_job_dispatcher_spec.rb
@@ -7,69 +7,125 @@ module RailsEventStore
   RSpec.describe ActiveJobDispatcher do
     it_behaves_like :dispatcher, ActiveJobDispatcher.new
 
+    around do |example|
+      with_queue_adapter(ActiveJob::Base, :test) do
+        original_logger = ActiveJob::Base.logger
+        ActiveJob::Base.logger = Logger.new(nil) # Silence messages "[ActiveJob] Enqueued ...".
+        example.run
+        ActiveJob::Base.logger = original_logger
+      end
+    end
+
+    before(:each) do
+      CallableHandler.reset
+      AsyncHandler.reset
+    end
+
+    let!(:event) { RailsEventStore::Event.new }
+    let!(:yaml)  { YAML.dump(event) }
+
     it "builds sync proxy for callable class" do
       handler = ActiveJobDispatcher.new.proxy_for(CallableHandler)
       expect(handler.respond_to?(:call)).to be_truthy
-      expect_any_instance_of(CallableHandler).to receive(:call)
-      handler.call(DummyEvent.new)
+      expect_any_instance_of(CallableHandler).to receive(:call).and_call_original
+      expect_no_enqueued_job do
+        handler.call(event)
+      end
     end
 
     it "builds async proxy for ActiveJob::Base ancestors" do
-      AsyncHandler.queue_adapter = :test
-      AsyncHandler.queue_adapter.perform_enqueued_jobs = true
-      AsyncHandler.queue_adapter.perform_enqueued_at_jobs = true
       handler = ActiveJobDispatcher.new.proxy_for(AsyncHandler)
 
       expect(handler.respond_to?(:call)).to be_truthy
-      event = DummyEvent.new
-      yaml = YAML.dump(event)
+      expect_to_have_enqueued_job(AsyncHandler) do
+        handler.call(event)
+      end
       expect(AsyncHandler.received).to be_nil
-      expect(AsyncHandler).to receive(:perform_later).with(yaml).and_call_original
-      expect_any_instance_of(AsyncHandler).to receive(:perform).with(yaml)
-      handler.call(event)
+      perform_enqueued_jobs(AsyncHandler.queue_adapter)
       expect(AsyncHandler.received).to eq(yaml)
     end
 
-    it "async proxy for defined adapter enqueue job immidiatelly when no transaction is open" do
-      AsyncHandler.queue_adapter = DummyAdapter.new
-      AsyncHandler.queue_adapter.perform_enqueued_jobs = true
-      AsyncHandler.queue_adapter.perform_enqueued_at_jobs = true
-      handler = ActiveJobDispatcher.new.proxy_for(AsyncHandler)
+    it "async proxy for defined adapter enqueue job immediately when no transaction is open" do
+      with_queue_adapter(AsyncHandler, DummyAdapter.new) do
+        handler = ActiveJobDispatcher.new.proxy_for(AsyncHandler)
 
-      expect(handler.respond_to?(:call)).to be_truthy
-      event = DummyEvent.new
-      yaml = YAML.dump(event)
-      expect(AsyncHandler.received).to be_nil
-      expect(AsyncHandler).to receive(:perform_later).with(yaml).and_call_original
-      expect_any_instance_of(AsyncHandler).to receive(:perform).with(yaml)
-      handler.call(event)
-      expect(AsyncHandler.received).to eq(yaml)
+        expect(handler.respond_to?(:call)).to be_truthy
+        expect_to_have_enqueued_job(AsyncHandler) do
+          handler.call(event)
+        end
+        expect(AsyncHandler.received).to be_nil
+        perform_enqueued_jobs(AsyncHandler.queue_adapter)
+        expect(AsyncHandler.received).to eq(yaml)
+      end
     end
 
     it "async proxy for defined adapter enqueue job only after transaction commit" do
-      AsyncHandler.queue_adapter = DummyAdapter.new
-      AsyncHandler.queue_adapter.perform_enqueued_jobs = true
-      AsyncHandler.queue_adapter.perform_enqueued_at_jobs = true
-      handler = ActiveJobDispatcher.new.proxy_for(AsyncHandler)
+      with_queue_adapter(AsyncHandler, DummyAdapter.new) do
+        handler = ActiveJobDispatcher.new.proxy_for(AsyncHandler)
 
-      expect(handler.respond_to?(:call)).to be_truthy
-      event = DummyEvent.new
-      yaml = YAML.dump(event)
-      expect(AsyncHandler).to receive(:perform_later).with(yaml).and_call_original
-      expect_any_instance_of(AsyncHandler).to receive(:perform).with(yaml)
-      ActiveRecord::Base.transaction do
-        handler.call(event)
+        expect(handler.respond_to?(:call)).to be_truthy
+        expect_to_have_enqueued_job(AsyncHandler) do
+          ActiveRecord::Base.transaction do
+            handler.call(event)
+          end
+        end
+        expect(AsyncHandler.received).to be_nil
+        perform_enqueued_jobs(AsyncHandler.queue_adapter)
+        expect(AsyncHandler.received).to eq(yaml)
+      end
+    end
+
+    it "async proxy for defined adapter do not enqueue job after transaction rollback" do
+      with_queue_adapter(AsyncHandler, DummyAdapter.new) do
+        handler = ActiveJobDispatcher.new.proxy_for(AsyncHandler)
+
+        expect_no_enqueued_job(AsyncHandler) do
+          ActiveRecord::Base.transaction do
+            handler.call(event)
+            raise ActiveRecord::Rollback
+          end
+        end
         expect(AsyncHandler.received).to be_nil
       end
-      expect(AsyncHandler.received).to eq(yaml)
+    end
+
+    def with_queue_adapter(job, queue_adapter, &proc)
+      raise unless block_given?
+      adapter = job.queue_adapter
+      job.queue_adapter = queue_adapter
+      yield
+      job.queue_adapter = adapter
+    end
+
+    def expect_no_enqueued_job(job = ActiveJob::Base, &proc)
+      raise unless block_given?
+      yield
+      expect(job.queue_adapter.enqueued_jobs).to be_empty
+    end
+
+    def expect_to_have_enqueued_job(job, &proc)
+        #expect {
+        #}.to have_enqueued_job(AsyncHandler)
+      raise unless block_given?
+      yield
+      found = job.queue_adapter.enqueued_jobs.select{|enqueued| enqueued[:job] == job}.count
+      expect(found).to eq(1)
+    end
+
+    def perform_enqueued_jobs(queue_adapter)
+      queue_adapter.enqueued_jobs.each do |enqueued|
+        enqueued[:job].perform_now(*enqueued[:args])
+      end
     end
 
     private
     DummyAdapter = Class.new(ActiveJob::QueueAdapters::TestAdapter)
-    DummyEvent = Class.new(RailsEventStore::Event)
 
     class CallableHandler
       @@received = nil
+      def self.reset
+        @@received = nil
+      end
       def self.received
         @@received
       end
@@ -80,6 +136,9 @@ module RailsEventStore
 
     class AsyncHandler < ActiveJob::Base
       @@received = nil
+      def self.reset
+        @@received = nil
+      end
       def self.received
         @@received
       end
