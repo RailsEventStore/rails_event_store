6ab7de16e59deb6dbfc30f8dae28b8f27c106c11
Robert Pankowecki <robert.pankowecki@gmail.com>
2017-09-29 14:34:11 +0200

    More specs

--- ruby_event_store/spec/dispatcher_spec.rb
+++ ruby_event_store/spec/dispatcher_spec.rb
@@ -3,8 +3,35 @@ require 'ruby_event_store/spec/dispatcher_lint'
 
 module RubyEventStore
   module PubSub
+
     RSpec.describe Dispatcher do
       it_behaves_like :dispatcher, Dispatcher.new
+
+      specify "does not allow silly subscribers" do
+        expect do
+          Dispatcher.new.verify(:symbol)
+        end.to raise_error(RubyEventStore::InvalidHandler, /\:symbol/)
+      end
+
+      specify "does not allow class without instance method #call" do
+        klass = Class.new do
+        end
+        expect do
+          Dispatcher.new.verify(klass)
+        end.to raise_error(RubyEventStore::InvalidHandler)
+      end
+
+      specify "does not allow class without constructor requiring arguments" do
+        klass = Class.new do
+          def initialize(something)
+            @something = something
+          end
+        end
+        expect do
+          Dispatcher.new.verify(klass)
+        end.to raise_error(RubyEventStore::InvalidHandler)
+      end
     end
+
   end
 end
