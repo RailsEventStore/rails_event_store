5fd0ebebff6c0ef564c04e51365c1033fd1e3252
Robert Pankowecki <robert.pankowecki@gmail.com>
2017-10-18 11:00:04 +0200

    Unify database setup across gems. part 2.

--- rails_event_store_active_record/lib/rails_event_store_active_record/generators/templates/migration_template.rb
+++ rails_event_store_active_record/lib/rails_event_store_active_record/generators/templates/migration_template.rb
@@ -1,21 +1,36 @@
 class CreateEventStoreEvents < ActiveRecord::Migration<%= migration_version %>
   def change
-    create_table(:event_store_events_in_streams) do |t|
+    postgres = ActiveRecord::Base.connection.adapter_name == "PostgreSQL"
+    enable_extension "pgcrypto" if postgres
+    create_table(:event_store_events_in_streams, force: false) do |t|
       t.string      :stream,      null: false
       t.integer     :position,    null: true
-      t.references  :event,       null: false, type: :uuid
+      if postgres
+        t.references :event, null: false, type: :uuid
+      else
+        t.references :event, null: false, type: :string
+      end
       t.datetime    :created_at,  null: false
     end
     add_index :event_store_events_in_streams, [:stream, :position], unique: true
     add_index :event_store_events_in_streams, [:created_at]
     add_index :event_store_events_in_streams, [:stream, :event_id], unique: true
-    # add_index :event_store_events_in_streams, [:event_uuid]
 
-    create_table(:event_store_events, id: :uuid) do |t|
-      t.string      :event_type,  null: false
-      t.text        :metadata
-      t.text        :data,        null: false
-      t.datetime    :created_at,  null: false
+    if postgres
+      create_table(:event_store_events, id: :uuid, default: 'gen_random_uuid()', force: false) do |t|
+        t.string      :event_type,  null: false
+        t.text        :metadata
+        t.text        :data,        null: false
+        t.datetime    :created_at,  null: false
+      end
+    else
+      create_table(:event_store_events, id: false, force: false) do |t|
+        t.string :id, limit: 36, primary_key: true, null: false
+        t.string      :event_type,  null: false
+        t.text        :metadata
+        t.text        :data,        null: false
+        t.datetime    :created_at,  null: false
+      end
     end
     add_index :event_store_events, :created_at
   end
--- rails_event_store_active_record/spec/spec_helper.rb
+++ rails_event_store_active_record/spec/spec_helper.rb
@@ -8,48 +8,19 @@ require 'rails_event_store_active_record'
 
 ENV['DATABASE_URL'] ||= "postgres://localhost/rails_event_store_active_record?pool=5"
 
+MigrationCode = File.read(File.expand_path('../../lib/rails_event_store_active_record/generators/templates/migration_template.rb', __FILE__) )
+migration_version = Gem::Version.new(ActiveRecord::VERSION::STRING) < Gem::Version.new("5.0.0") ? "" : "[4.2]"
+MigrationCode.gsub!("<%= migration_version %>", migration_version)
+MigrationCode.gsub!("force: false", "force: true")
+
 RSpec.configure do |config|
   config.failure_color = :magenta
   config.around(:each) do |example|
     ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
     ActiveRecord::Schema.define do
       self.verbose = false
-
-      enable_extension "plpgsql"
-      enable_extension "pgcrypto"
-
-      create_table(:event_store_events_in_streams, force: true) do |t|
-        t.string      :stream,      null: false
-        t.integer     :position,    null: true
-        if ENV['DATABASE_URL'].start_with?("postgres")
-          t.references :event, null: false, type: :uuid
-        else
-          t.references :event, null: false, type: :string
-        end
-        t.datetime    :created_at,  null: false
-      end
-      add_index :event_store_events_in_streams, [:stream, :position], unique: true
-      add_index :event_store_events_in_streams, [:created_at]
-      add_index :event_store_events_in_streams, [:stream, :event_id], unique: true
-      # add_index :event_store_events_in_streams, [:event_uuid]
-
-      if ENV['DATABASE_URL'].start_with?("postgres")
-        create_table(:event_store_events, id: :uuid, force: true) do |t|
-          t.string      :event_type,  null: false
-          t.text        :metadata
-          t.text        :data,        null: false
-          t.datetime    :created_at,  null: false
-        end
-      else
-        create_table(:event_store_events, id: false, force: true) do |t|
-          t.string :id, limit: 36, primary_key: true, null: false
-          t.string      :event_type,  null: false
-          t.text        :metadata
-          t.text        :data,        null: false
-          t.datetime    :created_at,  null: false
-        end
-      end
-      add_index :event_store_events, :created_at
+      eval(MigrationCode) unless defined?(CreateEventStoreEvents)
+      CreateEventStoreEvents.new.change
     end
     example.run
   end
