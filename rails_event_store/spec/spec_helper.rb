61ca2407673dfeed96ce09619a58db68051f362f
Robert Pankowecki <robert.pankowecki@gmail.com>
2017-10-18 10:14:42 +0200

    Use migration code, single source of truth

--- rails_event_store/spec/spec_helper.rb
+++ rails_event_store/spec/spec_helper.rb
@@ -2,6 +2,9 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rails_event_store'
 require 'example_invoicing_app'
 
+MigrationCode = File.read( File.expand_path('../../../rails_event_store_active_record/lib/rails_event_store_active_record/generators/templates/migration_template.rb', __FILE__) )
+MigrationCode.gsub!("<%= migration_version %>", "[4.2]")
+
 RSpec.configure do |config|
   config.disable_monkey_patching!
   
@@ -9,27 +12,8 @@ RSpec.configure do |config|
     ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
     ActiveRecord::Schema.define do
       self.verbose = false
-
-      create_table(:event_store_events_in_streams, force: true) do |t|
-        t.string      :stream,      null: false
-        t.integer     :position,    null: true
-        t.references :event, null: false, type: :string
-        t.datetime    :created_at,  null: false
-      end
-      add_index :event_store_events_in_streams, [:stream, :position], unique: true
-      add_index :event_store_events_in_streams, [:created_at]
-      add_index :event_store_events_in_streams, [:stream, :event_id], unique: true
-      # add_index :event_store_events_in_streams, [:event_uuid]
-
-      create_table(:event_store_events, id: false, force: true) do |t|
-        t.string :id, limit: 36, primary_key: true, null: false
-        t.string      :event_type,  null: false
-        t.text        :metadata
-        t.text        :data,        null: false
-        t.datetime    :created_at,  null: false
-      end
-      add_index :event_store_events, :created_at
-
+      eval(MigrationCode) unless defined?(CreateEventStoreEvents)
+      CreateEventStoreEvents.new.change
     end
     example.run
   end
