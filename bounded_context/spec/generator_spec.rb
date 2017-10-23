68a543d215efec4d3417f4b83e713727c4b62a63
Paweł Pacana <pawel.pacana@gmail.com>
2017-10-23 19:49:33 +0200

    No <<~ operator but activesupport/strip_heredoc already present.
    Leverage it for more eye-pleasing structure (indents).

--- bounded_context/spec/generator_spec.rb
+++ bounded_context/spec/generator_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-
+require 'active_support/core_ext/string/strip'
 
 module BoundedContext
   RSpec.describe Generator do
@@ -12,40 +12,40 @@ module BoundedContext
     specify do
       run_generator %w[payments]
 
-      expect('payments/lib/payments.rb').to match_content(<<-EOF)
-module Payments
-end
-EOF
+      expect('payments/lib/payments.rb').to match_content(<<-EOF.strip_heredoc)
+        module Payments
+        end
+      EOF
 
-      expect('config/application.rb').to match_content(<<-EOF)
-  config.paths.add 'payments/lib', eager_load: true
-EOF
+      expect('config/application.rb').to match_content(<<-EOF.strip_heredoc)
+        config.paths.add 'payments/lib', eager_load: true
+      EOF
     end
 
     specify do
       run_generator %w[Inventory]
 
-      expect('inventory/lib/inventory.rb').to match_content(<<-EOF)
-module Inventory
-end
-EOF
+      expect('inventory/lib/inventory.rb').to match_content(<<-EOF.strip_heredoc)
+        module Inventory
+        end
+      EOF
 
-      expect('config/application.rb').to match_content(<<-EOF)
-  config.paths.add 'inventory/lib', eager_load: true
-EOF
+      expect('config/application.rb').to match_content(<<-EOF.strip_heredoc)
+        config.paths.add 'inventory/lib', eager_load: true
+      EOF
     end
 
     specify do
       run_generator %w[mumbo_jumbo]
 
-      expect('mumbo_jumbo/lib/mumbo_jumbo.rb').to match_content(<<-EOF)
-module MumboJumbo
-end
-EOF
+      expect('mumbo_jumbo/lib/mumbo_jumbo.rb').to match_content(<<-EOF.strip_heredoc)
+        module MumboJumbo
+        end
+      EOF
 
-      expect('config/application.rb').to match_content(<<-EOF)
-  config.paths.add 'mumbo_jumbo/lib', eager_load: true
-EOF
+      expect('config/application.rb').to match_content(<<-EOF.strip_heredoc)
+        config.paths.add 'mumbo_jumbo/lib', eager_load: true
+      EOF
     end
   end
 end
