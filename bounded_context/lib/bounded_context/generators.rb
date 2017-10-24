fb0cb3c219e8deca2536ac5b4f706357c349e1f4
Paweł Pacana <pawel.pacana@gmail.com>
2017-10-24 12:34:20 +0200

    Revert generator base class.
    The only purpose was to share methods for templates -- not much of a
    problem.
    
    As a sideeffect it appeared as a standalone generator.

--- bounded_context/lib/bounded_context/generators.rb
+++ bounded_context/lib/bounded_context/generators.rb
@@ -2,7 +2,18 @@ require 'rails/generators'
 
 module BoundedContext
   module Generators
-    class Generator < Rails::Generators::NamedBase
+    class Module < Rails::Generators::NamedBase
+      source_root File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
+      hook_for :test_framework
+
+      def create_bounded_context
+        template "module.rb", "#{bounded_context_name}/lib/#{bounded_context_name}.rb"
+
+        application do
+          "config.paths.add '#{bounded_context_name}/lib', eager_load: true"
+        end
+      end
+
       private
 
       def bounded_context_namespace
@@ -14,32 +25,39 @@ module BoundedContext
       end
     end
 
-    class Module < Generator
-      source_root File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
-      hook_for :test_framework
-
-      def create_bounded_context
-        template "module.rb", "#{bounded_context_name}/lib/#{bounded_context_name}.rb"
-
-        application do
-          "config.paths.add '#{bounded_context_name}/lib', eager_load: true"
-        end
-      end
-    end
-
-    class Rspec < Generator
+    class Rspec < Rails::Generators::NamedBase
       source_root File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
 
       def spec_helper
         template "spec_helper.rb", "#{bounded_context_name}/spec/spec_helper.rb"
       end
+
+      private
+
+      def bounded_context_namespace
+        name.camelize
+      end
+
+      def bounded_context_name
+        name.underscore
+      end
     end
 
-    class TestUnit < Generator
+    class TestUnit < Rails::Generators::NamedBase
       source_root File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
 
       def test_helper
         template "test_helper.rb", "#{bounded_context_name}/test/test_helper.rb"
+      end
+
+      private
+
+      def bounded_context_namespace
+        name.camelize
+      end
+
+      def bounded_context_name
+        name.underscore
       end
     end
   end
