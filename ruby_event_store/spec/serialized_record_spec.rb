e4077c0771f4e6bb5cdb63fa53e16968feea3c2c
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2017-11-24 10:57:45 +0100

    fixed spec following mutant feedback

--- ruby_event_store/spec/serialized_record_spec.rb
+++ ruby_event_store/spec/serialized_record_spec.rb
@@ -1,22 +1,23 @@
 module RubyEventStore
 
   RSpec.describe SerializedRecord do
-    let(:event_id)   { "event_id" }
-    let(:data)       { "data" }
-    let(:metadata)   { "metadata" }
+    let(:event_id) { "event_id" }
+    let(:data) { "data" }
+    let(:metadata) { "metadata" }
     let(:event_type) { "event_type" }
 
     specify 'constructor accept all arguments and returns frozen instance' do
       record = described_class.new(event_id: event_id, data: data, metadata: metadata, event_type: event_type)
-      expect(record.event_id).to   be event_id
-      expect(record.metadata).to   be metadata
-      expect(record.data).to       be data
+      expect(record.event_id).to be event_id
+      expect(record.metadata).to be metadata
+      expect(record.data).to be data
       expect(record.event_type).to be event_type
-      expect(record.frozen?).to    be true
+      expect(record.frozen?).to be true
     end
 
     specify 'constructor raised SerializedRecord::StringsRequired when argument is not a String' do
-      [["string", 1, 1, 1],
+      [[1, 1, 1, 1],
+       ["string", 1, 1, 1],
        [1, "string", 1, 1],
        [1, 1, "string", 1],
        [1, 1, 1, "string"]].each do |sample|
@@ -26,5 +27,12 @@ module RubyEventStore
         end.to raise_error SerializedRecord::StringsRequired
       end
     end
+
+    specify 'constructor raised when required args are missing' do
+      expect do
+        described_class.new
+      end.to raise_error ArgumentError
+    end
   end
 end
+
