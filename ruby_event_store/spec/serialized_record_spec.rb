ead1fb9ff343c23f101fc7253c863ffc17287028
Andrzej Sliwa <andrzej.sliwa@i-tool.eu>
2017-11-24 10:33:07 +0100

    adjusted styling in rspec

--- ruby_event_store/spec/serialized_record_spec.rb
+++ ruby_event_store/spec/serialized_record_spec.rb
@@ -8,11 +8,11 @@ module RubyEventStore
 
     specify 'constructor accept all arguments and returns frozen instance' do
       record = described_class.new(event_id: event_id, data: data, metadata: metadata, event_type: event_type)
-      expect(record.event_id).to be event_id
-      expect(record.metadata).to be metadata
-      expect(record.data).to be data
+      expect(record.event_id).to   be event_id
+      expect(record.metadata).to   be metadata
+      expect(record.data).to       be data
       expect(record.event_type).to be event_type
-      expect(record.frozen?).to be true
+      expect(record.frozen?).to    be true
     end
 
     specify 'constructor raised SerializedRecord::StringsRequired when argument is not a String' do
