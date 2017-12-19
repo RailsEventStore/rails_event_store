9b334c5b58c411e02a41c4847def807e32f0cee4
Paweł Pacana <pawel.pacana@gmail.com>
2017-12-19 16:17:19 +0100

    Allow injecting clock to RailsEventStore client as well.
    This is an overlooked change that was present in RubyEventStore::Client but not
    in the RailsEventStore::Client counterpart.

--- rails_event_store/lib/rails_event_store/client.rb
+++ rails_event_store/lib/rails_event_store/client.rb
@@ -2,12 +2,14 @@ module RailsEventStore
   class Client < RubyEventStore::Client
     def initialize(repository: RailsEventStore.event_repository,
                    event_broker: EventBroker.new,
-                   page_size: PAGE_SIZE)
+                   page_size: PAGE_SIZE,
+                   clock: ->{ Time.now.utc })
       capture_metadata = ->{ Thread.current[:rails_event_store] }
       super(repository: repository,
             event_broker: event_broker,
             page_size: page_size,
-            metadata_proc: capture_metadata)
+            metadata_proc: capture_metadata,
+            clock: clock)
     end
   end
 end
