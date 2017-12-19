ec45e41d45644845c22533d761eb26e2178a47a5
Paweł Pacana <pawel.pacana@gmail.com>
2017-12-19 14:11:41 +0100

    Browser returns given stream events.
    - events are returned starting with most recent ones (like on stack)
    - there is no limit/pagination which can be dangerous, especially on
    "all" stream
    
       this would likely involve changes in frontend part as well to be
       well-suite for large streams, targeted for next iteration
    
    - additionaly improved spec on green with method extracts and matcher.

--- rails_event_store-browser/app/controllers/rails_event_store/browser/streams_controller.rb
+++ rails_event_store-browser/app/controllers/rails_event_store/browser/streams_controller.rb
@@ -6,13 +6,23 @@ module RailsEventStore
       end
 
       def show
-        render json: []
+        events = event_store.read_stream_events_backward(params[:id])
+        render json: events.map { |e| serialize_event(e) }
       end
 
       private
 
       def serialize_stream(stream)
         { name: stream.name }
+      end
+
+      def serialize_event(event)
+        {
+          event_id: event.event_id,
+          event_type: event.class.to_s,
+          data: event.data,
+          metadata: event.metadata
+        }
       end
 
       def event_store
--- rails_event_store-browser/spec/integration_spec.rb
+++ rails_event_store-browser/spec/integration_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'active_support/core_ext/hash/keys'
 
 DummyEvent = Class.new(::RailsEventStore::Event)
 
@@ -10,17 +11,59 @@ module RailsEventStore
     end
 
     specify do
-      event_store.publish_event(
-        DummyEvent.new(data: { foo: 1, bar: 2, baz: 3 }),
-        stream_name: 'dummy'
-      )
-
+      event_store.publish_event(DummyEvent.new, stream_name: 'dummy')
       get '/res/streams'
-      expect(response.body).to eq(JSON.dump([{ name: "all" }, { name: "dummy" }]))
+
+      expect(response.body).to be_collection([all_stream, dummy_stream])
+    end
+
+    specify do
+      dummy_event =
+        DummyEvent.new(data: {
+          foo: 1,
+          bar: 2.0,
+          baz: "3"
+        })
+      event_store.publish_event(dummy_event, stream_name: 'dummy')
+      expected_event =
+        {
+          event_type: "DummyEvent",
+          event_id: dummy_event.event_id,
+          data: dummy_event.data,
+          metadata: {
+            timestamp: dummy_event.metadata[:timestamp].as_json
+          }
+        }
+
+      get '/res/streams/all'
+      expect(response.body).to be_collection([expected_event])
+
+      get '/res/streams/dummy'
+      expect(response.body).to be_collection([expected_event])
+    end
+
+    def all_stream
+      { name: "all" }
+    end
+
+    def dummy_stream
+      { name: "dummy" }
     end
 
     def event_store
       Rails.configuration.event_store
+    end
+
+    RSpec::Matchers.define :be_collection do |structure|
+      match do |response_body|
+        json_body = JSON.parse(response_body)
+        @matcher = RSpec::Matchers::BuiltIn::Match.new(structure)
+        @matcher.matches?(json_body.map { |s| s.deep_symbolize_keys })
+      end
+
+      failure_message do
+        @matcher.failure_message
+      end
     end
   end
 end
