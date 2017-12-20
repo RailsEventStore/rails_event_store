c80a9bce09392d509b9df46ce5248fc42cc452b2
Paweł Pacana <pawel.pacana@gmail.com>
2017-12-20 01:53:23 +0100

    Revert "Allow injecting clock to RailsEventStore client as well."
    This reverts commit 9b334c5b58c411e02a41c4847def807e32f0cee4.
    
    Didn't help as planned, reverting until really useful.

--- rails_event_store/lib/rails_event_store/client.rb
+++ rails_event_store/lib/rails_event_store/client.rb
@@ -2,14 +2,12 @@ module RailsEventStore
   class Client < RubyEventStore::Client
     def initialize(repository: RailsEventStore.event_repository,
                    event_broker: EventBroker.new,
-                   page_size: PAGE_SIZE,
-                   clock: ->{ Time.now.utc })
+                   page_size: PAGE_SIZE)
       capture_metadata = ->{ Thread.current[:rails_event_store] }
       super(repository: repository,
             event_broker: event_broker,
             page_size: page_size,
-            metadata_proc: capture_metadata,
-            clock: clock)
+            metadata_proc: capture_metadata)
     end
   end
 end
