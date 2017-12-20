cff7c2edb7e3bc6141ad8187664093a4c98456e7
Paweł Pacana <pawel.pacana@gmail.com>
2017-12-20 01:00:14 +0100

    Mutation testing engine, for real.
    In order to mutate whole namespace we need to force-load autoloaded
    source files.
    
    Requiring Rails + eager_load does the trick to load everything in
    RailsEventStore::Browser namespace from engine.

--- rails_event_store-browser/spec/dummy/config/environments/development.rb
+++ rails_event_store-browser/spec/dummy/config/environments/development.rb
@@ -7,7 +7,7 @@ Rails.application.configure do
   config.cache_classes = false
 
   # Do not eager load code on boot.
-  config.eager_load = false
+  config.eager_load = true
 
   # Show full error reports.
   config.consider_all_requests_local = true
