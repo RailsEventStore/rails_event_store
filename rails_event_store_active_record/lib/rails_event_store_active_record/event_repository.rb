027e184fd838f0aa4c08d7954fa7c3d5db880fa9
Robert Pankowecki <robert.pankowecki@gmail.com>
2017-12-31 17:59:01 +0100

    Extract constants

--- rails_event_store_active_record/lib/rails_event_store_active_record/event_repository.rb
+++ rails_event_store_active_record/lib/rails_event_store_active_record/event_repository.rb
@@ -148,7 +148,7 @@ module RailsEventStoreActiveRecord
     end
 
     def raise_error(e)
-      if detect_pkey_index_violated(e)
+      if detect_index_violated(e)
         raise RubyEventStore::EventDuplicatedInStream
       end
       raise RubyEventStore::WrongExpectedEventVersion
@@ -174,15 +174,23 @@ module RailsEventStoreActiveRecord
       end
     end
 
-    def detect_pkey_index_violated(e)
-      m = e.message
-      m.include?("for key 'PRIMARY'")       ||  # MySQL
-      m.include?("event_store_events_pkey") ||  # Postgresql
-      m.include?("event_store_events.id")   ||  # Sqlite3
+    MYSQL_PKEY_ERROR    = "for key 'PRIMARY'"
+    POSTGRES_PKEY_ERROR = "event_store_events_pkey"
+    SQLITE3_PKEY_ERROR  = "event_store_events.id"
 
-      m.include?("for key 'index_event_store_events_in_streams_on_stream_and_event_id'")        || # Mysql
-      m.include?("Key (stream, event_id)")                                                      || # Postgresql
-      m.include?("event_store_events_in_streams.stream, event_store_events_in_streams.event_id")   # Sqlite3
+    MYSQL_INDEX_ERROR    = "for key 'index_event_store_events_in_streams_on_stream_and_event_id'"
+    POSTGRES_INDEX_ERROR = "Key (stream, event_id)"
+    SQLITE3_INDEX_ERROR  = "event_store_events_in_streams.stream, event_store_events_in_streams.event_id"
+
+    def detect_index_violated(e)
+      m = e.message
+      m.include?(MYSQL_PKEY_ERROR)     ||
+      m.include?(POSTGRES_PKEY_ERROR)  ||
+      m.include?(SQLITE3_PKEY_ERROR)   ||
+
+      m.include?(MYSQL_INDEX_ERROR)    ||
+      m.include?(POSTGRES_INDEX_ERROR) ||
+      m.include?(SQLITE3_INDEX_ERROR)
     end
 
     def build_event_record(event)
