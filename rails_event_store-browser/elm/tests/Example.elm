92e8f35f3ce13c50ec3949eafc0c046ce829a3f5
Paweł Pacana <pawel.pacana@gmail.com>
2018-01-17 14:08:58 +0100

    Verify decoder composition for all examples.
    After all we're testing against complete input JSON, not parts of it.

--- rails_event_store-browser/elm/tests/Example.elm
+++ rails_event_store-browser/elm/tests/Example.elm
@@ -2,49 +2,61 @@ module Example exposing (..)
 
 import Expect
 import Test exposing (..)
-import Json.Decode
-import Main exposing (streamDecoder, eventDecoder, eventWithDetailsDecoder, rawEventDecoder, Item(..))
+import Json.Decode exposing (list)
+import Main exposing (..)
 
 
 suite : Test
 suite =
     describe "JSONAPI decoders"
-        [ test "Stream decoder" <|
+        [ test "streams decoder" <|
             \_ ->
                 let
                     input =
                         """
-                        { "name": "all" }
-                        """
-
-                    output =
-                        Json.Decode.decodeString streamDecoder input
-                in
-                    Expect.equal output (Ok (Stream "all"))
-        , test "Event decoder" <|
-            \_ ->
-                let
-                    input =
-                        """
-                        {
-                          "event_id": "664ada1e-2f01-4ed0-9c16-63dbc82269d2",
-                          "event_type": "DummyEvent",
-                          "data": {
-                            "foo": 1,
-                            "bar": 2.0,
-                            "baz": "3"
-                          },
-                          "metadata": {
-                            "timestamp": "2017-12-20T23:49:45.273Z"
+                        [
+                          {
+                            "name": "all"
                           }
-                        }
+                        ]
                         """
 
+                    decoder =
+                        list streamDecoder
+
                     output =
-                        Json.Decode.decodeString eventDecoder input
+                        Json.Decode.decodeString decoder input
                 in
-                    Expect.equal output (Ok (Event "DummyEvent" "2017-12-20T23:49:45.273Z" "664ada1e-2f01-4ed0-9c16-63dbc82269d2"))
-        , test "detailed Event decoder" <|
+                    Expect.equal output (Ok ([ Stream "all" ]))
+        , test "events decoder" <|
+            \_ ->
+                let
+                    input =
+                        """
+                        [
+                          {
+                            "event_id": "664ada1e-2f01-4ed0-9c16-63dbc82269d2",
+                            "event_type": "DummyEvent",
+                            "data": {
+                              "foo": 1,
+                              "bar": 2.0,
+                              "baz": "3"
+                            },
+                            "metadata": {
+                              "timestamp": "2017-12-20T23:49:45.273Z"
+                            }
+                          }
+                        ]
+                        """
+
+                    decoder =
+                        list eventDecoder
+
+                    output =
+                        Json.Decode.decodeString decoder input
+                in
+                    Expect.equal output (Ok ([ Event "DummyEvent" "2017-12-20T23:49:45.273Z" "664ada1e-2f01-4ed0-9c16-63dbc82269d2" ]))
+        , test "detailed event decoder" <|
             \_ ->
                 let
                     input =
