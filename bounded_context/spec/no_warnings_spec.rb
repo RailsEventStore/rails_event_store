e999471ec3251a7748a3e674143ce4ae219b3534
Paweł Pacana <pawel.pacana@gmail.com>
2018-01-27 15:29:33 +0100

    Ensure no ruby warnings generated from RES code.

---
+++ bounded_context/spec/no_warnings_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+module RubyEventStore
+  RSpec.describe 'no warnings' do
+    specify do
+      expect(bounded_context_warnings).to eq([])
+    end
+
+    def bounded_context_warnings
+      warnings.select { |w| w =~ %r{lib/bounded_context} }
+    end
+
+    def warnings
+      `ruby -Ilib -w lib/bounded_context.rb 2>&1`.split("\n")
+    end
+  end
+end
---
+++ ruby_event_store/spec/no_warnings_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+module RubyEventStore
+  RSpec.describe 'no warnings' do
+    specify do
+      expect(ruby_event_store_warnings).to eq([])
+    end
+
+    def ruby_event_store_warnings
+      warnings.select { |w| w =~ %r{lib/ruby_event_store} }
+    end
+
+    def warnings
+      `ruby -Ilib -w lib/ruby_event_store.rb 2>&1`.split("\n")
+    end
+  end
+end
