c2783bdaf56201959274ce65f7dd3639c4d4da17
Paweł Pacana <pawel.pacana@gmail.com>
2018-01-28 19:16:05 +0100

    Introduce FakeConfiguration over leaking Rails::Application::Configuration in tests.
    The problem with R::A::C in tests is that it uses class variable to
    store configuration and that leaks between test examples.
    
    FakeConfiguration reimplements a subset of R::A::C and stores options
    privately for an instance.
    
    Compare:
    https://github.com/rails/rails/blob/a88eb9087260cca256c6faba40bf538d4a0289b3/railties/lib/rails/railtie/configuration.rb#L9

--- rails_event_store/spec/railtie_spec.rb
+++ rails_event_store/spec/railtie_spec.rb
@@ -5,40 +5,68 @@ require 'rails_event_store/railtie'
 module RailsEventStore
   RSpec.describe Railtie::RailsConfig do
     specify 'no config, use defaults' do
-      rails_config =
-        Railtie::RailsConfig.new(::Rails::Application::Configuration.new)
-      expect(rails_config.request_metadata.({
-        'action_dispatch.request_id' => 'dummy_id',
-        'action_dispatch.remote_ip'  => 'dummy_ip'
-      })).to(eq({
-        request_id: 'dummy_id',
-        remote_ip:  'dummy_ip'
-      }))
+      rails_config = Railtie::RailsConfig.new(app_configuration)
+
+      expect(rails_config.request_metadata.(dummy_env))
+        .to(eq({
+          request_id: 'dummy_id',
+          remote_ip:  'dummy_ip'
+        }))
     end
 
     specify 'config present' do
-      rails_config =
-        Railtie::RailsConfig.new(::Rails::Application::Configuration.new
-          .tap { |c| c.rails_event_store = { request_metadata: ->(env) { { kaka: 'dudu' } } } })
-      expect(rails_config.request_metadata.({
-        'action_dispatch.request_id' => 'dummy_id',
-        'action_dispatch.remote_ip'  => 'dummy_ip'
-      })).to(eq({
-        kaka: 'dudu',
-      }))
+      app_configuration.rails_event_store = { request_metadata: kaka_dudu }
+      rails_config = Railtie::RailsConfig.new(app_configuration)
+
+      expect(rails_config.request_metadata.(dummy_env)).to eq({ kaka: 'dudu' })
     end
 
     specify 'config present, no callable' do
-      rails_config =
-        Railtie::RailsConfig.new(::Rails::Application::Configuration.new
-          .tap { |c| c.rails_event_store = {} })
-      expect(rails_config.request_metadata.({
+      app_configuration.rails_event_store = {}
+      rails_config = Railtie::RailsConfig.new(app_configuration)
+
+      expect(rails_config.request_metadata.(dummy_env))
+        .to(eq({
+          request_id: 'dummy_id',
+          remote_ip:  'dummy_ip'
+        }))
+    end
+
+    def app_configuration
+      @app_configuration ||= FakeConfiguration.new
+    end
+
+    def kaka_dudu
+      ->(env) { { kaka: 'dudu' } }
+    end
+
+    def dummy_env
+      {
         'action_dispatch.request_id' => 'dummy_id',
         'action_dispatch.remote_ip'  => 'dummy_ip'
-      })).to(eq({
-        request_id: 'dummy_id',
-        remote_ip:  'dummy_ip'
-      }))
+      }
+    end
+  end
+
+  class FakeConfiguration
+    def initialize
+      @options = {}
+    end
+
+    private
+
+    def method_missing(name, *args, &blk)
+      if name.to_s =~ /=$/
+        @options[$`.to_sym] = args.first
+      elsif @options.key?(name)
+        @options[name]
+      else
+        super
+      end
+    end
+
+    def respond_to_missing?(name, include_private = false)
+      @options.key?(name) || super
     end
   end
 end
