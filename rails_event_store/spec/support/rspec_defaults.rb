69e701b7c88e96e4bcf83dbf3965b79af1f6f866
Paweł Pacana <pawel.pacana@gmail.com>
2018-01-28 16:49:37 +0100

    Good rspec defaults.
    Random (and reproducable with seed) test order, no monkeypatching.
    Now in one file.

---
+++ rails_event_store/spec/support/rspec_defaults.rb
@@ -0,0 +1,12 @@
+RSpec.configure do |config|
+  config.example_status_persistence_file_path = ".rspec_status"
+
+  config.disable_monkey_patching!
+
+  config.order = :random
+  Kernel.srand config.seed
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
