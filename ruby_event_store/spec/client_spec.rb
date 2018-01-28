f39b7c8c7dfce2e20c56dcd7625e19b86c7dff59
Paweł Pacana <pawel.pacana@gmail.com>
2018-01-28 19:55:44 +0100

    Injected clock over stubbed global.
    Several problems:
    - allow_any_instance_of(Time) could not have #now, it is .now (class
      method)
    - allow_any_instance_of(Time) to return same utc to compare with is a
      bit tautologic unless we check if rspec implemented stubs correctly
    
    Instead clock is injected (one that gives frozen time).

--- ruby_event_store/spec/client_spec.rb
+++ ruby_event_store/spec/client_spec.rb
@@ -146,14 +146,13 @@ module RubyEventStore
     end
 
     specify 'timestamp is utc time' do
-      now = Time.parse('2015-05-04 15:17:11 +0200')
-      utc = Time.parse('2015-05-04 13:17:23 UTC')
-      allow_any_instance_of(Time).to receive(:now).and_return(now)
-      allow_any_instance_of(Time).to receive(:utc).and_return(utc)
-      client = RubyEventStore::Client.new(repository: InMemoryRepository.new)
-      event = TestEvent.new
-      client.publish_event(event)
+      now    = Time.parse('2015-05-04 15:17:11 +0200')
+      utc    = Time.parse('2015-05-04 13:17:11 UTC')
+      client = RubyEventStore::Client.new(repository: InMemoryRepository.new, clock: ->{ now })
+
+      client.publish_event(TestEvent.new)
       published = client.read_all_streams_forward
+
       expect(published.size).to eq(1)
       expect(published.first.metadata[:timestamp]).to eq(utc)
     end
@@ -202,6 +201,5 @@ module RubyEventStore
       expect(client.read_stream_events_forward('cars')).to eq([first_event])
       expect(subscriber.handled_events).to be_empty
     end
-
   end
 end
