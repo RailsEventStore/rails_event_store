e583e473a529b312c6e6bb7b5411d8b85a4b6a77
Robert Pankowecki <robert.pankowecki@gmail.com>
2018-03-06 15:01:11 +0100

    Solve problem 1

--- contrib/distributed_rails_event_store/postgresql_queue/lib/postgresql_queue.rb
+++ contrib/distributed_rails_event_store/postgresql_queue/lib/postgresql_queue.rb
@@ -9,11 +9,29 @@ module PostgresqlQueue
     def events(after_event_id: :head)
       after_event_id ||= :head
       events = @res.read_all_streams_forward(start: after_event_id, count: 100)
-      # visible_since = EventInStream.
-      #   where(stream: RubyEventStore::GLOBAL_STREAM).
-      #   where(id: events.map(&:event_id))
-      #   pluck(:id, :xmin)
-      return events
+      sql = RailsEventStoreActiveRecord::EventInStream.
+        where(stream: RubyEventStore::GLOBAL_STREAM).
+        where(event_id: events.map(&:event_id)).
+        order("id ASC").
+        select("id, event_id, xmin").to_sql
+      results = ActiveRecord::Base.connection.execute(sql).each.to_a
+      txid_snapshot_xmin = Integer(get_xmin)
+
+      event_ids = results.select do |tuple|
+        tuple["xmin"].to_i < txid_snapshot_xmin
+      end.map{|tuple| tuple["event_id"] }.to_set
+
+      events.select{|e| event_ids.include?(e.event_id) }
+    end
+
+    private
+
+    def get_xmin
+      ActiveRecord::Base.connection.execute(xminc).each.first.fetch("txid_snapshot_xmin")
+    end
+
+    def xminc
+      "SELECT * FROM txid_snapshot_xmin(txid_current_snapshot());"
     end
   end
 end
