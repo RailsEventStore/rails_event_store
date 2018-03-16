8582e2d4bbe6431c3f113f3f737d3359ffabb7c2
Tomasz Wróbel <tomekwr@gmail.com>
2018-03-16 12:49:43 +0100

    Move TestEvent to spec_helper
    We'll need it in two spec files - client_spec & new_client_spec.
    
    (cherry pick commit b1713fa6)
    
    We've extracted client spec to shared_examples in fluent-api branch, and
    we're planning to move specs from spec/actions dir as well. It makes a
    lot of sense to make this changes on master to avoid potential
    conflicts.

--- ruby_event_store/spec/client_spec.rb
+++ ruby_event_store/spec/client_spec.rb
@@ -4,7 +4,6 @@ require_relative 'mappers/events_pb.rb'
 
 module RubyEventStore
   RSpec.describe Client do
-    TestEvent = Class.new(RubyEventStore::Event)
 
     specify 'publish_event returns :ok when success' do
       client = RubyEventStore::Client.new(repository: InMemoryRepository.new)
