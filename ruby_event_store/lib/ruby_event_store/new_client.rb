80a3ca1885da5677faed7018b69f92344e88b6a2
Tomasz Wróbel <tomekwr@gmail.com>
2018-03-16 11:53:40 +0100

    NewClient: delegate all remaining methods to old client
    To have as little failing tests as possible. We'll now continue
    implementation by un-delegating subsequent methods.
    
    Currently there are still a couple tests failing - the ones related to
    the method which is now being implemented - it's not delegated.

--- ruby_event_store/lib/ruby_event_store/new_client.rb
+++ ruby_event_store/lib/ruby_event_store/new_client.rb
@@ -5,11 +5,25 @@ module RubyEventStore
 
     extend Forwardable
 
-    def_delegators :@old_client, :publish_event, :publish_events
+    def_delegators :@old_client,
+      :publish_event,
+      :publish_events,
+      :append_to_stream,
+      :link_to_stream,
+      :delete_stream,
+      :read_events_forward,
+      :read_events_backward,
+      :read_stream_events_backward,
+      :read_all_streams_forward,
+      :read_all_streams_backward,
+      :read_event,
+      :get_all_streams,
+      :subscribe,
+      :subscribe_to_all_events
 
-    def initialize(repository:)
+    def initialize(repository:, metadata_proc: nil)
       @repository = repository
-      @old_client = Client.new(repository: repository)
+      @old_client = Client.new(repository: repository, metadata_proc: metadata_proc)
     end
 
     def read_stream_events_forward(stream_name)
