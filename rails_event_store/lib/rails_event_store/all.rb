ba5eb91b64a471ddb8cfe06612ea1dbf7243e2e1
Robert Pankowecki <robert.pankowecki@gmail.com>
2018-03-17 12:36:47 +0100

    Remove global
    No idea why it was introduced at all.
    
    Issue: #153

--- rails_event_store/lib/rails_event_store/all.rb
+++ rails_event_store/lib/rails_event_store/all.rb
@@ -1,4 +1,3 @@
-require 'active_support/core_ext/class/attribute_accessors'
 require 'ruby_event_store'
 require 'rails_event_store/active_job_dispatcher'
 require 'rails_event_store/client'
@@ -21,11 +20,4 @@ module RailsEventStore
   InvalidPageSize           = RubyEventStore::InvalidPageSize
   GLOBAL_STREAM             = RubyEventStore::GLOBAL_STREAM
   PAGE_SIZE                 = RubyEventStore::PAGE_SIZE
-
-  mattr_reader :event_repository
-
-  def self.event_repository=(event_repository)
-    raise ArgumentError unless event_repository
-    @@event_repository = event_repository
-  end
 end
