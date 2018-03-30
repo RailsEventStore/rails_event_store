af33431b42dca5c05f1a70cbc11fda71f34f7194
Tomasz Wróbel <tomekwr@gmail.com>
2018-03-30 12:53:34 +0200

    Inline let var in client spec
    We're preparing these client specs to merge them to client examples.

--- ruby_event_store/spec/actions/create_events_in_stream_spec.rb
+++ ruby_event_store/spec/actions/create_events_in_stream_spec.rb
@@ -3,38 +3,37 @@ require 'ostruct'
 
 module RubyEventStore
   RSpec.describe Client do
-    let(:stream_name) { 'stream_name' }
 
     specify 'create successfully event' do
       client = RubyEventStore::Client.new(repository: InMemoryRepository.new)
       event = OrderCreated.new(event_id: 'b2d506fd-409d-4ec7-b02f-c6d2295c7edd')
-      client.append_to_stream(event, stream_name: stream_name)
-      saved_events = client.read_stream_events_forward(stream_name)
+      client.append_to_stream(event, stream_name: 'stream_name')
+      saved_events = client.read_stream_events_forward('stream_name')
       expect(saved_events[0]).to eq(event)
     end
 
     specify 'generate guid and create successfully event' do
       client = RubyEventStore::Client.new(repository: InMemoryRepository.new)
       event = OrderCreated.new
-      client.append_to_stream(event, stream_name: stream_name)
-      saved_events = client.read_stream_events_forward(stream_name)
+      client.append_to_stream(event, stream_name: 'stream_name')
+      saved_events = client.read_stream_events_forward('stream_name')
       expect(saved_events[0]).to eq(event)
     end
 
     specify 'raise exception if expected version incorrect' do
       client = RubyEventStore::Client.new(repository: InMemoryRepository.new)
       event = OrderCreated.new
-      client.append_to_stream(event, stream_name: stream_name)
-      expect { client.publish_event(event, stream_name: stream_name, expected_version: 100) }.to raise_error(WrongExpectedEventVersion)
+      client.append_to_stream(event, stream_name: 'stream_name')
+      expect { client.publish_event(event, stream_name: 'stream_name', expected_version: 100) }.to raise_error(WrongExpectedEventVersion)
     end
 
     specify 'create event with optimistic locking' do
       client = RubyEventStore::Client.new(repository: InMemoryRepository.new)
       event = OrderCreated.new(event_id: 'b2d506fd-409d-4ec7-b02f-c6d2295c7edd')
-      client.append_to_stream(event, stream_name: stream_name)
+      client.append_to_stream(event, stream_name: 'stream_name')
 
       event = OrderCreated.new(event_id: '724dd49d-6e20-40e6-bc32-ed75258f886b')
-      client.append_to_stream(event, stream_name: stream_name, expected_version: 0)
+      client.append_to_stream(event, stream_name: 'stream_name', expected_version: 0)
     end
 
     specify 'expect no event handler is called' do
@@ -43,8 +42,8 @@ module RubyEventStore
       expect(handler).not_to receive(:call)
       event = OrderCreated.new
       client.subscribe_to_all_events(handler)
-      client.append_to_stream(event, stream_name: stream_name)
-      saved_events = client.read_stream_events_forward(stream_name)
+      client.append_to_stream(event, stream_name: 'stream_name')
+      saved_events = client.read_stream_events_forward('stream_name')
       expect(saved_events[0]).to eq(event)
     end
 
@@ -54,8 +53,8 @@ module RubyEventStore
       expect(handler).to receive(:call)
       event = OrderCreated.new
       client.subscribe_to_all_events(handler)
-      client.publish_event(event, stream_name: stream_name)
-      saved_events = client.read_stream_events_forward(stream_name)
+      client.publish_event(event, stream_name: 'stream_name')
+      saved_events = client.read_stream_events_forward('stream_name')
       expect(saved_events[0]).to eq(event)
     end
 
@@ -66,5 +65,6 @@ module RubyEventStore
       saved_events = client.read_stream_events_forward('all')
       expect(saved_events[0]).to eq(event)
     end
+
   end
 end
