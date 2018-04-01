dabf1edd1bdb84436b7b7c2b56062a92d53b1ebf
Robert Pankowecki <robert.pankowecki@gmail.com>
2018-04-01 12:07:22 +0200

    Confirm protobuf working with async handlers
    by using Proto wrapper and implementing YAML serialization which
    delegates for proto serialization in case of event's :data field.
    
    Issue: #228 #255 #257

--- rails_event_store/spec/protobuf_integration_spec.rb
+++ rails_event_store/spec/protobuf_integration_spec.rb
@@ -1,13 +1,27 @@
 require 'spec_helper'
 require_relative '../../ruby_event_store/spec/mappers/events_pb'
 
+class AsyncProtoHandler < ActiveJob::Base
+  self.queue_adapter = :inline
+
+  def perform(event)
+    @@event = YAML.load(event)
+  end
+
+  def self.event
+    @@event
+  end
+end
+
 module RailsEventStore
   RSpec.describe Client do
     specify 'can handle protobuf event class instead of RubyEventStore::Event' do
       client = Client.new(repository: RailsEventStoreActiveRecord::EventRepository.new(
         mapper: RubyEventStore::Mappers::Protobuf.new,
       ))
-      client.subscribe(->(ev){@ev = ev}, [ResTesting::OrderCreated.descriptor.name])
+      client.subscribe(->(ev){@ev = ev}, to: [ResTesting::OrderCreated.descriptor.name])
+      client.subscribe(AsyncProtoHandler, to: [ResTesting::OrderCreated.descriptor.name])
+
       event = RubyEventStore::Proto.new(
         data: ResTesting::OrderCreated.new(
           customer_id: 123,
@@ -17,7 +31,9 @@ module RailsEventStore
       client.publish_event(event, stream_name: 'test')
       expect(client.read_event(event.event_id)).to eq(event)
       expect(client.read_stream_events_forward('test')).to eq([event])
+
       expect(@ev).to eq(event)
+      expect(AsyncProtoHandler.event).to eq(event)
     end
   end
 
@@ -37,7 +53,7 @@ module RailsEventStore
           order_id: "K3THNX9",
         )
       )
-      # expect(event1.data).to eq(event2.data)
+      # expect(event1.data).to eql(event2.data)
       expect(event1.data).to eq(event2.data)
       expect(event1).to eq(event2)
     end
