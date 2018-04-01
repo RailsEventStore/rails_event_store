295c46c2d4b1d35142456236ea427360ab5cd837
Robert Pankowecki <robert.pankowecki@gmail.com>
2018-04-01 23:06:33 +0200

    repository linter uses SerializedRecord now
    instead of domain events.
    
    Issue: #257

--- ruby_event_store/lib/ruby_event_store/serialized_record.rb
+++ ruby_event_store/lib/ruby_event_store/serialized_record.rb
@@ -11,5 +11,26 @@ module RubyEventStore
     end
 
     attr_reader :event_id, :data, :metadata, :event_type
+
+    BIG_VALUE = 0b110011100100000010010010110011101011110101010101001100111110011
+    def hash
+      [
+        self.class,
+        event_id,
+        data,
+        metadata,
+        event_type,
+      ].hash ^ BIG_VALUE
+    end
+
+    def ==(other)
+      other.instance_of?(self.class) &&
+        other.event_id.eql?(event_id) &&
+        other.data.eql?(data) &&
+        other.metadata.eql?(metadata) &&
+        other.event_type.eql?(event_type)
+    end
+
+    alias_method :eql?, :==
   end
 end
