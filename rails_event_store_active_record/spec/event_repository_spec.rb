7e99e8deadd60d4ca9f7dc672bee24eef429c32c
Paweł Pacana <pawel.pacana@gmail.com>
2018-04-03 00:11:46 +0200

    Kill mutations.
    While some RDBMSes can implicitly sort ascending by table `id`,
    PostgreSQL will for sure given random order.
    
    Since we mutate on one engine for practical reasons, ensure correct SQL
    query is produced.

--- rails_event_store_active_record/spec/event_repository_spec.rb
+++ rails_event_store_active_record/spec/event_repository_spec.rb
@@ -138,6 +138,27 @@ module RailsEventStoreActiveRecord
       expect(repository.read_stream_events_backward("all").map(&:event_id)).to eq([u3,u2,u1])
     end
 
+    specify do
+      expect_query(/SELECT.*FROM.*event_store_events_in_streams.*WHERE.*event_store_events_in_streams.*stream.*=.*ORDER BY id ASC LIMIT.*/) do
+        repository = EventRepository.new
+        repository.read_all_streams_forward(:head, 3)
+      end
+    end
+
+    specify do
+      expect_query(/SELECT.*FROM.*event_store_events_in_streams.*WHERE.*event_store_events_in_streams.*stream.*=.*ORDER BY id ASC LIMIT.*/) do
+        repository = EventRepository.new
+        repository.read_events_forward("all", :head, 3)
+      end
+    end
+
+    specify do
+      expect_query(/SELECT.*FROM.*event_store_events_in_streams.*WHERE.*event_store_events_in_streams.*stream.*=.*ORDER BY id ASC.*/) do
+        repository = EventRepository.new
+        repository.read_stream_events_forward("all")
+      end
+    end
+
     specify "explicit ORDER BY position" do
       expect_query(/SELECT.*FROM.*event_store_events_in_streams.*WHERE.*event_store_events_in_streams.*stream.*=.*ORDER BY position DESC LIMIT.*/) do
         repository = EventRepository.new
