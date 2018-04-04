6f8a91789fc6a801a5e260c13b6c89168934a2b1
Jan Dudulski <jan@dudulski.pl>
2018-04-04 14:04:50 +0200

    Fix BC dummy app

--- bounded_context/spec/dummy_5_2/config/environments/development.rb
+++ bounded_context/spec/dummy_5_2/config/environments/development.rb
@@ -28,9 +28,9 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -41,5 +41,5 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
--- bounded_context/spec/dummy_5_2/config/environments/production.rb
+++ bounded_context/spec/dummy_5_2/config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "dummy_#{Rails.env}"
 
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
