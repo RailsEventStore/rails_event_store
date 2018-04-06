dad1fda69e4df0f1ad82d3950d52bc4139de0e2e
Paweł Pacana <pawel.pacana@gmail.com>
2018-04-06 21:43:23 +0200

    Disable event file watcher in dummy Rails app.
    Reason:
    
       bundler: failed to load command: mutant (/Users/pawelpacana/.rbenv/versions/2.5.0/bin/mutant)
       LoadError: Could not load the 'listen' gem. Add `gem 'listen'` to the development group of your Gemfile
    
    We've disabled it in other apps and in general have no benefit of using
    it in tests.

--- rails_event_store-browser/spec/dummy_5_2_0_rc2/config/environments/development.rb
+++ rails_event_store-browser/spec/dummy_5_2_0_rc2/config/environments/development.rb
@@ -40,5 +40,5 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
