047279cc37ec008932e24922711e8604ff6b504a
Paweł Pacana <pawel.pacana@gmail.com>
2018-04-17 00:54:52 +0200

    Use ExpectedVersion in InMemoryRepository.
    Also move it to RubyEventStore from ROM gem.

--- ruby_event_store/lib/ruby_event_store/expected_version.rb
+++ ruby_event_store/lib/ruby_event_store/expected_version.rb
@@ -15,6 +15,8 @@ module RubyEventStore
       new(:auto)
     end
 
+    attr_reader :version
+
     def initialize(version)
       @version = version
       invalid_version! unless [Integer, :any, :none, :auto].any? {|i| i === version}
@@ -24,26 +26,37 @@ module RubyEventStore
       version.equal?(:any)
     end
 
-    def resolve_for(stream, &resolver)
+    def resolve_for(stream, resolver = Proc.new {})
       invalid_version! if stream.global? && !any?
 
       case version
       when Integer
         version
-      when :any
-        nil
       when :none
         POSITION_DEFAULT
       when :auto
-        resolver && resolver.call(stream) || POSITION_DEFAULT
-      else
-        invalid_version!
+        resolver[stream] || POSITION_DEFAULT
       end
     end
 
-    private
+    BIG_VALUE = 0b110111100100000010010010110011101011000101010101001100100110011
+    private_constant :BIG_VALUE
 
-    attr_reader :version
+    def hash
+      [
+        self.class,
+        version
+      ].hash ^ BIG_VALUE
+    end
+
+    def ==(other_expected_version)
+      other_expected_version.instance_of?(self.class) &&
+        other_expected_version.version.eql?(version)
+    end
+
+    alias_method :eql?, :==
+
+    private
 
     def invalid_version!
       raise InvalidExpectedVersion
---
+++ ruby_event_store/spec/expected_version_spec.rb
@@ -0,0 +1,86 @@
+require 'spec_helper'
+
+module RubyEventStore
+  RSpec.describe ExpectedVersion do
+    specify { expect(ExpectedVersion.any).to eq(ExpectedVersion.new(:any)) }
+
+    specify { expect(ExpectedVersion.none).to eq(ExpectedVersion.new(:none)) }
+
+    specify { expect(ExpectedVersion.auto).to eq(ExpectedVersion.new(:auto)) }
+
+    specify { expect { ExpectedVersion.new(nil) }.to raise_error(InvalidExpectedVersion) }
+
+    specify { expect { ExpectedVersion.new("13") }.to raise_error(InvalidExpectedVersion) }
+
+    specify { expect(ExpectedVersion.new(:any).any?).to eq(true) }
+
+    specify { expect(ExpectedVersion.new(1).any?).to eq(false) }
+
+    specify { expect(ExpectedVersion.any.resolve_for(Stream.new(GLOBAL_STREAM))).to eq(nil) }
+
+    specify { expect { ExpectedVersion.auto.resolve_for(Stream.new(GLOBAL_STREAM)) }.to raise_error(InvalidExpectedVersion) }
+
+    specify { expect(ExpectedVersion.none.resolve_for(Stream.new('dummy'))).to eq(ExpectedVersion::POSITION_DEFAULT) }
+
+    specify { expect(ExpectedVersion.new(2).resolve_for(Stream.new('dummy'))).to eq(2) }
+
+    specify { expect(ExpectedVersion.any.resolve_for(Stream.new('dummy'))).to eq(nil) }
+
+    specify do
+      resolver = ->(stream) do
+        case stream.name
+        when "42" then
+          42
+        else
+          13
+        end
+      end
+
+      expect(ExpectedVersion.auto.resolve_for(Stream.new('42'), resolver)).to eq(42)
+      expect(ExpectedVersion.auto.resolve_for(Stream.new('dummy'), resolver)).to eq(13)
+    end
+
+    specify do
+      stream = Stream.new('some')
+      expect(ExpectedVersion.auto.resolve_for(stream)).to eq(ExpectedVersion::POSITION_DEFAULT)
+    end
+
+    specify do
+      stream = Stream.new('some')
+      expect(ExpectedVersion.auto.resolve_for(stream, Proc.new {})).to eq(ExpectedVersion::POSITION_DEFAULT)
+    end
+
+    specify "in-equality" do
+      %i(
+        any
+        none
+        auto
+      ).permutation(2).each do |one, two|
+        a = ExpectedVersion.new(one)
+        b = ExpectedVersion.new(two)
+        c = Class.new(ExpectedVersion).new(one)
+        expect(a).not_to eq(b)
+        expect(a).not_to eql(b)
+        expect(a.hash).not_to eq(b.hash)
+
+        expect(a).not_to eq(c)
+        expect(a).not_to eql(c)
+        expect(a.hash).not_to eq(c.hash)
+      end
+    end
+
+    specify "equality" do
+      a = ExpectedVersion.new(:any)
+      b = ExpectedVersion.new(:any)
+      expect(a).to eq(b)
+      expect(a).to eql(b)
+      expect(a.hash).to eql(b.hash)
+    end
+
+    specify "hash" do
+      a = ExpectedVersion.new(1)
+      expect(a.hash).not_to eq([ExpectedVersion, 1].hash)
+      expect(Set.new([a])).to eq(Set.new([a]))
+    end
+  end
+end
