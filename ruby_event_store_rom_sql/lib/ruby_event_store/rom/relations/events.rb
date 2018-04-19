2f21f6817f3b8005c220f6acfd4e2aec35dffbc3
Joel Van Horn <joel@joelvanhorn.com>
2018-04-19 18:20:33 -0400

    Remove attributes and just infer from schema

--- ruby_event_store_rom_sql/lib/ruby_event_store/rom/relations/events.rb
+++ ruby_event_store_rom_sql/lib/ruby_event_store/rom/relations/events.rb
@@ -3,11 +3,6 @@ module RubyEventStore
     module Relations
       class Events < ::ROM::Relation[:sql]
         schema(:event_store_events, as: :events, infer: true) do
-          attribute :id, ::ROM::Types::String.meta(primary_key: true)
-          attribute :event_type, ::ROM::Types::String
-          attribute :metadata, ::ROM::Types::String.optional
-          attribute :data, ::ROM::Types::String
-
           associations do
             has_many :stream_entries
           end
--- ruby_event_store_rom_sql/lib/ruby_event_store/rom/relations/stream_entries.rb
+++ ruby_event_store_rom_sql/lib/ruby_event_store/rom/relations/stream_entries.rb
@@ -3,11 +3,6 @@ module RubyEventStore
     module Relations
       class StreamEntries < ::ROM::Relation[:sql]
         schema(:event_store_events_in_streams, as: :stream_entries, infer: true) do
-          attribute :id, ::ROM::Types::Int.meta(primary_key: true)
-          attribute :stream, ::ROM::Types::String
-          attribute :position, ::ROM::Types::Int.optional
-          attribute :event_id, ::ROM::Types::String.meta(foreign_key: true, relation: :events)
-
           associations do
             belongs_to :events, as: :event, foreign_key: :event_id
           end
