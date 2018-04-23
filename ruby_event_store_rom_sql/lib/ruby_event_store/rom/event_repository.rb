0dc40f71fa191568265f2d04f6c17e2a4b696056
Paweł Pacana <pawel.pacana@gmail.com>
2018-04-23 22:30:37 +0200

    Obsolete reader methods in ROM::EventRepository.

--- ruby_event_store_rom_sql/lib/ruby_event_store/rom/event_repository.rb
+++ ruby_event_store_rom_sql/lib/ruby_event_store/rom/event_repository.rb
@@ -57,57 +57,6 @@ module RubyEventStore
           .first
       end
 
-      def read_events_forward(stream, after_event_id, count)
-        RubyEventStore::Specification.new(self)
-          .stream(stream.name)
-          .from(after_event_id)
-          .limit(count)
-          .each
-          .to_a
-      end
-
-      def read_events_backward(stream, before_event_id, count)
-        RubyEventStore::Specification.new(self)
-          .stream(stream.name)
-          .from(before_event_id)
-          .limit(count)
-          .backward
-          .each
-          .to_a
-      end
-
-      def read_stream_events_forward(stream)
-        RubyEventStore::Specification.new(self)
-          .stream(stream.name)
-          .each
-          .to_a
-      end
-
-      def read_stream_events_backward(stream)
-        RubyEventStore::Specification.new(self)
-          .stream(stream.name)
-          .backward
-          .each
-          .to_a
-      end
-
-      def read_all_streams_forward(after_event_id, count)
-        RubyEventStore::Specification.new(self)
-          .from(after_event_id)
-          .limit(count)
-          .each
-          .to_a
-      end
-
-      def read_all_streams_backward(before_event_id, count)
-        RubyEventStore::Specification.new(self)
-          .from(before_event_id)
-          .limit(count)
-          .backward
-          .each
-          .to_a
-      end
-
       def read_event(event_id)
         @events.by_id(event_id)
       rescue => ex
