75f42e097a2c6f53714ef1ce73e643c995fad020
Pawel Kondzior <pawel@kondzior.com>
2018-04-26 17:39:28 +0200

    Change event_id column length

--- rails_event_store_active_record/lib/rails_event_store_active_record/generators/templates/migration_template.rb
+++ rails_event_store_active_record/lib/rails_event_store_active_record/generators/templates/migration_template.rb
@@ -10,7 +10,7 @@ class CreateEventStoreEvents < ActiveRecord::Migration<%= migration_version %>
       if postgres
         t.references :event, null: false, type: :uuid
       else
-        t.references :event, null: false, type: :string
+        t.references :event, null: false, type: :string, limit: 36
       end
       t.datetime    :created_at,  null: false
     end
