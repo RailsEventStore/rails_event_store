cad01da339b329f2aff0abd3393af6b965759d92
Joel Van Horn <joel@joelvanhorn.com>
2018-04-25 20:40:43 -0400

    Updated migrations to more closely match ActiveRecord schema

--- ruby_event_store-rom/db/migrate/20180327044629_create_ruby_event_store_tables.rb
+++ ruby_event_store-rom/db/migrate/20180327044629_create_ruby_event_store_tables.rb
@@ -7,25 +7,25 @@ require 'rom/sql'
     
     run 'CREATE EXTENSION IF NOT EXISTS pgcrypto;' if postgres
 
-    create_table :event_store_events_in_streams do
-      primary_key :id, type: :Bignum
+    create_table? :event_store_events_in_streams do
+      primary_key :id, type: :Bignum, null: false
 
       column :stream, String, null: false
-      column :position, Integer, null: true
+      column :position, Integer
 
       if postgres
-        column :event_id, :uuid, null: false, index: true
+        column :event_id, :uuid, null: false
       else
-        column :event_id, String, size: 36, null: false, index: true
+        column :event_id, String, size: 36, null: false
       end
 
-      column :created_at, DateTime, null: false, index: true
+      column :created_at, DateTime, null: false, index: 'index_event_store_events_in_streams_on_created_at'
       
-      index %i[stream position], unique: true
+      index %i[stream position], unique: true, name: 'index_event_store_events_in_streams_on_stream_and_position'
       index %i[stream event_id], unique: true, name: 'index_event_store_events_in_streams_on_stream_and_event_id'
     end
 
-    create_table :event_store_events do
+    create_table? :event_store_events do
       if postgres
         column :id, :uuid, default: Sequel.function(:gen_random_uuid), primary_key: true
       else
@@ -35,7 +35,7 @@ require 'rom/sql'
       column :event_type, String, null: false
       column :metadata, String, text: true
       column :data, String, text: true, null: false
-      column :created_at, DateTime, null: false, index: true
+      column :created_at, DateTime, null: false, index: 'index_event_store_events_on_created_at'
 
       if sqlite # TODO: Is this relevant without ActiveRecord?
         index :id, unique: true
