36d3e8c46e852fecc83d449cefed53df6e124eb6
Paweł Pacana <pawel.pacana@gmail.com>
2018-05-05 02:29:55 +0200

    Revert "Add failure_message_when_negated for HavePublished matcher"
    This reverts commit 60fc6fcc9c07d59989865cfc9108b8c697cfc60d.

--- rails_event_store-rspec/lib/rails_event_store/rspec/have_published.rb
+++ rails_event_store-rspec/lib/rails_event_store/rspec/have_published.rb
@@ -34,13 +34,7 @@ module RailsEventStore
       end
 
       def failure_message
-        "expected #{expected} to be published, diff:" +
-            differ.diff_as_string(expected.to_s, events.to_s)
-      end
-
-      def failure_message_when_negated
-        "expected #{expected} not to be published, diff:" +
-            differ.diff_as_string(expected.to_s, events.to_s)
+        differ.diff_as_string(expected.to_s, events.to_s)
       end
 
       def description
