641a47d187eb6a13c23491cbf82bc165c92e3ff5
Paweł Pacana <pawel.pacana@gmail.com>
2018-05-15 17:48:04 +0200

    Refactor: use step on range.

--- rails_event_store_active_record/lib/rails_event_store_active_record/batch_enumerator.rb
+++ rails_event_store_active_record/lib/rails_event_store_active_record/batch_enumerator.rb
@@ -7,20 +7,17 @@ module RailsEventStoreActiveRecord
 
     def each(&block)
       Enumerator.new do |y|
-        0.upto(Float::INFINITY) do |index|
-          result = block.call(
-            batch_offset = batch_size * index,
-            [batch_size, total_limit - batch_offset, total_limit].min
-          )
+        (0..Float::INFINITY).step(batch_size) do |batch_offset|
+          batch_limit = [batch_size, total_limit - batch_offset, total_limit].min
+          result      = block.call(batch_offset, batch_limit)
+
           break if result.empty?
-
           y << result
-
           break if result.size < batch_size
         end
       end
     end
-    
+
     attr_accessor :batch_size, :total_limit
   end
 end
