6d638c019cb73473db944f69162a389bfd74409c
Szymon Fiedler <fidelski@gmail.com>
2018-05-18 19:01:57 +0200

    Less boilerplate

--- ruby_event_store/lib/ruby_event_store/batch_enumerator.rb
+++ ruby_event_store/lib/ruby_event_store/batch_enumerator.rb
@@ -7,18 +7,15 @@ module RubyEventStore
     end
 
     def each
-      enum = Enumerator.new do |y|
-        (0...total_limit).step(batch_size) do |batch_offset|
-          batch_offset = Integer(batch_offset)
-          batch_limit  = [batch_size, total_limit - batch_offset].min
-          result       = reader.call(batch_offset, batch_limit)
+      return to_enum unless block_given?
+      (0...total_limit).step(batch_size) do |batch_offset|
+        batch_offset = Integer(batch_offset)
+        batch_limit  = [batch_size, total_limit - batch_offset].min
+        result       = reader.call(batch_offset, batch_limit)
 
-          break if result.empty?
-          y << result
-        end
+        break if result.empty?
+        yield result
       end
-      enum.each { |batch| yield batch } if block_given?
-      enum
     end
 
     private
