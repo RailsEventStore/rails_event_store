47ece1c8167b06f81475194e76708fa6d0ed22eb
Joel Van Horn <joel@joelvanhorn.com>
2018-05-28 18:26:00 -0400

    Fix SQL tests to test pool size properly

--- ruby_event_store-rom/spec/rom/adapters/sql/spec_helper_spec.rb
+++ ruby_event_store-rom/spec/rom/adapters/sql/spec_helper_spec.rb
@@ -14,8 +14,16 @@ module RubyEventStore::ROM::SQL
       expect(helper.has_connection_pooling?).to eq(false)
     end
 
-    specify '#connection_pool_size is 1' do
-      expect(subject.connection_pool_size).to eq(1)
+    specify '#connection_pool_size is 1 when has_connection_pooling? is false' do
+      unless rom_helper.has_connection_pooling?
+        expect(subject.connection_pool_size).to eq(1)
+      end
+    end
+
+    specify '#connection_pool_size is 5 when has_connection_pooling? is true' do
+      if rom_helper.has_connection_pooling?
+        expect(subject.connection_pool_size).to eq(5)
+      end
     end
   end
 end
