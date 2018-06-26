bc13f846f9902263f6e4da905ff4d07ab811d0cf
Mirosław Pragłowski <m@praglowski.com>
2018-06-26 22:34:16 +0200

    Make AsyncDispatcher independent on what plumbing we are running it

--- rails_event_store/lib/rails_event_store/active_job_dispatcher.rb
+++ rails_event_store/lib/rails_event_store/active_job_dispatcher.rb
@@ -4,7 +4,17 @@ module RailsEventStore
 
   class ActiveJobDispatcher < AsyncDispatcher
     def initialize(proxy_strategy: AsyncProxyStrategy::Inline.new)
-      super(proxy_strategy: proxy_strategy)
+      super(proxy_strategy: proxy_strategy, async_call: ActiveJobCall.new)
+    end
+
+    class ActiveJobCall
+      def call(klass, serialized_event)
+        klass.perform_later(serialized_event.to_h)
+      end
+
+      def async_handler?(klass)
+        Class === klass && klass < ActiveJob::Base
+      end
     end
   end
 end
