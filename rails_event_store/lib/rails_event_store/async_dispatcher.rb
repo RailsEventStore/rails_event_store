eb91f5ea0a1be879999ff08a8c38fe3ba458ac48
Mirosław Pragłowski <m@praglowski.com>
2018-06-27 22:29:19 +0200

    Rename. Scheduler it is now.
    Dispatcher argument rename from `async_call` to `scheduler`.
    `ActiveJobCall` rename to `ActiveJobScheduler`.

--- rails_event_store/lib/rails_event_store/async_dispatcher.rb
+++ rails_event_store/lib/rails_event_store/async_dispatcher.rb
@@ -1,20 +1,20 @@
 module RailsEventStore
   class AsyncDispatcher < RubyEventStore::PubSub::Dispatcher
-    def initialize(proxy_strategy: AsyncProxyStrategy::Inline.new, async_call:)
+    def initialize(proxy_strategy: AsyncProxyStrategy::Inline.new, scheduler:)
       @async_proxy_strategy = proxy_strategy
-      @async_call = async_call
+      @scheduler = scheduler
     end
 
     def call(subscriber, _, serialized_event)
-      if @async_call.async_handler?(subscriber)
-        @async_proxy_strategy.call(->{ @async_call.call(subscriber, serialized_event) })
+      if @scheduler.async_handler?(subscriber)
+        @async_proxy_strategy.call(->{ @scheduler.call(subscriber, serialized_event) })
       else
         super
       end
     end
 
     def verify(subscriber)
-      super unless @async_call.async_handler?(subscriber)
+      super unless @scheduler.async_handler?(subscriber)
     end
   end
 end
