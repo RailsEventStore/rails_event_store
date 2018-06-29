1191347cca5ee58a5b902915d81e2e7c967118d2
Robert Pankowecki <robert.pankowecki@gmail.com>
2018-06-29 16:41:32 +0200

    RailsES::LinkByCorrelationId and RailsES::LinkByCausationId
    Issue: #221 #346

--- rails_event_store/lib/rails_event_store/link_by_metadata.rb
+++ rails_event_store/lib/rails_event_store/link_by_metadata.rb
@@ -1,7 +1,21 @@
 module RailsEventStore
+
   class LinkByMetadata < RubyEventStore::LinkByMetadata
-    def initialize(event_store: Rails.configuration.event_store, **conf)
-      super(event_store: event_store, **conf)
+    def initialize(event_store: Rails.configuration.event_store, key:, prefix: nil)
+      super(event_store: event_store, key: key, prefix: prefix)
     end
   end
+
+  class LinkByCorrelationId < RubyEventStore::LinkByCorrelationId
+    def initialize(event_store: Rails.configuration.event_store, prefix: nil)
+      super(event_store: event_store, prefix: prefix)
+    end
+  end
+
+  class LinkByCausationId < RubyEventStore::LinkByCausationId
+    def initialize(event_store: Rails.configuration.event_store, prefix: nil)
+      super(event_store: event_store, prefix: prefix)
+    end
+  end
+
 end
--- rails_event_store/spec/link_by_metadata_spec.rb
+++ rails_event_store/spec/link_by_metadata_spec.rb
@@ -8,7 +8,6 @@ module RailsEventStore
       rails = double("Rails", configuration: Rails::Application::Configuration.new)
       stub_const("Rails", rails)
       Rails.configuration.event_store = event_store
-      ActiveJob::Base.queue_adapter = SimpleAdapter
     end
 
     let(:event_store) { RailsEventStore::Client.new }
@@ -18,13 +17,68 @@ module RailsEventStore
         key: :city,
         prefix: "sweet+")
       )
-
       event_store.publish(ev = OrderCreated.new(metadata:{
         city: "Paris",
       }))
-
       expect(event_store.read.stream("sweet+Paris").each.to_a).to eq([ev])
     end
 
   end
+
+  RSpec.describe LinkByCorrelationId do
+    before do
+      rails = double("Rails", configuration: Rails::Application::Configuration.new)
+      stub_const("Rails", rails)
+      Rails.configuration.event_store = event_store
+    end
+
+    let(:event_store) { RailsEventStore::Client.new }
+    let(:event) do
+      OrderCreated.new.tap do |ev|
+        ev.correlation_id = "COR"
+        ev.causation_id   = "CAU"
+      end
+    end
+
+    specify "links" do
+      event_store.subscribe_to_all_events(LinkByCorrelationId.new)
+      event_store.publish(event)
+      expect(event_store.read.stream("$by_correlation_id_COR").each.to_a).to eq([event])
+    end
+
+    specify "defaults to Rails.configuration.event_store and passes rest of options" do
+      event_store.subscribe_to_all_events(LinkByCorrelationId.new(prefix: "sweet+"))
+      event_store.publish(event)
+      expect(event_store.read.stream("sweet+COR").each.to_a).to eq([event])
+    end
+  end
+
+  RSpec.describe LinkByCausationId do
+    before do
+      rails = double("Rails", configuration: Rails::Application::Configuration.new)
+      stub_const("Rails", rails)
+      Rails.configuration.event_store = event_store
+    end
+
+    let(:event_store) { RailsEventStore::Client.new }
+    let(:event) do
+      OrderCreated.new.tap do |ev|
+        ev.correlation_id = "COR"
+        ev.causation_id   = "CAU"
+      end
+    end
+
+    specify "links" do
+      event_store.subscribe_to_all_events(LinkByCausationId.new)
+      event_store.publish(event)
+      expect(event_store.read.stream("$by_causation_id_CAU").each.to_a).to eq([event])
+    end
+
+    specify "defaults to Rails.configuration.event_store and passes rest of options" do
+      event_store.subscribe_to_all_events(LinkByCausationId.new(prefix: "sweet+"))
+      event_store.publish(event)
+      expect(event_store.read.stream("sweet+CAU").each.to_a).to eq([event])
+    end
+  end
+
 end
