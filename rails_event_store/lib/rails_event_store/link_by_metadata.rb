bbc55e6842d9c45496322c1535ce3db65e40ab3d
Mirosław Pragłowski <m@praglowski.com>
2018-07-20 14:51:22 +0200

    Kills some mutants

--- rails_event_store/lib/rails_event_store/link_by_metadata.rb
+++ rails_event_store/lib/rails_event_store/link_by_metadata.rb
@@ -2,26 +2,26 @@ module RailsEventStore
 
   class LinkByMetadata < RubyEventStore::LinkByMetadata
     def initialize(event_store: Rails.configuration.event_store, key:, prefix: nil)
-      super(event_store: event_store, key: key, prefix: prefix)
+      super
     end
   end
 
   class LinkByCorrelationId < RubyEventStore::LinkByCorrelationId
     def initialize(event_store: Rails.configuration.event_store, prefix: nil)
-      super(event_store: event_store, prefix: prefix)
+      super
     end
   end
 
   class LinkByCausationId < RubyEventStore::LinkByCausationId
     def initialize(event_store: Rails.configuration.event_store, prefix: nil)
-      super(event_store: event_store, prefix: prefix)
+      super
     end
   end
 
   class LinkByEventType < RubyEventStore::LinkByEventType
     def initialize(event_store: Rails.configuration.event_store, prefix: nil)
-      super(event_store: event_store, prefix: prefix)
+      super
     end
   end
-  
+
 end
