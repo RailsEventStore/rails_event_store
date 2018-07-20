7082e7ae25679abf86e623a93977ee4aeedb3206
Joel Van Horn <joel@joelvanhorn.com>
2018-07-20 18:58:50 -0400

    DRY'd up and work around `last` to use `first`

--- ruby_event_store-rom/lib/ruby_event_store/rom/repositories/events.rb
+++ ruby_event_store-rom/lib/ruby_event_store/rom/repositories/events.rb
@@ -35,42 +35,45 @@ module RubyEventStore
           events.map_with(:event_to_serialized_record).by_pk(event_id).one!
         end
 
+        MATERIALIZE_READ_AS = {
+          RubyEventStore::Specification::BATCH => :to_ary,
+          RubyEventStore::Specification::FIRST => :first,
+          RubyEventStore::Specification::LAST  => :first
+        }.freeze
+
         def read(direction, stream, from:, limit:, read_as:, batch_size:)
           unless from.equal?(:head)
             offset_entry_id = stream_entries.by_stream_and_event_id(stream, from).fetch(:id)
           end
 
+          # Note: `last` is problematic, so we switch direction and get `first`.
+          #       See `MATERIALIZE_READ_AS`
+          if read_as == RubyEventStore::Specification::LAST
+            direction = direction == :forward ? :backward : :forward
+          end
+
+          query = stream_entries.ordered(direction, stream, offset_entry_id)
+
           if read_as == RubyEventStore::Specification::BATCH
             reader = ->(offset, limit) do
-              stream_entries
-                .ordered(direction, stream, offset_entry_id)
-                .offset(offset)
-                .take(limit)
-                .combine(:event)
-                .map_with(:stream_entry_to_serialized_record, auto_struct: false)
-                .to_ary
+              query_builder(query, offset: offset, limit: limit).to_ary
             end
             BatchEnumerator.new(batch_size, limit || Float::INFINITY, reader).each
-          elsif read_as == RubyEventStore::Specification::FIRST
-            stream_entries
-              .ordered(direction, stream, offset_entry_id)
-              .combine(:event)
-              .map_with(:stream_entry_to_serialized_record, auto_struct: false)
-              .first
-          elsif read_as == RubyEventStore::Specification::LAST
-            stream_entries
-              .ordered(direction, stream, offset_entry_id)
-              .combine(:event)
-              .map_with(:stream_entry_to_serialized_record, auto_struct: false)
-              .to_ary.last
           else
-            stream_entries
-              .ordered(direction, stream, offset_entry_id)
-              .take(limit)
-              .combine(:event)
-              .map_with(:stream_entry_to_serialized_record, auto_struct: false)
-              .each
+            materialize_method = MATERIALIZE_READ_AS.fetch(read_as, :each)
+            query_builder(query, limit: limit).__send__(materialize_method)
           end
+        end
+
+      protected
+
+        def query_builder(query, offset: nil, limit: nil)
+          query = query.offset(offset) if offset
+          query = query.take(limit)    if limit
+
+          query
+            .combine(:event)
+            .map_with(:stream_entry_to_serialized_record, auto_struct: false)
         end
       end
     end
