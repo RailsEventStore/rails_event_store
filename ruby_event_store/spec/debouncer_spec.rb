68e178b9ec6efa8448d82ce1f4595a4cf524d0cc
Robert Pankowecki <robert.pankowecki@gmail.com>
2018-07-26 12:52:49 +0200

    More reliable tests for Debouncer
    hopefully ;)

--- ruby_event_store/spec/debouncer_spec.rb
+++ ruby_event_store/spec/debouncer_spec.rb
@@ -6,57 +6,89 @@ module RubyEventStore
 
     specify "waits 'delay' amount of time when fast jobs" do
       a = []
-      d = Debouncer.new(delay: 0.020) do
-        a << nil
+      d = Debouncer.new(delay: delay = 0.020, timeout: 2) do
+        a << Time.now
       end
-      expect do
-        Timeout.timeout(0.3) do
-          loop{ rand(10).times{ d.call }; Thread.pass }
-        end
-      end.to raise_error(Timeout::Error)
-      expect(a.size).to be_between(13, 16)
+      while a.size < 10
+        rand(10).times{ d.call }
+        Thread.pass
+      end
+
+      a.each_cons(2) do |earlier, later|
+        expect(later.to_f - earlier.to_f).to be >= delay
+        expect(later.to_f - earlier.to_f).to be <= delay*2
+      end
     end
 
     specify "waits 'delay+job' amount of time normally" do
       a = []
-      d = Debouncer.new(delay: 0.020) do
-        a << nil
-        sleep(0.010)
+      job = 0.010
+      d = Debouncer.new(delay: delay = 0.020) do
+        a << Time.now
+        sleep(job)
       end
-      expect do
-        Timeout.timeout(0.3) do
-          loop{ rand(10).times{ d.call }; Thread.pass }
-        end
-      end.to raise_error(Timeout::Error)
-      expect(a.size).to be_between(8, 11)
+      while a.size < 10
+        rand(10).times{ d.call }
+        Thread.pass
+      end
+      a.each_cons(2) do |earlier, later|
+        expect(later.to_f - earlier.to_f).to be >= delay+job
+        expect(later.to_f - earlier.to_f).to be <= (delay+job)*2
+      end
     end
 
     specify "waits 'timeout' amount of time when job is lagging" do
       a = []
-      d = Debouncer.new(delay: 0.010, timeout: 0.020) do
-        a << nil
-        sleep(0.100)
+      job = 0.500
+      d = Debouncer.new(delay: 0.000, timeout: timeout = 0.020) do
+        a << Time.now
+        sleep(job)
       end
-      expect do
-        Timeout.timeout(0.3) do
-          loop{ rand(10).times{ d.call }; Thread.pass }
-        end
-      end.to raise_error(Timeout::Error)
-      expect(a.size).to be_between(13, 16)
+      while a.size < 10
+        rand(10).times{ d.call }
+        Thread.pass
+      end
+      a.each_cons(2) do |earlier, later|
+        expect(later.to_f - earlier.to_f).to be >= timeout
+        expect(later.to_f - earlier.to_f).to be <= timeout*2
+      end
+    end
+
+    specify "waits 'delay' amount of time when job is crashing" do
+      times = []
+      threads = []
+      d = Debouncer.new(delay: delay = 0.020, timeout: 2) do
+        times << Time.now
+        threads << Thread.current.object_id
+        raise "x"
+      end
+      while times.size < 10
+        rand(10).times{ d.call }
+        Thread.pass
+      end
+
+      times.each_cons(2) do |earlier, later|
+        expect(later.to_f - earlier.to_f).to be >= delay
+        expect(later.to_f - earlier.to_f).to be <= delay*2
+      end
+
+      expect(threads.uniq.size).to eq(1)
     end
 
     specify "creates new consumer if it crashes" do
-      a = []
+      threads = []
       d = Debouncer.new(delay: 0.020) do
-        a << nil
-        raise "x"
+        Thread.current.report_on_exception = false
+        threads << Thread.current.object_id
+        raise Exception
       end
-      expect do
-        Timeout.timeout(0.3) do
-          loop{ rand(10).times{ d.call }; Thread.pass }
-        end
-      end.to raise_error(Timeout::Error)
-      expect(a.size).to be_between(13, 16)
+      while threads.size < 10
+        rand(10).times{ d.call }
+        Thread.pass
+      end
+
+      expect(threads.size).to be >= 10
+      expect(threads.uniq.sort).to eq(threads.sort)
     end
   end
 end
