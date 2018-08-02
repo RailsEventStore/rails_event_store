9d8ed4e9628bb3fd2b79fe422fcf22221291a4d5
Robert Pankowecki <robert.pankowecki@gmail.com>
2018-08-02 16:31:36 +0200

    Drop unneeded rails frameworks
    we don't need them in testing our dres_rails gem

--- contrib/distributed_rails_event_store/dres_rails/spec/dummy/config/application.rb
+++ contrib/distributed_rails_event_store/dres_rails/spec/dummy/config/application.rb
@@ -6,11 +6,11 @@ require_relative 'boot'
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_view/railtie"
-require "action_mailer/railtie"
-require "active_job/railtie"
-require "action_cable/engine"
+# require "action_mailer/railtie"
+# require "active_job/railtie"
+# require "action_cable/engine"
 # require "rails/test_unit/railtie"
-require "sprockets/railtie"
+# require "sprockets/railtie"
 
 Bundler.require(*Rails.groups)
 require "dres_rails"
@@ -25,6 +25,10 @@ module Dummy
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |g|
+      g.test_framework :rspec
+    end
   end
 end
 
