d5ccda83d339706e93c9a766e427722e7cd63456
Rafał Łasocha <me@swistak35.com>
2018-08-09 10:17:21 +0200

    Use temporary subscriptions to avoid memory leaks
    Issue: https://github.com/RailsEventStore/rails_event_store/pull/423#discussion_r208813529

--- rails_event_store/spec/instrumented_repository_spec.rb
+++ rails_event_store/spec/instrumented_repository_spec.rb
@@ -15,17 +15,16 @@ module RailsEventStore
       end
 
       specify "instruments" do
-        some_repository = double
-        allow(some_repository).to receive(:append_to_stream)
-        instrumented_repository = InstrumentedRepository.new(some_repository)
-        notification_calls = subscribe_to("append_to_stream.repository.rails_event_store")
-        event1 = Object.new
+        instrumented_repository = InstrumentedRepository.new(spy)
+        subscribe_to("append_to_stream.repository.rails_event_store") do |notification_calls|
+          event1 = Object.new
 
-        instrumented_repository.append_to_stream([event1], "SomeStream", "c456845d-2b86-49c1-bdef-89e57b5d86b1")
+          instrumented_repository.append_to_stream([event1], "SomeStream", "c456845d-2b86-49c1-bdef-89e57b5d86b1")
 
-        expect(notification_calls).to eq([
-          { events: [event1], stream: "SomeStream" }
-        ])
+          expect(notification_calls).to eq([
+            { events: [event1], stream: "SomeStream" }
+          ])
+        end
       end
     end
 
@@ -40,16 +39,15 @@ module RailsEventStore
       end
 
       specify "instruments" do
-        some_repository = double
-        allow(some_repository).to receive(:link_to_stream)
-        instrumented_repository = InstrumentedRepository.new(some_repository)
-        notification_calls = subscribe_to("link_to_stream.repository.rails_event_store")
+        instrumented_repository = InstrumentedRepository.new(spy)
+        subscribe_to("link_to_stream.repository.rails_event_store") do |notification_calls|
 
-        instrumented_repository.link_to_stream([42], "SomeStream", "c456845d-2b86-49c1-bdef-89e57b5d86b1")
+          instrumented_repository.link_to_stream([42], "SomeStream", "c456845d-2b86-49c1-bdef-89e57b5d86b1")
 
-        expect(notification_calls).to eq([
-          { event_ids: [42], stream: "SomeStream" }
-        ])
+          expect(notification_calls).to eq([
+            { event_ids: [42], stream: "SomeStream" }
+          ])
+        end
       end
     end
 
@@ -64,16 +62,15 @@ module RailsEventStore
       end
 
       specify "instruments" do
-        some_repository = double
-        allow(some_repository).to receive(:delete_stream)
-        instrumented_repository = InstrumentedRepository.new(some_repository)
-        notification_calls = subscribe_to("delete_stream.repository.rails_event_store")
+        instrumented_repository = InstrumentedRepository.new(spy)
+        subscribe_to("delete_stream.repository.rails_event_store") do |notification_calls|
 
-        instrumented_repository.delete_stream("SomeStream")
+          instrumented_repository.delete_stream("SomeStream")
 
-        expect(notification_calls).to eq([
-          { stream: "SomeStream" }
-        ])
+          expect(notification_calls).to eq([
+            { stream: "SomeStream" }
+          ])
+        end
       end
     end
 
@@ -110,16 +107,15 @@ module RailsEventStore
       end
 
       specify "instruments" do
-        some_repository = double
-        allow(some_repository).to receive(:read_event)
-        instrumented_repository = InstrumentedRepository.new(some_repository)
-        notification_calls = subscribe_to("read_event.repository.rails_event_store")
+        instrumented_repository = InstrumentedRepository.new(spy)
+        subscribe_to("read_event.repository.rails_event_store") do |notification_calls|
 
-        instrumented_repository.read_event(42)
+          instrumented_repository.read_event(42)
 
-        expect(notification_calls).to eq([
-          { event_id: 42 }
-        ])
+          expect(notification_calls).to eq([
+            { event_id: 42 }
+          ])
+        end
       end
     end
 
@@ -135,26 +131,25 @@ module RailsEventStore
       end
 
       specify "instruments" do
-        some_repository = double
-        allow(some_repository).to receive(:read)
-        instrumented_repository = InstrumentedRepository.new(some_repository)
-        notification_calls = subscribe_to("read.repository.rails_event_store")
-        specification = double
+        instrumented_repository = InstrumentedRepository.new(spy)
+        subscribe_to("read.repository.rails_event_store") do |notification_calls|
+          specification = double
 
-        instrumented_repository.read(specification)
+          instrumented_repository.read(specification)
 
-        expect(notification_calls).to eq([
-          { specification: specification }
-        ])
+          expect(notification_calls).to eq([
+            { specification: specification }
+          ])
+        end
       end
     end
 
     def subscribe_to(name)
       received_payloads = []
-      ActiveSupport::Notifications.subscribe(name) do |_name, _start, _finish, _id, payload|
-        received_payloads << payload
+      callback = ->(_name, _start, _finish, _id, payload) { received_payloads << payload }
+      ActiveSupport::Notifications.subscribed(callback, name) do
+        yield received_payloads
       end
-      received_payloads
     end
   end
 end
