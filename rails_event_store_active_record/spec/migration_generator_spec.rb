876bf9d4cc4bc2d400f37a6ba23305d84d3c4581
Piotr Jaworski <piotrek.jaw@gmail.com>
2018-08-11 00:46:34 +0100

    Spec refactor

--- rails_event_store_active_record/spec/migration_generator_spec.rb
+++ rails_event_store_active_record/spec/migration_generator_spec.rb
@@ -28,16 +28,13 @@ module RailsEventStoreActiveRecord
       FakeFS.with_fresh do
         FakeFS::FileSystem.clone(File.expand_path('../../', __FILE__))
 
-        rails_version = '5.2.0'
-        rails_version_with_subnumber = rails_version.match(/\d\.\d/)[0]
-
-        stub_const('Rails::VERSION::STRING', rails_version)
+        stub_const('Rails::VERSION::STRING', '5.2.0')
 
         generator = MigrationGenerator.new
         allow(Time).to receive(:now).and_return(Time.new(2016,8,9,22,22,22))
         generator.create_migration
 
-        expect(File.read('db/migrate/20160809222222_create_event_store_events.rb')).to include("ActiveRecord::Migration[#{rails_version_with_subnumber}]")
+        expect(File.read('db/migrate/20160809222222_create_event_store_events.rb')).to match(/ActiveRecord::Migration\[5\.2\]$/)
       end
     end
   end
