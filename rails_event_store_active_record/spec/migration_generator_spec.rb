fd40b9e93dba3c2a5f3c22f859d1fe7a09bec480
Piotr Jaworski <piotrek.jaw@gmail.com>
2018-08-10 17:00:41 +0100

    Added specs to Rails migration number

--- rails_event_store_active_record/spec/migration_generator_spec.rb
+++ rails_event_store_active_record/spec/migration_generator_spec.rb
@@ -14,26 +14,30 @@ module RailsEventStoreActiveRecord
     specify do
       FakeFS.with_fresh do
         FakeFS::FileSystem.clone(File.expand_path('../../', __FILE__))
-        stub_const("Rails::VERSION::STRING", "4.2.8")
+        stub_const('Rails::VERSION::STRING', '4.2.8')
 
         generator = MigrationGenerator.new
         allow(Time).to receive(:now).and_return(Time.new(2016,8,9,22,22,22))
         generator.create_migration
 
-        expect(File.read("db/migrate/20160809222222_create_event_store_events.rb")).to match(/ActiveRecord::Migration$/)
+        expect(File.read('db/migrate/20160809222222_create_event_store_events.rb')).to match(/ActiveRecord::Migration$/)
       end
     end
 
     specify do
       FakeFS.with_fresh do
         FakeFS::FileSystem.clone(File.expand_path('../../', __FILE__))
-        stub_const("Rails::VERSION::STRING", "5.0.0")
+
+        rails_version = '5.2.0'
+        rails_version_with_subnumber = rails_version.match(/\d\.\d/)[0]
+
+        stub_const('Rails::VERSION::STRING', rails_version)
 
         generator = MigrationGenerator.new
         allow(Time).to receive(:now).and_return(Time.new(2016,8,9,22,22,22))
         generator.create_migration
 
-        expect(File.read("db/migrate/20160809222222_create_event_store_events.rb")).to match(/ActiveRecord::Migration\[4\.2\]$/)
+        expect(File.read('db/migrate/20160809222222_create_event_store_events.rb')).to include("ActiveRecord::Migration[#{rails_version_with_subnumber}]")
       end
     end
   end
