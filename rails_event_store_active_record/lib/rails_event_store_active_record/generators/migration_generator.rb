6f89757f4ab2abd05829621c248036116b5836c7
Piotr Jaworski <piotrek.jaw@gmail.com>
2018-08-10 16:58:10 +0100

    Add proper Rails version to migration

--- rails_event_store_active_record/lib/rails_event_store_active_record/generators/migration_generator.rb
+++ rails_event_store_active_record/lib/rails_event_store_active_record/generators/migration_generator.rb
@@ -18,8 +18,10 @@ module RailsEventStoreActiveRecord
     end
 
     def migration_version
-      return nil if Gem::Version.new(rails_version) < Gem::Version.new("5.0.0")
-      "[4.2]"
+      return nil if Gem::Version.new(rails_version) < Gem::Version.new('5.0.0')
+
+      rails_version_with_subnumber = rails_version.match(/\d\.\d/)[0]
+      "[#{rails_version_with_subnumber}]"
     end
 
     def timestamp
