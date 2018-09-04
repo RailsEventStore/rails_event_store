db0adb088c8436e95c5b64d148e1dd5709c31277
Rafał Łasocha <me@swistak35.com>
2018-09-04 10:59:07 +0200

    Add ComposedDispatcher
    Issue: #442

---
+++ ruby_event_store/spec/composed_dispatcher_spec.rb
@@ -0,0 +1,99 @@
+require "spec_helper"
+
+module RubyEventStore
+  RSpec.describe ComposedDispatcher do
+    let(:skippy_dispatcher) do
+      Class.new do
+        def call(subscriber, event, serialized_event)
+          @called = true
+        end
+
+        def verify(subscriber)
+          false
+        end
+        attr_reader :called
+      end
+    end
+    let(:real_dispatcher) do
+      Class.new do
+        def call(subscriber, event, serialized_event)
+          @called = true
+        end
+
+        def verify(subscriber)
+          true
+        end
+        attr_reader :called
+      end
+    end
+
+    describe "#verify" do
+      specify "pass subscriber to dispatcher" do
+        dispatcher = spy
+        composed_dispatcher = ComposedDispatcher.new(dispatcher)
+        subscriber = double
+
+        composed_dispatcher.verify(subscriber)
+
+        expect(dispatcher).to have_received(:verify).with(subscriber)
+      end
+
+      specify "true if at least one dispatcher truthy" do
+        composed_dispatcher = ComposedDispatcher.new(skippy_dispatcher.new, real_dispatcher.new)
+
+        subscriber = double
+        expect(composed_dispatcher.verify(subscriber)).to eq(true)
+      end
+
+      specify "false if all dispatchers falsey" do
+        composed_dispatcher = ComposedDispatcher.new(skippy_dispatcher.new)
+
+        subscriber = double
+        expect(composed_dispatcher.verify(subscriber)).to eq(false)
+      end
+    end
+
+    describe "#call" do
+      specify "pass arguments to dispatcher" do
+        dispatcher = spy
+        composed_dispatcher = ComposedDispatcher.new(dispatcher)
+        event = instance_double(::RubyEventStore::Event)
+        serialized_event = instance_double(::RubyEventStore::SerializedRecord)
+        subscriber = double
+
+        composed_dispatcher.call(subscriber, event, serialized_event)
+
+        expect(dispatcher).to have_received(:verify).with(subscriber)
+        expect(dispatcher).to have_received(:call).with(subscriber, event, serialized_event)
+      end
+
+      specify "calls only verified dispatcher" do
+        skippy = skippy_dispatcher.new
+        real = real_dispatcher.new
+        composed_dispatcher = ComposedDispatcher.new(skippy, real)
+        event = instance_double(::RubyEventStore::Event)
+        serialized_event = instance_double(::RubyEventStore::SerializedRecord)
+        subscriber = double
+
+        composed_dispatcher.call(subscriber, event, serialized_event)
+
+        expect(skippy.called).to be_falsey
+        expect(real.called).to be_truthy
+      end
+
+      specify "calls only first verified dispatcher" do
+        real1 = real_dispatcher.new
+        real2 = real_dispatcher.new
+        composed_dispatcher = ComposedDispatcher.new(real1, real2)
+        event = instance_double(::RubyEventStore::Event)
+        serialized_event = instance_double(::RubyEventStore::SerializedRecord)
+        subscriber = double
+
+        composed_dispatcher.call(subscriber, event, serialized_event)
+
+        expect(real1.called).to be_truthy
+        expect(real2.called).to be_falsey
+      end
+    end
+  end
+end
