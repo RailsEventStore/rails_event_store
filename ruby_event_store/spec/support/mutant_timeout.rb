a82b4ee650a7e4a94456122750de6da959f2c40b
Rafał Łasocha <me@swistak35.com>
2018-09-23 19:29:42 +0200

    Timeout only if mutating
    * We have these timeouts only for the sake of mutant sometimes
      going (intentionally) in infinite loop
    * If you wanted to use the debugger, you had to comment these timeouts
    * After debugging you had to remember to uncomment the timeouts
    
    This commit solves these problems

--- ruby_event_store/spec/support/mutant_timeout.rb
+++ ruby_event_store/spec/support/mutant_timeout.rb
@@ -2,7 +2,11 @@ require 'timeout'
 
 RSpec.configure do |config|
   config.around(:each) do |example|
-    timeout = example.metadata[:timeout] || 5
-    Timeout.timeout(timeout, &example)
+    if ENV["MUTATING"]
+      timeout = example.metadata[:timeout] || 5
+      Timeout.timeout(timeout, &example)
+    else
+      example.call
+    end
   end
 end
