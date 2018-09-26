60cf00d9b1ab908f8036678923ef3be8d82737f8
Paweł Pacana <pawel.pacana@gmail.com>
2018-09-26 14:17:57 +0200

    Much overdue deprecation removal.

--- rails_event_store/lib/rails_event_store/deprecations.rb
+++
@@ -1,7 +0,0 @@
-module RailsEventStore
-  def self.const_missing(const_name)
-    super unless const_name == :MethodNotDefined
-    warn "`RailsEventStore::MethodNotDefined` has been deprecated. Use `RailsEventStore::InvalidHandler` instead."
-    InvalidHandler
-  end
-end
--- ruby_event_store/lib/ruby_event_store/deprecations.rb
+++
@@ -1,7 +0,0 @@
-module RubyEventStore
-  def self.const_missing(const_name)
-    super unless const_name == :MethodNotDefined
-    warn "`RubyEventStore::MethodNotDefined` has been deprecated. Use `RubyEventStore::InvalidHandler` instead."
-    InvalidHandler
-  end
-end
