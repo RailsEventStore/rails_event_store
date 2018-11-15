812ee8233afaa8df790e134e97f0c23a6c37e34d
Paweł Pacana <pawel.pacana@gmail.com>
2018-11-15 02:38:48 +0100

    Run migration from the request gem version over AR::Schema copy-paste.

--- rails_event_store_active_record-legacy/spec/schema/fill_data.rb
+++ rails_event_store_active_record-legacy/spec/schema/fill_data.rb
@@ -2,26 +2,16 @@ require 'rails/generators' # doh
 require 'rails_event_store_active_record'
 require 'ruby_event_store'
 require 'logger'
+require_relative '../../../lib/migrator'
 
 $verbose = ENV.has_key?('VERBOSE') ? true : false
-
+ActiveRecord::Schema.verbose = $verbose
 ActiveRecord::Base.logger = Logger.new(STDOUT) if $verbose
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'].gsub("db.sqlite3", "../../db.sqlite3"))
-ActiveRecord::Schema.define do
-  self.verbose = $verbose
-  create_table(:event_store_events, force: false) do |t|
-    t.string      :stream,      null: false
-    t.string      :event_type,  null: false
-    t.string      :event_id,    null: false
-    t.text        :metadata
-    t.text        :data,        null: false
-    t.datetime    :created_at,  null: false
-  end
-  add_index :event_store_events, :stream
-  add_index :event_store_events, :created_at
-  add_index :event_store_events, :event_type
-  add_index :event_store_events, :event_id, unique: true
-end
+
+gem_path = $LOAD_PATH.find { |path| path.match(/rails_event_store_active_record/) }
+Migrator.new(File.expand_path('rails_event_store_active_record/generators/templates', gem_path))
+  .run_migration('create_event_store_events', 'migration')
 
 class EventAll < RubyEventStore::Event
 end
