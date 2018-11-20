c18b9b08819afd0fafca75ef2f2458390abf6237
Paweł Pacana <pawel.pacana@gmail.com>
2018-11-20 22:21:27 +0100

    More generous timeout that was present before.
    Back in 3a44d6d39b0e13cd47582e476cbb3fd330cca9ea only ruby_event_store
    had such short 5 second timeout. It is present only to somehow help in
    cases where mutant is really stuck in infinite loops via mutations.

--- lib/mutant_timeout.rb
+++ lib/mutant_timeout.rb
@@ -3,8 +3,7 @@ require 'timeout'
 RSpec.configure do |config|
   config.around(:each) do |example|
     if ENV["MUTATING"]
-      timeout = example.metadata[:timeout] || 5
-      Timeout.timeout(timeout, &example)
+      Timeout.timeout(example.metadata[:timeout] || 10, &example)
     else
       example.call
     end
