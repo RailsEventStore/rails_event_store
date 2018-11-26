25daa9244edde56e4f51900d495c7012193d3162
Mirosław Pragłowski <m@praglowski.com>
2018-11-26 23:51:35 +0100

    Make keys transformation non destructive

--- ruby_event_store/lib/ruby_event_store/mappers/protobuf.rb
+++ ruby_event_store/lib/ruby_event_store/mappers/protobuf.rb
@@ -1,20 +1,20 @@
 module RubyEventStore
   module TransformKeys
     private
-    def stringify_keys(data, initial = {})
-      transform_keys(data, initial) {|k| k.to_s}
+    def stringify_keys(data)
+      transform_keys(data) {|k| k.to_s}
     end
-    def symbolize_keys(data, initial = {})
-      transform_keys(data, initial) {|k| k.to_sym}
+    def symbolize_keys(data)
+      transform_keys(data) {|k| k.to_sym}
     end
 
-    def transform_keys(data, initial, &block)
-      data.each_with_object(initial) do |(k, v), h|
+    def transform_keys(data, &block)
+      data.dup.each_with_object({}) do |(k, v), h|
         h[yield(k)] = case v
           when Hash
-            transform_keys(v, {}, &block)
+            transform_keys(v, &block)
           when Array
-            v.map{|i| Hash === i ? transform_keys(i, {}, &block) : i}
+            v.map{|i| Hash === i ? transform_keys(i, &block) : i}
           else
             v
         end
