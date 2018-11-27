37fd059ede4f66067ce1e61e82f5b71b459774b6
Paweł Pacana <pawel.pacana@gmail.com>
2018-11-27 14:30:37 +0100

    test-fast for everyone!

--- lib/test.mk
+++ lib/test.mk
@@ -1,3 +1,7 @@
 test: ## Run unit tests
 	@echo "Running unit tests"
 	@bundle exec rspec
+
+test-fast: ## Run unit tests with --fail-fast --order defined --backtrace
+	@echo "Running unit tests with --fail-fast"
+	@bundle exec rspec --fail-fast --order defined --backtrace
