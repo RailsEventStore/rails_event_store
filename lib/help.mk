552380c7cf04e5687938b4eadcb1a5083526c85a
Paweł Pacana <pawel.pacana@gmail.com>
2018-11-27 14:49:36 +0100

    Better sorting after we filter out source makefile name.

--- lib/help.mk
+++ lib/help.mk
@@ -1,5 +1,5 @@
 help:
-	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = "(:|:[^:]*?## )"}; {printf "\033[36m%-30s\033[0m %s\n", $$2, $$3}'
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = "(:|:[^:]*?## )"}; {printf "\033[36m%-30s\033[0m %s\n", $$2, $$3}' | sort
 
 .PHONY: help
 .DEFAULT_GOAL := help
