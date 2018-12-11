e8f45b9083c999ebc8b3ab5108478533fdf4dbd8
Paweł Pacana <pawel.pacana@gmail.com>
2018-12-11 01:21:04 +0100

    Migration to global-stream-less schema.
    * MySQL copies ids for primary key from streams table to preserve
      insertion order when add new auto_increment primary key,
      test intentionally checks that new event added after migration get
      correct pkey value (and does not reuse gap value)
    
    * not sure about PostreSQL constraints on foreign_key when primary_key
      was changed, the order of events was maintained by adding new pkey
      but worth checking if that behaviour is stable

---
+++ rails_event_store_active_record/lib/rails_event_store_active_record/generators/no_global_stream_entries_generator.rb
@@ -0,0 +1,29 @@
+begin
+  require 'rails/generators'
+rescue LoadError
+end
+
+module RailsEventStoreActiveRecord
+  class NoGlobalStreamEntries < Rails::Generators::Base
+    source_root File.expand_path(File.join(File.dirname(__FILE__), '../generators/templates'))
+
+    def create_migration
+      template "no_global_stream_entries_template.rb", "db/migrate/#{timestamp}_no_global_stream_entries.rb"
+    end
+
+    private
+
+    def rails_version
+      Rails::VERSION::STRING
+    end
+
+    def migration_version
+      return nil if Gem::Version.new(rails_version) < Gem::Version.new("5.0.0")
+      "[4.2]"
+    end
+
+    def timestamp
+      Time.now.strftime("%Y%m%d%H%M%S")
+    end
+  end
+end if defined?(Rails::Generators::Base)
