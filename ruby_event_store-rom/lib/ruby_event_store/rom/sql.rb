afd5eb64ea95901f105778ca343d85a5da8d1786
Joel Van Horn <joel@joelvanhorn.com>
2018-12-30 03:39:01 -0500

    Check if upserts are supported (including Sqlite 3.24+)

--- ruby_event_store-rom/lib/ruby_event_store/rom/sql.rb
+++ ruby_event_store-rom/lib/ruby_event_store/rom/sql.rb
@@ -60,6 +60,27 @@ module RubyEventStore
             end
           }
         end
+
+        def supports_upsert?(db)
+          supports_on_duplicate_key_update?(db) ||
+          supports_insert_conflict_update?(db)
+        end
+
+        def supports_on_duplicate_key_update?(db)
+          db.adapter_scheme =~ /mysql/
+        end
+
+        def supports_insert_conflict_update?(db)
+          case db.adapter_scheme
+          when /postgres/
+            true
+          when /sqlite/
+            # Sqlite 3.24.0+ supports PostgreSQL upsert syntax
+            db.sqlite_version >= 32400
+          else
+            false
+          end
+        end
       end
 
       class SpecHelper
