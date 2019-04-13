698271976b42bad2cff05d20f9c7f044f98f21f1
Mirosław Pragłowski <m@praglowski.com>
2019-04-13 09:54:25 +0200

    Symbolize all the things ;)

--- ruby_event_store/lib/ruby_event_store/mappers/default.rb
+++ ruby_event_store/lib/ruby_event_store/mappers/default.rb
@@ -9,7 +9,7 @@ module RubyEventStore
         @pipeline = Pipeline.new([
           DomainEventMapper.new,
           EventClassRemapper.new(events_class_remapping),
-          SymbolizeKeys.new(symbolize_data: false),
+          SymbolizeKeys.new,
           SerializedRecordMapper.new(serializer: serializer)
         ])
       end
