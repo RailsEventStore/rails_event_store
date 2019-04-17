464af5111e0217904ae8a87b68f26775873abc6a
Paweł Pacana <pawel.pacana@gmail.com>
2019-04-17 15:07:43 +0200

    Mutant and OpenSSL don't go well together.
    - mutant insists on removing auth_data which according to openssl docs
      is required for authenticated mode to work, same story as
      crypto.decrypt/crypto.encrypt
    - there is no feedback from openssl when auth_data is skipped, although
      in consequence it may result in weaker crypto

--- ruby_event_store/lib/ruby_event_store/mappers/encryption_mapper.rb
+++ ruby_event_store/lib/ruby_event_store/mappers/encryption_mapper.rb
@@ -65,8 +65,7 @@ module RubyEventStore
         crypto.key = key
 
         if crypto.authenticated?
-          crypto.auth_data = ""
-          crypto.update(message) + crypto.final + crypto.auth_tag
+          encrypt_authenticated(crypto, message)
         else
           crypto.update(message) + crypto.final
         end
@@ -76,15 +75,13 @@ module RubyEventStore
         crypto     = prepare_decrypt(cipher)
         crypto.iv  = iv
         crypto.key = key
-
-        if crypto.authenticated?
-          ciphertext, auth_tag = message[0...-16], message[-16...message.length]
-          crypto.auth_tag      = auth_tag
-          crypto.auth_data     = ""
-          (crypto.update(ciphertext) + crypto.final).force_encoding("UTF-8")
-        else
-          (crypto.update(message) + crypto.final).force_encoding("UTF-8")
-        end
+        ciphertext =
+          if crypto.authenticated?
+            ciphertext_from_authenticated(crypto, message)
+          else
+            message
+          end
+        (crypto.update(ciphertext) + crypto.final).force_encoding("UTF-8")
       end
 
       def random_iv
@@ -95,6 +92,22 @@ module RubyEventStore
       attr_reader :cipher, :key
 
       private
+
+      def ciphertext_from_authenticated(crypto, message)
+        prepare_auth_data(crypto)
+        crypto.auth_tag = message[-16...message.length]
+        message[0...-16]
+      end
+
+      def encrypt_authenticated(crypto, message)
+        prepare_auth_data(crypto)
+        crypto.update(message) + crypto.final + crypto.auth_tag
+      end
+
+      def prepare_auth_data(crypto)
+        crypto.auth_data = ""
+        crypto
+      end
 
       def prepare_encrypt(cipher)
         crypto = OpenSSL::Cipher.new(cipher)
