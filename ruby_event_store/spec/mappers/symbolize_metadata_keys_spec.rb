c1da6e999482bfcc36cc00e8254b1c4f396c01f1
Mirosław Pragłowski <m@praglowski.com>
2019-04-20 11:34:10 +0200

    Tests for SymbolizeMetadataKeys transformation

---
+++ ruby_event_store/spec/mappers/symbolize_metadata_keys_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+require 'json'
+
+module RubyEventStore
+  module Mappers
+    RSpec.describe SymbolizeMetadataKeys do
+      let(:uuid)  { SecureRandom.uuid }
+      let(:item)  {
+        {
+          event_id:   uuid,
+          metadata:   {some: 'meta'},
+          data:       JSON.parse(JSON.dump({some: 'value'})),
+          event_type: 'TestEvent',
+        }
+      }
+      let(:changed_item)  {
+        {
+          event_id:   uuid,
+          metadata:   JSON.parse(JSON.dump({some: 'meta'})),
+          data:       JSON.parse(JSON.dump({some: 'value'})),
+          event_type: 'TestEvent',
+        }
+      }
+
+      specify "#dump" do
+        expect(SymbolizeMetadataKeys.new.dump(item)).to eq(item)
+      end
+
+      specify "#load" do
+        result = SymbolizeMetadataKeys.new.load(changed_item)
+        expect(result).to eq(item)
+        expect(result[:metadata].keys.map(&:class).uniq).to eq([Symbol])
+      end
+    end
+  end
+end
