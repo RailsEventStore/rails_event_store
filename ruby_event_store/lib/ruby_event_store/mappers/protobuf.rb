660d55230fcc5655a3baf00137bb5a3e73d55302
Mirosław Pragłowski <m@praglowski.com>
2019-04-20 12:40:44 +0200

    More granural pipeline for Protobuf mapper

--- ruby_event_store/lib/ruby_event_store/mappers/protobuf.rb
+++ ruby_event_store/lib/ruby_event_store/mappers/protobuf.rb
@@ -12,11 +12,51 @@ module RubyEventStore
   end
 
   module Mappers
+    class ProtobufNestedStructMetadataMapper
+      def initialize(serializer = ProtobufNestedStruct::HashMapStringValue)
+        @serializer = serializer
+      end
+      attr_reader :serializer
+
+      def dump(item)
+        metadata = serializer.dump(item.fetch(:metadata))
+        item.merge(metadata: metadata)
+      end
+
+      def load(item)
+        metadata = serializer.load(item.fetch(:metadata))
+        item.merge(metadata: metadata)
+      end
+    end
+
+    class ProtoMapper
+      def dump(item)
+        item.merge(data: encode_data(item.fetch(:data)))
+      end
+
+      def load(item)
+        item.merge(data: load_data(item.fetch(:event_type), item.fetch(:data)))
+      end
+
+      private
+      def encode_data(data)
+        begin
+          data.class.encode(data)
+        rescue NoMethodError
+          raise ProtobufEncodingFailed
+        end
+      end
+
+      def load_data(event_type, protobuf_data)
+        Google::Protobuf::DescriptorPool.generated_pool.lookup(event_type).msgclass.decode(protobuf_data)
+      end
+    end
+
     class DomainEventProtoMapper
       def dump(domain_event)
         {
           event_id:   domain_event.event_id,
-          metadata:   domain_event.metadata.to_h,
+          metadata:   domain_event.metadata,
           data:       domain_event.data,
           event_type: domain_event.type
         }
@@ -27,26 +67,19 @@ module RubyEventStore
       def load(item)
         Proto.new(
           event_id: item.fetch(:event_id),
-          data:     load_data(item.fetch(:event_type), item.fetch(:data)),
-          metadata: load_metadata(item.fetch(:metadata))
+          data:     item.fetch(:data),
+          metadata: item.fetch(:metadata)
         )
       end
 
-      def load_metadata(protobuf_metadata)
-        TransformKeys.symbolize(ProtobufNestedStruct::HashMapStringValue.load(protobuf_metadata))
-      end
-
-      def load_data(event_type, protobuf_data)
-        Google::Protobuf::DescriptorPool.generated_pool.lookup(event_type).msgclass.decode(protobuf_data)
-      end
     end
 
     class SerializedRecordProtoMapper
       def dump(item)
         SerializedRecord.new(
           event_id:   item.fetch(:event_id),
-          metadata:   encode_metadata(item.fetch(:metadata)),
-          data:       encode_data(item.fetch(:data)),
+          metadata:   item.fetch(:metadata),
+          data:       item.fetch(:data),
           event_type: item.fetch(:event_type)
         )
       end
@@ -59,19 +92,6 @@ module RubyEventStore
           event_type: serialized_record.event_type
         }
       end
-
-      private
-      def encode_data(data)
-        begin
-          data.class.encode(data)
-        rescue NoMethodError
-          raise ProtobufEncodingFailed
-        end
-      end
-
-      def encode_metadata(metadata)
-        ProtobufNestedStruct::HashMapStringValue.dump(TransformKeys.stringify(metadata))
-      end
     end
 
     class Protobuf
@@ -81,7 +101,11 @@ module RubyEventStore
         require_optional_dependency
         @pipeline = Pipeline.new([
           DomainEventProtoMapper.new,
+          ProtoMapper.new,
           EventClassRemapper.new(events_class_remapping),
+          SymbolizeMetadataKeys.new,
+          StringifyMetadataKeys.new,
+          ProtobufNestedStructMetadataMapper.new,
           SerializedRecordProtoMapper.new
         ])
       end
