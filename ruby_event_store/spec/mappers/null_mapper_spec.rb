d22f142ae27c75d51a6967c0fe4ffd4e755a5b0e
Paweł Pacana <pawel.pacana@gmail.com>
2019-04-23 15:47:19 +0200

    More specific test for NullMapper.
    Checking object equality on Object is fine until we have explicit
    expectations to which methods it has to respond.
    
    Ensure expectation on same object_id is held as it leaked to other gems
    (no change now).
    
    [#552]

--- ruby_event_store/spec/mappers/null_mapper_spec.rb
+++ ruby_event_store/spec/mappers/null_mapper_spec.rb
@@ -3,19 +3,34 @@ require 'spec_helper'
 module RubyEventStore
   module Mappers
     RSpec.describe NullMapper do
+      let(:data)         { {some_attribute: 5} }
+      let(:metadata)     { {some_meta: 1} }
+      let(:event_id)     { SecureRandom.uuid }
+      let(:domain_event) { SomethingHappened.new(data: data, metadata: metadata, event_id: event_id) }
 
-      specify '#event_to_serialized_record returns provided event' do
-        event = Object.new
-        record = subject.event_to_serialized_record(event)
-        expect(record).to eq event
+      specify '#event_to_serialized_record' do
+        record = subject.event_to_serialized_record(domain_event)
+
+        expect(record.event_id).to   eq(domain_event.event_id)
+        expect(record.data).to       eq(domain_event.data)
+        expect(record.metadata).to   eq(domain_event.metadata)
       end
 
-      specify '#serialized_record_to_event returns provided event' do
-        record = Object.new
-        event = subject.serialized_record_to_event(record)
-        expect(event).to eq record
+      specify '#serialized_record_to_event' do
+        event = subject.serialized_record_to_event(domain_event)
+
+        expect(event).to           eq(domain_event)
+        expect(event.event_id).to  eq(domain_event.event_id)
+        expect(event.data).to      eq(domain_event.data)
+        expect(event.metadata).to  eq(domain_event.metadata)
       end
 
+      specify "returns same object" do
+        event = subject.serialized_record_to_event(
+          subject.event_to_serialized_record(domain_event)
+        )
+        expect(event.object_id).to eq(domain_event.object_id)
+      end
     end
   end
 end
