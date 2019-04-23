dee35aa0f0d731fabb767b064a87d47b9abde2c8
Paweł Pacana <pawel.pacana@gmail.com>
2019-04-23 21:22:47 +0200

    Target current RES version with new releases.

--- contrib/distributed_rails_event_store/dres_rails/app/controllers/dres_rails/application_controller.rb
+++ contrib/distributed_rails_event_store/dres_rails/app/controllers/dres_rails/application_controller.rb
@@ -1,7 +1,5 @@
 module DresRails
   class ApplicationController < ActionController::Base
-    NEWER_PRIVATE_API = RubyEventStore::Specification.instance_method(:initialize).parameters.first.second == :reader
-
     def index
       spec   = build_initial_spec
       spec   = spec.limit(1000)
@@ -18,14 +16,9 @@ module DresRails
 
     def build_initial_spec
       repository = Rails.configuration.event_store.instance_variable_get(:@repository)
-      mapper = RubyEventStore::Mappers::NullMapper.new
-      if NEWER_PRIVATE_API
-        RubyEventStore::Specification.new(
-          RubyEventStore::SpecificationReader.new(repository, mapper)
-        )
-      else
-        RubyEventStore::Specification.new(repository, mapper)
-      end
+      mapper     = RubyEventStore::Mappers::NullMapper.new
+
+      RubyEventStore::Specification.new(RubyEventStore::SpecificationReader.new(repository, mapper))
     end
 
     def after
