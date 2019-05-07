a891745034f92a592b93fc03e56662de642e7f41
Paweł Pacana <pawel.pacana@gmail.com>
2019-05-07 11:42:59 +0200

    Reduce semantics until proved wrong.

--- aggregate_root/lib/aggregate_root.rb
+++ aggregate_root/lib/aggregate_root.rb
@@ -24,8 +24,8 @@ module AggregateRoot
   end
 
   module Constructor
-    def new(*)
-      super.tap do |instance|
+    def new
+      super().tap do |instance|
         instance.instance_variable_set(:@version, -1)
         instance.instance_variable_set(:@unpublished_events, [])
       end
