4c214ecf3d050183ec6601c43921ddabfcd29b21
Rafał Łasocha <me@swistak35.com>
2019-06-04 13:26:24 +0200

    Use shorter version

--- ruby_event_store-browser/elm/src/Main.elm
+++ ruby_event_store-browser/elm/src/Main.elm
@@ -129,7 +129,7 @@ urlUpdate model location =
             case Url.percentDecode encodedEventId of
                 Just eventId ->
                     ( { model | page = Route.ShowEvent eventId, event = Just (OpenedEventUI.initModel eventId) }
-                    , Cmd.map (\msg -> OpenedEventUIChanged msg) (OpenedEventUI.initCmd model.flags eventId)
+                    , Cmd.map OpenedEventUIChanged (OpenedEventUI.initCmd model.flags eventId)
                     )
 
                 Nothing ->
