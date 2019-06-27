7125cc26bed9c97872d7cb566b6347ff135f1ea2
Paweł Pacana <pawel.pacana@gmail.com>
2019-06-27 19:54:22 +0200

    Add frozen string literals.

--- rails_event_store-rspec/lib/rails_event_store/rspec/publish.rb
+++ rails_event_store-rspec/lib/rails_event_store/rspec/publish.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rspec/matchers/built_in/base_matcher'
 
 module RailsEventStore
--- rails_event_store-rspec/lib/rails_event_store/rspec/version.rb
+++ rails_event_store-rspec/lib/rails_event_store/rspec/version.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module RailsEventStore
   module RSpec
     VERSION = "0.40.1"
