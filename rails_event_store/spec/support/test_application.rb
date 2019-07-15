76637a4b863ae62f4b913ae79ca0219653df22a1
Paweł Pacana <pawel.pacana@gmail.com>
2019-07-15 13:06:02 +0200

    Disable HostAuthorization middleware in testing.
    https://stackoverflow.com/a/56238430

--- rails_event_store/spec/support/test_application.rb
+++ rails_event_store/spec/support/test_application.rb
@@ -3,6 +3,7 @@ require 'rails_event_store/railtie'
 require 'securerandom'
 
 class TestApplication < Rails::Application
+  config.hosts = nil
   config.eager_load = false
   config.secret_key_base = SecureRandom.hex(16)
   config.event_store = RailsEventStore::Client.new
