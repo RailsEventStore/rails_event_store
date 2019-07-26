60593194fdff92b1da41305c117ee4310ace7aef
Paweł Pacana <pawel.pacana@gmail.com>
2019-07-26 11:30:14 +0200

    Complement with assert_not_dispatched.

--- contrib/minitest-rails_event_store/lib/minitest/rails_event_store/assertions.rb
+++ contrib/minitest-rails_event_store/lib/minitest/rails_event_store/assertions.rb
@@ -11,4 +11,17 @@ module Minitest::Assertions
       assert collected_events.include?(expected.to_s), "bazinga"
     end
   end
+
+  def assert_not_dispatched(event_store, expected_events, &block)
+    collected_events = []
+    event_store.within do
+      block.call
+    end.subscribe_to_all_events do |event|
+      collected_events << event.type
+    end.call
+
+    Array(expected_events).each do |expected|
+      refute collected_events.include?(expected.to_s), "bazinga"
+    end
+  end
 end
