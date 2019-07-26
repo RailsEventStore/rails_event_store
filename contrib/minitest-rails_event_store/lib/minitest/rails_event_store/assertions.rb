a7f4b4fdcca31e7ae63c4f618f182d809001107a
Paweł Pacana <pawel.pacana@gmail.com>
2019-07-26 11:48:15 +0200

    Extract collecting methods.
    A bit premature but CodeClimate decided it is important enought to fail a build.
    Improve code until CC does not act like Mordac The Preventer.

--- contrib/minitest-rails_event_store/lib/minitest/rails_event_store/assertions.rb
+++ contrib/minitest-rails_event_store/lib/minitest/rails_event_store/assertions.rb
@@ -1,11 +1,6 @@
 module Minitest::Assertions
   def assert_dispatched(event_store, expected_events, &block)
-    collected_events = []
-    event_store.within do
-      block.call
-    end.subscribe_to_all_events do |event|
-      collected_events << event.type
-    end.call
+    collected_events = collect_events(&block)
 
     Array(expected_events).each do |expected|
       assert collected_events.include?(expected.to_s), "bazinga"
@@ -13,15 +8,22 @@ module Minitest::Assertions
   end
 
   def assert_not_dispatched(event_store, expected_events, &block)
+    collected_events = collect_events(&block)
+
+    Array(expected_events).each do |expected|
+      refute collected_events.include?(expected.to_s), "bazinga"
+    end
+  end
+
+  private
+
+  def collect_events(&block)
     collected_events = []
     event_store.within do
       block.call
     end.subscribe_to_all_events do |event|
       collected_events << event.type
     end.call
-
-    Array(expected_events).each do |expected|
-      refute collected_events.include?(expected.to_s), "bazinga"
-    end
+    collected_events
   end
 end
