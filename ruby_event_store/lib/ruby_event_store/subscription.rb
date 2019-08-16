4f76f27896c3abf06f84107adb7a3e4690b600a1
Mirosław Pragłowski <m@praglowski.com>
2019-08-16 15:33:37 +0200

    Docs & specs for Subscription object

--- ruby_event_store/lib/ruby_event_store/subscription.rb
+++ ruby_event_store/lib/ruby_event_store/subscription.rb
@@ -1,32 +1,56 @@
+# frozen_string_literal: true
+
 module RubyEventStore
+  # Subsciption object
   class Subscription
+    # Instantiates a new subscription
+    #
+    # @param subscriber - a callable object / class or lambda execured when subscription is triggered
+    # @param event_types [Array<Class, String>] - types of events for which subscription should be triggered. When no event types are given a global subscription is created.
+    # @param store - subscription store where subscription is persisted. When subscription store is nil a subscription is not persisted (usefull in tests).
+    #
+    # @return [Subscription]
     def initialize(subscriber, event_types = [GLOBAL_SUBSCRIPTION], store: nil)
-      raise SubscriberNotExist, "subscriber must exists" unless subscriber
+      raise SubscriberNotExist, 'subscriber must exists' unless subscriber
 
       @subscriber = subscriber
       @event_types = event_types
       @store = store
-      event_types.each{ |type| @store.add(self, type) } if @store
+      event_types.each{ |type| @store.add(self, type) } if persisted?
     end
 
+    # Triggers the defined subscribed
+    # When subscriber is a [Class] a new subscriber's class object is created
+    #
+    # @param event - [Event] to pass to subscriber
     def call(event)
       (Class === subscriber ? subscriber.new : subscriber).call(event)
     end
 
+    # Unsubscribe subscription for all defined event types
+    # No-op when subscription store is nil
     def unsubscribe
-      event_types.each{ |type| @store.delete(self, type) } if @store
+      event_types.each{ |type| @store.delete(self, type) } if persisted?
     end
 
+    # Return true is this is a global subscription
+    # Global subscriptions are triggered by each domain event
+    #
+    # @return [TrueClass, FalseClass]
     def global?
       event_types.include?(GLOBAL_SUBSCRIPTION)
     end
 
+    # Return true is this subscription is stored
+    # It will return true also after subscription is unsubscribed
+    #
+    # @return [TrueClass, FalseClass]
+    def persisted?
+      !@store.nil?
+    end
+
     def inspect
-      <<~EOS.strip
-          #<#{self.class}:0x#{__id__.to_s(16)}>
-            - #{ event_types != [GLOBAL_SUBSCRIPTION] ? "event types: #{event_types.inspect}" : "global subscription"}
-            - subscriber: #{subscriber.inspect}
-      EOS
+      "#<#{self.class}:0x#{__id__.to_s(16)}>\n  - #{ !event_types.eql?([GLOBAL_SUBSCRIPTION]) ? "event types: #{event_types}" : "global subscription"}\n  - subscriber: #{subscriber.inspect}"
     end
 
     # Two subscriptions are equal if:
@@ -41,7 +65,7 @@ module RubyEventStore
     def ==(other)
       other.instance_of?(self.class) &&
         other.event_types.eql?(event_types) &&
-        other.subscriber.eql?(subscriber)
+        other.subscriber.equal?(subscriber)
     end
     alias_method :eql?, :==
 
@@ -62,7 +86,7 @@ module RubyEventStore
       [
         self.class,
         event_types,
-        subscriber.object_id
+        subscriber,
       ].hash ^ BIG_VALUE
     end
 
---
+++ ruby_event_store/spec/subscription_spec.rb
@@ -0,0 +1,159 @@
+require 'spec_helper'
+
+module RubyEventStore
+  RSpec.describe Subscription do
+    class Subscriber
+      def initialize(no = 0)
+        @no = no
+        @event = nil
+      end
+      attr_reader :event
+
+      def call(event)
+        @event = event
+      end
+
+      def inspect
+        "Subscriber#{@no}"
+      end
+    end
+
+    it { expect{ Subscription.new(nil) }.to raise_error(SubscriberNotExist, 'subscriber must exists') }
+
+    specify 'subscribe for all event types given' do
+      store = InMemorySubscriptionsStore.new
+      sub1 = Subscription.new(-> { }, [OrderCreated, ProductAdded], store: store)
+      sub2 = Subscription.new(-> { }, [OrderCreated], store: store)
+      sub3 = Subscription.new(-> { }, store: store)
+      expect(store.all).to match_array [sub1, sub2, sub3]
+      expect(store.all_for(OrderCreated)).to eq [sub1, sub2]
+      expect(store.all_for(ProductAdded)).to eq [sub1]
+      expect(store.all_for(GLOBAL_SUBSCRIPTION)).to eq [sub3]
+
+      expect([sub1, sub2, sub3].map(&:persisted?).uniq).to eq [true]
+    end
+
+    specify 'when store not given do not add or delete from it' do
+      store = double(:store)
+      allow(store).to receive(:nil?).and_return(true)
+      expect(store).not_to receive(:add)
+      expect(store).not_to receive(:delete)
+      sub = Subscription.new(-> { }, store: store)
+      sub.unsubscribe
+      expect(sub.persisted?).to eq(false)
+    end
+
+    specify 'unsubscribe from all event types' do
+      store = InMemorySubscriptionsStore.new
+      sub1 = Subscription.new(-> { }, [OrderCreated, ProductAdded], store: store)
+      sub2 = Subscription.new(-> { }, [OrderCreated], store: store)
+      sub3 = Subscription.new(-> { }, store: store)
+      expect(store.all).to match_array [sub1, sub2, sub3]
+
+      sub1.unsubscribe
+      expect(store.all_for(OrderCreated)).to eq [sub2]
+      expect(store.all_for(ProductAdded)).to eq []
+      expect(store.all_for(GLOBAL_SUBSCRIPTION)).to eq [sub3]
+
+      sub2.unsubscribe
+      expect(store.all_for(OrderCreated)).to eq []
+      expect(store.all_for(ProductAdded)).to eq []
+      expect(store.all_for(GLOBAL_SUBSCRIPTION)).to eq [sub3]
+
+      sub3.unsubscribe
+      expect(store.all_for(OrderCreated)).to eq []
+      expect(store.all_for(ProductAdded)).to eq []
+      expect(store.all_for(GLOBAL_SUBSCRIPTION)).to eq []
+    end
+
+    specify '#global?' do
+      sub1 = Subscription.new(-> { }, [OrderCreated, ProductAdded])
+      sub2 = Subscription.new(-> { }, [OrderCreated])
+      sub3 = Subscription.new(-> { })
+
+      expect(sub1.global?).to eq false
+      expect(sub2.global?).to eq false
+      expect(sub3.global?).to eq true
+    end
+
+    specify '#inspect' do
+      sub1 = Subscription.new(Subscriber.new(1), [OrderCreated, ProductAdded])
+      sub2 = Subscription.new(Subscriber.new(2), [OrderCreated])
+      sub3 = Subscription.new(Subscriber.new(3))
+
+      expect(sub1.inspect).to eq <<~EOS.strip
+          #<RubyEventStore::Subscription:0x#{sub1.object_id.to_s(16)}>
+            - event types: [OrderCreated, ProductAdded]
+            - subscriber: Subscriber1
+      EOS
+
+      expect(sub2.inspect).to eq <<~EOS.strip
+          #<RubyEventStore::Subscription:0x#{sub2.object_id.to_s(16)}>
+            - event types: [OrderCreated]
+            - subscriber: Subscriber2
+      EOS
+
+      expect(sub3.inspect).to eq <<~EOS.strip
+          #<RubyEventStore::Subscription:0x#{sub3.object_id.to_s(16)}>
+            - global subscription
+            - subscriber: Subscriber3
+      EOS
+    end
+
+    specify '#call' do
+      event = RubyEventStore::Event.new
+
+      handler1 = Subscriber.new
+      Subscription.new(handler1).call(event)
+      expect(handler1.event).to eq(event)
+
+      handler2 = Subscriber.new
+      expect(Subscriber).to receive(:new).and_return(handler2)
+      Subscription.new(Subscriber).call(event)
+      expect(handler2.event).to eq(event)
+
+      result = nil
+      Subscription.new(->(e) { result = e }).call(event)
+      expect(result).to eq(event)
+    end
+
+    specify '#==' do
+      subscriber1 = Subscriber.new(1)
+      subscriber2 = Subscriber.new(2)
+      expect(Subscription.new(subscriber1) == Subscription.new(subscriber1)).to eq true
+      expect(Subscription.new(subscriber2) == Subscription.new(subscriber1)).to eq false
+
+      expect(Subscription.new(subscriber1, [OrderCreated]) == Subscription.new(subscriber1, [OrderCreated])).to eq true
+      expect(Subscription.new(subscriber1, [OrderCreated, ProductAdded]) == Subscription.new(subscriber1, [OrderCreated])).to eq false
+      expect(Subscription.new(subscriber1, [OrderCreated]) == Subscription.new(subscriber1)).to eq false
+
+      klass = Class.new(Subscription)
+      expect(klass.new(subscriber1) == Subscription.new(subscriber1)).to eq false
+    end
+
+    specify '#hash' do
+      subscriber1 = Subscriber.new(1)
+      subscriber2 = Subscriber.new(2)
+      expect(Subscription.new(subscriber1).hash).to eq(Subscription.new(subscriber1).hash)
+      expect(Subscription.new(subscriber2).hash).not_to eq(Subscription.new(subscriber1).hash)
+
+      expect(Subscription.new(subscriber1, [OrderCreated]).hash).to eq(Subscription.new(subscriber1, [OrderCreated]).hash)
+      expect(Subscription.new(subscriber1, [OrderCreated, ProductAdded]).hash).not_to eq(Subscription.new(subscriber1, [OrderCreated]).hash)
+      expect(Subscription.new(subscriber1, [OrderCreated]).hash).not_to eq(Subscription.new(subscriber1).hash)
+
+      klass = Class.new(Subscription)
+      expect(
+        klass.new(subscriber1).hash
+      ).not_to eq(Subscription.new(subscriber1).hash)
+      expect(
+        klass.new(subscriber1).hash
+      ).to eq(klass.new(subscriber1).hash)
+
+      expect(Subscription.new(subscriber1, [OrderCreated]).hash).not_to eq([
+        Subscription,
+        [OrderCreated],
+        subscriber1
+      ].hash)
+    end
+  end
+end
