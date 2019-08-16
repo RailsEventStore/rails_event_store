ff945e764277d9e6fcfa9982e1d4da909b1e0543
Mirosław Pragłowski <m@praglowski.com>
2019-08-16 18:28:30 +0200

    Fix subscriptions store lint namespace

--- ruby_event_store/lib/ruby_event_store/spec/subscription_store_lint.rb
+++ ruby_event_store/lib/ruby_event_store/spec/subscription_store_lint.rb
@@ -1,35 +1,41 @@
-RSpec.shared_examples :subscription_store do |subscription_store|
+module RubyEventStore
+  # @private
   FirstEvent = Class.new(RubyEventStore::Event)
+  # @private
   SecondEvent = Class.new(RubyEventStore::Event)
+end
 
-  specify do
-    expect(subscription_store.all).to eq []
-    expect(subscription_store.all_for(FirstEvent)).to eq []
-    expect(subscription_store.all_for(SecondEvent)).to eq []
-    expect(subscription_store.all_for(RubyEventStore::GLOBAL_SUBSCRIPTION)).to eq []
+module RubyEventStore
+  RSpec.shared_examples :subscription_store do |subscription_store|
+    specify do
+      expect(subscription_store.all).to eq []
+      expect(subscription_store.all_for(FirstEvent)).to eq []
+      expect(subscription_store.all_for(SecondEvent)).to eq []
+      expect(subscription_store.all_for(RubyEventStore::GLOBAL_SUBSCRIPTION)).to eq []
 
-    global = RubyEventStore::Subscription.new(-> { }, store: subscription_store)
-    first  = RubyEventStore::Subscription.new(-> { }, [FirstEvent], store: subscription_store)
-    second = RubyEventStore::Subscription.new(-> { }, [FirstEvent, SecondEvent], store: subscription_store)
+      global = RubyEventStore::Subscription.new(-> { }, store: subscription_store)
+      first  = RubyEventStore::Subscription.new(-> { }, [FirstEvent], store: subscription_store)
+      second = RubyEventStore::Subscription.new(-> { }, [FirstEvent, SecondEvent], store: subscription_store)
 
-    expect(subscription_store.all).to match_array [global, first, second]
+      expect(subscription_store.all).to match_array [global, first, second]
 
-    expect(subscription_store.all_for(FirstEvent)).to match_array [first, second]
-    expect(subscription_store.all_for(SecondEvent)).to match_array [second]
-    expect(subscription_store.all_for(RubyEventStore::GLOBAL_SUBSCRIPTION)).to match_array [global]
+      expect(subscription_store.all_for(FirstEvent)).to match_array [first, second]
+      expect(subscription_store.all_for(SecondEvent)).to match_array [second]
+      expect(subscription_store.all_for(RubyEventStore::GLOBAL_SUBSCRIPTION)).to match_array [global]
 
-    global.unsubscribe
-    expect(subscription_store.all_for(RubyEventStore::GLOBAL_SUBSCRIPTION)).to eq []
-    expect(subscription_store.all).to match_array [first, second]
+      global.unsubscribe
+      expect(subscription_store.all_for(RubyEventStore::GLOBAL_SUBSCRIPTION)).to eq []
+      expect(subscription_store.all).to match_array [first, second]
 
-    first.unsubscribe
-    expect(subscription_store.all_for(FirstEvent)).to match_array [second]
-    expect(subscription_store.all_for(SecondEvent)).to match_array [second]
-    expect(subscription_store.all).to match_array [second]
+      first.unsubscribe
+      expect(subscription_store.all_for(FirstEvent)).to match_array [second]
+      expect(subscription_store.all_for(SecondEvent)).to match_array [second]
+      expect(subscription_store.all).to match_array [second]
 
-    second.unsubscribe
-    expect(subscription_store.all_for(FirstEvent)).to eq []
-    expect(subscription_store.all_for(SecondEvent)).to eq []
-    expect(subscription_store.all).to eq []
+      second.unsubscribe
+      expect(subscription_store.all_for(FirstEvent)).to eq []
+      expect(subscription_store.all_for(SecondEvent)).to eq []
+      expect(subscription_store.all).to eq []
+    end
   end
 end
