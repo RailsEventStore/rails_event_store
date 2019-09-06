25dd989c50d3e0c49b1d9b433ffb77a26cbc2bb7
Mirosław Pragłowski <m@praglowski.com>
2019-09-06 10:58:30 +0200

    Add frozen_string_literal: true in all changed files

--- ruby_event_store/lib/ruby_event_store/spec/subscription_store_lint.rb
+++ ruby_event_store/lib/ruby_event_store/spec/subscription_store_lint.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module RubyEventStore
   # @private
   FirstEvent = Class.new(RubyEventStore::Event)
--- ruby_event_store/spec/in_memory_subscriptions_store_spec.rb
+++ ruby_event_store/spec/in_memory_subscriptions_store_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 require 'ruby_event_store/spec/subscription_store_lint'
 
