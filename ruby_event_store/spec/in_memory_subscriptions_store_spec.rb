7140093f7865b3e5d20922ab4541289eec0b254a
Mirosław Pragłowski <m@praglowski.com>
2019-09-06 10:48:53 +0200

    Kill mutations

--- ruby_event_store/spec/in_memory_subscriptions_store_spec.rb
+++ ruby_event_store/spec/in_memory_subscriptions_store_spec.rb
@@ -4,5 +4,15 @@ require 'ruby_event_store/spec/subscription_store_lint'
 module RubyEventStore
   RSpec.describe InMemorySubscriptionsStore do
     it_behaves_like :subscription_store, InMemorySubscriptionsStore.new
+
+    it { expect(InMemorySubscriptionsStore.new.all).to eq [] }
+    it { expect(InMemorySubscriptionsStore.new.all_for('not-existing')).to eq [] }
+    it {
+      store = InMemorySubscriptionsStore.new
+      subscription = RubyEventStore::Subscription.new(-> { }, [TestEvent])
+      store.add(subscription)
+      expect(store.all_for('not-existing')).to eq []
+      expect(store.all_for(TestEvent)).to eq [subscription]
+    }
   end
 end
