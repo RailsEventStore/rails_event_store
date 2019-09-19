4cc0f1bdac8ca84b963d7d972e193dc080e8a2e0
Mirosław Pragłowski <m@praglowski.com>
2019-09-19 16:26:18 +0200

    Deprecated methods must still work, and it shall be tested

--- ruby_event_store/spec/subscriptions_spec.rb
+++ ruby_event_store/spec/subscriptions_spec.rb
@@ -7,16 +7,21 @@ module RubyEventStore
 
     it do
       subscriptions = Subscriptions.new
-      expect { subscriptions.add_subscription(-> {}, ["Test1DomainEvent"]) }.to output(<<~MSG).to_stderr
+      handler = -> {}
+      expect(subscriptions).to receive(:add).with(handler, ["Test1DomainEvent"])
+      expect { subscriptions.add_subscription(handler, ["Test1DomainEvent"]) }.to output(<<~MSG).to_stderr
         Method `add_subscription` has been deprecated. Use `add` method instead.
       MSG
-      expect { subscriptions.add_global_subscription(-> {}) }.to output(<<~MSG).to_stderr
+      expect(subscriptions).to receive(:add).with(handler)
+      expect { subscriptions.add_global_subscription(handler) }.to output(<<~MSG).to_stderr
         Method `add_global_subscription` has been deprecated. Use `add` method instead.
       MSG
-      expect { subscriptions.add_thread_subscription(-> {}, ["Test1DomainEvent"]) }.to output(<<~MSG).to_stderr
+      expect(subscriptions).to receive(:add).with(handler, ["Test1DomainEvent"])
+      expect { subscriptions.add_thread_subscription(handler, ["Test1DomainEvent"]) }.to output(<<~MSG).to_stderr
         Method `add_thread_subscription` has been deprecated. Use `add` method instead.
       MSG
-      expect { subscriptions.add_thread_global_subscription(-> {}) }.to output(<<~MSG).to_stderr
+      expect(subscriptions).to receive(:add).with(handler)
+      expect { subscriptions.add_thread_global_subscription(handler) }.to output(<<~MSG).to_stderr
         Method `add_thread_global_subscription` has been deprecated. Use `add` method instead.
       MSG
     end
