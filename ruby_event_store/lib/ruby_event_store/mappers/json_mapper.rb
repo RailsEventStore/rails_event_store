21d4da9958083450312de42d235ed0e56e19c823
Scott Bartell <me@scottbartell.com>
2019-10-29 22:58:23 -0400

    Implement mapper for JSON data types

---
+++ ruby_event_store/lib/ruby_event_store/mappers/json_mapper.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module RubyEventStore
+  module Mappers
+    class JSONMapper < PipelineMapper
+      def initialize(events_class_remapping: {})
+        super(Pipeline.new(
+          transformations: [
+            Transformation::EventClassRemapper.new(events_class_remapping),
+            Transformation::SymbolizeMetadataKeys.new,
+          ],
+        ))
+      end
+    end
+  end
+end
