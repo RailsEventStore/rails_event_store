d79c72acffc06f857f4363dcc0a8fb9bee63e0bd
Mirosław Pragłowski <m@praglowski.com>
2020-04-24 09:58:23 +0200

    New apply matcher for testing AggregateRoot
    Instead of checking what have_applied by aggregate action this matcher
    will check what have applied during the action.
    
    This will allow to write test like:
    
    ```
    aggtegate = SomeAggregate.new
    aggregate.do_something
    
    expect { aggregate.do_anything_else }.to
      apply(an_event(SomeEvent)).in(aggregate)
    ```
    
    The advantage is that we will be able what exactly have been
    applied during the aggregate logic execution.

--- rails_event_store-rspec/lib/rails_event_store/rspec.rb
+++ rails_event_store-rspec/lib/rails_event_store/rspec.rb
@@ -13,6 +13,7 @@ require "rails_event_store/rspec/be_event"
 require "rails_event_store/rspec/have_published"
 require "rails_event_store/rspec/have_applied"
 require "rails_event_store/rspec/publish"
+require "rails_event_store/rspec/apply"
 require "rails_event_store/rspec/matchers"
 
 ::RSpec.configure do |config|
