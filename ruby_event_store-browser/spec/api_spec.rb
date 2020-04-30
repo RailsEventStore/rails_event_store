948974543dba1e416739aa21b55c2543f32dd1dc
Rafał Łasocha <me@swistak35.com>
2020-04-30 12:14:51 +0200

    Extract spec/api dir to group spec with features

--- ruby_event_store-browser/spec/api_spec.rb
+++ ruby_event_store-browser/spec/api_spec.rb
@@ -22,35 +22,6 @@ module RubyEventStore
       })
     end
 
-    specify "requsting stream resource with related streams" do
-      app = RubyEventStore::Browser::App.for(
-        event_store_locator: -> { event_store },
-        related_streams_query: ->(stream_name) { stream_name == "dummy" ? ["even-dummier"] : [] },
-        host: 'http://www.example.com'
-      )
-      test_client = TestClient.with_linter(app)
-
-      test_client.get "/streams/all"
-      expect(test_client.last_response).to be_ok
-      expect(test_client.parsed_body["data"]["attributes"]["related_streams"]).to eq([])
-
-      test_client.get "/streams/dummy"
-      expect(test_client.last_response).to be_ok
-      expect(test_client.parsed_body["data"]["attributes"]["related_streams"]).to eq(["even-dummier"])
-    end
-
-    specify "default related streams query returns nil" do
-      app = RubyEventStore::Browser::App.for(
-        event_store_locator: -> { event_store },
-        host: 'http://www.example.com'
-      )
-      test_client = TestClient.with_linter(app)
-
-      test_client.get "/streams/all"
-      expect(test_client.last_response).to be_ok
-      expect(test_client.parsed_body["data"]["attributes"]["related_streams"]).to eq(nil)
-    end
-
     specify do
       event_store.publish(dummy_event, stream_name: "dummy")
       test_client.get "/streams/all/relationships/events"
