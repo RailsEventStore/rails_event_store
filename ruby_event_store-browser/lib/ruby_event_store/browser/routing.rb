3b76e8fc3e42fe8c09b57cf64c2591d0a1b671e5
Rafał Łasocha <me@swistak35.com>
2020-04-30 13:13:42 +0200

    Refactoring: Extract method Routing#base_url

--- ruby_event_store-browser/lib/ruby_event_store/browser/routing.rb
+++ ruby_event_store-browser/lib/ruby_event_store/browser/routing.rb
@@ -7,33 +7,34 @@ module RubyEventStore
       end
 
       def root_url
-        [host, root_path].join
+        base_url
       end
 
       def events_url
-        base = [host, root_path].join
-        "#{base}/api/events"
+        "#{base_url}/api/events"
       end
 
       def streams_url
-        base = [host, root_path].join
-        "#{base}/api/streams"
+        "#{base_url}/api/streams"
       end
 
       def paginated_events_from_stream_url(id:, position: nil, direction: nil, count: nil)
-        base = [host, root_path].join
         args = [position, direction, count].compact
         stream_name = Rack::Utils.escape(id)
 
         if args.empty?
-          "#{base}/api/streams/#{stream_name}/relationships/events"
+          "#{base_url}/api/streams/#{stream_name}/relationships/events"
         else
-          "#{base}/api/streams/#{stream_name}/relationships/events/#{args.join('/')}"
+          "#{base_url}/api/streams/#{stream_name}/relationships/events/#{args.join('/')}"
         end
       end
 
       private
       attr_reader :host, :root_path
+
+      def base_url
+        [host, root_path].join
+      end
     end
   end
 end
