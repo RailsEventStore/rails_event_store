cbb65aba2310044e825065337ea33bf019c8ebbf
Rafał Łasocha <me@swistak35.com>
2020-04-30 13:02:49 +0200

    Use nesting in tests

--- ruby_event_store-browser/spec/routing_spec.rb
+++ ruby_event_store-browser/spec/routing_spec.rb
@@ -1,29 +1,31 @@
 require "spec_helper"
 
 module RubyEventStore
-  RSpec.describe Browser do
-    specify do
-      routing = Browser::Routing.new("http://example.com:9393", nil)
+  module Browser
+    RSpec.describe Routing do
+      specify do
+        routing = Routing.new("http://example.com:9393", nil)
 
-      url = routing.paginated_events_from_stream_url(id: "all")
+        url = routing.paginated_events_from_stream_url(id: "all")
 
-      expect(url).to eq("http://example.com:9393/api/streams/all/relationships/events")
-    end
+        expect(url).to eq("http://example.com:9393/api/streams/all/relationships/events")
+      end
 
-    specify do
-      routing = Browser::Routing.new("http://example.com:9393", "")
+      specify do
+        routing = Routing.new("http://example.com:9393", "")
 
-      url = routing.paginated_events_from_stream_url(id: "all", position: "forward")
+        url = routing.paginated_events_from_stream_url(id: "all", position: "forward")
 
-      expect(url).to eq("http://example.com:9393/api/streams/all/relationships/events/forward")
-    end
+        expect(url).to eq("http://example.com:9393/api/streams/all/relationships/events/forward")
+      end
 
-    specify "escaping stream name" do
-      routing = Browser::Routing.new("http://example.com:9393", "")
+      specify "escaping stream name" do
+        routing = Routing.new("http://example.com:9393", "")
 
-      url = routing.paginated_events_from_stream_url(id: "foo/bar.xml")
+        url = routing.paginated_events_from_stream_url(id: "foo/bar.xml")
 
-      expect(url).to eq("http://example.com:9393/api/streams/foo%2Fbar.xml/relationships/events")
+        expect(url).to eq("http://example.com:9393/api/streams/foo%2Fbar.xml/relationships/events")
+      end
     end
   end
 end
