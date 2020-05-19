b8f0f121bc7ded31a7f50241ee3d223ba0aa875d
Mirosław Pragłowski <m@praglowski.com>
2020-05-19 15:03:53 +0200

    Establish database schema for tests

--- contrib/connected_active_record/spec/connected_active_record_spec.rb
+++ contrib/connected_active_record/spec/connected_active_record_spec.rb
@@ -4,6 +4,18 @@ require 'ruby_event_store/spec/event_repository_lint'
 
 module ConnectedActiveRecord
   RSpec.describe Repository do
+    include SchemaHelper
+
+    around(:each) do |example|
+      begin
+        establish_database_connection
+        load_database_schema
+        example.run
+      ensure
+        drop_database
+      end
+    end
+
     let(:test_race_conditions_any)   { false }
     let(:test_race_conditions_auto)  { false }
     let(:test_binary) { false }
--- contrib/connected_active_record/spec/spec_helper.rb
+++ contrib/connected_active_record/spec/spec_helper.rb
@@ -1,2 +1,8 @@
 require '../../support/helpers/rspec_defaults'
+require_relative '../../../support/helpers/schema_helper'
 require 'connected_active_record'
+
+$verbose = ENV.has_key?('VERBOSE') ? true : false
+ActiveRecord::Schema.verbose = $verbose
+
+ENV['DATABASE_URL']  ||= 'sqlite3:db.sqlite3'
