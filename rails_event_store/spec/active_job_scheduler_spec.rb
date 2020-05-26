f696e599bd2b2a72c326cb827e38eb46418009d5
David Roth <dneilroth@gmail.com>
2020-05-25 21:22:10 -0700

    Fix active_job_scheduler test

--- rails_event_store/spec/active_job_scheduler_spec.rb
+++ rails_event_store/spec/active_job_scheduler_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 require 'ruby_event_store/spec/scheduler_lint'
 
@@ -24,10 +26,10 @@ module RailsEventStore
 
     it_behaves_like :scheduler, ActiveJobScheduler.new
 
-    let(:event) { TimestampEnrichment.with_timestamp(Event.new(event_id: "83c3187f-84f6-4da7-8206-73af5aca7cc8"), Time.utc(2019, 9, 30)) }
+    let(:event) { TimestampEnrichment.with_timestamp(Event.new(event_id: '83c3187f-84f6-4da7-8206-73af5aca7cc8'), Time.utc(2019, 9, 30)) }
     let(:serialized_event) { RubyEventStore::Mappers::Default.new.event_to_serialized_record(event) }
 
-    describe "#verify" do
+    describe '#verify' do
       specify do
         scheduler = ActiveJobScheduler.new
         proper_handler = Class.new(ActiveJob::Base)
@@ -51,26 +53,30 @@ module RailsEventStore
       end
     end
 
-    describe "#call" do
+    describe '#call' do
+      before do
+      end
       specify do
         scheduler = ActiveJobScheduler.new
         scheduler.call(MyAsyncHandler, serialized_event)
 
         enqueued_jobs = ActiveJob::Base.queue_adapter.enqueued_jobs
         expect(enqueued_jobs.size).to eq(1)
-        expect(enqueued_jobs[0]).to eq({
-          job: MyAsyncHandler,
-          args: [
-            {
-              "event_id" => "83c3187f-84f6-4da7-8206-73af5aca7cc8",
-              "event_type" => "RubyEventStore::Event",
-              "data" => "--- {}\n",
-              "metadata" => "---\n:timestamp: 2019-09-30 00:00:00.000000000 Z\n",
-              "_aj_symbol_keys" => ["event_id", "data", "metadata", "event_type"]
-            }
-          ],
-          queue: "default"
-        })
+        expect(enqueued_jobs[0]).to include(
+          {
+            job:   MyAsyncHandler,
+            args:  [
+              {
+                'event_id'        => '83c3187f-84f6-4da7-8206-73af5aca7cc8',
+                'event_type'      => 'RubyEventStore::Event',
+                'data'            => "--- {}\n",
+                'metadata'        => "---\n:timestamp: 2019-09-30 00:00:00.000000000 Z\n",
+                '_aj_symbol_keys' => %w[event_id data metadata event_type],
+              },
+            ],
+            queue: 'default',
+          },
+        )
       end
     end
 
@@ -79,13 +85,14 @@ module RailsEventStore
       def self.reset
         @@received = nil
       end
+
       def self.received
         @@received
       end
+
       def perform(event)
         @@received = event
       end
     end
   end
 end
-
