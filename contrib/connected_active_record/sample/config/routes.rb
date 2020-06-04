4ee7e127824211ea5418bbb7cc06460dc712cde8
Mirosław Pragłowski <m@praglowski.com>
2020-06-04 15:32:42 +0200

    Multiple databases sample Rails app - initial commit
    App generated with RES template with options:
    
    ```
    rails new sample --template "https://railseventstore.org/new" --skip-spring --skip-listen --skip-bootsnap --skip-git --skip-keeps --skip-action-text --skip-active-storage --skip-action-cable --skip-action-mailer --skip-action-mailbox --skip-sprockets --skip-javascript --skip-turbolinks --skip-webpack-install --skip-yarn --skip-test --skip-system-test
    ```

---
+++ contrib/connected_active_record/sample/config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+  mount RailsEventStore::Browser => '/res' if Rails.env.development?
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
