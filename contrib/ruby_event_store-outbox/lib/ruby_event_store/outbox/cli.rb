7d4f148f11888ddc07afe8454072b1f3d32b3d15
Rafał Łasocha <me@swistak35.com>
2020-06-09 20:52:02 +0200

    Make log level configurable

--- contrib/ruby_event_store-outbox/lib/ruby_event_store/outbox/cli.rb
+++ contrib/ruby_event_store-outbox/lib/ruby_event_store/outbox/cli.rb
@@ -4,11 +4,11 @@ require "ruby_event_store/outbox/consumer"
 module RubyEventStore
   module Outbox
     class CLI
-      Options = Struct.new(:database_url, :redis_url)
+      Options = Struct.new(:database_url, :redis_url, :log_level)
 
       class Parser
         def self.parse(argv)
-          options = Options.new(nil, nil)
+          options = Options.new(nil, nil, :warn)
           OptionParser.new do |option_parser|
             option_parser.banner = "Usage: res_outbox [options]"
 
@@ -19,6 +19,10 @@ module RubyEventStore
             option_parser.on("--redis-url=REDIS_URL", "URL to redis database") do |redis_url|
               options.redis_url = redis_url
             end
+
+            option_parser.on("--log-level=log_level", [:fatal, :error, :warn, :info, :debug], "Logging level, one of: fatal, error, warn, info, debug") do |log_level|
+              options.log_level = log_level.to_sym
+            end
           end.parse(argv)
           return options
         end
@@ -26,10 +30,13 @@ module RubyEventStore
 
       def run(argv)
         options = Parser.parse(argv)
+        logger = Logger.new(STDOUT)
+        logger.level = options.log_level
         outbox_consumer = RubyEventStore::Outbox::Consumer.new(
           ["default"],
           database_url: options.database_url,
           redis_url: options.redis_url,
+          logger: logger,
         )
         outbox_consumer.init
         outbox_consumer.run
--- contrib/ruby_event_store-outbox/spec/cli_spec.rb
+++ contrib/ruby_event_store-outbox/spec/cli_spec.rb
@@ -4,16 +4,28 @@ module RubyEventStore
   module Outbox
     class CLI
       RSpec.describe Parser do
-        specify "#parse" do
+        specify "#parse storage urls" do
           argv = [
             '--database-url=mysql2://root@0.0.0.0:3306/dbname',
-            '--redis-url=redis://localhost:6379/0'
+            '--redis-url=redis://localhost:6379/0',
           ]
 
           options = Parser.parse(argv)
 
           expect(options.database_url).to eq("mysql2://root@0.0.0.0:3306/dbname")
           expect(options.redis_url).to eq("redis://localhost:6379/0")
+        end
+
+        specify "#parse log levels" do
+          expect(Parser.parse(["--log-level=fatal"]).log_level).to eq(:fatal)
+          expect(Parser.parse(["--log-level=error"]).log_level).to eq(:error)
+          expect(Parser.parse(["--log-level=warn"]).log_level).to eq(:warn)
+          expect(Parser.parse(["--log-level=info"]).log_level).to eq(:info)
+          expect(Parser.parse(["--log-level=debug"]).log_level).to eq(:debug)
+          expect(Parser.parse([]).log_level).to eq(:warn)
+          expect do
+            Parser.parse(["--log-level=rubbish"])
+          end.to raise_error(OptionParser::InvalidArgument)
         end
       end
     end
