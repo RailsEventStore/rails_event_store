3f7f8b5f57ecef027f4ba6c33273d711abfe5080
Rafał Łasocha <me@swistak35.com>
2020-06-09 09:26:49 +0200

    Make generators work

--- contrib/ruby_event_store-outbox/spec/sidekiq_scheduler_spec.rb
+++ contrib/ruby_event_store-outbox/spec/sidekiq_scheduler_spec.rb
@@ -64,12 +64,15 @@ module RubyEventStore
           begin
             establish_database_connection
             # load_database_schema
-            m = Migrator.new(File.expand_path('../lib/generators/templates', __dir__))
+            m = Migrator.new(File.expand_path('../lib/generators/ruby_event_store/outbox/templates', __dir__))
             m.run_migration('create_event_store_outbox')
             example.run
           ensure
             # drop_database
-            ActiveRecord::Migration.drop_table("event_store_outbox")
+            begin
+              ActiveRecord::Migration.drop_table("event_store_outbox")
+            rescue ActiveRecord::StatementInvalid
+            end
           end
         end
 
