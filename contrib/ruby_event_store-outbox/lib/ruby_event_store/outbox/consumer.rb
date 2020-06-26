2cc332f04c6f6b40a116ce5f152d2877e38fc74a
Rafał Łasocha <me@swistak35.com>
2020-06-26 16:25:47 +0200

    Set very short lock wait timeout

--- contrib/ruby_event_store-outbox/lib/ruby_event_store/outbox/consumer.rb
+++ contrib/ruby_event_store-outbox/lib/ruby_event_store/outbox/consumer.rb
@@ -48,6 +48,7 @@ module RubyEventStore
         ActiveRecord::Base.establish_connection(configuration.database_url) unless ActiveRecord::Base.connected?
         if ActiveRecord::Base.connection.adapter_name == "Mysql2"
           ActiveRecord::Base.connection.execute("SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;")
+          ActiveRecord::Base.connection.execute("SET SESSION innodb_lock_wait_timeout = 1;")
         end
 
         raise "Unknown format" if configuration.message_format != SIDEKIQ5_FORMAT
