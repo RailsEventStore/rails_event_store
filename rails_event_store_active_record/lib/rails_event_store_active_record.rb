023d84026813aca022ec209dea11ae5821bed493
Piotr Romańczuk <piotr.romanczuk@gmail.com>
2020-07-26 20:11:35 +0200

    move RailsEventStoreActiveRecord::BatchEnumerator to its own file
    * add tests for it
    * make it's initializer signature simpler and more consistent
      with existing RubyEventStore::BatchEnumerator

--- rails_event_store_active_record/lib/rails_event_store_active_record.rb
+++ rails_event_store_active_record/lib/rails_event_store_active_record.rb
@@ -6,6 +6,7 @@ require 'rails_event_store_active_record/generators/limit_for_event_id_generator
 require 'rails_event_store_active_record/generators/binary_data_and_metadata_generator'
 require 'rails_event_store_active_record/event'
 require 'rails_event_store_active_record/event_repository'
+require 'rails_event_store_active_record/batch_enumerator'
 require 'rails_event_store_active_record/event_repository_reader'
 require 'rails_event_store_active_record/index_violation_detector'
 require 'rails_event_store_active_record/pg_linearized_event_repository'
