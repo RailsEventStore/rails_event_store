423031d5f814bd02937a33bd4e55636d57a14ebc
Szymon Fiedler <szymon.fiedler@gmail.com>
2020-09-01 13:51:49 +0200

    Rename events to serialized_records since they're the subject here

--- ruby_event_store/lib/ruby_event_store/in_memory_repository.rb
+++ ruby_event_store/lib/ruby_event_store/in_memory_repository.rb
@@ -10,13 +10,13 @@ module RubyEventStore
       @global = Array.new
     end
 
-    def append_to_stream(events, stream, expected_version)
-      add_to_stream(events, expected_version, stream, true)
+    def append_to_stream(serialized_records, stream, expected_version)
+      add_to_stream(serialized_records, expected_version, stream, true)
     end
 
     def link_to_stream(event_ids, stream, expected_version)
-      events = Array(event_ids).map {|eid| read_event(eid)}
-      add_to_stream(events, expected_version, stream, nil)
+      serialized_records = Array(event_ids).map {|eid| read_event(eid)}
+      add_to_stream(serialized_records, expected_version, stream, nil)
     end
 
     def delete_stream(stream)
@@ -32,16 +32,16 @@ module RubyEventStore
     end
 
     def read(spec)
-      events = read_scope(spec)
+      serialized_records = read_scope(spec)
       if spec.batched?
-        batch_reader = ->(offset, limit) { events.drop(offset).take(limit) }
-        BatchEnumerator.new(spec.batch_size, events.size, batch_reader).each
+        batch_reader = ->(offset, limit) { serialized_records.drop(offset).take(limit) }
+        BatchEnumerator.new(spec.batch_size, serialized_records.size, batch_reader).each
       elsif spec.first?
-        events.first
+        serialized_records.first
       elsif spec.last?
-        events.last
+        serialized_records.last
       else
-        events.each
+        serialized_records.each
       end
     end
 
@@ -68,14 +68,14 @@ module RubyEventStore
 
     private
     def read_scope(spec)
-      events = spec.stream.global? ? global : stream_of(spec.stream.name)
-      events = events.select{|e| spec.with_ids.any?{|x| x.eql?(e.event_id)}} if spec.with_ids?
-      events = events.select{|e| spec.with_types.any?{|x| x.eql?(e.event_type)}} if spec.with_types?
-      events = events.reverse if spec.backward?
-      events = events.drop(index_of(events, spec.start) + 1) if spec.start
-      events = events.take(index_of(events, spec.stop)) if spec.stop
-      events = events[0...spec.limit] if spec.limit?
-      events
+      serialized_records = spec.stream.global? ? global : stream_of(spec.stream.name)
+      serialized_records = serialized_records.select{|e| spec.with_ids.any?{|x| x.eql?(e.event_id)}} if spec.with_ids?
+      serialized_records = serialized_records.select{|e| spec.with_types.any?{|x| x.eql?(e.event_type)}} if spec.with_types?
+      serialized_records = serialized_records.reverse if spec.backward?
+      serialized_records = serialized_records.drop(index_of(serialized_records, spec.start) + 1) if spec.start
+      serialized_records = serialized_records.take(index_of(serialized_records, spec.stop)) if spec.stop
+      serialized_records = serialized_records[0...spec.limit] if spec.limit?
+      serialized_records
     end
 
     def read_event(event_id)
@@ -86,15 +86,15 @@ module RubyEventStore
       streams.fetch(name, Array.new)
     end
 
-    def add_to_stream(events, expected_version, stream, include_global)
-      append_with_synchronize(Array(events), expected_version, stream, include_global)
+    def add_to_stream(serialized_records, expected_version, stream, include_global)
+      append_with_synchronize(Array(serialized_records), expected_version, stream, include_global)
     end
 
     def last_stream_version(stream)
       stream_of(stream.name).size - 1
     end
 
-    def append_with_synchronize(events, expected_version, stream, include_global)
+    def append_with_synchronize(serialized_records, expected_version, stream, include_global)
       resolved_version = expected_version.resolve_for(stream, method(:last_stream_version))
 
       # expected_version :auto assumes external lock is used
@@ -107,23 +107,23 @@ module RubyEventStore
       Thread.pass
       mutex.synchronize do
         resolved_version = last_stream_version(stream) if expected_version.any?
-        append(events, resolved_version, stream, include_global)
+        append(serialized_records, resolved_version, stream, include_global)
       end
     end
 
-    def append(events, resolved_version, stream, include_global)
-      stream_events = stream_of(stream.name)
+    def append(serialized_records, resolved_version, stream, include_global)
+      serialized_records_of_stream = stream_of(stream.name)
       raise WrongExpectedEventVersion unless last_stream_version(stream).equal?(resolved_version)
 
-      events.each do |event|
-        raise EventDuplicatedInStream if stream_events.any? {|ev| ev.event_id.eql?(event.event_id)}
+      serialized_records.each do |serialized_record|
+        raise EventDuplicatedInStream if serialized_records_of_stream.any? {|ev| ev.event_id.eql?(serialized_record.event_id)}
         if include_global
-          raise EventDuplicatedInStream if has_event?(event.event_id)
-          global.push(event)
+          raise EventDuplicatedInStream if has_event?(serialized_record.event_id)
+          global.push(serialized_record)
         end
-        stream_events.push(event)
+        serialized_records_of_stream.push(serialized_record)
       end
-      streams[stream.name] = stream_events
+      streams[stream.name] = serialized_records_of_stream
       self
     end
 
