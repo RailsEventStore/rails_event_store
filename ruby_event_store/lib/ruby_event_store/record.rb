b0e383ad08fc8902128f00585f4a2133bb9892f7
Mirosław Pragłowski <m@praglowski.com>
2020-09-01 13:23:44 +0200

    Just a copy of SerializedRecord with a new name

---
+++ ruby_event_store/lib/ruby_event_store/record.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+module RubyEventStore
+  class Record
+    StringsRequired = Class.new(StandardError)
+    def initialize(event_id:, data:, metadata:, event_type:)
+      raise StringsRequired unless [event_id, event_type].all? { |v| v.instance_of?(String) }
+      @event_id   = event_id
+      @data       = data
+      @metadata   = metadata
+      @event_type = event_type
+      freeze
+    end
+
+    attr_reader :event_id, :data, :metadata, :event_type
+
+    BIG_VALUE = 0b110011100100000010010010110011101011110101010101001100111110011
+    def hash
+      [
+        self.class,
+        event_id,
+        data,
+        metadata,
+        event_type,
+      ].hash ^ BIG_VALUE
+    end
+
+    def ==(other)
+      other.instance_of?(self.class) &&
+        other.event_id.eql?(event_id) &&
+        other.data.eql?(data) &&
+        other.metadata.eql?(metadata) &&
+        other.event_type.eql?(event_type)
+    end
+
+    def to_h
+      {
+        event_id: event_id,
+        data: data,
+        metadata: metadata,
+        event_type: event_type,
+      }
+    end
+
+    alias_method :eql?, :==
+  end
+end
---
+++ ruby_event_store/spec/record_spec.rb
@@ -0,0 +1,88 @@
+require 'spec_helper'
+
+module RubyEventStore
+
+  RSpec.describe Record do
+    let(:event_id)   { "event_id" }
+    let(:data)       { "data" }
+    let(:metadata)   { "metadata" }
+    let(:event_type) { "event_type" }
+
+    specify 'constructor accept all arguments and returns frozen instance' do
+      record = described_class.new(event_id: event_id, data: data, metadata: metadata, event_type: event_type)
+      expect(record.event_id).to   be event_id
+      expect(record.metadata).to   be metadata
+      expect(record.data).to       be data
+      expect(record.event_type).to be event_type
+      expect(record.frozen?).to    be true
+    end
+
+    specify 'constructor raised Record::StringsRequired when argument is not a String' do
+      [[1, 1, 1, 1],
+       [1, "string", "string", "string"],
+       ["string", "string", "string", 1]].each do |sample|
+        event_id, data, metadata, event_type = sample
+        expect do
+          described_class.new(event_id: event_id, data: data, metadata: metadata, event_type: event_type)
+        end.to raise_error Record::StringsRequired
+      end
+    end
+
+    specify "in-equality" do
+      [
+        ["a", "a", "a", "a"],
+        ["b", "a", "a", "a"],
+        ["a", "b", "a", "a"],
+        ["a", "a", "b", "a"],
+        ["a", "a", "a", "b"],
+      ].permutation(2).each do |one, two|
+        a = Record.new(event_id: one[0], data: one[1], metadata: one[2], event_type: one[3])
+        b = Record.new(event_id: two[0], data: two[1], metadata: two[2], event_type: two[3])
+        c = Class.new(Record).new(event_id: one[0], data: one[1], metadata: one[2], event_type: one[3])
+        expect(a).not_to eq(b)
+        expect(a).not_to eql(b)
+        expect(a.hash).not_to eq(b.hash)
+        h = {a => :val}
+        expect(h[b]).to be_nil
+
+        expect(a).not_to eq(c)
+        expect(a).not_to eql(c)
+        expect(a.hash).not_to eq(c.hash)
+        h = {a => :val}
+        expect(h[c]).to be_nil
+      end
+    end
+
+    specify "equality" do
+      a = Record.new(event_id: "a", data: "b", metadata: "c", event_type: "d")
+      b = Record.new(event_id: "a", data: "b", metadata: "c", event_type: "d")
+      expect(a).to eq(b)
+      expect(a).to eql(b)
+      expect(a.hash).to eql(b.hash)
+      h = {a => :val}
+      expect(h[b]).to eq(:val)
+    end
+
+    specify "hash" do
+      a = Record.new(event_id: "a", data: "b", metadata: "c", event_type: "d")
+      expect(a.hash).not_to eq([Record, "a", "b", "c", "d"].hash)
+    end
+
+    specify "to_h" do
+      a = Record.new(event_id: "a", data: "b", metadata: "c", event_type: "d")
+      expect(a.to_h).to eq({
+        event_id: "a",
+        data: "b",
+        metadata: "c",
+        event_type: "d",
+      })
+    end
+
+    specify 'constructor raised when required args are missing' do
+      expect do
+        described_class.new
+      end.to raise_error ArgumentError
+    end
+  end
+end
+
