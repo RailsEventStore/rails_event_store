e50e362a79bea39ea5000740591a0c3ade102e06
Paweł Pacana <pawel.pacana@gmail.com>
2020-09-09 21:41:44 +0200

    Dead code.

--- ruby_event_store-rom/spec/spec_helper.rb
+++ ruby_event_store-rom/spec/spec_helper.rb
@@ -3,24 +3,4 @@ require_relative '../../support/helpers/rspec_defaults'
 require_relative '../../support/helpers/mutant_timeout'
 require 'dry/inflector'
 
-begin
-  require 'pry'
-  require 'pry-byebug'
-rescue LoadError
-end
-
 ENV['DATABASE_URL'] ||= 'sqlite:db.sqlite3'
-
-module RomHelpers
-  def env
-    rom_helper.env
-  end
-
-  def rom_container
-    env.rom_container
-  end
-
-  def rom_db
-    rom_container.gateways[:default]
-  end
-end
