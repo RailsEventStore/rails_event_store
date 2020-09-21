023e53afae6ed0622db92a20a649e7e4d9692c37
Paweł Pacana <pawel.pacana@gmail.com>
2020-09-21 14:24:18 +0200

    Ruby Hash is ordered
    This simplifies interal storage access a bit.

--- ruby_event_store/lib/ruby_event_store/in_memory_repository.rb
+++ ruby_event_store/lib/ruby_event_store/in_memory_repository.rb
@@ -7,8 +7,8 @@ module RubyEventStore
     def initialize(serializer: NULL)
       @serializer = serializer
       @streams = Hash.new
-      @mutex = Mutex.new
-      @global = Array.new
+      @mutex   = Mutex.new
+      @global  = Hash.new
     end
 
     def append_to_stream(records, stream, expected_version)
@@ -26,7 +26,7 @@ module RubyEventStore
     end
 
     def has_event?(event_id)
-       global.any?{ |item| item.event_id.eql?(event_id) }
+      global.has_key?(event_id)
     end
 
     def last_stream_event(stream)
@@ -62,16 +62,16 @@ module RubyEventStore
 
     def update_messages(records)
       records.each do |record|
-        location = global.index{|m| record.event_id.eql?(m.event_id)} or raise EventNotFound.new(record.event_id)
+        read_event(record.event_id)
         serialized_record =
           Record.new(
             event_id:   record.event_id,
             event_type: record.event_type,
             data:       record.data,
             metadata:   record.metadata,
-            timestamp: Time.iso8601(global.fetch(location).timestamp),
+            timestamp:  Time.iso8601(global.fetch(record.event_id).timestamp),
           ).serialize(serializer)
-        global[location] = serialized_record
+        global[record.event_id] = serialized_record
         streams.values.each do |str|
           location = str.index{|m| record.event_id.eql?(m.event_id)}
           str[location] = serialized_record if location
@@ -87,7 +87,7 @@ module RubyEventStore
 
     private
     def read_scope(spec)
-      serialized_records = spec.stream.global? ? global : serialized_records_of_stream(spec.stream.name)
+      serialized_records = spec.stream.global? ? global.values : serialized_records_of_stream(spec.stream.name)
       serialized_records = serialized_records.select{|e| spec.with_ids.any?{|x| x.eql?(e.event_id)}} if spec.with_ids?
       serialized_records = serialized_records.select{|e| spec.with_types.any?{|x| x.eql?(e.event_type)}} if spec.with_types?
       serialized_records = serialized_records.reverse if spec.backward?
@@ -102,7 +102,7 @@ module RubyEventStore
     end
 
     def read_event(event_id)
-      global.find {|e| event_id.eql?(e.event_id)} or raise EventNotFound.new(event_id)
+      global.fetch(event_id) { raise EventNotFound.new(event_id) }
     end
 
     def serialized_records_of_stream(name)
@@ -142,7 +142,7 @@ module RubyEventStore
         raise EventDuplicatedInStream if serialized_records_of_stream_.any? {|ev| ev.event_id.eql?(serialized_record.event_id)}
         if include_global
           raise EventDuplicatedInStream if has_event?(serialized_record.event_id)
-          global.push(serialized_record)
+          global[serialized_record.event_id] = serialized_record
         end
         serialized_records_of_stream_.push(serialized_record)
       end
