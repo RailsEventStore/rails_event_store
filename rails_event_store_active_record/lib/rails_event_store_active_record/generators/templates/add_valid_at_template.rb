4cb051096725052bf261ec0f0df7fb4b3e5a52cb
Paweł Pacana <pawel.pacana@gmail.com>
2020-10-01 16:31:34 +0200

    Precision is meaningless for Postgres

--- rails_event_store_active_record/lib/rails_event_store_active_record/generators/templates/add_valid_at_template.rb
+++ rails_event_store_active_record/lib/rails_event_store_active_record/generators/templates/add_valid_at_template.rb
@@ -2,7 +2,13 @@
 
 class AddValidAt < ActiveRecord::Migration<%= migration_version %>
   def change
-    add_column :event_store_events, :valid_at, :datetime, precision: 6, null: true
+    case ActiveRecord::Base.connection.adapter_name
+    when "PostgreSQL"
+       add_column :event_store_events, :valid_at, :datetime, null: true
+    else
+       add_column :event_store_events, :valid_at, :datetime, precision: 6, null: true
+    end
+
     add_index  :event_store_events, :valid_at
   end
 end
