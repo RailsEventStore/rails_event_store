c547c844b02b7208e56bcbc12dabe0a1d20a5d6e
Paweł Pacana <pawel.pacana@gmail.com>
2020-12-02 00:37:31 +0100

    Lift common mutant configuration to global config

--- support/make/mutant.mk
+++ support/make/mutant.mk
@@ -1,25 +1,25 @@
 mutate: ## Run mutation tests
 	@echo "Running mutation tests"
-	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run -t 10 --include lib \
+	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run \
 		$(addprefix --require ,$(REQUIRE)) \
 		$(addprefix --ignore-subject ,$(IGNORE)) \
 		$(if $(MUTANT_JOBS), --jobs $(MUTANT_JOBS)) \
-		--use rspec "$(SUBJECT)"
+		"$(SUBJECT)"
 
 mutate-fast: ## Run mutation tests with --fail-fast
 	@echo "Running mutation tests"
-	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run -t 10 --include lib \
+	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run \
 		$(addprefix --require ,$(REQUIRE)) \
 		$(addprefix --ignore-subject ,$(IGNORE)) \
 		$(if $(MUTANT_JOBS), --jobs $(MUTANT_JOBS)) \
 		--fail-fast \
-		--use rspec "$(SUBJECT)"
+		"$(SUBJECT)"
 
 mutate-changes: ## Run mutation tests for all changes since origin/HEAD
 	@echo "Running mutation tests"
-	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run -t 10 --include lib \
+	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run \
 		$(addprefix --require ,$(REQUIRE)) \
 		$(addprefix --ignore-subject ,$(IGNORE)) \
 		$(if $(MUTANT_JOBS), --jobs $(MUTANT_JOBS)) \
 		--since origin/HEAD \
-		--use rspec "$(SUBJECT)"
+		"$(SUBJECT)"
