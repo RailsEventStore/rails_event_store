4bc61e36694347cbdb937b76a55308933ca34285
Paweł Pacana <pawel.pacana@gmail.com>
2020-12-04 12:42:07 +0100

    Kill mutants
    Forgive me for what I've $done ;)

--- ruby_event_store/spec/mappers/deprecated_wrapper_spec.rb
+++ ruby_event_store/spec/mappers/deprecated_wrapper_spec.rb
@@ -6,37 +6,40 @@ module RubyEventStore
     RSpec.describe DeprecatedWrapper do
       it_behaves_like :mapper, DeprecatedWrapper.new(Default.new), TimeEnrichment.with(TestEvent.new)
 
-      specify "with deprecated mapper" do
-        mapper = Object.new
-        def mapper.serialized_record_to_event(record)
-          OrderCreated.new
-        end
+      let(:event) { OrderCreated.new }
+      let(:record) {
+        Record.new(
+          event_type: event.event_type,
+          event_id:   event.event_id,
+          timestamp:  Time.at(0),
+          valid_at:   Time.at(0),
+          data:       '',
+          metadata:   '',
+        )
+      }
 
-        def mapper.event_to_serialized_record(event)
-          Record.new(
-            event_type: event.event_type,
-            event_id:   event.event_id,
-            timestamp:  Time.at(0),
-            valid_at:   Time.at(0),
-            data:       '',
-            metadata:   '',
-          )
-        end
-        expect(mapper).to receive(:event_to_serialized_record).and_call_original
-        expect(mapper).to receive(:serialized_record_to_event).and_call_original
+      specify "with deprecated mapper" do
+        mapper  = Object.new
+        $event  = event
+        $record = record
+        def mapper.serialized_record_to_event(*); $event;  end
+        def mapper.event_to_serialized_record(*); $record; end
+
+        expect(mapper).to receive(:event_to_serialized_record).with(event).and_call_original
+        expect(mapper).to receive(:serialized_record_to_event).with(record).and_call_original
 
         client =
           Client.new(mapper: mapper, repository: InMemoryRepository.new)
         expect {
-          client.append(OrderCreated.new)
+          client.append(event)
         }.to output(<<~EOW).to_stderr
-        Deprecation: Please rename Object#event_to_serialized_record to Object#event_to_record.
+          Deprecation: Please rename Object#event_to_serialized_record to Object#event_to_record.
         EOW
 
         expect {
           client.read.last
         }.to output(<<~EOW).to_stderr
-        Deprecation: Please rename Object#serialized_record_to_event to Object#record_to_event.
+          Deprecation: Please rename Object#serialized_record_to_event to Object#record_to_event.
         EOW
       end
     end
