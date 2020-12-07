87a03bdcfd3320bd5e4a86be64cb4e22df422da1
Paweł Pacana <pawel.pacana@gmail.com>
2020-12-07 21:19:13 +0100

    Obsolete with multiple Gemfiles

--- support/make/install.mk
+++ support/make/install.mk
@@ -2,8 +2,3 @@ install: ## Install gem dependencies
 	@echo "Installing gem dependencies"
 	@bundle check || bundle install
 
-remove-lock:
-	@echo "Removing resolved dependency versions"
-	-rm Gemfile.lock
-
-reinstall: remove-lock install ## Removing resolved dependency versions
