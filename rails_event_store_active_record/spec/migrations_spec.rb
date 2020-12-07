5e7e03586184c6296b77ca9b9118a4762abfa36b
Paweł Pacana <pawel.pacana@gmail.com>
2020-12-07 13:37:56 +0100

    Drop pre-1.0 migrations testing
    Major releases are a good hook to leave such legacy behind.

--- rails_event_store_active_record/spec/migrations_spec.rb
+++ rails_event_store_active_record/spec/migrations_spec.rb
@@ -3,44 +3,6 @@ require 'spec_helper'
 RSpec.describe "database schema migrations" do
   include SchemaHelper
 
-  specify "migrate from v0.33.0 to v0.34.0" do
-    validate_migration('Gemfile.0_33_0', 'Gemfile.0_34_0',
-      source_template_name: 'migration') do
-      run_migration('index_by_event_type')
-      run_migration('limit_for_event_id')
-    end
-  end
-
-  specify "migrate from v0.34.0 to v0.35.0" do
-    skip("incompatible version of activerecord-import") if rails_6? && sqlite?
-
-    validate_migration('Gemfile.0_34_0', 'Gemfile.0_35_0',
-      source_template_name: 'create_event_store_events') do
-      run_code(<<~EOF, gemfile: 'Gemfile.0_34_0')
-        DummyEvent = Class.new(RubyEventStore::Event)
-
-        client = RubyEventStore::Client.new(repository: RailsEventStoreActiveRecord::EventRepository.new)
-        client.append(DummyEvent.new(event_id: "94b297a3-5a29-4942-9038-3efeceb4d905", data: {
-          all: true,
-          a: 1,
-          text: "text",
-        }))
-      EOF
-
-      run_migration('binary_data_and_metadata')
-      run_code(<<~EOF, gemfile: 'Gemfile.0_35_0')
-        DummyEvent = Class.new(RubyEventStore::Event)
-
-        client = RubyEventStore::Client.new(repository: RailsEventStoreActiveRecord::EventRepository.new)
-        raise unless client.read.event("94b297a3-5a29-4942-9038-3efeceb4d905").data == {
-          all: true,
-          a: 1,
-          text: "text",
-        }
-      EOF
-    end
-  end
-
   specify "migrate from v1.1.1 to master" do
     validate_migration('Gemfile.1_1_1', 'Gemfile.master',
       source_template_name: 'create_event_store_events') do
