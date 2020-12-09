0f0cd536bfe65c843b34d16b07d0b0899165aec2
Paweł Pacana <pawel.pacana@gmail.com>
2020-12-09 11:56:56 +0100

    Only clone templates into fake filesystem
    Otherwise we keep cloning irrelevant files for specs, sometimes even
    vendor/bundle which is sick.
    
    See:
    https://github.com/RailsEventStore/rails_event_store/pull/882

--- rails_event_store_active_record/spec/no_global_stream_entries_generator_spec.rb
+++ rails_event_store_active_record/spec/no_global_stream_entries_generator_spec.rb
@@ -11,30 +11,31 @@ module RailsEventStoreActiveRecord
       $stdout = current_stdout
     end
 
-    specify do
+    around do |example|
       FakeFS.with_fresh do
-        FakeFS::FileSystem.clone(File.expand_path('../../', __FILE__))
-        stub_const("Rails::VERSION::STRING", "4.2.8")
-
-        generator = NoGlobalStreamEntriesGenerator.new
-        allow(Time).to receive(:now).and_return(Time.new(2016,8,9,22,22,22))
-        generator.create_migration
-
-        expect(File.read("db/migrate/20160809222222_no_global_stream_entries.rb")).to match(/ActiveRecord::Migration$/)
+        FakeFS::FileSystem.clone(File.expand_path('../../lib/rails_event_store_active_record/generators/templates', __FILE__))
+        example.run
       end
     end
 
     specify do
-      FakeFS.with_fresh do
-        FakeFS::FileSystem.clone(File.expand_path('../../', __FILE__))
-        stub_const("Rails::VERSION::STRING", "5.0.0")
+      stub_const("Rails::VERSION::STRING", "4.2.8")
 
-        generator = NoGlobalStreamEntriesGenerator.new
-        allow(Time).to receive(:now).and_return(Time.new(2016,8,9,22,22,22))
-        generator.create_migration
+      generator = NoGlobalStreamEntriesGenerator.new
+      allow(Time).to receive(:now).and_return(Time.new(2016,8,9,22,22,22))
+      generator.create_migration
 
-        expect(File.read("db/migrate/20160809222222_no_global_stream_entries.rb")).to match(/ActiveRecord::Migration\[4\.2\]$/)
-      end
+      expect(File.read("db/migrate/20160809222222_no_global_stream_entries.rb")).to match(/ActiveRecord::Migration$/)
+    end
+
+    specify do
+      stub_const("Rails::VERSION::STRING", "5.0.0")
+
+      generator = NoGlobalStreamEntriesGenerator.new
+      allow(Time).to receive(:now).and_return(Time.new(2016,8,9,22,22,22))
+      generator.create_migration
+
+      expect(File.read("db/migrate/20160809222222_no_global_stream_entries.rb")).to match(/ActiveRecord::Migration\[4\.2\]$/)
     end
   end
 end
