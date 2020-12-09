677078d605a03adfe78a745f49521a1ba0a4d101
Paweł Pacana <pawel.pacana@gmail.com>
2020-12-09 11:36:20 +0100

    Remove obsolete migrations when starting on 1.x
    If you havent't applied them already, downgrade.

--- rails_event_store_active_record/lib/rails_event_store_active_record/generators/templates/index_by_event_type_template.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class IndexByEventType < ActiveRecord::Migration<%= migration_version %>
-  def change
-    add_index :event_store_events, :event_type unless index_exists? :event_store_events, :event_type
-  end
-end
--- rails_event_store_active_record/lib/rails_event_store_active_record/generators/templates/limit_for_event_id_template.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class LimitForEventId < ActiveRecord::Migration<%= migration_version %>
-  def change
-    postgres = ActiveRecord::Base.connection.adapter_name == "PostgreSQL"
-    change_column(:event_store_events_in_streams, :event_id, :string, limit: 36) unless postgres
-  end
-end
