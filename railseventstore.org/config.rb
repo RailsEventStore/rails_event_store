4e6b5c4964cee0b2559051a1f00fb85512f1331c
Paweł Pacana <pawel.pacana@gmail.com>
2020-12-10 02:33:27 +0100

    Make sure there's a default documentation version

--- railseventstore.org/config.rb
+++ railseventstore.org/config.rb
@@ -139,6 +139,7 @@ helpers do
   end
 end
 
-page "/", layout: "landing",                locals: { version: "v1" }
+page "/", layout: "landing"
 page "/docs/v1/*", layout: "documentation", locals: { version: "v1" }
 page "/docs/v2/*", layout: "documentation", locals: { version: "v2" }
+page "*", locals: { version: "v1" }
