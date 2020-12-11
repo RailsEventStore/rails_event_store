5b7e29c7fe7a9e5125bbd4a36aa94993e15c1914
Mirosław Pragłowski <m@praglowski.com>
2020-12-11 16:26:58 +0100

    Migrate to better precision for created_at

--- contrib/multiple_databases_repository_sample_app/db/payments_schema.rb
+++ contrib/multiple_databases_repository_sample_app/db/payments_schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_04_152933) do
+ActiveRecord::Schema.define(version: 2020_12_11_162505) do
 
   create_table "event_store_events", id: :string, limit: 36, force: :cascade do |t|
     t.string "event_type", null: false
     t.binary "metadata"
     t.binary "data", null: false
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: 6, null: false
     t.index ["created_at"], name: "index_event_store_events_on_created_at"
     t.index ["event_type"], name: "index_event_store_events_on_event_type"
   end
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2020_06_04_152933) do
     t.string "stream", null: false
     t.integer "position"
     t.string "event_id", limit: 36, null: false
-    t.datetime "created_at", null: false
+    t.datetime "created_at", precision: 6, null: false
     t.index ["created_at"], name: "index_event_store_events_in_streams_on_created_at"
     t.index ["stream", "event_id"], name: "index_event_store_events_in_streams_on_stream_and_event_id", unique: true
     t.index ["stream", "position"], name: "index_event_store_events_in_streams_on_stream_and_position", unique: true
---
+++ contrib/multiple_databases_repository_sample_app/db/shipping/20201211162423_created_at_precision.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+class CreatedAtPrecision < ActiveRecord::Migration[4.2]
+  def change
+    rails_42 = Gem::Version.new(ActiveRecord::VERSION::STRING) < Gem::Version.new("5.0.0")
+
+    case ActiveRecord::Base.connection.adapter_name
+    when "SQLite"
+      rename_table :event_store_events, :old_event_store_events
+      create_table(:event_store_events, id: false, force: false) do |t|
+        t.string :id, limit: 36, primary_key: true, null: false
+        t.string      :event_type,  null: false
+        t.binary      :metadata
+        t.binary      :data,        null: false
+        t.datetime    :created_at,  null: false, precision: 6
+      end
+      add_index :event_store_events, :id, unique: true if rails_42
+      add_index :event_store_events, :created_at
+      add_index :event_store_events, :event_type
+      execute <<-SQL
+        INSERT INTO event_store_events(id, event_type, metadata, data, created_at)
+        SELECT id, event_type, metadata, data, created_at FROM old_event_store_events;
+      SQL
+      drop_table :old_event_store_events
+
+      rename_table :event_store_events_in_streams, :old_event_store_events_in_streams
+      create_table(:event_store_events_in_streams, force: false) do |t|
+        t.string      :stream,      null: false
+        t.integer     :position,    null: true
+        t.references  :event,       null: false, type: :string, limit: 36
+        t.datetime    :created_at,  null: false, precision: 6
+      end
+      add_index :event_store_events_in_streams, [:stream, :position], unique: true
+      add_index :event_store_events_in_streams, [:created_at]
+      add_index :event_store_events_in_streams, [:stream, :event_id], unique: true
+
+      execute <<-SQL
+        INSERT INTO event_store_events_in_streams(id, stream, position, event_id, created_at)
+        SELECT id, stream, position, event_id, created_at FROM old_event_store_events_in_streams;
+      SQL
+      drop_table :old_event_store_events_in_streams
+    when "PostgreSQL"
+    else
+      change_column :event_store_events,            :created_at, :datetime, precision: 6
+      change_column :event_store_events_in_streams, :created_at, :datetime, precision: 6
+    end
+  end
+end
