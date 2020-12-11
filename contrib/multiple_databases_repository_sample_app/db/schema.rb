1f8b5b9a39f4e877a3278fc64ebda4e98b8210a8
Mirosław Pragłowski <m@praglowski.com>
2020-12-11 16:32:11 +0100

    Migrate to enable support for BI temporal event sourcing

--- contrib/multiple_databases_repository_sample_app/db/schema.rb
+++ contrib/multiple_databases_repository_sample_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_11_162740) do
+ActiveRecord::Schema.define(version: 2020_12_11_163039) do
 
   create_table "event_store_events", force: :cascade do |t|
     t.string "event_id", limit: 36, null: false
@@ -18,9 +18,11 @@ ActiveRecord::Schema.define(version: 2020_12_11_162740) do
     t.binary "metadata"
     t.binary "data", null: false
     t.datetime "created_at", precision: 6, null: false
+    t.datetime "valid_at", precision: 6
     t.index ["created_at"], name: "index_event_store_events_on_created_at"
     t.index ["event_id"], name: "index_event_store_events_on_event_id", unique: true
     t.index ["event_type"], name: "index_event_store_events_on_event_type"
+    t.index ["valid_at"], name: "index_event_store_events_on_valid_at"
   end
 
   create_table "event_store_events_in_streams", force: :cascade do |t|
