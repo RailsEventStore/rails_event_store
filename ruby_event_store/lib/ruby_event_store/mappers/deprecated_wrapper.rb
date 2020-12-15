9521991ac45e60d1b5e3987b2425ee30c738c68a
Mirosław Pragłowski <m@praglowski.com>
2020-12-15 22:40:27 +0100

    Not all NoMethodError errors are equal
    Deprectation message only on the ones we care about

--- ruby_event_store/lib/ruby_event_store/mappers/deprecated_wrapper.rb
+++ ruby_event_store/lib/ruby_event_store/mappers/deprecated_wrapper.rb
@@ -11,7 +11,8 @@ module RubyEventStore
 
       def event_to_record(any)
         @mapper.event_to_record(any)
-      rescue NoMethodError
+      rescue NoMethodError => e
+        raise unless e.message =~ /undefined method `event_to_record/
         warn <<~EOW
           Deprecation: Please rename #{@mapper.class}#event_to_serialized_record to #{@mapper.class}#event_to_record.
         EOW
@@ -20,7 +21,8 @@ module RubyEventStore
 
       def record_to_event(any)
         @mapper.record_to_event(any)
-      rescue NoMethodError
+      rescue NoMethodError => e
+        raise unless e.message =~ /undefined method `record_to_event/
         warn <<~EOW
           Deprecation: Please rename #{@mapper.class}#serialized_record_to_event to #{@mapper.class}#record_to_event.
         EOW
