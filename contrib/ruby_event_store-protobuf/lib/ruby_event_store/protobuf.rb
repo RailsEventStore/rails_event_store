cd736f97a61b0307b39ebb3c6c29adde309da97a
Mirosław Pragłowski <m@praglowski.com>
2020-12-28 19:46:13 +0100

    Define aliases for backward compatibility

--- contrib/ruby_event_store-protobuf/lib/ruby_event_store/protobuf.rb
+++ contrib/ruby_event_store-protobuf/lib/ruby_event_store/protobuf.rb
@@ -14,3 +14,17 @@ require 'ruby_event_store/protobuf/mappers/transformation/protobuf_encoder'
 require 'ruby_event_store/protobuf/mappers/transformation/protobuf_nested_struct_metadata'
 require 'ruby_event_store/protobuf/mappers/protobuf'
 require 'ruby_event_store/protobuf/version'
+
+module RubyEventStore
+  Proto = Protobuf::Proto
+
+  module Mappers
+    Protobuf = RubyEventStore::Protobuf::Mappers::Protobuf
+
+    module Transformation
+      ProtoEvent = RubyEventStore::Protobuf::Mappers::Transformation::ProtoEvent
+      ProtobufEncoder = RubyEventStore::Protobuf::Mappers::Transformation::ProtobufEncoder
+      ProtobufNestedStructMetadata = RubyEventStore::Protobuf::Mappers::Transformation::ProtobufNestedStructMetadata
+    end
+  end
+end
