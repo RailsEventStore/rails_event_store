d575e05d60ed07952a7653b53cdc14c302fe6f4b
Mirosław Pragłowski <m@praglowski.com>
2020-12-28 21:03:59 +0100

    No warning please

---
+++ contrib/ruby_event_store-protobuf/spec/no_warnings_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+module RubyEventStore
+  module Protobuf
+    RSpec.describe 'no warnings', mutant: false do
+      specify do
+        expect(ruby_event_store_protobuf_warnings).to eq([])
+      end
+
+      def ruby_event_store_protobuf_warnings
+        warnings.select { |w| w =~ %r{lib/ruby_event_store/protobuf} }
+      end
+
+      def warnings
+        `ruby -Ilib -w lib/ruby_event_store/protobuf.rb 2>&1`.split("\n")
+      end
+    end
+  end
+end
