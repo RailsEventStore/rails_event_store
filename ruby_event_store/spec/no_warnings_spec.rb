0a9b9162cb1dfaa8b2f9358b8a3805ac7214aab2
Mirosław Pragłowski <m@praglowski.com>
2020-12-28 20:36:21 +0100

    Restore warnings check, removed too much

--- ruby_event_store/spec/no_warnings_spec.rb
+++ ruby_event_store/spec/no_warnings_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 module RubyEventStore
   RSpec.describe 'no warnings' do
+    specify do
+      expect(ruby_event_store_warnings).to eq([])
+    end
+
     def ruby_event_store_warnings
       warnings.select { |w| w =~ %r{lib/ruby_event_store} }
     end
