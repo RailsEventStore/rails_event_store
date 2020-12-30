ab0e6feba789a64bc033d24455297f2378a243c9
Paweł Pacana <pawel.pacana@gmail.com>
2020-12-30 19:17:42 +0100

    Sort out versions

--- railseventstore.org/config.rb
+++ railseventstore.org/config.rb
@@ -53,8 +53,9 @@ activate :external_pipeline,
   latency: 1
 
 set :markdown_engine, :redcarpet
-set :res_version, File.read('../RES_VERSION')
-set :res_version_next, '2.0.0'
+set :res_version_v1, '1.3.0'
+set :res_version_v2, File.read('../RES_VERSION')
+set :res_version,    File.read('../RES_VERSION')
 
 set :markdown,
   tables: true,
