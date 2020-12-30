7dbdd8bd3d89c6c72a4ea256005376d645de777b
Paweł Pacana <pawel.pacana@gmail.com>
2020-12-30 19:29:46 +0100

    New default version of docs

--- railseventstore.org/config.rb
+++ railseventstore.org/config.rb
@@ -143,4 +143,4 @@ end
 page "/", layout: "landing"
 page "/docs/v1/*", layout: "documentation", locals: { version: "v1" }
 page "/docs/v2/*", layout: "documentation", locals: { version: "v2" }
-page "*", locals: { version: "v1" }
+page "*", locals: { version: "v2" }
