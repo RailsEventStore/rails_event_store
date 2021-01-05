91608868e07243cf3263e0af309d7badef5f21cb
Paweł Pacana <pawel.pacana@gmail.com>
2021-01-05 15:23:19 +0100

    Move makefile mutant scaffolding into dedicated configs
    * per-project config requires/subjects/ignores from per-project
      makefiles
    * simpler mutate commands wrapper, now also easier to skip it and use
      mutant CLI directly
    * MUTANT_JOBS was obsolete was CircleCI was dropped
    
    Closes #960

--- support/make/mutant.mk
+++ support/make/mutant.mk
@@ -1,25 +1,11 @@
 mutate: ## Run mutation tests
 	@echo "Running mutation tests"
-	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run \
-		$(addprefix --require ,$(REQUIRE)) \
-		$(addprefix --ignore-subject ,$(IGNORE)) \
-		$(if $(MUTANT_JOBS), --jobs $(MUTANT_JOBS)) \
-		"$(SUBJECT)"
+	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run
 
 mutate-fast: ## Run mutation tests with --fail-fast
 	@echo "Running mutation tests"
-	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run \
-		$(addprefix --require ,$(REQUIRE)) \
-		$(addprefix --ignore-subject ,$(IGNORE)) \
-		$(if $(MUTANT_JOBS), --jobs $(MUTANT_JOBS)) \
-		--fail-fast \
-		"$(SUBJECT)"
+	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run --fail-fast
 
 mutate-changes: ## Run incremental mutation tests
 	@echo "Running mutation tests"
-	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run \
-		$(addprefix --require ,$(REQUIRE)) \
-		$(addprefix --ignore-subject ,$(IGNORE)) \
-		$(if $(MUTANT_JOBS), --jobs $(MUTANT_JOBS)) \
-		--since HEAD~1 \
-		"$(SUBJECT)"
+	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run --since HEAD~1
