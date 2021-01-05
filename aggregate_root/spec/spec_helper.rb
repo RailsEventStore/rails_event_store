81d7a47f867018e17f15681f6a8526cca727c3b7
Mirosław Pragłowski <m@praglowski.com>
2021-01-05 13:07:29 +0100

    No more aggregate root configuration
    Since we have introduced AggregateRoot::Repository it is
    easy to pass event store as a dependency there. No need
    to use AggregateRoot::Configuration and default_event_store.
    
    Make it explicte which event store is used. Less "magic".

--- aggregate_root/spec/spec_helper.rb
+++ aggregate_root/spec/spec_helper.rb
@@ -4,8 +4,6 @@ require "aggregate_root"
 require "ruby_event_store"
 require_relative "../../support/helpers/rspec_defaults"
 
-RSpec.configure { |spec| spec.before(:each) { AggregateRoot.configure { |config| config.default_event_store = nil } } }
-
 module Orders
   module Events
     OrderCreated = Class.new(RubyEventStore::Event)
