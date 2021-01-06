3547ac918702c266c1e1e2aad2fd3c654e6f5d2b
Rafał Łasocha <me@swistak35.com>
2021-01-06 11:59:37 +0100

    Add test for changing the formatter

--- ruby_event_store-rspec/spec/ruby_event_store/rspec/have_published_spec.rb
+++ ruby_event_store-rspec/spec/ruby_event_store/rspec/have_published_spec.rb
@@ -273,6 +273,24 @@ module RubyEventStore
         expect(matcher_.description)
           .to eq("have published events that have to (be a FooEvent and be a BazEvent)")
       end
+
+      specify do
+        old_formatter = HavePublished.default_formatter
+        HavePublished.default_formatter = HavePublished::StepByStepFailureMessageFormatter
+        matcher_ = matcher(expected = matchers.an_event(BarEvent))
+        matcher_.matches?(event_store)
+
+        expect(matcher_.failure_message.to_s).to eq(<<~EOS)
+        expected [
+          be an event BarEvent
+        ] to be published
+
+        i.e. expected event
+          be an event BarEvent
+        to be published, but there is no event with such type
+        EOS
+        HavePublished.default_formatter = old_formatter
+      end
     end
   end
 end
