af10673c3391094e46b8d7ba8480812db0a668c6
Paweł Pacana <pawel.pacana@gmail.com>
2021-01-09 14:11:52 +0100

    Poor way to ensure with_strategy(->{ Default }) has OnDSL

--- aggregate_root/lib/aggregate_root.rb
+++ aggregate_root/lib/aggregate_root.rb
@@ -80,7 +80,6 @@ module AggregateRoot
   def self.with_default_apply_strategy
     Module.new do
       def self.included(host_class)
-        host_class.extend OnDSL
         host_class.include AggregateRoot.with_strategy(-> { DefaultApplyStrategy.new })
       end
     end
@@ -89,7 +88,8 @@ module AggregateRoot
   def self.with_strategy(strategy)
     Module.new do
       def self.included(host_class)
-        host_class.extend Constructor
+        host_class.extend  OnDSL
+        host_class.extend  Constructor
         host_class.include AggregateMethods
       end
 
