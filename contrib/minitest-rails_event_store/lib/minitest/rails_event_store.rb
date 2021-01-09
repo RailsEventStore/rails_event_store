1b1dc193b55df05338a6ed2688f9be1a17acd504
Paweł Pacana <pawel.pacana@gmail.com>
2021-01-09 15:00:31 +0100

    No-fluff gem ifrastructure

--- contrib/minitest-rails_event_store/lib/minitest/rails_event_store.rb
+++ contrib/minitest-rails_event_store/lib/minitest/rails_event_store.rb
@@ -1,10 +1,8 @@
-require "minitest/rails_event_store/version"
-
 module Minitest
   module RailsEventStore
     class Error < StandardError; end
-    # Your code goes here...
   end
 end
 
-require "minitest/rails_event_store/assertions"
+require_relative "rails_event_store/version"
+require_relative "rails_event_store/assertions"
