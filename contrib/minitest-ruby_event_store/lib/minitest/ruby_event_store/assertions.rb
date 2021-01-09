3903a3df3a2c5f385331a95d166dfc58d83691f1
Paweł Pacana <pawel.pacana@gmail.com>
2021-01-09 15:27:39 +0100

    Refactor

--- contrib/minitest-ruby_event_store/lib/minitest/ruby_event_store/assertions.rb
+++ contrib/minitest-ruby_event_store/lib/minitest/ruby_event_store/assertions.rb
@@ -1,29 +1,33 @@
-module Minitest::Assertions
-  def assert_dispatched(event_store, expected_events, &block)
-    collected_events = collect_events(&block)
+module Minitest
+  module RubyEventStore
+    module Assertions
+      def assert_dispatched(event_store, expected_events, &block)
+        collected_events = collect_events(&block)
 
-    Array(expected_events).each do |expected|
-      assert collected_events.include?(expected.to_s), "bazinga"
+        Array(expected_events).each do |expected|
+          assert collected_events.include?(expected.to_s), "bazinga"
+        end
+      end
+
+      def assert_not_dispatched(event_store, expected_events, &block)
+        collected_events = collect_events(&block)
+
+        Array(expected_events).each do |expected|
+          refute collected_events.include?(expected.to_s), "bazinga"
+        end
+      end
+
+      private
+
+      def collect_events(&block)
+        collected_events = []
+        event_store.within do
+          block.call
+        end.subscribe_to_all_events do |event|
+          collected_events << event.event_type
+        end.call
+        collected_events
+      end
     end
-  end
-
-  def assert_not_dispatched(event_store, expected_events, &block)
-    collected_events = collect_events(&block)
-
-    Array(expected_events).each do |expected|
-      refute collected_events.include?(expected.to_s), "bazinga"
-    end
-  end
-
-  private
-
-  def collect_events(&block)
-    collected_events = []
-    event_store.within do
-      block.call
-    end.subscribe_to_all_events do |event|
-      collected_events << event.event_type
-    end.call
-    collected_events
   end
 end
