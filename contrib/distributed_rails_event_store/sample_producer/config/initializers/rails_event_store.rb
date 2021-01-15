1dd075f51c6d1f16a4cd345a73384bd885cd2ade
Paweł Pacana <pawel.pacana@gmail.com>
2021-01-15 20:44:51 +0100

    Reboot sample apps

--- contrib/distributed_rails_event_store/sample_producer/config/initializers/rails_event_store.rb
+++ contrib/distributed_rails_event_store/sample_producer/config/initializers/rails_event_store.rb
@@ -3,12 +3,10 @@ require 'aggregate_root'
 require 'arkency/command_bus'
 
 Rails.configuration.to_prepare do
-  Rails.configuration.event_store = RailsEventStore::Client.new(repository: RailsEventStoreActiveRecord::PgLinearizedEventRepository.new)
+  Rails.configuration.event_store = RailsEventStore::Client.new(
+    repository: RailsEventStoreActiveRecord::PgLinearizedEventRepository.new(serializer: YAML)
+  )
   Rails.configuration.command_bus = Arkency::CommandBus.new
-
-  AggregateRoot.configure do |config|
-    config.default_event_store = Rails.configuration.event_store
-  end
 
   # Subscribe event handlers below
   # Rails.configuration.event_store.tap do |store|
--- contrib/distributed_rails_event_store/sample_producer/db/migrate/20210115210630_create_event_store_events.rb
+++ contrib/distributed_rails_event_store/sample_producer/db/migrate/20210115210630_create_event_store_events.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class CreateEventStoreEvents < ActiveRecord::Migration[4.2]
   def change
     postgres = ActiveRecord::Base.connection.adapter_name == "PostgreSQL"
     sqlite   = ActiveRecord::Base.connection.adapter_name == "SQLite"
-    rails_42 = Gem::Version.new(ActiveRecord::VERSION::STRING) < Gem::Version.new("5.0.0")
+
     enable_extension "pgcrypto" if postgres
     create_table(:event_store_events_in_streams, force: false) do |t|
       t.string      :stream,      null: false
@@ -12,32 +14,38 @@ class CreateEventStoreEvents < ActiveRecord::Migration[4.2]
       else
         t.references :event, null: false, type: :string, limit: 36
       end
-      t.datetime    :created_at,  null: false
+      if postgres
+        t.datetime    :created_at,  null: false
+      else
+        t.datetime    :created_at,  null: false, precision: 6
+      end
     end
     add_index :event_store_events_in_streams, [:stream, :position], unique: true
     add_index :event_store_events_in_streams, [:created_at]
     add_index :event_store_events_in_streams, [:stream, :event_id], unique: true
 
     if postgres
-      create_table(:event_store_events, id: :uuid, default: 'gen_random_uuid()', force: false) do |t|
+      create_table(:event_store_events, force: false) do |t|
+        t.references  :event,       null: false, type: :uuid
         t.string      :event_type,  null: false
         t.binary      :metadata
         t.binary      :data,        null: false
         t.datetime    :created_at,  null: false
+        t.datetime    :valid_at,    null: true
       end
     else
-      create_table(:event_store_events, id: false, force: false) do |t|
-        t.string :id, limit: 36, primary_key: true, null: false
+      create_table(:event_store_events, force: false) do |t|
+        t.references  :event,       null: false, type: :string, limit: 36
         t.string      :event_type,  null: false
         t.binary      :metadata
         t.binary      :data,        null: false
-        t.datetime    :created_at,  null: false
-      end
-      if sqlite && rails_42
-        add_index :event_store_events, :id, unique: true
+        t.datetime    :created_at,  null: false, precision: 6
+        t.datetime    :valid_at,    null: true,  precision: 6
       end
     end
+    add_index :event_store_events, :event_id, unique: true
     add_index :event_store_events, :created_at
+    add_index :event_store_events, :valid_at
     add_index :event_store_events, :event_type
   end
 end
