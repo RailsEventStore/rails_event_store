7368e94f55d6b622e5b9bd489d237c60a5c066f3
Paweł Pacana <pawel.pacana@gmail.com>
2021-01-15 02:12:35 +0100

    Rename to match instrumented methods and their arguments

--- ruby_event_store/lib/ruby_event_store/mappers/instrumented_mapper.rb
+++ ruby_event_store/lib/ruby_event_store/mappers/instrumented_mapper.rb
@@ -9,13 +9,13 @@ module RubyEventStore
       end
 
       def event_to_record(event)
-        instrumentation.instrument("serialize.mapper.ruby_event_store", domain_event: event) do
+        instrumentation.instrument("event_to_record.mapper.ruby_event_store", event: event) do
           mapper.event_to_record(event)
         end
       end
 
       def record_to_event(record)
-        instrumentation.instrument("deserialize.mapper.ruby_event_store", record: record) do
+        instrumentation.instrument("record_to_event.mapper.ruby_event_store", record: record) do
           mapper.record_to_event(record)
         end
       end
