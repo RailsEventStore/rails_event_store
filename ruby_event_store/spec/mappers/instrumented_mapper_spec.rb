6f03b6672445d9a1c39dfd4bcca1a83a75254c48
Paweł Pacana <pawel.pacana@gmail.com>
2021-01-15 02:12:35 +0100

    Rename to match instrumented methods and their arguments

--- ruby_event_store/spec/mappers/instrumented_mapper_spec.rb
+++ ruby_event_store/spec/mappers/instrumented_mapper_spec.rb
@@ -6,23 +6,23 @@ require "active_support/notifications"
 module RubyEventStore
   module Mappers
     ::RSpec.describe InstrumentedMapper do
-      let(:domain_event) { instance_double(RubyEventStore::Event) }
+      let(:event) { instance_double(RubyEventStore::Event) }
       let(:record) { instance_double(RubyEventStore::Record) }
 
       describe "#event_to_record" do
         specify "wraps around original implementation" do
           some_mapper = instance_double(RubyEventStore::Mappers::Default)
-          allow(some_mapper).to receive(:event_to_record).with(domain_event).and_return(record)
+          allow(some_mapper).to receive(:event_to_record).with(event).and_return(record)
           instrumented_mapper = InstrumentedMapper.new(some_mapper, ActiveSupport::Notifications)
 
-          expect(instrumented_mapper.event_to_record(domain_event)).to eq(record)
+          expect(instrumented_mapper.event_to_record(event)).to eq(record)
         end
 
         specify "instruments" do
           instrumented_mapper = InstrumentedMapper.new(spy, ActiveSupport::Notifications)
-          subscribe_to("serialize.mapper.ruby_event_store") do |notification_calls|
-            instrumented_mapper.event_to_record(domain_event)
-            expect(notification_calls).to eq([{ domain_event: domain_event }])
+          subscribe_to("event_to_record.mapper.ruby_event_store") do |notification_calls|
+            instrumented_mapper.event_to_record(event)
+            expect(notification_calls).to eq([{ event: event }])
           end
         end
       end
@@ -30,15 +30,15 @@ module RubyEventStore
       describe "#record_to_event" do
         specify "wraps around original implementation" do
           some_mapper = instance_double(RubyEventStore::Mappers::Default)
-          allow(some_mapper).to receive(:record_to_event).with(record).and_return(domain_event)
+          allow(some_mapper).to receive(:record_to_event).with(record).and_return(event)
           instrumented_mapper = InstrumentedMapper.new(some_mapper, ActiveSupport::Notifications)
 
-          expect(instrumented_mapper.record_to_event(record)).to eq(domain_event)
+          expect(instrumented_mapper.record_to_event(record)).to eq(event)
         end
 
         specify "instruments" do
           instrumented_mapper = InstrumentedMapper.new(spy, ActiveSupport::Notifications)
-          subscribe_to("deserialize.mapper.ruby_event_store") do |notification_calls|
+          subscribe_to("record_to_event.mapper.ruby_event_store") do |notification_calls|
             instrumented_mapper.record_to_event(record)
             expect(notification_calls).to eq([{ record: record }])
           end
