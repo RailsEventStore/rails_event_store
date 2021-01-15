6ca7974e9d5d669ba1d77b5935ddb11c000d659b
Paweł Pacana <pawel.pacana@gmail.com>
2021-01-15 02:00:17 +0100

    rails_event_store -> ruby_event_store

--- ruby_event_store/lib/ruby_event_store/mappers/instrumented_mapper.rb
+++ ruby_event_store/lib/ruby_event_store/mappers/instrumented_mapper.rb
@@ -9,13 +9,13 @@ module RubyEventStore
       end
 
       def event_to_record(domain_event)
-        instrumentation.instrument("serialize.mapper.rails_event_store", domain_event: domain_event) do
+        instrumentation.instrument("serialize.mapper.ruby_event_store", domain_event: domain_event) do
           mapper.event_to_record(domain_event)
         end
       end
 
       def record_to_event(record)
-        instrumentation.instrument("deserialize.mapper.rails_event_store", record: record) do
+        instrumentation.instrument("deserialize.mapper.ruby_event_store", record: record) do
           mapper.record_to_event(record)
         end
       end
