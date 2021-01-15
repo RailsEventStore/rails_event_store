3941049348c37c4d4c1f3df3eca99d36f5c154a6
Paweł Pacana <pawel.pacana@gmail.com>
2021-01-15 13:34:39 +0100

    Add identity map transformation
    [#983]

--- contrib/minitest-ruby_event_store/test/test_helper.rb
+++ contrib/minitest-ruby_event_store/test/test_helper.rb
@@ -1,30 +1,7 @@
 require "minitest/ruby_event_store"
 require "ruby_event_store"
+require "ruby_event_store/transformations"
 require "mutant/minitest/coverage"
 require "minitest/autorun"
 require_relative '../../../support/helpers/time_enrichment'
 
-class IdentityMapTransformation
-  def initialize
-    @identity_map = {}
-  end
-
-  def dump(domain_event)
-    @identity_map[domain_event.event_id] = domain_event
-    metadata = domain_event.metadata.to_h
-    timestamp = metadata.delete(:timestamp)
-    valid_at = metadata.delete(:valid_at)
-    RubyEventStore::Record.new(
-      event_id:   domain_event.event_id,
-      metadata:   metadata,
-      data:       domain_event.data,
-      event_type: domain_event.event_type,
-      timestamp:  timestamp,
-      valid_at:   valid_at,
-    )
-  end
-
-  def load(record)
-    @identity_map.fetch(record.event_id)
-  end
-end
--- ruby_event_store-rspec/spec/spec_helper.rb
+++ ruby_event_store-rspec/spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require "aggregate_root"
 require "ruby_event_store"
 require "ruby_event_store/rspec"
+require "ruby_event_store/transformations"
 require_relative '../../support/helpers/rspec_defaults'
 
 FooEvent = Class.new(RubyEventStore::Event)
@@ -33,32 +34,6 @@ class TestAggregate
   def apply_baz_event(*)
   end
 end
-
-class IdentityMapTransformation
-  def initialize
-    @identity_map = {}
-  end
-
-  def dump(domain_event)
-    @identity_map[domain_event.event_id] = domain_event
-    metadata = domain_event.metadata.to_h
-    timestamp = metadata.delete(:timestamp)
-    valid_at = metadata.delete(:valid_at)
-    RubyEventStore::Record.new(
-      event_id:   domain_event.event_id,
-      metadata:   metadata,
-      data:       domain_event.data,
-      event_type: domain_event.event_type,
-      timestamp:  timestamp,
-      valid_at:   valid_at,
-    )
-  end
-
-  def load(record)
-    @identity_map.fetch(record.event_id)
-  end
-end
-
 
 class Handler
   def call(event)
