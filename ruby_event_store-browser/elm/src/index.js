fd0864ecb0f75d8404bcf6988ffc7a3388f31740
Paweł Pacana <pawel.pacana@gmail.com>
2021-03-02 23:36:56 +0100

    Converge remaining stylesheets into one

--- ruby_event_store-browser/elm/src/index.js
+++ ruby_event_store-browser/elm/src/index.js
@@ -1,6 +1,4 @@
-require("./style/tailwind.css");
 require("./style/style.css");
-require("./style/spinner.css");
 
 window.RubyEventStore = {};
 window.RubyEventStore.Browser = require("./Main.elm");
