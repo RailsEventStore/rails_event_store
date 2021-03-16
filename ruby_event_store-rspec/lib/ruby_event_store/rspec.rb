22a764bf495f25ee6c46c32858011df3e09c58d6
Rafał Łasocha <rafal@lasocha.net>
2021-03-16 16:06:27 +0100

    Move setting formatter to RSpec module
    setting configuration on RSpec is better than setting it on the class which is not part of the public API (RSpec::HavePublished)

--- ruby_event_store-rspec/lib/ruby_event_store/rspec.rb
+++ ruby_event_store-rspec/lib/ruby_event_store/rspec.rb
@@ -15,7 +15,21 @@ require_relative "rspec/have_applied"
 require_relative "rspec/have_subscribed_to_events"
 require_relative "rspec/publish"
 require_relative "rspec/apply"
+require_relative "rspec/crude_failure_message_formatter"
+require_relative "rspec/step_by_step_failure_message_formatter"
 require_relative "rspec/matchers"
+
+module RubyEventStore
+  module RSpec
+    def self.default_formatter=(new_formatter)
+      @@default_formatter = new_formatter
+    end
+
+    def self.default_formatter
+      @@default_formatter ||= CrudeFailureMessageFormatter
+    end
+  end
+end
 
 ::RSpec.configure do |config|
   config.include ::RubyEventStore::RSpec::Matchers
