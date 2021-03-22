2df95cd526f546304cfa2ad60927254bda838840
Rafał Łasocha <me@swistak35.com>
2021-03-22 23:29:53 +0100

    No more needed in InfluxDB 0.8.1

--- contrib/ruby_event_store-outbox/lib/ruby_event_store/outbox/metrics/influx.rb
+++ contrib/ruby_event_store-outbox/lib/ruby_event_store/outbox/metrics/influx.rb
@@ -6,14 +6,11 @@ module RubyEventStore
       class Influx
         def initialize(url)
           uri = URI.parse(url)
-          params = CGI.parse(uri.query || "")
           options = {
             url: url,
             async: true,
             time_precision: 'ns',
           }
-          options[:username] = params.fetch("username").first if params.key?("username")
-          options[:password] = params.fetch("password").first if params.key?("password")
           @influxdb_client = InfluxDB::Client.new(**options)
         end
 
--- contrib/ruby_event_store-outbox/spec/metrics/influx_spec.rb
+++ contrib/ruby_event_store-outbox/spec/metrics/influx_spec.rb
@@ -21,20 +21,6 @@ module RubyEventStore
         expect(client.config.time_precision).to eq('ns')
       end
 
-      # This is needed for cases when password contains characters which are allowed as influxdb password, but not allowed as password in URI standard -- in params they can be percent-encoded
-      specify "initialization with username & password as params works" do
-        influx_url = "http://influxdb.example:9000/db?username=someuser&password=some%24weird%25pass"
-
-        influx = Metrics::Influx.new(influx_url)
-
-        client = influx.influxdb_client
-        expect(client.config.username).to eq("someuser")
-        expect(client.config.password).to eq("some$weird%pass")
-        expect(client.config.hosts).to eq(["influxdb.example"])
-        expect(client.config.port).to eq(9000)
-        expect(client.config.database).to eq("db")
-      end
-
       specify "#write_point_queue defaults" do
         influx = Metrics::Influx.new(influx_url)
         client = influx.influxdb_client
