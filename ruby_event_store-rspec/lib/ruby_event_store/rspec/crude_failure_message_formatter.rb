2889b52a9ff8c892f6a35a119a5f8342f42116c6
Rafał Łasocha <me@swistak35.com>
2021-04-01 01:01:05 +0200

    Move things other way around
    Because we want matchers to be open for a new formatter more, we don't
    really gain anything from formatters being open to new
    matcher (because current formatters wouldn't help, and someone
    would have to write custom formatter anyway).

--- ruby_event_store-rspec/lib/ruby_event_store/rspec/crude_failure_message_formatter.rb
+++ ruby_event_store-rspec/lib/ruby_event_store/rspec/crude_failure_message_formatter.rb
@@ -3,8 +3,27 @@
 module RubyEventStore
   module RSpec
     module CrudeFailureMessageFormatter
+      class HavePublished
+        def initialize(differ)
+          @differ = differ
+        end
+
+        def failure_message(expected, events, _stream_name)
+          "expected #{expected.events} to be published, diff:" +
+            differ.diff(expected.events.to_s + "\n", events.to_a)
+        end
+
+        def failure_message_when_negated(expected, events)
+          "expected #{expected.events} not to be published, diff:" +
+            differ.diff(expected.events.to_s + "\n", events.to_a)
+        end
+
+        private
+        attr_reader :differ
+      end
+
       def self.have_published
-        HavePublished::CrudeFailureMessageFormatter
+        HavePublished
       end
     end
   end
