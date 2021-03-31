daf45084d8f286a9b479dddc1cb9e9f57ab822e5
Rafał Łasocha <me@swistak35.com>
2021-04-01 01:03:23 +0200

    Move Publish formatter to respective classes
    In StepByStepFormatter, the Crude code is copied over.

--- ruby_event_store-rspec/lib/ruby_event_store/rspec/crude_failure_message_formatter.rb
+++ ruby_event_store-rspec/lib/ruby_event_store/rspec/crude_failure_message_formatter.rb
@@ -22,8 +22,50 @@ module RubyEventStore
         attr_reader :differ
       end
 
+      class Publish
+        def failure_message(expected, events, stream)
+          if match_events?(expected)
+            <<~EOS
+            expected block to have published:
+
+            #{expected.events}
+
+            #{"in stream #{stream} " if stream}but published:
+
+            #{events}
+            EOS
+          else
+            "expected block to have published any events"
+          end
+        end
+
+        def failure_message_when_negated(expected, events, stream)
+          if match_events?(expected)
+            <<~EOS
+            expected block not to have published:
+
+            #{expected.events}
+
+            #{"in stream #{stream} " if stream}but published:
+
+            #{events}
+            EOS
+          else
+            "expected block not to have published any events"
+          end
+        end
+
+        def match_events?(expected)
+          !expected.events.empty?
+        end
+      end
+
       def self.have_published
         HavePublished
+      end
+
+      def self.publish
+        Publish
       end
     end
   end
