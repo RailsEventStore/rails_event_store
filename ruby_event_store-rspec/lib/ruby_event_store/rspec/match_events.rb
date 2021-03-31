f36ffb20b5e6e5ffc7ae63ba7bdcbfcd685f7494
Rafał Łasocha <me@swistak35.com>
2021-04-01 00:38:21 +0200

    Extract matching list of expectations to actual events
    Because this is really the same in all four matchers.

---
+++ ruby_event_store-rspec/lib/ruby_event_store/rspec/match_events.rb
@@ -0,0 +1,24 @@
+module RubyEventStore
+  module RSpec
+    class MatchEvents
+      def call(expected, events)
+        matcher(expected).matches?(events) && matches_count?(expected, events)
+      end
+
+      private
+
+      def matches_count?(expected, events)
+        return true unless expected.specified_count?
+        events.select { |e| expected.events.first === e }.size.equal?(expected.count)
+      end
+
+      def matcher(expected)
+        if expected.strict?
+          ::RSpec::Matchers::BuiltIn::Match.new(expected.events)
+        else
+          ::RSpec::Matchers::BuiltIn::Include.new(*expected.events)
+        end
+      end
+    end
+  end
+end
