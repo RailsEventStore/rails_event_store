f6953ce703d4ded0b1a227feb4f24334a4a5ebdc
Paweł Pacana <pawel.pacana@gmail.com>
2021-03-31 21:00:06 +0200

    Resolve the problem with publishing an event of custom event class
    The problem did not expose itself with default resolver which does to_s.
    String#to_s is still a string.
    
    Unfortunately event_type_resolver had to be exposed that way to avoid
    breaking API.
    
    It will be considered a subject to change in 3.0

--- ruby_event_store/lib/ruby_event_store/client.rb
+++ ruby_event_store/lib/ruby_event_store/client.rb
@@ -131,8 +131,8 @@ module RubyEventStore
     #
     # @param to [Class, String] type of events to get list of sybscribed handlers
     # @return [Array<Object, Class>]
-    def subscribers_for(event_type)
-      subscriptions.all_for(event_type)
+    def subscribers_for(event_class)
+      subscriptions.all_for(event_type_resolver.call(event_class))
     end
 
     # Builder object for collecting temporary handlers (subscribers)
@@ -327,6 +327,10 @@ module RubyEventStore
     end
 
     protected
+
+    def event_type_resolver
+      subscriptions.event_type_resolver
+    end
 
     def metadata=(value)
       @metadata.value = value
