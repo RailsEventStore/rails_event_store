c40dbcddd4ef812a03042b88b0f2bb9c8176ce92
Rafał Łasocha <me@swistak35.com>
2021-04-09 02:14:14 +0200

    Add tests for ExpectedCollection

---
+++ ruby_event_store-rspec/spec/ruby_event_store/rspec/expected_collection_spec.rb
@@ -0,0 +1,78 @@
+require "spec_helper"
+
+module RubyEventStore
+  module RSpec
+    ::RSpec.describe ExpectedCollection do
+      let(:matchers) { Object.new.tap { |o| o.extend(Matchers) } }
+
+      specify do
+        collection = ExpectedCollection.new([matchers.an_event(FooEvent)])
+
+        collection.exactly(3)
+
+        expect(collection.specified_count?).to be(true)
+        expect(collection.count).to eq(3)
+      end
+
+      specify do
+        collection = ExpectedCollection.new([matchers.an_event(FooEvent)])
+
+        collection.once
+
+        expect(collection.specified_count?).to be(true)
+        expect(collection.count).to eq(1)
+      end
+
+      specify do
+        collection = ExpectedCollection.new([])
+
+        expect(collection.specified_count?).to be(false)
+        expect(collection.strict?).to be(false)
+      end
+
+      specify do
+        collection = ExpectedCollection.new([expected_event = matchers.an_event(FooEvent)])
+
+        expect(collection.event).to eq(expected_event)
+      end
+
+      specify do
+        collection = ExpectedCollection.new([
+          matchers.an_event(FooEvent).with_data(a: 1),
+          matchers.an_event(FooEvent).with_data(a: 2),
+        ])
+
+        expect do
+          collection.event
+        end.to raise_error(NotSupported)
+      end
+
+      specify do
+        collection = ExpectedCollection.new([matchers.an_event(FooEvent)])
+
+        expect do
+          collection.exactly(0)
+        end.to raise_error(NotSupported)
+      end
+
+      specify do
+        collection = ExpectedCollection.new([matchers.an_event(FooEvent)])
+
+        collection.strict
+
+        expect(collection.strict?).to be(true)
+      end
+
+      specify do
+        collection = ExpectedCollection.new([
+          matchers.an_event(FooEvent).with_data(a: 1),
+          matchers.an_event(FooEvent).with_data(a: 2),
+        ])
+
+        expect do
+          collection.exactly(3)
+        end.to raise_error(NotSupported)
+      end
+    end
+  end
+end
