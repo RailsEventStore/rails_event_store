b0033ba618c15c947e40713b000e99e1900ede3a
Rafał Łasocha <me@swistak35.com>
2021-05-04 14:45:22 +0200

    Add test

--- contrib/ruby_event_store-flipper/spec/enable_spec.rb
+++ contrib/ruby_event_store-flipper/spec/enable_spec.rb
@@ -2,6 +2,17 @@ require "spec_helper"
 
 module RubyEventStore
   ::RSpec.describe Flipper do
+    specify "enable hooks only into flipper notifications" do
+      event_store = RubyEventStore::Client.new(repository: RubyEventStore::InMemoryRepository.new)
+      flipper = ::Flipper.new(::Flipper::Adapters::Memory.new, instrumenter: ActiveSupport::Notifications)
+
+      Flipper.enable(event_store)
+
+      ActiveSupport::Notifications.instrument('some_other_notification')
+
+      expect(event_store).not_to have_published
+    end
+
     specify do
       event_store = RubyEventStore::Client.new(repository: RubyEventStore::InMemoryRepository.new)
       flipper = ::Flipper.new(::Flipper::Adapters::Memory.new, instrumenter: ActiveSupport::Notifications)
