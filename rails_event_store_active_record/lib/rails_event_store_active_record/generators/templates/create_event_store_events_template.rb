90ec8b2b3d52f64eac85b34240e8492d753321e4
Paweł Pacana <pawel.pacana@gmail.com>
2021-05-13 21:18:58 +0200

    Coarse-grained conditionals
    Duplication to gain some readability

--- rails_event_store_active_record/lib/rails_event_store_active_record/generators/templates/create_event_store_events_template.rb
+++ rails_event_store_active_record/lib/rails_event_store_active_record/generators/templates/create_event_store_events_template.rb
@@ -2,36 +2,42 @@
 
 class CreateEventStoreEvents < ActiveRecord::Migration<%= migration_version %>
   def change
-    postgres = ActiveRecord::Base.connection.adapter_name == "PostgreSQL"
-
-    create_table(:event_store_events_in_streams, force: false) do |t|
-      t.string      :stream,      null: false
-      t.integer     :position,    null: true
-      if postgres
-        t.references :event, null: false, type: :uuid
-      else
-        t.references :event, null: false, type: :string, limit: 36
-      end
-      if postgres
-        t.datetime    :created_at,  null: false
-      else
-        t.datetime    :created_at,  null: false, precision: 6
-      end
-    end
-    add_index :event_store_events_in_streams, [:stream, :position], unique: true
-    add_index :event_store_events_in_streams, [:created_at]
-    add_index :event_store_events_in_streams, [:stream, :event_id], unique: true
-
+    postgres =
+      ActiveRecord::Base.connection.adapter_name == "PostgreSQL"
     if postgres
+      create_table(:event_store_events_in_streams, force: false) do |t|
+        t.string      :stream,      null: false
+        t.integer     :position,    null: true
+        t.references  :event,       null: false, type: :uuid
+        t.datetime    :created_at,  null: false
+      end
+      add_index :event_store_events_in_streams, [:stream, :position], unique: true
+      add_index :event_store_events_in_streams, [:created_at]
+      add_index :event_store_events_in_streams, [:stream, :event_id], unique: true
+
       create_table(:event_store_events, force: false) do |t|
         t.references  :event,       null: false, type: :uuid
         t.string      :event_type,  null: false
         t.<%= data_type %>      :metadata
-        t.<%= data_type %>      :data,        null: false
+        t.<%= data_type %>      :data, null: false
         t.datetime    :created_at,  null: false
         t.datetime    :valid_at,    null: true
       end
+      add_index :event_store_events, :event_id, unique: true
+      add_index :event_store_events, :created_at
+      add_index :event_store_events, :valid_at
+      add_index :event_store_events, :event_type
     else
+      create_table(:event_store_events_in_streams, force: false) do |t|
+        t.string      :stream,      null: false
+        t.integer     :position,    null: true
+        t.references  :event,       null: false, type: :string, limit: 36
+        t.datetime    :created_at,  null: false, precision: 6
+      end
+      add_index :event_store_events_in_streams, [:stream, :position], unique: true
+      add_index :event_store_events_in_streams, [:created_at]
+      add_index :event_store_events_in_streams, [:stream, :event_id], unique: true
+
       create_table(:event_store_events, force: false) do |t|
         t.references  :event,       null: false, type: :string, limit: 36
         t.string      :event_type,  null: false
@@ -40,10 +46,10 @@ class CreateEventStoreEvents < ActiveRecord::Migration<%= migration_version %>
         t.datetime    :created_at,  null: false, precision: 6
         t.datetime    :valid_at,    null: true,  precision: 6
       end
+      add_index :event_store_events, :event_id, unique: true
+      add_index :event_store_events, :created_at
+      add_index :event_store_events, :valid_at
+      add_index :event_store_events, :event_type
     end
-    add_index :event_store_events, :event_id, unique: true
-    add_index :event_store_events, :created_at
-    add_index :event_store_events, :valid_at
-    add_index :event_store_events, :event_type
   end
 end
