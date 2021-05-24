55f46b70bc34c37903b28b895a3a6461f8cad788
Paweł Pacana <pawel.pacana@gmail.com>
2021-05-24 08:25:33 +0200

    Bump development dependencies

--- rails_event_store_active_record/lib/rails_event_store_active_record/generators/templates/create_event_store_events_template.rb
+++ rails_event_store_active_record/lib/rails_event_store_active_record/generators/templates/create_event_store_events_template.rb
@@ -5,7 +5,7 @@ class CreateEventStoreEvents < ActiveRecord::Migration<%= migration_version %>
     postgres =
       ActiveRecord::Base.connection.adapter_name == "PostgreSQL"
     if postgres
-      create_table(:event_store_events_in_streams, force: false) do |t|
+      create_table(:event_store_events_in_streams, id: :bigserial, force: false) do |t|
         t.string      :stream,      null: false
         t.integer     :position,    null: true
         t.references  :event,       null: false, type: :uuid
@@ -15,7 +15,7 @@ class CreateEventStoreEvents < ActiveRecord::Migration<%= migration_version %>
       add_index :event_store_events_in_streams, [:created_at]
       add_index :event_store_events_in_streams, [:stream, :event_id], unique: true
 
-      create_table(:event_store_events, force: false) do |t|
+      create_table(:event_store_events, id: :bigserial, force: false) do |t|
         t.references  :event,       null: false, type: :uuid
         t.string      :event_type,  null: false
         t.<%= data_type %>      :metadata
