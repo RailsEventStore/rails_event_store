3b9e5f2658a2e143051d0f6ce5e8bdb46cea3dbe
Paweł Pacana <pawel.pacana@gmail.com>
2021-08-02 14:14:08 +0200

    Sane event factories for shared specs
    * class is a factory
    * some classed (Proto) make no sense with empty data
    * provide realistic defaults, where shared tests cannot bend to one
      particular implementation

--- ruby_event_store/spec/support/correlatable.rb
+++ ruby_event_store/spec/support/correlatable.rb
@@ -1,23 +1,23 @@
-RSpec.shared_examples :correlatable do |klass|
+RSpec.shared_examples :correlatable do |factory|
   specify "correlation_id && causation_id" do
-    e0 = klass.new(event_id: "doh", data: nil)
+    e0 = factory.call(event_id: "doh")
     expect(e0.event_id).to       eq("doh")
     expect(e0.correlation_id).to eq(nil)
     expect(e0.causation_id).to   eq(nil)
 
-    e1 = klass.new(event_id: "yay", data: nil)
+    e1 = factory.call(event_id: "yay")
     e1.correlate_with(e0)
     expect(e1.event_id).to       eq("yay")
     expect(e1.correlation_id).to eq("doh")
     expect(e1.causation_id).to   eq("doh")
 
-    e2 = klass.new(event_id: "jeb", data: nil)
+    e2 = factory.call(event_id: "jeb")
     e2.correlate_with(e1)
     expect(e2.event_id).to       eq("jeb")
     expect(e2.correlation_id).to eq("doh")
     expect(e2.causation_id).to   eq("yay")
 
-    event = klass.new(
+    event = factory.call(
       event_id: "mem",
       data: nil,
       metadata: {
@@ -30,8 +30,8 @@ RSpec.shared_examples :correlatable do |klass|
   end
 
   specify "chainable" do
-    e0 = klass.new(event_id: "doh", data: nil)
-    e1 = klass.new(event_id: "yay", data: nil)
+    e0 = factory.call(event_id: "doh")
+    e1 = factory.call(event_id: "yay")
     chained = e1.correlate_with(e0)
     
     expect(chained).to eq(e1)
@@ -39,14 +39,14 @@ RSpec.shared_examples :correlatable do |klass|
 
   specify "correlate_with a command" do
     command = Struct.new(:correlation_id, :message_id).new("correlation", "command_id")
-    event = klass.new(event_id: "event", data: nil)
+    event = factory.call(event_id: "event")
     event.correlate_with(command)
     expect(event.event_id).to       eq("event")
     expect(event.correlation_id).to eq("correlation")
     expect(event.causation_id).to   eq("command_id")
 
     command = Struct.new(:correlation_id, :message_id).new(nil, "command_id")
-    event = klass.new(event_id: "event", data: nil)
+    event = factory.call(event_id: "event")
     event.correlate_with(command)
     expect(event.event_id).to       eq("event")
     expect(event.correlation_id).to eq("command_id")
