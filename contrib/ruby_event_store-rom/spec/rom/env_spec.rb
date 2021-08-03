bad206a8273f862ee35aade7cdf956b6262cca45
Paweł Pacana <pawel.pacana@gmail.com>
2021-08-03 10:47:46 +0200

    Drop redundant in-memory ROM repository
    * one, no-dependency in-memory adapter is enough
    * no ROM maintainer actively working on this
    * additional work to maintain -- ROM APIs a bit different than in SQL

--- contrib/ruby_event_store-rom/spec/rom/env_spec.rb
+++ contrib/ruby_event_store-rom/spec/rom/env_spec.rb
@@ -2,17 +2,17 @@ require 'spec_helper'
 
 module RubyEventStore
   module ROM
-  RSpec.describe Env do
-    let(:rom_container) { ::ROM.container }
-    let(:instance) { Env.new(rom_container) }
+    RSpec.describe Env do
+      let(:rom_container) { ::ROM.container }
+      let(:instance) { Env.new(rom_container) }
 
-    specify '#container gives access to ROM container' do
-      expect(instance.rom_container).to be_a(::ROM::Container)
-    end
+      specify '#container gives access to ROM container' do
+        expect(instance.rom_container).to be_a(::ROM::Container)
+      end
 
-    specify '#logger gives access to Logger' do
-      expect(instance.logger).to be_a(Logger)
+      specify '#logger gives access to Logger' do
+        expect(instance.logger).to be_a(Logger)
+      end
     end
-  end
   end
 end
--- contrib/ruby_event_store-rom/spec/rom/unit_of_work_spec.rb
+++ contrib/ruby_event_store-rom/spec/rom/unit_of_work_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 require 'ruby_event_store/rom/sql'
-require 'ruby_event_store/spec/rom/event_repository_lint'
+require 'ruby_event_store/spec/rom/unit_of_work_lint'
 
 module RubyEventStore
   module ROM
-  RSpec.describe UnitOfWork do
-    let(:rom_helper) { Memory::SpecHelper.new }
+    RSpec.describe UnitOfWork do
+      let(:rom_helper) { SQL::SpecHelper.new }
 
-    it_behaves_like :unit_of_work, UnitOfWork
-  end
+      it_behaves_like :unit_of_work, UnitOfWork
+    end
   end
 end
