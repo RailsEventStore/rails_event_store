ebe57dddbb0c4b7b70999d9d0879ae2ad465ea35
Paweł Pacana <pawel.pacana@gmail.com>
2021-08-04 15:10:04 +0200

    Reduce surface area

--- contrib/ruby_event_store-rom/lib/ruby_event_store/rom/unit_of_work.rb
+++ contrib/ruby_event_store-rom/lib/ruby_event_store/rom/unit_of_work.rb
@@ -3,17 +3,13 @@
 module RubyEventStore
   module ROM
     class UnitOfWork
-      attr_reader :env
-
       def initialize(rom: ROM.env)
         @env = rom
       end
 
       def call(**options)
         gateway = @env.rom_container.gateways.fetch(options.delete(:gateway) { :default })
-
         yield(changesets = [])
-
         gateway.transaction(options) { changesets.each(&:commit) }
       end
     end
