883ad8737634dae778ab17560f4d7f2de8f0c1bb
Paweł Pacana <pawel.pacana@gmail.com>
2021-08-05 14:56:06 +0200

    Ensure setup described in documentation works as expected

---
+++ contrib/ruby_event_store-rom/spec/rom/rom_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+module RubyEventStore
+  module ROM
+    RSpec.describe "setup" do
+      specify "via database URL", mutant: "RubyEventStore::ROM.setup" do
+        config = ::ROM::Configuration.new(:sql, "sqlite://tmp/dummy.db")
+        config.default.run_migrations
+
+        rom = ROM.setup(:sql, "sqlite://tmp/dummy.db")
+        expect(rom.gateways.fetch(:default).connection.database_type).to eq(:sqlite)
+      end
+
+      specify "via ROM configuration", mutant: "RubyEventStore::ROM.setup" do
+        config = ::ROM::Configuration.new(:sql, "sqlite::memory:")
+        config.default.run_migrations
+
+        rom = ::RubyEventStore::ROM.setup(config)
+        expect(rom.gateways.fetch(:default).connection.database_type).to eq(:sqlite)
+      end
+    end
+  end
+end
