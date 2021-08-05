3e27020f3529033a1dd3ae81f0eb6971723d6b1e
Paweł Pacana <pawel.pacana@gmail.com>
2021-08-05 12:37:39 +0200

    Refactor

--- contrib/ruby_event_store-rom/spec/rom/event_repository_spec.rb
+++ contrib/ruby_event_store-rom/spec/rom/event_repository_spec.rb
@@ -73,7 +73,7 @@ module RubyEventStore
           event = SRecord.new(event_id: SecureRandom.uuid)
         ], Stream.new('stream'), ExpectedVersion.none)
 
-        repository.unit_of_work do
+        UnitOfWork.new(rom: env) do
           expect do
             repository.append_to_stream([
               SRecord.new(event_id: '9bedf448-e4d0-41a3-a8cd-f94aec7aa763')
