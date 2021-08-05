4eae173b33830c7611600cc3b6cc8ea47c896e11
Paweł Pacana <pawel.pacana@gmail.com>
2021-08-05 12:51:25 +0200

    Reduce according to officially supported and tested database matrix

--- contrib/ruby_event_store-rom/lib/ruby_event_store/rom/changesets/update_events.rb
+++ contrib/ruby_event_store-rom/lib/ruby_event_store/rom/changesets/update_events.rb
@@ -17,13 +17,7 @@ module RubyEventStore
         UPSERT_COLUMNS = %i[event_type data metadata valid_at].freeze
 
         def commit
-          if supports_on_duplicate_key_update?
-            commit_on_duplicate_key_update
-          elsif supports_insert_conflict_update?
-            commit_insert_conflict_update
-          else
-            raise "Database doesn't support upserts: #{relation.dataset.db.adapter_scheme}"
-          end
+          supports_on_duplicate_key_update? ? commit_on_duplicate_key_update : commit_insert_conflict_update
         end
 
         private
@@ -32,25 +26,12 @@ module RubyEventStore
           relation.dataset.db.adapter_scheme =~ /mysql/
         end
 
-        def supports_insert_conflict_update?
-          case relation.dataset.db.adapter_scheme
-          when :postgres
-            true
-          when :sqlite
-            # Sqlite 3.24.0+ supports PostgreSQL upsert syntax
-            relation.dataset.db.sqlite_version >= 32_400
-          else
-            false
-          end
-        end
-
         def commit_on_duplicate_key_update
           relation.dataset.on_duplicate_key_update(*UPSERT_COLUMNS).multi_insert(to_a)
         end
 
         def commit_insert_conflict_update
           relation.dataset.insert_conflict(
-            # constraint: 'index_name',
             target: :event_id,
             update: UPSERT_COLUMNS.each_with_object({}) do |column, memo|
               memo[column] = Sequel[:excluded][column]
