f7e470a76e9a71c48035b3c76d0390c2ee3f2fba
Piotrek Bator <piotrek@nibynic.com>
2021-09-10 12:40:30 +0200

    Flatten settings passed to Browser
    And remove a forgotten console.debug 😳

--- ruby_event_store-browser/elm/src/index.js
+++ ruby_event_store-browser/elm/src/index.js
@@ -3,6 +3,5 @@ require("./style/style.css");
 var Browser = require("./Main.elm");
 
 var settings = document.querySelector("meta[name='ruby-event-store-browser-settings']").getAttribute("content");
-console.debug("settings", settings);
 settings = JSON.parse(settings);
-Browser.Elm.Main.init(settings);
+Browser.Elm.Main.init({ flags: settings });
--- ruby_event_store-browser/lib/ruby_event_store/browser/app.rb
+++ ruby_event_store-browser/lib/ruby_event_store/browser/app.rb
@@ -88,11 +88,9 @@ module RubyEventStore
 
         def browser_settings
           JSON.dump({
-            flags: {
-              rootUrl:    routing.root_url,
-              apiUrl:     settings.api_url || routing.api_url,
-              resVersion: RubyEventStore::VERSION
-            }
+            rootUrl:    routing.root_url,
+            apiUrl:     settings.api_url || routing.api_url,
+            resVersion: RubyEventStore::VERSION
           })
         end
 
