a2a0b47c8107f9d297a2d1b8b6f6930262b83b2e
Rafał Łasocha <me@swistak35.com>
2021-10-27 11:51:11 +0200

    Add test metrics adapter

---
+++ contrib/ruby_event_store-outbox/lib/ruby_event_store/outbox/metrics/test.rb
@@ -0,0 +1,24 @@
+require 'influxdb'
+
+module RubyEventStore
+  module Outbox
+    module Metrics
+      class Test
+        def initialize
+          @operation_results = []
+          @queue_stats = []
+        end
+
+        def write_operation_result(operation, result)
+          @operation_results << { operation: operation, result: result }
+        end
+
+        def write_point_queue(enqueued: 0, failed: 0, remaining: 0, format: nil, split_key: nil)
+          @queue_stats << { enqueued: enqueued, failed: failed, remaining: remaining, format: format, split_key: split_key}
+        end
+
+        attr_reader :operation_results, :queue_stats
+      end
+    end
+  end
+end
