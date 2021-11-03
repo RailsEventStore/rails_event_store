dab99d974c5683609aca1d330165eea007a6262e
Rafał Łasocha <me@swistak35.com>
2021-11-03 07:33:43 +0100

    Load the template by default

--- contrib/ruby_event_store-flipper/spec/spec_helper.rb
+++ contrib/ruby_event_store-flipper/spec/spec_helper.rb
@@ -3,7 +3,6 @@
 require_relative '../../../support/helpers/rspec_defaults'
 require "ruby_event_store/flipper"
 require "ruby_event_store/rspec"
-require_relative '../lib/generators/ruby_event_store/flipper/templates/events.rb'
 require "ruby_event_store"
 require "active_support/notifications"
 require "flipper"
