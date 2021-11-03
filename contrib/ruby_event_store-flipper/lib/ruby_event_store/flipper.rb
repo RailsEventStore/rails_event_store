8cb534d2291635717c86824bdae1f1f75aa2fac1
Rafał Łasocha <me@swistak35.com>
2021-11-03 07:18:28 +0100

    RubyEventStore::Flipper with generated code

--- contrib/ruby_event_store-flipper/lib/ruby_event_store/flipper.rb
+++ contrib/ruby_event_store-flipper/lib/ruby_event_store/flipper.rb
@@ -11,44 +11,6 @@ module RubyEventStore
       instrumenter.subscribe("feature_operation.flipper", NotificationHandler.new(event_store, stream_pattern))
     end
 
-    module Events
-      class ToggleAdded < RubyEventStore::Event
-      end
-
-      class ToggleRemoved < RubyEventStore::Event
-      end
-
-      class ToggleGloballyEnabled < RubyEventStore::Event
-      end
-
-      class ToggleGloballyDisabled < RubyEventStore::Event
-      end
-
-      class ToggleEnabledForActor < RubyEventStore::Event
-      end
-
-      class ToggleDisabledForActor < RubyEventStore::Event
-      end
-
-      class ToggleEnabledForGroup < RubyEventStore::Event
-      end
-
-      class ToggleDisabledForGroup < RubyEventStore::Event
-      end
-
-      class ToggleEnabledForPercentageOfActors < RubyEventStore::Event
-      end
-
-      class ToggleDisabledForPercentageOfActors < RubyEventStore::Event
-      end
-
-      class ToggleEnabledForPercentageOfTime < RubyEventStore::Event
-      end
-
-      class ToggleDisabledForPercentageOfTime < RubyEventStore::Event
-      end
-    end
-
     class NotificationHandler
       def initialize(event_store, stream_pattern)
         @event_store = event_store
