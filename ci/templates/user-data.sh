c9ae7d547996526497f09770a0c2eb6ba0e29261
Rafał Łasocha <me@swistak35.com>
2021-11-12 11:50:49 +0100

    Install chrome

--- ci/templates/user-data.sh
+++ ci/templates/user-data.sh
@@ -73,6 +73,13 @@ echo ImageOS=ubuntu20 >> .env
 mkdir /opt/hostedtoolcache
 chmod -R 777 /opt/hostedtoolcache
 
+# Install Chrome.
+sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
+sudo echo "deb https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get -y update
+sudo apt-get -y install google-chrome-stable
+
+
 # Chromedriver
 export CHROME_DRIVER_VERSION=96.0.4664.35
 wget -N https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/
