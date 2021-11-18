59027ff8ad078e0948bbb190b8a6985c075dcc08
Rafał Łasocha <me@swistak35.com>
2021-11-18 13:46:36 +0100

    Fix the RubyEventStore::ROM for ruby 3.0

--- contrib/ruby_event_store-rom/lib/ruby_event_store/rom/mappers/event_to_serialized_record.rb
+++ contrib/ruby_event_store-rom/lib/ruby_event_store/rom/mappers/event_to_serialized_record.rb
@@ -7,12 +7,16 @@ module RubyEventStore
         relation :events
         register_as :event_to_serialized_record
 
-        map_array do
+        map do
           map_value :created_at, ->(time) { time.iso8601(TIMESTAMP_PRECISION) }
           map_value :valid_at,   ->(time) { time.iso8601(TIMESTAMP_PRECISION) }
           rename_keys created_at: :timestamp
           accept_keys %i[event_id data metadata event_type timestamp valid_at]
-          constructor_inject RubyEventStore::SerializedRecord
+          create_serialized_record
+        end
+
+        def create_serialized_record(attributes)
+          RubyEventStore::SerializedRecord.new(**attributes)
         end
       end
     end
--- contrib/ruby_event_store-rom/lib/ruby_event_store/rom/mappers/stream_entry_to_serialized_record.rb
+++ contrib/ruby_event_store-rom/lib/ruby_event_store/rom/mappers/stream_entry_to_serialized_record.rb
@@ -7,13 +7,17 @@ module RubyEventStore
         relation :stream_entries
         register_as :stream_entry_to_serialized_record
 
-        map_array do
+        map do
           unwrap :event, %i[event_id data metadata event_type created_at valid_at]
           map_value :created_at, ->(time) { time.iso8601(TIMESTAMP_PRECISION) }
           map_value :valid_at,   ->(time) { time.iso8601(TIMESTAMP_PRECISION) }
           rename_keys created_at: :timestamp
           accept_keys %i[event_id data metadata event_type timestamp valid_at]
-          constructor_inject RubyEventStore::SerializedRecord
+          create_serialized_record
+        end
+
+        def create_serialized_record(attributes)
+          RubyEventStore::SerializedRecord.new(**attributes)
         end
       end
     end
