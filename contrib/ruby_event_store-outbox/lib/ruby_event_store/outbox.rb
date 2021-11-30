2a052299ec81118ae123815fa9a57e5c0a0582df
Rafał Łasocha <me@swistak35.com>
2021-11-30 14:37:06 +0100

    Add LegacySidekiqScheduler
    To support RES 1.3.1, maybe for some time. SidekiqScheduler will be a
    scheduler for RES 2.x now.

--- contrib/ruby_event_store-outbox/lib/ruby_event_store/outbox.rb
+++ contrib/ruby_event_store-outbox/lib/ruby_event_store/outbox.rb
@@ -8,4 +8,5 @@ end
 require_relative 'outbox/fetch_specification'
 require_relative 'outbox/repository'
 require_relative 'outbox/sidekiq_scheduler'
+require_relative 'outbox/legacy_sidekiq_scheduler'
 require_relative 'outbox/version'
