a17a5998841594962ed2c245fd907161b06d03cd
Piotr Romańczuk <porbas@gmail.com>
2022-03-12 20:32:41 +0100

    Pass MUTANT_JOBS variable to mutant explicitly
    It seems mutant don't get it into account itself
    Mutant spawns two processes by default on CI regardless MUTANT_JOBS=1
    
    RedisIsolation is broken currently. I'll investigate it later.

--- support/make/mutant.mk
+++ support/make/mutant.mk
@@ -1,11 +1,16 @@
+ifdef MUTANT_JOBS
+	jobs=--jobs $(MUTANT_JOBS)
+else
+	jobs=
+endif
 mutate: ## Run mutation tests
 	@echo "Running mutation tests"
-	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run $(SUBJECT)
+	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run $(jobs) $(SUBJECT)
 
 mutate-fast: ## Run mutation tests with --fail-fast
 	@echo "Running mutation tests"
-	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run --fail-fast $(SUBJECT)
+	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run --fail-fast $(jobs) $(SUBJECT)
 
 mutate-changes: ## Run incremental mutation tests
 	@echo "Running mutation tests"
-	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run --since HEAD~1 $(SUBJECT)
+	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run --since HEAD~1 $(jobs) $(SUBJECT)
