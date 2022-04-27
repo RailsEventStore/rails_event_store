3b7771a4f6018b5976932933c8865bdebe26b482
deepj <deepjungle.maca@gmail.com>
2022-04-27 15:30:22 +0200

    Remove all Rails 5.2's things

--- rails_event_store/spec/dummy_5_2/test/test_helper.rb
+++
@@ -1,7 +0,0 @@
-ENV["RAILS_ENV"] ||= "test"
-require_relative "../config/environment"
-require "rails/test_help"
-
-class ActiveSupport::TestCase
-  # Add more helper methods to be used by all tests here...
-end
