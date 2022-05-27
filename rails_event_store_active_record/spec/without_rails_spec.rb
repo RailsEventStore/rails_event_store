925f05f8ffe2d615de5f9211cef2cefb884306ae
Paweł Pacana <pawel.pacana@gmail.com>
2022-05-27 20:24:37 +0200

    Less surprises when running the same version as the latest one in matrix
    I.e. different behaviour of insert_all/upsert_all that is harder to
    debug from subprocess-run test.
    
    Such issues manifest itself when running on matrix anyway, this saves
    some headaches figuring out why this one particular test would behave
    differently than as in default Gemfile.

--- rails_event_store_active_record/spec/without_rails_spec.rb
+++ rails_event_store_active_record/spec/without_rails_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe RailsEventStoreActiveRecord, :integration do
         source 'https://rubygems.org'
         gem 'ruby_event_store',                path: '../ruby_event_store'
         gem 'rails_event_store_active_record', path: '../rails_event_store_active_record'
-        gem 'activerecord', '6.0.3.4'
+        gem 'activerecord', '7.0.3'
         gem 'pg',           '1.2.3'
         gem 'mysql2',       '0.5.3'
         gem 'sqlite3',      '1.4.2'
