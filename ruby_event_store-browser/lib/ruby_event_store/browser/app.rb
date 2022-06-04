c48335ae4d7db609249b0835164c4fc9f096e585
Paweł Pacana <pawel.pacana@gmail.com>
2022-06-04 19:35:31 +0200

    Dead-simple route pattern matching with regex over full-blown router
    https://github.com/RailsEventStore/rails_event_store/issues/958
    
    * Rack::Static to serve compiled JS from public path
    * Rack::Request for nice facade over Rack env
    * lacking Rack::Protection, which can be re-added if neeeded
    * case pattern matching seems simple and sufficient for such a tiny app that Browser is,
      good thing that it uses === and that plays nicely with Regexp,
    * one drawback is that it is also quite verbose without that fancy DSL for regex-matching
      URL patterns behind the scenes
    * a few rough edges like unnamed and a bit too wide (thus order dependent) regexes,
      decoding params and endless string-vs-symbol whack-a-mole
    
    Examples of router implementation:
    https://github.com/pjb3/rack-router
    https://github.com/mperham/sidekiq/commit/9ea167db16bf7f4cc9ee94967d0ea0b4dee6a7c5

--- ruby_event_store-browser/lib/ruby_event_store/browser/app.rb
+++ ruby_event_store-browser/lib/ruby_event_store/browser/app.rb
@@ -1,98 +1,122 @@
 # frozen_string_literal: true
 
 require_relative "../browser"
-require "sinatra/base"
+require "rack"
+require "erb"
+require "json"
 
 module RubyEventStore
   module Browser
-    class App < Sinatra::Base
+    class App
       def self.for(
         event_store_locator:,
         host: nil,
         path: nil,
         api_url: nil,
-        environment: :production,
+        environment: nil,
         related_streams_query: DEFAULT_RELATED_STREAMS_QUERY
       )
-        self.tap do |app|
-          app.settings.instance_exec do
-            set :event_store_locator, event_store_locator
-            set :related_streams_query, -> { related_streams_query }
-            set :host, host
-            set :root_path, path
-            set :api_url, api_url
-            set :environment, environment
-          end
+        Rack::Builder.new do
+          use Rack::Static,
+              urls: {
+                "/ruby_event_store_browser.js" => "ruby_event_store_browser.js",
+                "/bootstrap.js" => "bootstrap.js"
+              },
+              root: "#{__dir__}/../../../public"
+          run App.new(
+                event_store_locator: event_store_locator,
+                related_streams_query: related_streams_query,
+                host: host,
+                root_path: path,
+                api_url: api_url
+              )
         end
       end
 
-      use Rack::Static,
-          urls: {
-            "/ruby_event_store_browser.js" => "ruby_event_store_browser.js",
-            "/bootstrap.js" => "bootstrap.js"
-          },
-          root: "#{__dir__}/../../../public"
-
-      configure { set :protection, except: :path_traversal }
-
-      get "/api/events/:id" do
-        json Event.new(event_store: settings.event_store_locator, event_id: params["id"])
-      rescue RubyEventStore::EventNotFound
-        404
+      def initialize(event_store_locator:, related_streams_query:, host:, root_path:, api_url:)
+        @event_store_locator = event_store_locator
+        @related_streams_query = related_streams_query
+        @host = host
+        @root_path = root_path
+        @api_url = api_url
       end
 
-      get "/api/streams/:stream_name" do
-        json GetStream.new(
-               stream_name: params["stream_name"],
-               routing: routing,
-               related_streams_query: settings.related_streams_query
-             )
-      end
+      def call(env)
+        request = Rack::Request.new(env)
+        routing = Routing.new(host || request.base_url, root_path || request.script_name)
+        event_store = event_store_locator.call
 
-      get "/api/streams/:stream_name/relationships/events" do
-        json GetEventsFromStream.new(
-               event_store: settings.event_store_locator,
-               stream_name: params["stream_name"],
-               page: params["page"],
-               routing: routing
-             )
-      end
-
-      get %r{/(events/.*|streams/.*)?} do
-        erb(<<~ERB, locals: { path: settings.root_path || request.script_name })
-          <!DOCTYPE html>
-          <html>
-            <head>
-              <title>RubyEventStore::Browser</title>
-              <meta name="ruby-event-store-browser-settings" content='<%= browser_settings %>'>
-            </head>
-            <body>
-              <script type="text/javascript" src="<%= path %>/ruby_event_store_browser.js"></script>
-              <script type="text/javascript" src="<%= path %>/bootstrap.js"></script>
-            </body>
-          </html>
-        ERB
-      end
-
-      helpers do
-        def routing
-          Routing.new(settings.host || request.base_url, settings.root_path || request.script_name)
-        end
-
-        def browser_settings
-          JSON.dump(
-            {
-              rootUrl: routing.root_url,
-              apiUrl: settings.api_url || routing.api_url,
-              resVersion: RubyEventStore::VERSION
-            }
+        case [request.request_method, request.path]
+        in "GET", %r{/api/events/(.+)}
+          render_json_api(Event.new(event_store: event_store, event_id: URI.decode_www_form_component($1)))
+        in "GET", %r{/api/streams/(.+)/relationships/events}
+          render_json_api(
+            GetEventsFromStream.new(
+              event_store: event_store,
+              routing: routing,
+              stream_name: URI.decode_www_form_component($1),
+              page: request.params["page"]
+            )
           )
+        in "GET", %r{/api/streams/(.+)}
+          render_json_api(
+            GetStream.new(
+              stream_name: URI.decode_www_form_component($1),
+              routing: routing,
+              related_streams_query: @related_streams_query
+            )
+          )
+        in "GET", %r{/(events/.*|streams/.*)?}
+          render_html(
+            ERB.new(<<~HTML).result_with_hash(path: routing.root_path, browser_settings: browser_settings(routing))
+              <!DOCTYPE html>
+              <html>
+                <head>
+                  <title>RubyEventStore::Browser</title>
+                  <meta name="ruby-event-store-browser-settings" content='<%= browser_settings %>'>
+                </head>
+                <body>
+                  <script type="text/javascript" src="<%= path %>/ruby_event_store_browser.js"></script>
+                  <script type="text/javascript" src="<%= path %>/bootstrap.js"></script>
+                </body>
+              </html>
+          HTML
+          )
+        else
+          render_404
         end
+      rescue RubyEventStore::EventNotFound
+        render_404
+      end
 
-        def json(data)
-          content_type "application/vnd.api+json"
-          JSON.dump data.to_h
-        end
+      private
+
+      attr_reader :event_store_locator, :related_streams_query, :host, :root_path, :api_url
+
+      def render_404
+        [404, {}, []]
+      end
+
+      def render_html(html)
+        [200, html_content_type, [html]]
+      end
+
+      def render_json_api(body)
+        [200, json_api_content_type, [JSON.dump(body.to_h)]]
+      end
+
+      def html_content_type
+        { "Content-Type" => "text/html;charset=utf-8" }
+      end
+
+      def json_api_content_type
+        { "Content-Type" => "application/vnd.api+json" }
+      end
+
+      def browser_settings(routing)
+        JSON.dump(
+          { rootUrl: routing.root_url, apiUrl: api_url || routing.api_url, resVersion: RubyEventStore::VERSION }
+        )
       end
     end
   end
