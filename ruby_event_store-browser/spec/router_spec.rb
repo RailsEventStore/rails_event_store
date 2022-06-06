a601f50bf5bb13858aa59307a0c391084b09bee4
Paweł Pacana <pawel.pacana@gmail.com>
2022-06-06 23:14:13 +0200

    Focused test for quicker mutations
    https://github.com/mbj/mutant/blob/main/docs/mutant-rspec.md#test-selection

---
+++ ruby_event_store-browser/spec/router_spec.rb
@@ -0,0 +1,76 @@
+require "spec_helper"
+
+module RubyEventStore
+  module Browser
+    RSpec.describe Router do
+      specify "params from multiple route segments" do
+        expect do |probe|
+          router = Router.new
+          router.add_route("GET", "/some/:funky/:segment", &probe)
+          router.handle(mock_request("GET", "/some/foo/bar"))
+        end.to yield_with_args({ "funky" => "foo", "segment" => "bar" })
+      end
+
+      specify "encoded params" do
+        expect do |probe|
+          router = Router.new
+          router.add_route("GET", "/:try_me", &probe)
+          router.handle(mock_request("GET", "/foo%2Dbar.xml"))
+        end.to yield_with_args({ "try_me" => "foo-bar.xml" })
+      end
+
+      specify "route params and query params" do
+        expect do |probe|
+          router = Router.new
+          router.add_route("GET", "/:try_me", &probe)
+          router.handle(mock_request("GET", "/dont?try=me"))
+        end.to yield_with_args({ "try_me" => "dont", "try" => "me" })
+      end
+
+      specify "not found by path" do
+        expect do
+          router = Router.new
+          router.add_route("GET", "/some", &no_op)
+          router.handle(mock_request("GET", "/none"))
+        end.to raise_error(Router::NoMatch)
+      end
+
+      specify "not found by method" do
+        expect do
+          router = Router.new
+          router.add_route("GET", "/some", &no_op)
+          router.handle(mock_request("POST", "/some"))
+        end.to raise_error(Router::NoMatch)
+      end
+
+      specify "root route when mounted" do
+        router = Router.new
+        router.add_route("GET", "/", &no_content)
+
+        response =
+          Rack::MockRequest
+            .new(
+              Rack::Builder.new do
+                map "/mounted" do
+                  run ->(env) { router.handle(Rack::Request.new(env)) }
+                end
+              end
+            )
+            .get("/mounted")
+        expect(response).to be_no_content
+      end
+
+      def no_content
+        Proc.new { [204, {}, []] }
+      end
+
+      def no_op
+        Proc.new {}
+      end
+
+      def mock_request(method, path, mount_point: nil)
+        Rack::Request.new(Rack::MockRequest.env_for(path, method: method, script_mame: mount_point))
+      end
+    end
+  end
+end
