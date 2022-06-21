8399c413ad64396798652c8b50c1d83cc6dc91c5
Paweł Pacana <pawel.pacana@gmail.com>
2022-06-21 21:35:44 +0200

    Let sample run on Rails 7 settings

--- contrib/dres_rails/spec/dummy/config/application.rb
+++ contrib/dres_rails/spec/dummy/config/application.rb
@@ -18,7 +18,7 @@ require "dres_rails"
 module Dummy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 7.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- contrib/dres_rails/spec/dummy/db/schema.rb
+++ contrib/dres_rails/spec/dummy/db/schema.rb
@@ -2,31 +2,31 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_09_123950) do
+ActiveRecord::Schema[7.0].define(version: 2018_08_09_123950) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "dres_rails_queue_jobs", force: :cascade do |t|
     t.integer "queue_id", null: false
     t.string "event_id", null: false
     t.string "state", null: false
-    t.index %w[queue_id event_id], name: "index_dres_rails_queue_jobs_on_queue_id_and_event_id"
+    t.index ["queue_id", "event_id"], name: "index_dres_rails_queue_jobs_on_queue_id_and_event_id"
   end
 
   create_table "dres_rails_queues", force: :cascade do |t|
     t.string "name", null: false
     t.string "last_processed_event_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["name"], name: "index_dres_rails_queues_on_name", unique: true
   end
+
 end
