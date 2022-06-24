359ba08fa7167714ed05eb1f705548c0eed32b7e
Paweł Pacana <pawel.pacana@gmail.com>
2022-06-24 17:57:53 +0200

    Revert "Weak-public but still some kind-of-public Browser API"
    This reverts commit 4f6ea6d8517443026d17de7867e1be51a8358e53.

--- ruby_event_store-browser/spec/root_spec.rb
+++ ruby_event_store-browser/spec/root_spec.rb
@@ -3,26 +3,22 @@ require "spec_helper"
 module RubyEventStore
   RSpec.describe Browser do
     specify { expect(test_client.get("/")).to be_ok }
-
     specify { expect(test_client.post("/")).to be_not_found }
-
     specify { expect(test_client.get("/streams/all")).to be_ok }
-
     specify do
       event_store.append(DummyEvent.new(event_id: event_id))
       expect(test_client.get("/events/#{event_id}")).to be_ok
     end
-
     specify { expect(test_client.get("/").content_type).to eq("text/html;charset=utf-8") }
-
-    specify { expect(Browser::App.for(event_store_locator: -> { event_store })).to be_a(Browser::App) }
 
     let(:event_store) { RubyEventStore::Client.new(repository: RubyEventStore::InMemoryRepository.new) }
     let(:test_client) { TestClient.new(app_builder(event_store), "www.example.com") }
     let(:event_id) { SecureRandom.uuid }
 
     def app_builder(event_store)
-      Rack::Lint.new(RubyEventStore::Browser::App.for(event_store_locator: -> { event_store }))
+      Rack::Lint.new(
+        RubyEventStore::Browser::App.for(event_store_locator: -> { event_store })
+      )
     end
   end
 end
