2286cbbbf0ab725135f055c360b65f94a4977842
Paweł Pacana <pawel.pacana@gmail.com>
2022-11-22 00:05:33 +0100

    Revert "Pass MUTANT_JOBS variable to mutant explicitly"
    This reverts commit a17a5998841594962ed2c245fd907161b06d03cd.

--- support/make/mutant.mk
+++ support/make/mutant.mk
@@ -1,16 +1,11 @@
-ifdef MUTANT_JOBS
-	jobs=--jobs $(MUTANT_JOBS)
-else
-	jobs=
-endif
 mutate: ## Run mutation tests
 	@echo "Running mutation tests"
-	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run $(jobs) $(SUBJECT)
+	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run $(SUBJECT)
 
 mutate-fast: ## Run mutation tests with --fail-fast
 	@echo "Running mutation tests"
-	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run --fail-fast $(jobs) $(SUBJECT)
+	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run --fail-fast $(SUBJECT)
 
 mutate-changes: ## Run incremental mutation tests
 	@echo "Running mutation tests"
-	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run --since HEAD~1 $(jobs) $(SUBJECT)
+	@DATABASE_URL=$(DATABASE_URL) bundle exec mutant run --since HEAD~1 $(SUBJECT)
