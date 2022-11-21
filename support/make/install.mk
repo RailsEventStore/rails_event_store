6e9d73c48b97578f2aa6b7784a4c89f2487f6452
Paweł Pacana <pawel.pacana@gmail.com>
2022-11-21 20:02:59 +0100

    The make install tries to doo too much
    Separate `install` from `install-all`. I'm not sure we need installing
    all Gemfile gems as a default. It is sure usefull to have install-all
    and update-all for maintenance-related work.

--- support/make/install.mk
+++ support/make/install.mk
@@ -1,10 +1,14 @@
-install: ## Install gem dependencies
+install: ## Install gem dependencies for current Gemfile 
+	@echo "Installing gem dependencies"
+	@bundle install
+
+install-all: ## Install gem dependencies from all Gemfiles
 	@echo "Installing gem dependencies"
 	@find . -name Gemfile\* -a ! -name \*.lock -exec bundle install --gemfile {} \;
 
-update: ## Update gem dependencies
+update-all: ## Update gem dependencies in all Gemfiles
 	@echo "Updating gem dependencies"
 	@find . -name Gemfile\* -a ! -name \*.lock -exec bundle update --gemfile {} \;
 
-local-install:
+local-install-all:
 	@find . -name Gemfile\* -a ! -name \*.lock -exec bundle install --local --quiet --gemfile {} \;
