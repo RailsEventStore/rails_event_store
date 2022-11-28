a8b5ebe6f36480a13d1516c1fb4036a7d6790689
Łukasz Reszke <lukaszreszke93@gmail.com>
2022-11-28 22:52:12 +0100

    Fix tests

--- ruby_event_store-active_record/spec/no_warnings_spec.rb
+++ ruby_event_store-active_record/spec/no_warnings_spec.rb
@@ -3,9 +3,9 @@ require "spec_helper"
 module RubyEventStore
 module ActiveRecord
   RSpec.describe "no warnings", mutant: false do
-    specify { expect(rails_event_store_active_record_warnings).to eq([]) }
+    specify { expect(ruby_event_store_active_record_warnings).to eq([]) }
 
-    def rails_event_store_active_record_warnings
+    def ruby_event_store_active_record_warnings
       warnings.select { |w| w =~ %r{lib/ruby_event_store/active_record} }
     end
 
