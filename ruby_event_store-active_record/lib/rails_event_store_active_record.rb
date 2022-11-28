15307fa9a8f46b4ac897db404db4ba3bac531dd0
Łukasz Reszke <lukaszreszke93@gmail.com>
2022-11-28 19:33:17 +0100

    Backward compability

---
+++ ruby_event_store-active_record/lib/rails_event_store_active_record.rb
@@ -0,0 +1,13 @@
+require 'ruby_event_store-active_record/lib/ruby_event_store-active_record'
+
+warn <<~EOW
+  The 'rails_event_store_active_record' gem has been renamed.
+
+  Please change your Gemfile or gemspec
+  to reflect its new name:
+
+    'ruby_event_store-active_record'
+
+EOW
+
+RailsEventStoreActiveRecord = RubyEventStore::ActiveRecord
