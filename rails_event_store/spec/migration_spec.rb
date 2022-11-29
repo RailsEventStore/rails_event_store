c00fc37f24317bc691cfcc2ebca26e7afa7400aa
Paweł Pacana <pawel.pacana@gmail.com>
2022-11-29 22:16:33 +0100

    Failing test case for broken public API
    It is expected that initial schema migration task is available at:
    
      rails g rails_event_store_active_record:migration

---
+++ rails_event_store/spec/migration_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module RailsEventStore
+  RSpec.describe "Migration" do
+    specify do
+      expect(Rails::Generators.public_namespaces).to include("rails_event_store_active_record:migration")
+    end
+  end
+end
