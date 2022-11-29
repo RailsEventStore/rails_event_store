7d3dd3f75338fead86a2a2f1f2eaae66e3d550e4
Szymon Fiedler <szymon.fiedler@gmail.com>
2022-11-29 13:48:24 +0100

    Get off my lawn
    Those are internals not meant to be used outside the RubyEventStore.

--- rails_event_store_active_record/lib/rails_event_store_active_record/event.rb
+++ rails_event_store_active_record/lib/rails_event_store_active_record/event.rb
@@ -7,10 +7,12 @@ module RailsEventStoreActiveRecord
     self.primary_key = :id
     self.table_name = "event_store_events"
   end
+  private_constant :Event
 
   class EventInStream < ::ActiveRecord::Base
     self.primary_key = :id
     self.table_name = "event_store_events_in_streams"
     belongs_to :event, primary_key: :event_id
   end
+  private_constant :EventInStream
 end
