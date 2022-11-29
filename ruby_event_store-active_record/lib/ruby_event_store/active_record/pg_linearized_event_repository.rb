516b17a999a40b5743ab4b7d07f6952f1cb7a3d4
Łukasz Reszke <lukaszreszke93@gmail.com>
2022-11-29 11:15:52 +0100

    scope

--- ruby_event_store-active_record/lib/ruby_event_store/active_record/pg_linearized_event_repository.rb
+++ ruby_event_store-active_record/lib/ruby_event_store/active_record/pg_linearized_event_repository.rb
@@ -4,8 +4,8 @@ module RubyEventStore
   module ActiveRecord
     class PgLinearizedEventRepository < EventRepository
       def start_transaction(&proc)
-        ActiveRecord::Base.transaction(requires_new: true) do
-          ActiveRecord::Base.connection.execute("SELECT pg_advisory_xact_lock(1845240511599988039) as l").each {}
+        ::ActiveRecord::Base.transaction(requires_new: true) do
+          ::ActiveRecord::Base.connection.execute("SELECT pg_advisory_xact_lock(1845240511599988039) as l").each {}
 
           proc.call
         end
