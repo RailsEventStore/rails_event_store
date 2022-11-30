efe4d7009e9e9f4491037f7d52c355f6bf26f8f5
pjurewicz <piotr.jurewicz@arkency.com>
2022-11-30 13:28:10 +0100

    Do not create snapshot on first event

--- aggregate_root/spec/snapshot_repository_spec.rb
+++ aggregate_root/spec/snapshot_repository_spec.rb
@@ -46,22 +46,11 @@ module AggregateRoot
 
       order.create
 
+      repository = AggregateRoot::SnapshotRepository.new(event_store)
       repository.store(order, stream_name)
 
       expect_certain_event_types(stream_name, 'Orders::Events::OrderCreated')
-      expect_snapshot(stream_name)
-    end
-
-    specify do
-      order = order_klass.new(uuid)
-
-      order.create
-
-      repository = AggregateRoot::SnapshotRepository.new(event_store, 2)
-      repository.store(order, stream_name)
-
-      expect_certain_event_types(stream_name, 'Orders::Events::OrderCreated')
-      expect_snapshot(stream_name)
+      expect_no_snapshot(stream_name)
 
       order.expire
       repository.store(order, stream_name)
@@ -77,6 +66,7 @@ module AggregateRoot
 
       repository = AggregateRoot::SnapshotRepository.new(event_store)
       repository.store(order, stream_name)
+      expect_snapshot(stream_name)
 
       order_from_snapshot = repository.load(order_klass.new(uuid), stream_name)
 
@@ -92,10 +82,11 @@ module AggregateRoot
     end
 
     def expect_snapshot(stream_name)
-      expect(event_store.read.stream("#{stream_name}_snapshots").map(&:event_type))
-        .to eq(
-              ['AggregateRoot::SnapshotRepository::Snapshot']
-            )
+      expect_certain_event_types("#{stream_name}_snapshots", 'AggregateRoot::SnapshotRepository::Snapshot')
+    end
+
+    def expect_no_snapshot(stream_name)
+      expect_certain_event_types("#{stream_name}_snapshots")
     end
   end
 end
