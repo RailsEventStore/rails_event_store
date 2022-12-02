b76e96cbdaed51bffe263cbfbf8dbc0bdc200941
Łukasz Reszke <lukaszreszke93@gmail.com>
2022-12-02 18:10:38 +0100

    Remove rename warning

--- ruby_event_store-active_record/lib/rails_event_store_active_record.rb
+++ ruby_event_store-active_record/lib/rails_event_store_active_record.rb
@@ -1,13 +1,3 @@
 require "ruby_event_store/active_record"
 
-warn <<~EOW
-  The 'rails_event_store_active_record' gem has been renamed.
-
-  Please change your Gemfile or gemspec
-  to reflect its new name:
-
-    'ruby_event_store-active_record'
-
-EOW
-
 RailsEventStoreActiveRecord = RubyEventStore::ActiveRecord
