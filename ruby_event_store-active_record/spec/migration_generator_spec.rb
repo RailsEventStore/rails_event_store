50f4bb69bfe531e717d2ff6486cd2b079aa9cf05
Łukasz Reszke <lukaszreszke93@gmail.com>
2022-12-02 20:50:52 +0100

    Move default path responsibility
    Rake task has to know about default path. The 'db/migrate' is specific
    use case for rails that rake is supposed to serve.
    
    The MigrationGenerator however might be reused in different context. In
    that context the default path may differ.

--- ruby_event_store-active_record/spec/migration_generator_spec.rb
+++ ruby_event_store-active_record/spec/migration_generator_spec.rb
@@ -5,98 +5,67 @@ module RubyEventStore
   module ActiveRecord
     RSpec.describe MigrationGenerator do
       around { |example| SilenceStdout.silence_stdout { example.run } }
+      around do |example|
+        begin
+          @dir = Dir.mktmpdir(nil, "./")
+          example.call
+        ensure
+          FileUtils.rm_r(@dir)
+        end
+      end
 
       before { allow(Time).to receive(:now).and_return(Time.new(2022, 11, 30, 21, 37, 00)) }
 
-      specify "it is created at default migration path when path is not specified" do
-        dir = FileUtils.mkdir_p("./db/migrate").first
-
-        RubyEventStore::ActiveRecord::MigrationGenerator.new.call("binary")
-
-        expect(migration_exists?(dir)).to be_truthy
-      ensure
-        FileUtils.rm_rf(dir)
-        FileUtils.rmdir("./db")
-      end
-
       specify "it is created within specified directory" do
-        dir = Dir.mktmpdir(nil, "./")
+        migration_generator(@dir)
 
-        migration_generator(dir)
-
-        expect(migration_exists?(dir)).to be_truthy
-      ensure
-        FileUtils.rm_r(dir)
+        expect(migration_exists?(@dir)).to be_truthy
       end
 
       specify "returns path to migration file" do
-        dir = Dir.mktmpdir(nil, "./")
+        path = migration_generator(@dir)
 
-        path = migration_generator(dir)
-
-        expect(path).to match(File.expand_path("../#{dir}", __dir__))
-      ensure
-        FileUtils.rm_r(dir)
+        expect(path).to match(File.expand_path("../#{@dir}", __dir__))
       end
 
       specify "uses particular migration version" do
-        dir = Dir.mktmpdir(nil, "./")
+        migration_generator(@dir)
 
-        migration_generator(dir)
-
-        expect(read_migration(dir)).to match(/ActiveRecord::Migration\[4\.2\]$/)
-      ensure
-        FileUtils.rm_r(dir)
+        expect(read_migration(@dir)).to match(/ActiveRecord::Migration\[4\.2\]$/)
       end
 
       specify "creates migration with binary data type" do
-        dir = Dir.mktmpdir(nil, "./")
+        migration_generator(@dir, "binary")
 
-        migration_generator(dir, "binary")
-
-        expect(read_migration(dir)).to match(/t.binary\s+:data/)
-        expect(read_migration(dir)).to match(/t.binary\s+:metadata/)
-      ensure
-        FileUtils.rm_r(dir)
+        expect(read_migration(@dir)).to match(/t.binary\s+:data/)
+        expect(read_migration(@dir)).to match(/t.binary\s+:metadata/)
       end
 
       specify "creates migration with json data type" do
-        dir = Dir.mktmpdir(nil, "./")
+        migration_generator(@dir, "json")
 
-        migration_generator(dir, "json")
-
-        expect(read_migration(dir)).to match(/t.json\s+:data/)
-        expect(read_migration(dir)).to match(/t.json\s+:metadata/)
-      ensure
-        FileUtils.rm_r(dir)
+        expect(read_migration(@dir)).to match(/t.json\s+:data/)
+        expect(read_migration(@dir)).to match(/t.json\s+:metadata/)
       end
 
       specify "creates migration with jsonb data type" do
-        dir = Dir.mktmpdir(nil, "./")
+        migration_generator(@dir, "jsonb")
 
-        migration_generator(dir, "jsonb")
-
-        expect(read_migration(dir)).to match(/t.jsonb\s+:data/)
-        expect(read_migration(dir)).to match(/t.jsonb\s+:metadata/)
-      ensure
-        FileUtils.rm_r(dir)
+        expect(read_migration(@dir)).to match(/t.jsonb\s+:data/)
+        expect(read_migration(@dir)).to match(/t.jsonb\s+:metadata/)
       end
 
       specify "raises error when data type is not supported" do
-        dir = Dir.mktmpdir(nil, "./")
-
-        expect { migration_generator(dir, "invalid") }.to raise_error(
+        expect { migration_generator(@dir, "invalid") }.to raise_error(
           ArgumentError,
           "Invalid value for --data-type option. Supported for options are: binary, json, jsonb."
         )
-      ensure
-        FileUtils.rm_r(dir)
       end
 
       private
 
       def migration_generator(dir, data_type = "binary")
-        RubyEventStore::ActiveRecord::MigrationGenerator.new.call(data_type, migration_path: dir)
+        RubyEventStore::ActiveRecord::MigrationGenerator.new.call(data_type, dir)
       end
 
       def migration_exists?(dir)
