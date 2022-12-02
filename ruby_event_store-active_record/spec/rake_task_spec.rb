5686ecf4a01bd2e075789faef65ea9f8539310be
Łukasz Reszke <lukaszreszke93@gmail.com>
2022-12-02 13:39:59 +0100

    change context

--- ruby_event_store-active_record/spec/rake_task_spec.rb
+++ ruby_event_store-active_record/spec/rake_task_spec.rb
@@ -17,7 +17,7 @@ module RailsEventStore
       allow(Time).to receive(:now).and_return(Time.new(2022, 11, 30, 21, 37, 00))
     end
 
-    context "when data_type option is specified" do
+    context "when custom path provided" do
       it "is created" do
        load File.expand_path(File.expand_path("../../", __FILE__) + "/Rakefile")
         SilenceStdout.silence_stdout do
