e5ac18762c9c1cffccb19cd6679f70eac8305508
Paweł Pacana <pawel.pacana@gmail.com>
2022-12-06 20:24:51 +0100

    Formatting

--- railseventstore.org/config.rb
+++ railseventstore.org/config.rb
@@ -12,12 +12,13 @@ activate :external_pipeline,
          latency: 1
 
 set :markdown_engine, :redcarpet
+set :markdown, tables: true, autolink: true, fenced_code_blocks: true, with_toc_data: true, no_intra_emphasis: true
+
 set :res_version_v1, "1.3.1"
 set :res_version_v2, "2.6.0"
 set :res_version, "2.6.0"
-set :markdown, tables: true, autolink: true, fenced_code_blocks: true, with_toc_data: true, no_intra_emphasis: true
 
 page "/"
-page "/docs/v1/*", layout: "documentation", locals: { version: "v1" }
-page "/docs/v2/*", layout: "documentation", locals: { version: "v2" }
+page "/docs/v1/*", locals: { version: "v1" }, layout: "documentation"
+page "/docs/v2/*", locals: { version: "v2" }, layout: "documentation"
 page "*", locals: { version: "v2" }
