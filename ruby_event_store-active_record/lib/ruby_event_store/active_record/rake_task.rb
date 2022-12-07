fb2afea02b59ba7a6b156db734c9993d9e491f7f
Łukasz Reszke <lukaszreszke93@gmail.com>
2022-12-07 09:53:12 +0100

    Expose ActiveRecord migration tasks

--- ruby_event_store-active_record/lib/ruby_event_store/active_record/rake_task.rb
+++ ruby_event_store-active_record/lib/ruby_event_store/active_record/rake_task.rb
@@ -1,4 +1,22 @@
 # frozen_string_literal: true
 
 require "ruby_event_store/active_record"
+require "active_record"
 load "ruby_event_store/active_record/tasks/migration_tasks.rake"
+
+include ActiveRecord::Tasks
+
+db_dir = './db'
+
+DatabaseTasks.env = ENV['ENV'] || 'development'
+DatabaseTasks.db_dir = db_dir
+DatabaseTasks.database_configuration = ENV['DATABASE_CONFIG'] || YAML.load(File.read('./config/database.yml'), aliases: true)
+
+DatabaseTasks.migrations_paths = File.join(db_dir, 'migrate')
+
+task :environment do
+  ActiveRecord::Base.configurations = DatabaseTasks.database_configuration
+  ActiveRecord::Base.establish_connection DatabaseTasks.env
+end
+
+load 'active_record/railties/databases.rake'
