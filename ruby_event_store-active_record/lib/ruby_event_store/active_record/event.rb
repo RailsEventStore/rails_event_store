e46958ee7546026fe7c4e7ec8f2be01859004d93
Szymon Fiedler <szymon.fiedler@gmail.com>
2022-12-09 12:24:47 +0100

    Big :brain: time
    Passing Proc leads us here:
      * https://github.com/rails/rails/blob/f04074cfa134df01ead7ec8c90cf03feeb749dc1/activerecord/lib/active_record/attributes.rb#L232
    
    And then here
      * https://github.com/rails/rails/blob/f04074cfa134df01ead7ec8c90cf03feeb749dc1/activerecord/lib/active_record/attributes.rb#L266

--- ruby_event_store-active_record/lib/ruby_event_store/active_record/event.rb
+++ ruby_event_store-active_record/lib/ruby_event_store/active_record/event.rb
@@ -8,8 +8,22 @@ module RubyEventStore
       self.primary_key = :id
       self.table_name = "event_store_events"
 
-      attribute :data, ActiveModel::Type::Value.new
-      attribute :metadata, ActiveModel::Type::Value.new
+      attribute :data,
+                Proc.new {
+                  if %i[json jsonb].include?(self.columns_hash["data"].type)
+                    ActiveModel::Type::Value.new
+                  else
+                    ActiveModel::Type::Binary.new
+                  end
+                }
+      attribute :metadata,
+                Proc.new {
+                  if %i[json jsonb].include?(self.columns_hash["metadata"].type)
+                    ActiveModel::Type::Value.new
+                  else
+                    ActiveModel::Type::Binary.new
+                  end
+                }
     end
     private_constant :Event
 
