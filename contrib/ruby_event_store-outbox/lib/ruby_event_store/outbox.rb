c90fb324eee41882c30a48cff1201faa533cd952
Rafał Łasocha <rafal@lasocha.net>
2022-12-16 22:54:31 +0100

    Drop LegacySidekiqScheduler

--- contrib/ruby_event_store-outbox/lib/ruby_event_store/outbox.rb
+++ contrib/ruby_event_store-outbox/lib/ruby_event_store/outbox.rb
@@ -10,7 +10,6 @@ end
 require_relative "outbox/fetch_specification"
 require_relative "outbox/repository"
 require_relative "outbox/sidekiq_scheduler"
-require_relative "outbox/legacy_sidekiq_scheduler"
 require_relative "outbox/version"
 require_relative "outbox/tempo"
 require_relative "outbox/batch_result"
