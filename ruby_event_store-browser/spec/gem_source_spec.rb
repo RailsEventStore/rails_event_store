6f707be567c2729adb6ceae88493104286495a28
Szymon Fiedler <szymon.fiedler@gmail.com>
2022-12-19 12:11:51 +0100

    Prettier

--- ruby_event_store-browser/spec/gem_source_spec.rb
+++ ruby_event_store-browser/spec/gem_source_spec.rb
@@ -13,8 +13,7 @@ module RubyEventStore
       end
 
       specify "local path source" do
-        path =
-          "/Users/mostlyobvious/Code/rails_event_store/ruby_event_store-browser/lib"
+        path = "/Users/mostlyobvious/Code/rails_event_store/ruby_event_store-browser/lib"
         source = GemSource.new([random_unrelated_path, path])
 
         expect(source.version).to be_nil
@@ -22,8 +21,7 @@ module RubyEventStore
       end
 
       specify "rubygems source" do
-        path =
-          "/Users/mostlyobvious/.rubies/ruby-3.1.2/lib/ruby/gems/3.1.0/gems/ruby_event_store-browser-2.5.1/lib"
+        path = "/Users/mostlyobvious/.rubies/ruby-3.1.2/lib/ruby/gems/3.1.0/gems/ruby_event_store-browser-2.5.1/lib"
         source = GemSource.new([random_unrelated_path, path])
 
         expect(source.version).to eq("2.5.1")
