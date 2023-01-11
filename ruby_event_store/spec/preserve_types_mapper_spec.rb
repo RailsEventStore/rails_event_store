54f9d378ded761ec2bc6450f63354dc19f46a282
Szymon Fiedler <szymon.fiedler@gmail.com>
2023-01-11 15:30:25 +0100

    Integration testing of types being preserved
    Co-authored-by: Paweł Pacana <pawel.pacana@gmail.com>
    Co-authored-by: Łukasz Reszke <lukaszreszke93@gmail.com>

--- ruby_event_store/spec/preserve_types_mapper_spec.rb
+++ ruby_event_store/spec/preserve_types_mapper_spec.rb
@@ -1,24 +1,30 @@
 require "spec_helper"
 require "active_support"
 require "active_support/core_ext/time"
+require "json"
 
 module RubyEventStore
   module Mappers
     ::RSpec.describe PreserveTypesMapper do
+      let(:client) do
+        RubyEventStore::Client.new(
+          mapper: PreserveTypesMapper.new,
+          repository: RubyEventStore::InMemoryRepository.new(serializer: JSON)
+        )
+      end
 
       specify "preserves type of ActiveSupport::TimeWithZone" do
         time_zone = Time.zone
         Time.zone = "Europe/Warsaw"
 
-        record = subject.event_to_record(TestEvent.new(
-          data: { active_support_time_with_zone: Time.zone.local(2021, 8, 5, 12, 0, 0) })
-        )
+        record =
+          subject.event_to_record(
+            TestEvent.new(data: { active_support_time_with_zone: Time.zone.local(2021, 8, 5, 12, 0, 0) })
+          )
 
-        expect(record.metadata[:types])
-          .to eq({
-                   :data => { :active_support_time_with_zone => ["Symbol", "ActiveSupport::TimeWithZone"] },
-                   :metadata => {}
-                 })
+        expect(record.metadata[:types]).to eq(
+          { data: { active_support_time_with_zone: %w[Symbol ActiveSupport::TimeWithZone] }, metadata: {} }
+        )
       ensure
         Time.zone = time_zone
       end
@@ -26,38 +32,39 @@ module RubyEventStore
       specify "preserves type of Time" do
         record = subject.event_to_record(TestEvent.new(data: { time: Time.new(2021, 8, 5, 12, 0, 0) }))
 
-        expect(record.metadata[:types]).to eq({ :data => { :time => ["Symbol", "Time"] }, :metadata => {} })
+        expect(record.metadata[:types]).to eq({ data: { time: %w[Symbol Time] }, metadata: {} })
       end
 
       specify "preserves type of UTC Time" do
         record = subject.event_to_record(TestEvent.new(data: { time: Time.new(2021, 8, 5, 12, 0, 0).utc }))
 
-        expect(record.metadata[:types]).to eq({ :data => { :time => ["Symbol", "Time"] }, :metadata => {} })
+        expect(record.metadata[:types]).to eq({ data: { time: %w[Symbol Time] }, metadata: {} })
       end
 
       specify "preserves type of DateTime" do
         record = subject.event_to_record(TestEvent.new(data: { datetime: DateTime.new(2021, 8, 5, 12, 0, 0) }))
 
-        expect(record.metadata[:types]).to eq({ :data => { :datetime => ["Symbol", "DateTime"] }, :metadata => {} })
+        expect(record.metadata[:types]).to eq({ data: { datetime: %w[Symbol DateTime] }, metadata: {} })
       end
 
       specify "preserves type of Date" do
         record = subject.event_to_record(TestEvent.new(data: { date: Date.new(2021, 8, 5, 12) }))
 
-        expect(record.metadata[:types]).to eq({ :data => { :date => ["Symbol", "Date"] }, :metadata => {} })
+        expect(record.metadata[:types]).to eq({ data: { date: %w[Symbol Date] }, metadata: {} })
       end
 
       specify "preserves type of metadata keys" do
-        record = subject.event_to_record(TestEvent.new(data: {}, metadata: { "hashhash": "test" }))
+        record = subject.event_to_record(TestEvent.new(data: {}, metadata: { hashhash: "test" }))
 
-        expect(record.metadata[:types]).to eq({ data: {}, metadata: { hashhash: ["Symbol", "String"] } })
+        expect(record.metadata[:types]).to eq({ data: {}, metadata: { hashhash: %w[Symbol String] } })
       end
 
       specify "symbolizes metadata keys" do
-        record = subject.event_to_record(TestEvent.new(data: {}, metadata: { "hashhash": "test" }))
+        record = subject.event_to_record(TestEvent.new(data: {}, metadata: { hashhash: "test" }))
 
-        expect(record.metadata)
-          .to eq({ :hashhash => "test", types: { data: {}, metadata: { hashhash: ["Symbol", "String"] } } })
+        expect(record.metadata).to eq(
+          { hashhash: "test", types: { data: {}, metadata: { hashhash: %w[Symbol String] } } }
+        )
       end
 
       specify "reads type of ActiveSupport::TimeWithZone" do
@@ -65,38 +72,46 @@ module RubyEventStore
         Time.zone = "Europe/Warsaw"
 
         event = TestEvent.new(data: { active_support_time_with_zone: Time.zone.local(2021, 8, 5, 12, 0, 0) })
-        expect(subject.record_to_event(subject.event_to_record(event))).to eq(event)
+        client.append(event)
+
+        expect(client.read.event(event.event_id)).to eq(event)
       ensure
         Time.zone = time_zone
       end
 
       specify "reads type of Time" do
         event = TestEvent.new(data: { time: Time.new(2021, 8, 5, 12, 0, 0) })
-        expect(subject.record_to_event(subject.event_to_record(event))).to eq(event)
+        client.append(event)
+
+        expect(client.read.event(event.event_id)).to eq(event)
       end
 
       specify "reads type of UTC Time" do
         event = TestEvent.new(data: { time: Time.new(2021, 8, 5, 12, 0, 0).utc })
+        client.append(event)
 
-        expect(subject.record_to_event(subject.event_to_record(event))).to eq(event)
+        expect(client.read.event(event.event_id)).to eq(event)
       end
 
       specify "reads type of DateTime" do
         event = TestEvent.new(data: { datetime: DateTime.new(2021, 8, 5, 12, 0, 0) })
+        client.append(event)
 
-        expect(subject.record_to_event(subject.event_to_record(event))).to eq(event)
+        expect(client.read.event(event.event_id)).to eq(event)
       end
 
       specify "reads type of Date" do
         event = TestEvent.new(data: { date: Date.new(2021, 8, 5, 12) })
+        client.append(event)
 
-        expect(subject.record_to_event(subject.event_to_record(event))).to eq(event)
+        expect(client.read.event(event.event_id)).to eq(event)
       end
 
       specify "reads type of metadata keys" do
-        event = TestEvent.new(data: {}, metadata: { "hashhash": "test" })
+        event = TestEvent.new(data: {}, metadata: { hashhash: "test" })
+        client.append(event)
 
-        expect(subject.record_to_event(subject.event_to_record(event))).to eq(event)
+        expect(client.read.event(event.event_id)).to eq(event)
       end
     end
   end
