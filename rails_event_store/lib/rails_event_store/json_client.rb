676f9b54496453aa1812e56c135f73b3d89cc06f
Szymon Fiedler <szymon.fiedler@gmail.com>
2023-01-12 15:19:01 +0100

    It reveals intention better
    Co-authored-by: Paweł Pacana <pawel.pacana@gmail.com>
    Co-authored-by: Łukasz Reszke <lukaszreszke93@gmail.com>

--- rails_event_store/lib/rails_event_store/json_client.rb
+++ rails_event_store/lib/rails_event_store/json_client.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module RailsEventStore
-  class PgClient < Client
+  class JSONClient < Client
     def initialize
       super(
         repository: RubyEventStore::ActiveRecord::EventRepository.new(serializer: RubyEventStore::NULL),
