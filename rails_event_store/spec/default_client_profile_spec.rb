998e0ed49f10f3db6b8c975954b44f7345d88a84
Szymon Fiedler <szymon.fiedler@gmail.com>
2023-01-12 16:24:19 +0100

    Mind the tests
    Co-authored-by: Łukasz Reszke <lukaszreszke93@gmail.com>

--- rails_event_store/spec/default_client_profile_spec.rb
+++ rails_event_store/spec/default_client_profile_spec.rb
@@ -2,32 +2,12 @@ require "spec_helper"
 
 module RailsEventStore
   ::RSpec.describe DefaultClientProfile do
-    specify do
-      expect(DefaultClientProfile.new.call("PostgreSQL"))
-        .to eq(
-              <<~PROFILE
-                RailsEventStore::PgClient.new
-            PROFILE
-            )
-    end
+    specify { expect(DefaultClientProfile.new.call("PostgreSQL")).to eq("RailsEventStore::JSONClient.new") }
 
-    specify do
-      expect(DefaultClientProfile.new.call("postgresql"))
-        .to eq(
-              <<~PROFILE
-                RailsEventStore::PgClient.new
-            PROFILE
-            )
-    end
+    specify { expect(DefaultClientProfile.new.call("postgresql")).to eq("RailsEventStore::JSONClient.new") }
 
-    specify do
-      expect(DefaultClientProfile.new.call("sqlite3"))
-        .to eq("RailsEventStore::Client.new")
-    end
+    specify { expect(DefaultClientProfile.new.call("sqlite3")).to eq("RailsEventStore::Client.new") }
 
-    specify do
-      expect(DefaultClientProfile.new.call("mysql2"))
-        .to eq("RailsEventStore::Client.new")
-    end
+    specify { expect(DefaultClientProfile.new.call("mysql2")).to eq("RailsEventStore::Client.new") }
   end
 end
