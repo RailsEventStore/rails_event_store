3b5a8167f0d81223a51f83a48c03496d44e244d9
Szymon Fiedler <szymon.fiedler@gmail.com>
2023-01-12 15:54:06 +0100

    Inline PreserveTypes transformation into JSONClient
    We want to have top–level preconfigured clients rather than bunch of
    preconfigured low–level components. This seems to be more flexible
    approach, but still providing sensible defaults.
    
    Co-authored-by: Łukasz Reszke <lukaszreszke93@gmail.com>

--- rails_event_store/lib/rails_event_store/json_client.rb
+++ rails_event_store/lib/rails_event_store/json_client.rb
@@ -2,10 +2,31 @@
 
 module RailsEventStore
   class JSONClient < Client
-    def initialize
+    def initialize(repository: RubyEventStore::ActiveRecord::EventRepository.new(serializer: RubyEventStore::NULL))
       super(
-        repository: RubyEventStore::ActiveRecord::EventRepository.new(serializer: RubyEventStore::NULL),
-        mapper: RubyEventStore::Mappers::PreserveTypesMapper.new
+        mapper:
+          RubyEventStore::Mappers::PipelineMapper.new(
+            RubyEventStore::Mappers::Pipeline.new(
+              RubyEventStore::Mappers::Transformation::PreserveTypes
+                .new
+                .register(Symbol, serializer: ->(v) { v.to_s }, deserializer: ->(v) { v.to_sym })
+                .register(
+                  Time,
+                  serializer: ->(v) { v.iso8601(RubyEventStore::TIMESTAMP_PRECISION) },
+                  deserializer: ->(v) { Time.iso8601(v) }
+                )
+                .register(
+                  ActiveSupport::TimeWithZone,
+                  serializer: ->(v) { v.iso8601(RubyEventStore::TIMESTAMP_PRECISION) },
+                  deserializer: ->(v) { Time.iso8601(v).in_time_zone },
+                  stored_type: ->(*) { "ActiveSupport::TimeWithZone" }
+                )
+                .register(Date, serializer: ->(v) { v.iso8601 }, deserializer: ->(v) { Date.iso8601(v) })
+                .register(DateTime, serializer: ->(v) { v.iso8601 }, deserializer: ->(v) { DateTime.iso8601(v) }),
+              RubyEventStore::Mappers::Transformation::SymbolizeMetadataKeys.new
+            )
+          ),
+        repository: repository
       )
     end
   end
---
+++ rails_event_store/spec/json_client_spec.rb
@@ -0,0 +1,66 @@
+require "spec_helper"
+require "active_support"
+require "active_support/core_ext/time"
+require "json"
+
+module RailsEventStore
+  ::RSpec.describe JSONClient do
+    let(:client) { JSONClient.new(repository: RubyEventStore::InMemoryRepository.new(serializer: JSON)) }
+
+    specify "reads type of ActiveSupport::TimeWithZone" do
+      time_zone = Time.zone
+      Time.zone = "Europe/Warsaw"
+
+      event =
+        DummyEvent.new(data: { active_support_time_with_zone: with_precision(Time.zone.local(2021, 8, 5, 12, 0, 0.1)) })
+      client.append(event)
+
+      event_ = client.read.event(event.event_id)
+      expect(event_).to eq(event)
+      expect(event_.data[:active_support_time_with_zone]).to be_a(ActiveSupport::TimeWithZone)
+    ensure
+      Time.zone = time_zone
+    end
+
+    specify "reads type of Time" do
+      event = DummyEvent.new(data: { time: with_precision(Time.new(2021, 8, 5, 12, 0, 0.1)) })
+      client.append(event)
+
+      expect(client.read.event(event.event_id)).to eq(event)
+    end
+
+    specify "reads type of UTC Time" do
+      event = DummyEvent.new(data: { time: with_precision(Time.new(2021, 8, 5, 12, 0, 0.1).utc) })
+      client.append(event)
+
+      expect(client.read.event(event.event_id)).to eq(event)
+    end
+
+    specify "reads type of DateTime" do
+      event = DummyEvent.new(data: { datetime: DateTime.new(2021, 8, 5, 12, 0, 0) })
+      client.append(event)
+
+      expect(client.read.event(event.event_id)).to eq(event)
+    end
+
+    specify "reads type of Date" do
+      event = DummyEvent.new(data: { date: Date.new(2021, 8, 5, 12) })
+      client.append(event)
+
+      expect(client.read.event(event.event_id)).to eq(event)
+    end
+
+    specify "reads type of metadata keys" do
+      event = DummyEvent.new(data: {}, metadata: { hashhash: "test" })
+      client.append(event)
+
+      expect(client.read.event(event.event_id)).to eq(event)
+    end
+
+    private
+
+    def with_precision(time)
+      time.round(RubyEventStore::TIMESTAMP_PRECISION)
+    end
+  end
+end
