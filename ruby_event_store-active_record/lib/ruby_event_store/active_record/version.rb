b3d6f21082a4dc6e87af549d33fd53e24bf58d59
Paweł Pacana <pawel.pacana@gmail.com>
2023-01-13 18:48:09 +0100

    Version v2.8.0

--- ruby_event_store-active_record/lib/ruby_event_store/active_record/version.rb
+++ ruby_event_store-active_record/lib/ruby_event_store/active_record/version.rb
@@ -2,6 +2,6 @@
 
 module RubyEventStore
   module ActiveRecord
-    VERSION = "2.7.0"
+    VERSION = "2.8.0"
   end
 end
--- ruby_event_store-browser/lib/ruby_event_store/browser/version.rb
+++ ruby_event_store-browser/lib/ruby_event_store/browser/version.rb
@@ -2,6 +2,6 @@
 
 module RubyEventStore
   module Browser
-    VERSION = "2.7.0"
+    VERSION = "2.8.0"
   end
 end
