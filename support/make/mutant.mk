fb912ee9fdbfac4dcc7cccd8cc87b3a2a5f470be
Paweł Pacana <pawel.pacana@gmail.com>
2023-01-13 18:39:29 +0100

    Follow-up to 337057ea
    Co-authored-by: Łukasz Reszke <lukaszreszke93@gmail.com>

--- support/make/mutant.mk
+++ support/make/mutant.mk
@@ -1,11 +1,11 @@
 mutate: ## Run mutation tests
 	@echo "Running mutation tests"
-	@DATA_TYPE=$(DATA_TYPE) DATABASE_URL=$(DATABASE_URL) bundle exec mutant run $(SUBJECT)
+	@bundle exec mutant run $(SUBJECT)
 
 mutate-fast: ## Run mutation tests with --fail-fast
 	@echo "Running mutation tests"
-	@DATA_TYPE=$(DATA_TYPE) DATABASE_URL=$(DATABASE_URL) bundle exec mutant run --fail-fast $(SUBJECT)
+	@bundle exec mutant run --fail-fast $(SUBJECT)
 
 mutate-changes: ## Run incremental mutation tests
 	@echo "Running mutation tests"
-	@DATA_TYPE=$(DATA_TYPE) DATABASE_URL=$(DATABASE_URL) bundle exec mutant run --since HEAD~1 $(SUBJECT)
+	@bundle exec mutant run --since HEAD~1 $(SUBJECT)
