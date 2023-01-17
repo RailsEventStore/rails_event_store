b45461cd744421a8783c51b2652746b6d710857d
Szymon Fiedler <szymon.fiedler@gmail.com>
2023-01-17 14:03:26 +0100

    Align the API and configuration with Client
    Co-authored-by: Paweł Pacana <pawel.pacana@gmail.com>

--- rails_event_store/lib/rails_event_store/json_client.rb
+++ rails_event_store/lib/rails_event_store/json_client.rb
@@ -2,33 +2,54 @@
 
 module RailsEventStore
   class JSONClient < Client
-    def initialize(repository: RubyEventStore::ActiveRecord::EventRepository.new(serializer: RubyEventStore::NULL))
+    def initialize(
+      mapper: RubyEventStore::Mappers::Default.new,
+      repository: RubyEventStore::ActiveRecord::EventRepository.new(serializer: RubyEventStore::NULL),
+      subscriptions: RubyEventStore::Subscriptions.new,
+      dispatcher: RubyEventStore::ComposedDispatcher.new(
+        RailsEventStore::AfterCommitAsyncDispatcher.new(
+          scheduler: ActiveJobScheduler.new(serializer: RubyEventStore::Serializers::YAML)
+        ),
+        RubyEventStore::Dispatcher.new
+      ),
+      clock: default_clock,
+      correlation_id_generator: default_correlation_id_generator,
+      request_metadata: default_request_metadata
+    )
       super(
         mapper:
-          RubyEventStore::Mappers::PipelineMapper.new(
-            RubyEventStore::Mappers::Pipeline.new(
-              RubyEventStore::Mappers::Transformation::PreserveTypes
-                .new
-                .register(Symbol, serializer: ->(v) { v.to_s }, deserializer: ->(v) { v.to_sym })
-                .register(
-                  Time,
-                  serializer: ->(v) { v.iso8601(RubyEventStore::TIMESTAMP_PRECISION) },
-                  deserializer: ->(v) { Time.iso8601(v) }
-                )
-                .register(
-                  ActiveSupport::TimeWithZone,
-                  serializer: ->(v) { v.iso8601(RubyEventStore::TIMESTAMP_PRECISION) },
-                  deserializer: ->(v) { Time.iso8601(v).in_time_zone },
-                  stored_type: ->(*) { "ActiveSupport::TimeWithZone" }
-                )
-                .register(Date, serializer: ->(v) { v.iso8601 }, deserializer: ->(v) { Date.iso8601(v) })
-                .register(DateTime, serializer: ->(v) { v.iso8601 }, deserializer: ->(v) { DateTime.iso8601(v) },)
-                .register(BigDecimal, serializer: ->(v) { v.to_s }, deserializer: ->(v) { BigDecimal(v) }),
-              RubyEventStore::Mappers::Transformation::SymbolizeMetadataKeys.new
-            )
+          RubyEventStore::Mappers::InstrumentedMapper.new(
+            RubyEventStore::Mappers::PipelineMapper.new(
+              RubyEventStore::Mappers::Pipeline.new(
+                RubyEventStore::Mappers::Transformation::PreserveTypes
+                  .new
+                  .register(Symbol, serializer: ->(v) { v.to_s }, deserializer: ->(v) { v.to_sym })
+                  .register(
+                    Time,
+                    serializer: ->(v) { v.iso8601(RubyEventStore::TIMESTAMP_PRECISION) },
+                    deserializer: ->(v) { Time.iso8601(v) }
+                  )
+                  .register(
+                    ActiveSupport::TimeWithZone,
+                    serializer: ->(v) { v.iso8601(RubyEventStore::TIMESTAMP_PRECISION) },
+                    deserializer: ->(v) { Time.iso8601(v).in_time_zone },
+                    stored_type: ->(*) { "ActiveSupport::TimeWithZone" }
+                  )
+                  .register(Date, serializer: ->(v) { v.iso8601 }, deserializer: ->(v) { Date.iso8601(v) })
+                  .register(DateTime, serializer: ->(v) { v.iso8601 }, deserializer: ->(v) { DateTime.iso8601(v) })
+                  .register(BigDecimal, serializer: ->(v) { v.to_s }, deserializer: ->(v) { BigDecimal(v) }),
+                RubyEventStore::Mappers::Transformation::SymbolizeMetadataKeys.new
+              )
+            ),
+            ActiveSupport::Notifications
           ),
-        repository: repository
+        repository: RubyEventStore::InstrumentedRepository.new(repository, ActiveSupport::Notifications),
+        subscriptions: RubyEventStore::InstrumentedSubscriptions.new(subscriptions, ActiveSupport::Notifications),
+        clock: clock,
+        correlation_id_generator: correlation_id_generator,
+        dispatcher: RubyEventStore::InstrumentedDispatcher.new(dispatcher, ActiveSupport::Notifications)
       )
+      @request_metadata = request_metadata
     end
   end
 end
