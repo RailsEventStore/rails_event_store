5d768930191cf43cdb2691c2f7e51bc134abbee2
Szymon Fiedler <szymon.fiedler@gmail.com>
2023-01-17 14:14:27 +0100

    Predefined clients are sufficient abstraction
    we can imagine that someone would use this class in production and made
    decision on client on app boot, rather when picking the desired client
    :see_no_evil:
    
    Co-authored-by: Paweł Pacana <pawel.pacana@gmail.com>

--- rails_event_store/lib/rails_event_store/default_client_profile.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module RailsEventStore
-  class DefaultClientProfile
-    def call(adapter)
-      adapter.downcase.eql?("postgresql") ? "RailsEventStore::JSONClient.new" : "RailsEventStore::Client.new"
-    end
-  end
-end
