a5ef54f862539caac76a8ef92116c1ecc7e5dd36
Paweł Pacana <pawel.pacana@gmail.com>
2023-01-30 11:47:32 +0100

    Revert "Revert "No idea how to achieve the same in Rails < 6.1""
    This reverts commit b5a4e306630124fc8b9db01b836d0bc8da39c0d3.

--- ruby_event_store-active_record/lib/ruby_event_store/active_record/event.rb
+++ ruby_event_store-active_record/lib/ruby_event_store/active_record/event.rb
@@ -8,12 +8,14 @@ module RubyEventStore
       self.primary_key = :id
       self.table_name = "event_store_events"
 
-      skip_json_serialization = ->(initial_column_type) do
-        %i[json jsonb].include?(initial_column_type.type) ? ActiveModel::Type::Value.new : initial_column_type
-      end
+      if Gem::Version.new(::ActiveRecord::VERSION::STRING) >= Gem::Version.new("6.1.0")
+        skip_json_serialization = ->(initial_column_type) do
+          %i[json jsonb].include?(initial_column_type.type) ? ActiveModel::Type::Value.new : initial_column_type
+        end
 
-      attribute :data, skip_json_serialization
-      attribute :metadata, skip_json_serialization
+        attribute :data, skip_json_serialization
+        attribute :metadata, skip_json_serialization
+      end
     end
     private_constant :Event
 
