4d1508fddbb0c0a5b12ec6449aed275c63cc74e5
Paweł Pacana <pawel.pacana@gmail.com>
2023-01-30 11:42:01 +0100

    Revert "Extract to variable"
    This reverts commit 8d159f50e400594fefefa8593ffde4189cf5eeaa.

--- ruby_event_store-active_record/lib/ruby_event_store/active_record/event.rb
+++ ruby_event_store-active_record/lib/ruby_event_store/active_record/event.rb
@@ -8,10 +8,8 @@ module RubyEventStore
       self.primary_key = :id
       self.table_name = "event_store_events"
 
-      skip_json_serialization = ->(type) { %i[json jsonb].include?(type.type) ? ActiveModel::Type::Value.new : type }
-
-      attribute :data, skip_json_serialization
-      attribute :metadata, skip_json_serialization
+      attribute :data, ->(type) { %i[json jsonb].include?(type.type) ? ActiveModel::Type::Value.new : type }
+      attribute :metadata, ->(type) { %i[json jsonb].include?(type.type) ? ActiveModel::Type::Value.new : type }
     end
     private_constant :Event
 
