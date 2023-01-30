3573e7b0cdbea424cf0521efa05b5978ddc08aea
Paweł Pacana <pawel.pacana@gmail.com>
2023-01-30 11:42:01 +0100

    Revert "We don't introduce anything new over Value here"
    This reverts commit c5812c24c69587806558c5d1466b3ca5b67a0f7c.

--- ruby_event_store-active_record/lib/ruby_event_store/active_record/event.rb
+++ ruby_event_store-active_record/lib/ruby_event_store/active_record/event.rb
@@ -1,15 +1,13 @@
 # frozen_string_literal: true
 
-require "active_record"
-
 module RubyEventStore
   module ActiveRecord
     class Event < ::ActiveRecord::Base
       self.primary_key = :id
       self.table_name = "event_store_events"
 
-      attribute :data, ActiveModel::Type::Value.new
-      attribute :metadata, ActiveModel::Type::Value.new
+      attribute :data, PassThrough.new
+      attribute :metadata, PassThrough.new
     end
     private_constant :Event
 
