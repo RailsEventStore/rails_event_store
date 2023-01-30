2b18c0828282f1ba7b22a9c52010942cc1172e01
Paweł Pacana <pawel.pacana@gmail.com>
2023-01-30 11:47:32 +0100

    Revert "Revert "Pick correct serializer when testing""
    This reverts commit bda28e04218fd0ef68a9e60e7f3b99d689e5680e.

--- ruby_event_store-active_record/spec/pg_linearized_event_repository_spec.rb
+++ ruby_event_store-active_record/spec/pg_linearized_event_repository_spec.rb
@@ -6,7 +6,17 @@ module RubyEventStore
   module ActiveRecord
     ::RSpec.describe PgLinearizedEventRepository do
       helper = SpecHelper.new
-      mk_repository = -> { PgLinearizedEventRepository.new(serializer: RubyEventStore::Serializers::YAML) }
+
+      mk_repository = -> {
+        serializer =
+          case ENV["DATA_TYPE"]
+          when /json/
+            JSON
+          else
+            RubyEventStore::Serializers::YAML
+          end
+        PgLinearizedEventRepository.new(serializer: serializer)
+      }
 
       it_behaves_like :event_repository, mk_repository, helper
 
