5f18c70eb4edd972706ddda6e4fe3ae9e2b34f70
Paweł Pacana <pawel.pacana@gmail.com>
2023-01-31 20:00:58 +0100

    Don't even bother when gem not loaded

--- support/helpers/redis_client_unix_socket_patch.rb
+++ support/helpers/redis_client_unix_socket_patch.rb
@@ -1,21 +1,23 @@
-raise unless RedisClient::VERSION == "0.11.2"
+if defined? RedisClient
+  raise unless RedisClient::VERSION == "0.11.2"
 
-class RedisClient
-  class Config
-    prepend(
-      Module.new do
-        def initialize(url: nil, **kwargs)
-          return super unless url
+  class RedisClient
+    class Config
+      prepend(
+        Module.new do
+          def initialize(url: nil, **kwargs)
+            return super unless url
 
-          uri = URI.parse(url)
-          if uri.scheme == "unix"
-            super(**kwargs, url: nil)
-            @path = uri.path
-          else
-            super
+            uri = URI.parse(url)
+            if uri.scheme == "unix"
+              super(**kwargs, url: nil)
+              @path = uri.path
+            else
+              super
+            end
           end
         end
-      end
-    )
+      )
+    end
   end
 end
