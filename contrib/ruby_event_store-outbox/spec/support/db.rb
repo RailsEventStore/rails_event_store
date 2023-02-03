9ec093559d8477fc196003c569fd4d8b9c65e98c
Łukasz Reszke <lukaszreszke93@gmail.com>
2023-02-03 08:42:12 +0100

    Refactor

--- contrib/ruby_event_store-outbox/spec/support/db.rb
+++ contrib/ruby_event_store-outbox/spec/support/db.rb
@@ -1,12 +1,14 @@
+def drop_tables
+  ActiveRecord::Migration.drop_table("event_store_outbox")
+  ActiveRecord::Migration.drop_table("event_store_outbox_locks")
+rescue ::ActiveRecord::StatementInvalid
+end
+
 RSpec.configure do |config|
   config.around(:each, db: true) do |example|
     begin
       establish_database_connection
-      begin
-        ActiveRecord::Migration.drop_table("event_store_outbox")
-        ActiveRecord::Migration.drop_table("event_store_outbox_locks")
-      rescue ::ActiveRecord::StatementInvalid
-      end
+      drop_tables
       m = Migrator.new(File.expand_path("../../lib/generators/ruby_event_store/outbox/templates", __dir__))
       m.run_migration("create_event_store_outbox")
       example.run
