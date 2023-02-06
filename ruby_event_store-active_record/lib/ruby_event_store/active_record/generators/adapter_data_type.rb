a987f0c871db34021487060ef0fe5f7fb7cca7b1
Łukasz Reszke <lukaszreszke93@gmail.com>
2023-02-06 09:39:40 +0100

    Introduce class describing adapters allowed data types

---
+++ ruby_event_store-active_record/lib/ruby_event_store/active_record/generators/adapter_data_type.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module RubyEventStore
+  module ActiveRecord
+    class AdapterDataType
+
+      SUPPORTED_POSTGRES_DATA_TYPES = %w[binary json jsonb].freeze
+      SUPPORTED_MYSQL_DATA_TYPES = %w[binary json].freeze
+      SUPPORTED_SQLITE_DATA_TYPES = %w[binary].freeze
+
+      def call(adapter, data_type)
+        raise "unsupported adapter" unless %w[mysql2 postgres sqlite].include?(adapter.downcase)
+        raise "MySQL2 doesn't support #{data_type}" if adapter.downcase == "mysql2" && !SUPPORTED_MYSQL_DATA_TYPES.include?(data_type)
+        raise "" if adapter.downcase == "postgres" && !SUPPORTED_POSTGRES_DATA_TYPES.include?(data_type)
+        raise "sqlite doesn't support #{data_type}" if adapter.downcase == "sqlite" && !SUPPORTED_SQLITE_DATA_TYPES.include?(data_type)
+      end
+    end
+  end
+end
