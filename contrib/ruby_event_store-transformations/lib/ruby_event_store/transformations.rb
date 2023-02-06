80fad98c2e5c098211be38a883baa96f6fd82052
Łukasz Reszke <lukaszreszke93@gmail.com>
2023-02-06 15:25:27 +0100

    Recreate EventClassRemapper in contrib/transformations

--- contrib/ruby_event_store-transformations/lib/ruby_event_store/transformations.rb
+++ contrib/ruby_event_store-transformations/lib/ruby_event_store/transformations.rb
@@ -3,6 +3,7 @@
 require_relative "transformations/version"
 require_relative "transformations/with_indifferent_access"
 require_relative "transformations/identity_map"
+require_relative "transformations/event_class_remapper"
 
 module RubyEventStore
   module Transformations
