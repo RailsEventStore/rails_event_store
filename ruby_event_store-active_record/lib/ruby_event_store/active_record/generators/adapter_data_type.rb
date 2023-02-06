4923351a9d7136eae717d23bce41a79a7b436aec
Łukasz Reszke <lukaszreszke93@gmail.com>
2023-02-06 09:50:09 +0100

    Fix adapter names
    it's postgresql and sqlite
    
    irb(main):002:0> ActiveRecord::Base.connection.adapter_name
    => "SQLite"
    
    irb(main):001:0> ActiveRecord::Base.connection.adapter_name
    => "PostgreSQL"

--- ruby_event_store-active_record/lib/ruby_event_store/active_record/generators/adapter_data_type.rb
+++ ruby_event_store-active_record/lib/ruby_event_store/active_record/generators/adapter_data_type.rb
@@ -9,9 +9,9 @@ module RubyEventStore
       SUPPORTED_SQLITE_DATA_TYPES = %w[binary].freeze
 
       def call(adapter, data_type)
-        raise "unsupported adapter" unless %w[mysql2 postgres sqlite].include?(adapter.downcase)
+        raise "unsupported adapter" unless %w[mysql2 postgresql sqlite].include?(adapter.downcase)
         raise "MySQL2 doesn't support #{data_type}" if adapter.downcase == "mysql2" && !SUPPORTED_MYSQL_DATA_TYPES.include?(data_type)
-        raise "" if adapter.downcase == "postgres" && !SUPPORTED_POSTGRES_DATA_TYPES.include?(data_type)
+        raise  if adapter.downcase == "postgresql" && !SUPPORTED_POSTGRES_DATA_TYPES.include?(data_type)
         raise "sqlite doesn't support #{data_type}" if adapter.downcase == "sqlite" && !SUPPORTED_SQLITE_DATA_TYPES.include?(data_type)
       end
     end
