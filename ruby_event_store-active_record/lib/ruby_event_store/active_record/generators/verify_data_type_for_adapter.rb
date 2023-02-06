3d5482423f05b862dade8205a7701a4823d6a057
Łukasz Reszke <lukaszreszke93@gmail.com>
2023-02-06 11:21:52 +0100

    Don't rely on generic error

--- ruby_event_store-active_record/lib/ruby_event_store/active_record/generators/verify_data_type_for_adapter.rb
+++ ruby_event_store-active_record/lib/ruby_event_store/active_record/generators/verify_data_type_for_adapter.rb
@@ -2,17 +2,18 @@
 
 module RubyEventStore
   module ActiveRecord
+    InvalidDataTypeForAdapter = Class.new(StandardError)
+    
     class VerifyDataTypeForAdapter
-
       SUPPORTED_POSTGRES_DATA_TYPES = %w[binary json jsonb].freeze
       SUPPORTED_MYSQL_DATA_TYPES = %w[binary json].freeze
       SUPPORTED_SQLITE_DATA_TYPES = %w[binary].freeze
 
       def call(adapter, data_type)
-        raise "unsupported adapter" unless %w[mysql2 postgresql sqlite].include?(adapter.downcase)
-        raise "MySQL2 doesn't support #{data_type}" if adapter.downcase == "mysql2" && !SUPPORTED_MYSQL_DATA_TYPES.include?(data_type)
-        raise "PostgreSQL doesn't support #{data_type}" if adapter.downcase == "postgresql" && !SUPPORTED_POSTGRES_DATA_TYPES.include?(data_type)
-        raise "sqlite doesn't support #{data_type}" if adapter.downcase == "sqlite" && !SUPPORTED_SQLITE_DATA_TYPES.include?(data_type)
+        raise InvalidDataTypeForAdapter, "unsupported adapter" unless %w[mysql2 postgresql sqlite].include?(adapter.downcase)
+        raise InvalidDataTypeForAdapter, "MySQL2 doesn't support #{data_type}" if adapter.downcase == "mysql2" && !SUPPORTED_MYSQL_DATA_TYPES.include?(data_type)
+        raise InvalidDataTypeForAdapter, "PostgreSQL doesn't support #{data_type}" if adapter.downcase == "postgresql" && !SUPPORTED_POSTGRES_DATA_TYPES.include?(data_type)
+        raise InvalidDataTypeForAdapter, "sqlite doesn't support #{data_type}" if adapter.downcase == "sqlite" && !SUPPORTED_SQLITE_DATA_TYPES.include?(data_type)
       end
     end
   end
