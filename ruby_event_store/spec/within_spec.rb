e21a55c99c3bd6e661291ec66208c4778fcdedc3
Łukasz Reszke <lukaszreszke93@gmail.com>
2023-02-10 12:32:09 +0100

    Add tests

--- ruby_event_store/spec/within_spec.rb
+++ ruby_event_store/spec/within_spec.rb
@@ -2,10 +2,18 @@ require "spec_helper"
 
 module RubyEventStore
   ::RSpec.describe Client do
-    subject(:within) { Client::Within.new(nil, nil) }
+    subject(:within) { Client::Within.new(nil, nil, nil) }
 
     specify "subscribe with handler as object and block" do
       expect { within.subscribe(:handler, to: []) {} }.to raise_error(ArgumentError)
+    end
+
+    specify "subscribe with handler as object" do
+      expect { within.subscribe(:handler, to: []) }.not_to raise_error
+    end
+
+    specify "subscribe with handler as block" do
+      expect { within.subscribe(to: []) {} }.not_to raise_error
     end
 
     specify "within without block" do
