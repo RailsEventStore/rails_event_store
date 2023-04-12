d1f504be1e9453c28312c82357cd9966612382a2
Paweł Pacana <pawel.pacana@gmail.com>
2023-04-12 12:47:01 +0200

    Pass 20% of event repository linter with this simplistic implementation
    Co-authored-by: Łukasz Reszke <lukaszreszke93@gmail.com>

--- contrib/ruby_event_store-sequel/lib/ruby_event_store/sequel/event_repository.rb
+++ contrib/ruby_event_store-sequel/lib/ruby_event_store/sequel/event_repository.rb
@@ -5,12 +5,36 @@ module RubyEventStore
     class EventRepository
       def initialize(serializer:)
         @serializer = serializer
+        @db = ::Sequel.sqlite
+        @db.create_table(:event_store_events) do
+          primary_key :id
+          String :event_id
+          String :event_type
+          File :data
+          File :metadata
+          Time :created_at
+          Time :valid_at
+        end
       end
 
       def append_to_stream(records, stream, expected_version)
+        records.map do |r|
+          sr = r.serialize(@serializer)
+
+          @db[:event_store_events].insert(
+            event_id: sr.event_id,
+            event_type: sr.event_type,
+            data: sr.data,
+            metadata: sr.metadata,
+            created_at: sr.timestamp,
+            valid_at: sr.valid_at
+          )
+        end
+        self
       end
 
       def link_to_stream(event_ids, stream, expected_version)
+        self
       end
 
       def position_in_stream(event_id, stream)
@@ -32,6 +56,16 @@ module RubyEventStore
       end
 
       def read(specification)
+        @db[:event_store_events].all.map do |h|
+          SerializedRecord.new(
+            event_id: h[:event_id],
+            event_type: h[:event_type],
+            data: h[:data],
+            metadata: h[:metadata],
+            timestamp: h[:created_at].iso8601(TIMESTAMP_PRECISION),
+            valid_at: h[:valid_at].iso8601(TIMESTAMP_PRECISION)
+          ).deserialize(@serializer)
+        end
       end
 
       def count(specification)
