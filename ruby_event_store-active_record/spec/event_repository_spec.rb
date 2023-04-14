c64c8fb7164b4c16db3132379a9744cc1698cb02
Paweł Pacana <pawel.pacana@gmail.com>
2023-04-14 12:05:42 +0200

    Improve test matcher in RES::AR
    Co-authored-by: Łukasz Reszke <lukaszreszke93@gmail.com>

--- ruby_event_store-active_record/spec/event_repository_spec.rb
+++ ruby_event_store-active_record/spec/event_repository_spec.rb
@@ -61,12 +61,15 @@ module RubyEventStore
         expect do
           expect do
             repository.link_to_stream(
-              ["72922e65-1b32-4e97-8023-03ae81dd3a27"],
+              %w[
+                72922e65-1b32-4e97-8023-03ae81dd3a27
+                d9f6d02a-05f0-4c27-86a9-ad7c4ef73042
+              ],
               Stream.new("flow"),
               ExpectedVersion.none
             )
           end.to raise_error(EventNotFound)
-        end.to match_query /SELECT.*event_store_events.*id.*FROM.*event_store_events.*WHERE.*event_store_events.*id.*=.*/
+        end.to match_query 'SELECT "event_store_events"."event_id" FROM "event_store_events" WHERE "event_store_events"."event_id" IN (?, ?)'
       end
 
       specify "use default models" do
