53ee5ae52d35cb2e831f0a1ea9266696eb81b41e
Łukasz Reszke <lukaszreszke93@gmail.com>
2023-04-14 18:59:18 +0200

    Sequel migration file for sqlite

---
+++ contrib/ruby_event_store-sequel/lib/ruby_event_store/generators/templates/1_create_ruby_event_store_tables.rb
@@ -0,0 +1,43 @@
+::Sequel.migration do
+  up do
+    ENV["DATA_TYPE"] ||= "text"
+    data_type = ENV["DATA_TYPE"].to_sym
+    data_types = %i[text]
+    raise ArgumentError, "DATA_TYPE must be: #{data_types.join(", ")}" unless data_types.include?(data_type)
+
+    create_table :event_store_events_in_streams do
+      primary_key :id, type: :Bignum, null: false
+
+      column :stream, String, null: false
+      column :position, Integer
+      column :event_id, String, size: 36, null: false
+      column :created_at,
+             DateTime,
+             null: false,
+             type: "DATETIME(6)",
+             index: "index_event_store_events_in_streams_on_created_at"
+
+      index %i[stream position], unique: true, name: "index_event_store_events_in_streams_on_stream_and_position"
+      index %i[stream event_id], unique: true, name: "index_event_store_events_in_streams_on_stream_and_event_id"
+    end
+
+    create_table :event_store_events do
+      primary_key :id, type: :Bignum, null: false
+
+      column :event_id, String, size: 36, null: false
+      column :event_type, String, null: false
+      column :metadata, String, text: true
+      column :data, String, text: true, null: false
+      column :created_at, DateTime, null: false, type: "DATETIME(6)", index: "index_event_store_events_on_created_at"
+      column :valid_at, DateTime, null: false, type: "DATETIME(6)", index: "index_event_store_events_on_valid_at"
+
+      index :event_id, unique: true,  name: "index_event_store_events_on_event_id"
+    end
+  end
+
+  down do
+    drop_table :event_store_events
+    drop_table :event_store_events_in_streams
+  end
+
+end
