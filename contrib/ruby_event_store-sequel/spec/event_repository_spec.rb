20b3c0cb5e7f67a9d2f2ef3d090fb77ac8bb279b
Paweł Pacana <pawel.pacana@gmail.com>
2023-04-14 12:00:16 +0200

    Backport test asserting on SQL query from RES::AR
    Makes use of recently-added instrumenatation.
    
    Co-authored-by: Łukasz Reszke <lukaszreszke93@gmail.com>

--- contrib/ruby_event_store-sequel/spec/event_repository_spec.rb
+++ contrib/ruby_event_store-sequel/spec/event_repository_spec.rb
@@ -14,11 +14,35 @@ module RubyEventStore
             RubyEventStore::Serializers::YAML
           end
         EventRepository.new(sequel: helper.sequel, serializer: serializer)
-      end
+      }
 
       it_behaves_like :event_repository, mk_repository, helper
 
       around(:each) { |example| helper.run_lifecycle { example.run } }
+
+      let(:repository) { mk_repository.call }
+      let(:specification) do
+        Specification.new(
+          SpecificationReader.new(repository, Mappers::Default.new)
+        )
+      end
+
+      specify "limited query when looking for non-existing events during linking" do
+        expect do
+          expect do
+            repository.link_to_stream(
+              %w[
+                72922e65-1b32-4e97-8023-03ae81dd3a27
+                d9f6d02a-05f0-4c27-86a9-ad7c4ef73042
+              ],
+              Stream.new("flow"),
+              ExpectedVersion.none
+            )
+          end.to raise_error(EventNotFound)
+        end.to match_query <<~SQL.strip
+          SELECT `event_store_events`.`event_id` FROM `event_store_events` WHERE (`event_store_events`.`event_id` IN ('72922e65-1b32-4e97-8023-03ae81dd3a27', 'd9f6d02a-05f0-4c27-86a9-ad7c4ef73042'))
+        SQL
+      end
     end
   end
 end
