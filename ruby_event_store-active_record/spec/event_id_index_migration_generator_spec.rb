5b8589b604e710bbc28dc09cd6e730271af8b0a7
Łukasz Reszke <lukaszreszke93@gmail.com>
2023-07-17 22:51:29 +0200

    Ruby Migration generator for event_id index
    RES Browser slowed down when displaying event overview.
    It turned out it was because of "Event Streams" involving full table scan on
    event_store_events_in_streams. Adding the index fixes the issue,
    obviously.
    
    Co-authored-by: Łukasz Reszke <lukaszreszke93@gmail.com>

---
+++ ruby_event_store-active_record/spec/event_id_index_migration_generator_spec.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require_relative "../../support/helpers/silence_stdout"
+
+module RubyEventStore
+  module ActiveRecord
+    ::RSpec.describe EventIdIndexMigrationGenerator do
+      around { |example| SilenceStdout.silence_stdout { example.run } }
+      around do |example|
+        begin
+          @dir = Dir.mktmpdir(nil, "./")
+          example.call
+        ensure
+          FileUtils.rm_r(@dir)
+          FileUtils.rm_f(["./20221130213700_add_event_id_index_to_event_store_events_in_streams.rb"])
+        end
+      end
+
+      before { allow(Time).to receive(:now).and_return(Time.new(2022, 11, 30, 21, 37, 00)) }
+
+      specify "it is created within specified directory" do
+        migration_generator(@dir)
+
+        expect(migration_exists?(@dir)).to be_truthy
+      end
+
+      specify "returns path to migration file" do
+        path = migration_generator(@dir)
+
+        expected_path = "#{@dir}/20221130213700_add_event_id_index_to_event_store_events_in_streams.rb"
+        expect(path).to match(expected_path)
+      end
+
+      specify "uses particular migration version" do
+        migration_generator(@dir)
+
+        expect(read_migration(@dir)).to include("ActiveRecord::Migration[#{::ActiveRecord::Migration.current_version}]")
+      end
+
+      specify "uses particular migration version for rails 6.0" do
+        skip unless ENV["BUNDLE_GEMFILE"]&.include?("rails_6_0")
+
+        migration_generator(@dir)
+
+        expect(read_migration(@dir)).to include("ActiveRecord::Migration[6.0]")
+      end
+
+      specify "uses particular migration version for rails 6.1" do
+        skip unless ENV["BUNDLE_GEMFILE"]&.include?("rails_6_1")
+
+        migration_generator(@dir)
+
+        expect(read_migration(@dir)).to include("ActiveRecord::Migration[6.1]")
+      end
+
+      private
+
+      def migration_generator(dir)
+        EventIdIndexMigrationGenerator.new.call(dir)
+      end
+
+      def migration_exists?(dir)
+        File.exist?("#{dir}/20221130213700_add_event_id_index_to_event_store_events_in_streams.rb")
+      end
+
+      def read_migration(dir)
+        File.read("#{dir}/20221130213700_add_event_id_index_to_event_store_events_in_streams.rb")
+      end
+    end
+  end
+end
