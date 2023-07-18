efa1163b2e2a6a46255e67d5b7c14c800dcb353f
Paweł Pacana <pawel.pacana@gmail.com>
2023-07-18 12:49:38 +0200

    Refactor

--- support/templates/main.rb
+++ support/templates/main.rb
@@ -18,8 +18,6 @@ mk_indented_yaml =
     Psych.dump(shit).lines.drop(1).join(" " * indent).strip
   end
 
-template = DATA.read
-
 [
   {
     name: "aggregate_root",
@@ -64,77 +62,13 @@ template = DATA.read
 ].each do |gem|
   name, matrix = gem.values_at(:name, :matrix)
 
-  yaml =
-    ERB.new(template).result_with_hash(
-      name: name,
-      working_directory: name,
-      matrix: matrix
-    )
-  workflow_path = File.join(__dir__, "../../.github/workflows/#{name}.yml")
-
-  File.write(workflow_path, yaml)
+  File.write(File.join(__dir__, "../../.github/workflows/#{name}.yml"), ERB.new(File.read(File.join(__dir__, "res_gem.yaml.erb"))).result_with_hash(
+    name: name,
+    working_directory: name,
+    matrix: matrix
+  ))
 
   print "."
 end
 
 puts
-
-__END__
-name: <%= name %>
-on:
-  repository_dispatch:
-    types:
-      - script
-  workflow_dispatch:
-  push:
-    paths-ignore:
-      - "railseventstore.org/**"
-      - "contrib/**"
-  pull_request:
-    types: [opened, reopened]
-    paths-ignore:
-      - "railseventstore.org/**"
-      - "contrib/**"
-jobs:
-  test:
-    runs-on: ubuntu-20.04
-    env:
-      WORKING_DIRECTORY: <%= working_directory %>
-      BUNDLE_GEMFILE: ${{ matrix.gemfile }}
-    strategy:
-      fail-fast: false
-      matrix:
-        include:
-          <%= mk_indented_yaml.call(matrix, 10) %>
-    steps:
-      - uses: actions/checkout@v3
-      - run: test -e ${{ matrix.gemfile }}.lock
-        working-directory: ${{ env.WORKING_DIRECTORY }}
-      - uses: ruby/setup-ruby@v1
-        with:
-          ruby-version: ${{ matrix.ruby }}
-          bundler-cache: true
-          working-directory: ${{ env.WORKING_DIRECTORY }}
-      - run: make test
-        working-directory: ${{ env.WORKING_DIRECTORY }}
-
-  mutate:
-    runs-on: ubuntu-20.04
-    env:
-      WORKING_DIRECTORY: <%= working_directory %>
-      BUNDLE_GEMFILE: Gemfile
-    strategy:
-      fail-fast: false
-    steps:
-      - uses: actions/checkout@v3
-        with:
-          fetch-depth: 0
-      - run: test -e ${{ env.BUNDLE_GEMFILE }}.lock
-        working-directory: ${{ env.WORKING_DIRECTORY }}
-      - uses: ruby/setup-ruby@v1
-        with:
-          ruby-version: ruby-3.1
-          bundler-cache: true
-          working-directory: ${{ env.WORKING_DIRECTORY }}
-      - run: make mutate-changes
-        working-directory: ${{ env.WORKING_DIRECTORY }}
