0eac3bbc191ecc9b2022e8ab8b7165754260e062
Paweł Pacana <pawel.pacana@gmail.com>
2023-07-18 12:52:43 +0200

    Refactor

--- support/templates/main.rb
+++ support/templates/main.rb
@@ -21,33 +21,28 @@ mk_indented_yaml =
 [
   {
     name: "aggregate_root",
-    matrix: mk_matrix.call(ruby: RUBY_VERSIONS, gemfile: GEMFILES)
+    matrix: mk_matrix[ruby: RUBY_VERSIONS, gemfile: GEMFILES]
   },
   {
     name: "ruby_event_store",
-    matrix: mk_matrix.call(ruby: RUBY_VERSIONS, gemfile: GEMFILES)
+    matrix: mk_matrix[ruby: RUBY_VERSIONS, gemfile: GEMFILES]
   },
   {
     name: "ruby_event_store-rspec",
-    matrix: mk_matrix.call(ruby: RUBY_VERSIONS, gemfile: GEMFILES)
+    matrix: mk_matrix[ruby: RUBY_VERSIONS, gemfile: GEMFILES]
   },
   {
     name: "ruby_event_store-browser",
-    matrix:
-      mk_matrix.call(ruby: RUBY_VERSIONS, gemfile: RACK_GEMFILES)
+    matrix: mk_matrix[ruby: RUBY_VERSIONS, gemfile: RACK_GEMFILES]
   },
   {
     name: "rails_event_store",
-    matrix:
-      mk_matrix.call(
-        ruby: RUBY_VERSIONS,
-        gemfile: RAILS_GEMFILES
-      )
+    matrix: mk_matrix[ruby: RUBY_VERSIONS, gemfile: RAILS_GEMFILES]
   },
   {
     name: "ruby_event_store-active_record",
     matrix:
-      mk_matrix.call(
+      mk_matrix[
         ruby: RUBY_VERSIONS,
         gemfile: RAILS_GEMFILES,
         database: %w[
@@ -57,16 +52,19 @@ mk_indented_yaml =
           mysql2://root:secret@127.0.0.1:10008/rails_event_store?pool=5
         ],
         datatype: %w[binary json jsonb]
-      )
+      ]
   }
 ].each do |gem|
   name, matrix = gem.values_at(:name, :matrix)
 
-  File.write(File.join(__dir__, "../../.github/workflows/#{name}.yml"), ERB.new(File.read(File.join(__dir__, "res_gem.yaml.erb"))).result_with_hash(
-    name: name,
-    working_directory: name,
-    matrix: mk_indented_yaml[matrix, 10]
-  ))
+  File.write(
+    File.join(__dir__, "../../.github/workflows/#{name}.yml"),
+    ERB.new(File.read(File.join(__dir__, "res_gem.yaml.erb"))).result_with_hash(
+      name: name,
+      working_directory: name,
+      matrix: mk_indented_yaml[matrix, 10]
+    )
+  )
 
   print "."
 end
