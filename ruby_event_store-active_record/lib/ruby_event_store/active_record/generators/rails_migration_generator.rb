c0a80bd506ce1118732fc7e90290d72972684ad9
Szymon Fiedler <szymon.fiedler@gmail.com>
2023-08-29 10:45:13 +0200

    DatabaseAdapter takes care of that already

--- ruby_event_store-active_record/lib/ruby_event_store/active_record/generators/rails_migration_generator.rb
+++ ruby_event_store-active_record/lib/ruby_event_store/active_record/generators/rails_migration_generator.rb
@@ -49,7 +49,7 @@ module RubyEventStore
       end
 
       def adapter_name
-        ::ActiveRecord::Base.connection.adapter_name.downcase
+        ::ActiveRecord::Base.connection.adapter_name
       end
 
       def migration_version
