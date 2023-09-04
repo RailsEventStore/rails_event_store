ca8561dec829d50a10ae05cefe951bc8f1a2a8c8
pjurewicz <piotr.jurewicz@arkency.com>
2023-09-04 18:56:10 +0200

    Avoid multiple migrations having the same version number

--- rails_event_store/spec/rails_foreign_key_on_event_id_migration_generator_spec.rb
+++ rails_event_store/spec/rails_foreign_key_on_event_id_migration_generator_spec.rb
@@ -78,7 +78,7 @@ module RailsEventStore
     end
 
     def second_step_migration_exists?(dir)
-      File.exist?("#{dir}/db/migrate/20160809222222_validate_add_foreign_key_on_event_id_to_event_store_events_in_streams.rb")
+      File.exist?("#{dir}/db/migrate/20160809222223_validate_add_foreign_key_on_event_id_to_event_store_events_in_streams.rb")
     end
 
     def generated_files_count(dir)
--- ruby_event_store-active_record/lib/ruby_event_store/active_record/generators/rails_foreign_key_on_event_id_migration_generator.rb
+++ ruby_event_store-active_record/lib/ruby_event_store/active_record/generators/rails_foreign_key_on_event_id_migration_generator.rb
@@ -27,13 +27,14 @@ if defined?(Rails::Generators::Base)
         def create_migration
           case @database_adapter
           when DatabaseAdapter::PostgreSQL
+            time = Time.now
             template "#{@database_adapter.template_directory}add_foreign_key_on_event_id_to_event_store_events_in_streams_template.erb",
-                     "db/migrate/#{timestamp}_add_foreign_key_on_event_id_to_event_store_events_in_streams.rb"
+                     "db/migrate/#{migration_verion_number(time)}_add_foreign_key_on_event_id_to_event_store_events_in_streams.rb"
             template "#{@database_adapter.template_directory}validate_add_foreign_key_on_event_id_to_event_store_events_in_streams_template.erb",
-                     "db/migrate/#{timestamp}_validate_add_foreign_key_on_event_id_to_event_store_events_in_streams.rb"
+                     "db/migrate/#{migration_verion_number(time + 1)}_validate_add_foreign_key_on_event_id_to_event_store_events_in_streams.rb"
           else
             template "#{@database_adapter.template_directory}add_foreign_key_on_event_id_to_event_store_events_in_streams_template.erb",
-                     "db/migrate/#{timestamp}_add_foreign_key_on_event_id_to_event_store_events_in_streams.rb"
+                     "db/migrate/#{migration_verion_number(Time.now)}_add_foreign_key_on_event_id_to_event_store_events_in_streams.rb"
           end
         end
 
@@ -47,8 +48,8 @@ if defined?(Rails::Generators::Base)
           ::ActiveRecord::Migration.current_version
         end
 
-        def timestamp
-          Time.now.strftime("%Y%m%d%H%M%S")
+        def migration_verion_number(time)
+          time.strftime("%Y%m%d%H%M%S")
         end
       end
     end
