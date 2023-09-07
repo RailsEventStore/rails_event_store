ff78064b40465f828e21a946e13f7caff7942e35
Paweł Pacana <pawel.pacana@gmail.com>
2023-09-07 10:57:29 +0200

    Refactor
    Co-authored-by: Piotr Jurewicz <pjurewicz93@gmail.com>
    Co-authored-by: Szymon Fiedler <szymon.fiedler@gmail.com>

--- contrib/ruby_event_store-sidekiq_scheduler/spec/async_handler_helpers_spec.rb
+++ contrib/ruby_event_store-sidekiq_scheduler/spec/async_handler_helpers_spec.rb
@@ -13,26 +13,30 @@ module RubyEventStore
   end
 
   ::RSpec.describe RailsEventStore::AsyncHandler do
+    let(:event_store) do
+      RubyEventStore::Client.new(
+        dispatcher:
+          ImmediateAsyncDispatcher.new(
+            scheduler: SidekiqScheduler.new(serializer: YAML)
+          )
+      )
+    end
+    let(:event) { RubyEventStore::Event.new }
+
     specify "Sidekiq::Worker without ActiveJob that requires serialization" do
       $queue = Queue.new
 
-      event_store =
-        RubyEventStore::Client.new(
-          dispatcher: ImmediateAsyncDispatcher.new(scheduler: SidekiqScheduler.new(serializer: YAML))
-        )
-      ev = RubyEventStore::Event.new
       Sidekiq::Testing.fake! do
         SidekiqHandlerWithHelper.prepend RailsEventStore::AsyncHandler.with(
-          event_store: event_store,
-          serializer: YAML
-        )
+                                           event_store: event_store,
+                                           serializer: YAML
+                                         )
         event_store.subscribe_to_all_events(SidekiqHandlerWithHelper)
-        event_store.publish(ev)
+        event_store.publish(event)
         Thread.new { Sidekiq::Worker.drain_all }.join
       end
 
-      expect($queue.pop).to eq(ev)
+      expect($queue.pop).to eq(event)
     end
   end
 end
-
