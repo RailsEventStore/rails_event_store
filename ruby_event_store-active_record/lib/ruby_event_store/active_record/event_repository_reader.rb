0055e1f3711ca8926492c42565f1bf5d21765237
pjurewicz <piotr.jurewicz@arkency.com>
2023-12-06 11:11:37 +0100

    Prefer includes over preload
    To avoid redundant 2nd query being called after the call with join

--- ruby_event_store-active_record/lib/ruby_event_store/active_record/event_repository_reader.rb
+++ ruby_event_store-active_record/lib/ruby_event_store/active_record/event_repository_reader.rb
@@ -96,7 +96,7 @@ module RubyEventStore
           stream = stream.where(newer_than_or_equal_condition(spec)) if spec.newer_than_or_equal
           stream.order(id: order(spec))
         else
-          stream = @stream_klass.preload(:event).where(stream: spec.stream.name)
+          stream = @stream_klass.includes(:event).where(stream: spec.stream.name)
           stream = stream.where(event_id: spec.with_ids) if spec.with_ids?
           stream = stream.joins(:event).where(@event_klass.table_name => { event_type: spec.with_types }) if spec.with_types?
           stream = stream.joins(:event).order(as_at(spec)) if spec.time_sort_by_as_at?
