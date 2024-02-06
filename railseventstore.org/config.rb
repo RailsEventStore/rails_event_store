29d75099b9ae442e32158fbff19e8a566d480029
Paweł Pacana <pawel.pacana@gmail.com>
2024-02-06 15:54:42 +0100

    Cleanup two-digit version fallout leftover from 2.10 release
    Regexes in `make set-version` do not match two-digit release versions.
    We don't use two-digit release versions, unless accident happens.
    
    Co-authored-by: Szymon Fiedler <szymon.fiedler@gmail.com>

--- railseventstore.org/config.rb
+++ railseventstore.org/config.rb
@@ -4,7 +4,8 @@ require "tilt"
 require "arbre"
 
 class ArbreTemplate < ::Tilt::Template
-  def prepare; end
+  def prepare
+  end
 
   def precompiled_template(locals)
     <<-END
@@ -29,12 +30,17 @@ activate :external_pipeline,
          latency: 1
 
 set :markdown_engine, :redcarpet
-set :markdown, tables: true, autolink: true, fenced_code_blocks: true, with_toc_data: true, no_intra_emphasis: true
+set :markdown,
+    tables: true,
+    autolink: true,
+    fenced_code_blocks: true,
+    with_toc_data: true,
+    no_intra_emphasis: true
 
 set :res_version_v1, "1.3.1"
-set :res_version_v2, "2.10"
+set :res_version_v2, "2.14.0"
 set :res_version_v3, "3.0.0"
-set :res_version, "2.10"
+set :res_version, "2.14.0"
 
 page "/"
 page "/docs/v1/*", locals: { version: "v1" }, layout: "documentation"
