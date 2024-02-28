72f1d85303b7c59155584892da64ebd7c8454b6a
Rafał Łasocha <rafal@lasocha.net>
2024-02-28 22:32:54 +0100

    Move extraction of params into Api, where the relevant params are

--- ruby_event_store-browser/elm/src/Pagination.elm
+++ ruby_event_store-browser/elm/src/Pagination.elm
@@ -1,6 +1,5 @@
-module Pagination exposing (Specification, empty, specificationFromUrl)
+module Pagination exposing (Specification, empty)
 
-import Regex
 import Url
 
 type alias Specification =
@@ -12,18 +11,3 @@ type alias Specification =
 
 empty : Specification
 empty = Specification Nothing Nothing Nothing
-
-
-extractStringFromMatch : Regex.Match -> String
-extractStringFromMatch match =
-    Maybe.withDefault "" (Maybe.withDefault (Just "") (List.head match.submatches))
-
-
-extractPaginationPart : String -> String -> Maybe String
-extractPaginationPart regexString link = 
-    List.head (List.map extractStringFromMatch (Regex.find (Maybe.withDefault Regex.never (Regex.fromString regexString)) link))
-
-
-specificationFromUrl : String -> Specification
-specificationFromUrl link =
-    Specification (extractPaginationPart "page%5Bposition%5D=([a-zA-Z0-9-]+)" link) (extractPaginationPart "page%5Bdirection%5D=([a-zA-Z0-9-]+)" link) (extractPaginationPart "page%5Bcount%5D=([a-zA-Z0-9-]+)" link)
