31e850bac74256a98af526e70c258ce9bcd47c4a
Maciek Korsan <maciejkorsan@gmail.com>
2024-03-20 17:48:48 +0100

    Add missing "valid_at" field to tests

--- ruby_event_store-browser/elm/tests/DecodersTest.elm
+++ ruby_event_store-browser/elm/tests/DecodersTest.elm
@@ -35,7 +35,8 @@ suite =
                                         "baz": "3"
                                     },
                                     "metadata": {
-                                        "timestamp": "2017-12-20T23:49:45.273Z"
+                                        "timestamp": "2017-12-20T23:49:45.273Z",
+                                        "valid_at": "2017-12-20T23:49:45.273Z"
                                     },
                                     "parent_event_id": null,
                                     "type_stream_name": "$by_type_DummyEvent"
@@ -55,12 +56,13 @@ suite =
                                   , eventId = "664ada1e-2f01-4ed0-9c16-63dbc82269d2"
                                   , createdAt = Time.millisToPosix 1513813785273
                                   , rawData = "{\n  \"foo\": 1,\n  \"bar\": 2,\n  \"baz\": \"3\"\n}"
-                                  , rawMetadata = "{\n  \"timestamp\": \"2017-12-20T23:49:45.273Z\"\n}"
+                                  , rawMetadata = "{\n  \"timestamp\": \"2017-12-20T23:49:45.273Z\",\n  \"valid_at\": \"2017-12-20T23:49:45.273Z\"\n}"
                                   , correlationStreamName = Nothing
                                   , causationStreamName = Nothing
                                   , typeStreamName = "$by_type_DummyEvent"
                                   , parentEventId = Nothing
                                   , streams = Nothing
+                                  , validAt = Time.millisToPosix 1513813785273
                                   }
                                 ]
                             , pagination =
@@ -90,7 +92,8 @@ suite =
                                         "baz": "3"
                                     },
                                     "metadata": {
-                                        "timestamp": "2017-12-20T23:49:45.273Z"
+                                        "timestamp": "2017-12-20T23:49:45.273Z",
+                                        "valid_at": "2017-12-20T23:49:45.273Z"
                                     },
                                     "correlation_stream_name": "$by_correlation_id_a7243789-999f-4ef2-8511-b1c686b83fad",
                                     "causation_stream_name": "$by_causation_id_664ada1e-2f01-4ed0-9c16-63dbc82269d2",
@@ -132,12 +135,13 @@ suite =
                             , eventId = "664ada1e-2f01-4ed0-9c16-63dbc82269d2"
                             , createdAt = Time.millisToPosix 1513813785273
                             , rawData = "{\n  \"foo\": 1,\n  \"bar\": 3.4,\n  \"baz\": \"3\"\n}"
-                            , rawMetadata = "{\n  \"timestamp\": \"2017-12-20T23:49:45.273Z\"\n}"
+                            , rawMetadata = "{\n  \"timestamp\": \"2017-12-20T23:49:45.273Z\",\n  \"valid_at\": \"2017-12-20T23:49:45.273Z\"\n}"
                             , correlationStreamName = Just "$by_correlation_id_a7243789-999f-4ef2-8511-b1c686b83fad"
                             , causationStreamName = Just "$by_causation_id_664ada1e-2f01-4ed0-9c16-63dbc82269d2"
                             , typeStreamName = "$by_type_DummyEvent"
                             , parentEventId = Just "cb12f84b-b9e4-439b-8442-50fae6244dc9"
                             , streams = Just [ "dummy", "$by_correlation_id_a7243789-999f-4ef2-8511-b1c686b83fad", "$by_causation_id_664ada1e-2f01-4ed0-9c16-63dbc82269d2", "$by_type_DummyEvent" ]
+                            , validAt = Time.millisToPosix 1513813785273
                             }
                         )
             , test "search decoder" <|
