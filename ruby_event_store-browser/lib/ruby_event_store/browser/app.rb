ae7b96bd73c9a81fbb6a8d518e19182ec239d78f
Łukasz Reszke <lukaszreszke93@gmail.com>
2024-03-21 00:10:58 +0100

    kill mutants

--- ruby_event_store-browser/lib/ruby_event_store/browser/app.rb
+++ ruby_event_store-browser/lib/ruby_event_store/browser/app.rb
@@ -94,7 +94,7 @@ module RubyEventStore
                  page: params["page"]
                )
         end
-        router.add_route("GET", "/api/search_streams/:stream_name") do |params, urls|
+        router.add_route("GET", "/api/search_streams/:stream_name") do |params|
           json SearchStreams.new(
             event_store: event_store,
             stream_name: params.fetch("stream_name"),
