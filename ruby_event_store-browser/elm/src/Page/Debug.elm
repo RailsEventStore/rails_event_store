e0b6a24c013cb0a4d8732e3419c5b75ac2655cac
Paweł Pacana <pawel.pacana@gmail.com>
2024-03-21 22:48:48 +0100

    Format Page.Debug

--- ruby_event_store-browser/elm/src/Page/Debug.elm
+++ ruby_event_store-browser/elm/src/Page/Debug.elm
@@ -1,17 +1,21 @@
 module Page.Debug exposing (..)
 
-import Html exposing (..)
 import Flags exposing (Flags)
+import Html exposing (..)
 
-type alias Model = { resVersion : String, repositoryAdapter : String }
+
+type alias Model =
+    { resVersion : String, repositoryAdapter : String }
+
 
 init : Flags -> Model
-init flags  =
-  { resVersion = flags.resVersion, repositoryAdapter = flags.repositoryAdapter }
+init flags =
+    { resVersion = flags.resVersion, repositoryAdapter = flags.repositoryAdapter }
 
-view: Model -> Html a
+
+view : Model -> Html a
 view model =
-    div [] [
-        p [] [text ("RubyEventStore version: " ++ model.resVersion)],
-        p [] [text ("RubyEventStore adapter: " ++ model.repositoryAdapter)]
-    ]
+    div []
+        [ p [] [ text ("RubyEventStore version: " ++ model.resVersion) ]
+        , p [] [ text ("RubyEventStore adapter: " ++ model.repositoryAdapter) ]
+        ]
