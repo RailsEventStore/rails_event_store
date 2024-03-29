321013577dbc017ed7acd8a2fcb27367dde79acb
Paweł Pacana <pawel.pacana@gmail.com>
2024-03-29 16:15:32 +0100

    Formatting

--- ruby_event_store-active_record/spec/migration_test_spec.rb
+++ ruby_event_store-active_record/spec/migration_test_spec.rb
@@ -25,10 +25,14 @@ module RubyEventStore
         data_type = data_type_to_pg_type(ENV["DATA_TYPE"])
 
         create_event_store_events = pg_show_create_table("event_store_events")
-        create_event_store_events_in_streams = pg_show_create_table("event_store_events_in_streams")
+        create_event_store_events_in_streams =
+          pg_show_create_table("event_store_events_in_streams")
 
         expect(
-          [create_event_store_events, create_event_store_events_in_streams].join("\n").gsub(/\s+/, " ")
+          [
+            create_event_store_events,
+            create_event_store_events_in_streams
+          ].join("\n").gsub(/\s+/, " ")
         ).to eq <<~SCHEMA.strip.gsub(/\s+/, " ")
           Table "public.event_store_events"
              Column   |            Type                | Collation | Nullable |                    Default                     
@@ -72,16 +76,25 @@ module RubyEventStore
 
         data_type = data_type_to_mysql_type(ENV["DATA_TYPE"])
 
-        my_sql_major_version = ::ActiveRecord::Base.connection.select_value("SELECT VERSION();").to_i
-        collation = my_sql_major_version == 8 ? " COLLATE=utf8mb4_0900_ai_ci" : ""
+        my_sql_major_version =
+          ::ActiveRecord::Base.connection.select_value("SELECT VERSION();").to_i
+        collation =
+          my_sql_major_version == 8 ? " COLLATE=utf8mb4_0900_ai_ci" : ""
         charset = my_sql_major_version == 8 ? "utf8mb4" : "latin1"
         int_lenght = my_sql_major_version == 8 ? "" : "(11)"
         bigint_lenght = my_sql_major_version == 8 ? "" : "(20)"
 
-        create_event_store_events = mysql_show_create_table("event_store_events")
-        create_event_store_events_in_streams = mysql_show_create_table("event_store_events_in_streams")
+        create_event_store_events =
+          mysql_show_create_table("event_store_events")
+        create_event_store_events_in_streams =
+          mysql_show_create_table("event_store_events_in_streams")
 
-        expect([create_event_store_events, create_event_store_events_in_streams].join("\n")).to eq <<~SCHEMA.strip
+        expect(
+          [
+            create_event_store_events,
+            create_event_store_events_in_streams
+          ].join("\n")
+        ).to eq <<~SCHEMA.strip
           CREATE TABLE `event_store_events` (
             `id` bigint#{bigint_lenght} NOT NULL AUTO_INCREMENT,
             `event_id` varchar(36) NOT NULL,
@@ -115,11 +128,17 @@ module RubyEventStore
       specify "sqlite" do
         skip unless ENV["DATABASE_URL"].include?("sqlite")
 
-        expect(::ActiveRecord::Base.connection.execute(<<~SQL).map { |x| x["sql"] }.join("\n")).to eq <<~SCHEMA.strip
+        expect(
+          ::ActiveRecord::Base
+            .connection
+            .execute(<<~SQL)
           SELECT  sql
           FROM    sqlite_schema
           WHERE   NAME LIKE '%event_store_events%'
         SQL
+            .map { |x| x["sql"] }
+            .join("\n")
+        ).to eq <<~SCHEMA.strip
           CREATE TABLE "event_store_events" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "event_id" varchar(36) NOT NULL, "event_type" varchar NOT NULL, "metadata" blob, "data" blob NOT NULL, "created_at" datetime(6) NOT NULL, "valid_at" datetime(6))
           CREATE UNIQUE INDEX "index_event_store_events_on_event_id" ON "event_store_events" ("event_id")
           CREATE INDEX "index_event_store_events_on_event_type" ON "event_store_events" ("event_type")
@@ -139,7 +158,9 @@ module RubyEventStore
       private
 
       def data_type_to_pg_type(data_type)
-        { "binary" => "bytea", "json" => "json", "jsonb" => "jsonb" }.fetch(data_type)
+        { "binary" => "bytea", "json" => "json", "jsonb" => "jsonb" }.fetch(
+          data_type
+        )
       end
 
       def data_type_to_mysql_type(data_type)
@@ -154,7 +175,12 @@ module RubyEventStore
       end
 
       def mysql_show_create_table(name)
-        ::ActiveRecord::Base.connection.execute("SHOW CREATE TABLE #{name}").to_a.flatten.last
+        ::ActiveRecord::Base
+          .connection
+          .execute("SHOW CREATE TABLE #{name}")
+          .to_a
+          .flatten
+          .last
       end
     end
   end
