2a7c0267f0914b07d76ffd1a04c95fb5562fff8c
Maciej Korsan <maciejkorsan@gmail.com>
2024-07-24 22:15:45 +0200

    introduce versioning

--- railseventstore.org/source/docs/docusaurus/docusaurus.config.js
+++ railseventstore.org/source/docs/docusaurus/docusaurus.config.js
@@ -4,59 +4,67 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 import tailwindPlugin from "./plugins/tailwind-config.cjs"; // add this
-
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Rails Event Store',
-  tagline: 'The open-source implementation of an Event Store for Ruby and Rails',
-  favicon: 'img/favicon.ico',
+  title: "Rails Event Store",
+  tagline:
+    "The open-source implementation of an Event Store for Ruby and Rails",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://railseventstore.org',
+  url: "https://railseventstore.org",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'RailsEventStore', // Usually your GitHub org/user name.
-  projectName: 'rails_event_store', // Usually your repo name.
+  organizationName: "RailsEventStore", // Usually your GitHub org/user name.
+  projectName: "rails_event_store", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: './sidebars.js',
+          lastVersion: "current",
+          versions: {
+            current: {
+              label: "2.14.0",
+              path: "",
+            },
+
+          },
+          sidebarPath: "./sidebars.js",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       }),
     ],
@@ -66,102 +74,106 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: 'Rails Event Store',
+        title: "Rails Event Store",
         logo: {
-          alt: 'Rails Event Store Logo',
-          src: 'img/logo.svg',
+          alt: "Rails Event Store Logo",
+          src: "img/logo.svg",
         },
 
         items: [
           {
-            type: 'doc',
+            type: 'docsVersionDropdown',
             position: 'right',
-            docId: 'start',
+            dropdownActiveClassDisabled: true,
+          },
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'right',
             label: 'Docs',
           },
           {
-            href: '/community',
-            position: 'right',
-            label: 'Community',
+            href: "/community",
+            position: "right",
+            label: "Community",
           },
           {
-            href: '/contributing',
-            position: 'right',
-            label: 'Contributing',
+            href: "/contributing",
+            position: "right",
+            label: "Contributing",
           },
           {
-            href: '/support',
-            position: 'right',
-            label: 'Support',
+            href: "/support",
+            position: "right",
+            label: "Support",
           },
           {
-            href: 'https://github.com/RailsEventStore/rails_event_store',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/RailsEventStore/rails_event_store",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
 
         links: [
           {
-            title: 'Rails Event Store',
+            title: "Rails Event Store",
             items: [
               {
-                label: 'Community',
-                to: '/community',
+                label: "Community",
+                to: "/community",
               },
               {
-                label: 'Support',
-                to: '/support',
+                label: "Support",
+                to: "/support",
               },
               {
-                label: 'Contributing',
-                to: '/contributing',
+                label: "Contributing",
+                to: "/contributing",
               },
             ],
           },
           {
-            title: 'Documentation',
+            title: "Documentation",
             items: [
               {
-                label: 'Geting Started',
-                to: '/docs/start',
-              }
+                label: "Geting Started",
+                to: "/docs/start",
+              },
             ],
           },
           {
-            title: 'Connect',
+            title: "Connect",
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/rails-event-store',
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/rails-event-store",
               },
               {
-                label: 'Discord',
-                href: 'https://discord.gg/qjPr9ZBpX6',
+                label: "Discord",
+                href: "https://discord.gg/qjPr9ZBpX6",
               },
               {
-                label: 'X',
-                href: 'https://x.com/RailsEventStore',
+                label: "X",
+                href: "https://x.com/RailsEventStore",
               },
             ],
           },
-         
         ],
         copyright: `<p class="mt-8">Supported by <a href="https://arkency.com" target="_blank">Arkency</a></p>`,
       },
       prism: {
         theme: prismThemes.vsLight,
         darkTheme: prismThemes.dracula,
-        additionalLanguages: ['bash', 'ruby'],
+        additionalLanguages: ["bash", "ruby"],
       },
     }),
 
-    plugins: [tailwindPlugin]
+  plugins: [tailwindPlugin],
 };
 
 export default config;
