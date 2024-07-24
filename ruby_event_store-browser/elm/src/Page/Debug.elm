0b42fb3cefc2c007ff0622eecebc8779bca7caf5
Łukasz Reszke <lukaszreszke93@gmail.com>
2024-07-24 14:32:59 +0200

    Revert "Format Page.Debug"
    This reverts commit e0b6a24c013cb0a4d8732e3419c5b75ac2655cac.

--- ruby_event_store-browser/elm/src/Page/Debug.elm
+++ ruby_event_store-browser/elm/src/Page/Debug.elm
@@ -1,21 +1,17 @@
 module Page.Debug exposing (..)
 
-import Flags exposing (Flags)
 import Html exposing (..)
+import Flags exposing (Flags)
 
-
-type alias Model =
-    { resVersion : String, repositoryAdapter : String }
-
+type alias Model = { resVersion : String, repositoryAdapter : String }
 
 init : Flags -> Model
-init flags =
-    { resVersion = flags.resVersion, repositoryAdapter = flags.repositoryAdapter }
+init flags  =
+  { resVersion = flags.resVersion, repositoryAdapter = flags.repositoryAdapter }
 
-
-view : Model -> Html a
+view: Model -> Html a
 view model =
-    div []
-        [ p [] [ text ("RubyEventStore version: " ++ model.resVersion) ]
-        , p [] [ text ("RubyEventStore adapter: " ++ model.repositoryAdapter) ]
-        ]
+    div [] [
+        p [] [text ("RubyEventStore version: " ++ model.resVersion)],
+        p [] [text ("RubyEventStore adapter: " ++ model.repositoryAdapter)]
+    ]
