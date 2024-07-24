dede46747a1d1a6c2cc465f472568ef082b9b8de
Łukasz Reszke <lukaszreszke93@gmail.com>
2024-07-24 15:08:14 +0200

    Revert "lowercase"
    This reverts commit d22b979cca41bff080d5eb0b48488f46849a2b64.

--- ruby_event_store-active_record/lib/ruby_event_store/active_record/event_repository_reader.rb
+++ ruby_event_store-active_record/lib/ruby_event_store/active_record/event_repository_reader.rb
@@ -58,7 +58,7 @@ module RubyEventStore
       end
 
       def search_streams(stream)
-        @stream_klass.where("lower(stream) LIKE ?", "#{stream.downcase}%").order("position DESC, id DESC").pluck(:stream).map { |name| Stream.new(name) }
+        @stream_klass.where("stream LIKE ?", "#{stream}%").order("position DESC, id DESC").pluck(:stream).map { |name| Stream.new(name) }
       end
 
       private
