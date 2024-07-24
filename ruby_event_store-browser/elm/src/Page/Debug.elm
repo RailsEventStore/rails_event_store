adde5f135d6f4c45dd44cb2ee5eb2e42ac532f16
Łukasz Reszke <lukaszreszke93@gmail.com>
2024-07-24 14:51:06 +0200

    Revert "Add repositoryAdapter eml flag"
    This reverts commit 2d8de23e070eaf5cecb59d28a79a96fbf9f44e2b.

--- ruby_event_store-browser/elm/src/Page/Debug.elm
+++ ruby_event_store-browser/elm/src/Page/Debug.elm
@@ -1,17 +1,13 @@
 module Page.Debug exposing (..)
 
 import Html exposing (..)
-import Flags exposing (Flags)
 
-type alias Model = { resVersion : String, repositoryAdapter : String }
+type alias Model = { resVersion : String }
 
-init : Flags -> Model
-init flags  =
-  { resVersion = flags.resVersion, repositoryAdapter = flags.repositoryAdapter }
+init : String -> Model
+init resVersion =
+  { resVersion = resVersion }
 
 view: Model -> Html a
 view model =
-    div [] [
-        p [] [text ("RubyEventStore version: " ++ model.resVersion)],
-        p [] [text ("RubyEventStore adapter: " ++ model.repositoryAdapter)]
-    ]
+    text ("RubyEventStore version: " ++ model.resVersion)
