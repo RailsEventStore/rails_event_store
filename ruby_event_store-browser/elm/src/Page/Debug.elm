5b4930fa3e8d58f579c9ff8ee7e0d6359214ad44
Łukasz Reszke <lukaszreszke93@gmail.com>
2024-07-25 08:39:30 +0200

    Revert "formatting"
    This reverts commit df409f4dabcf6b09e8fd5e5a05096609756cde92.

--- ruby_event_store-browser/elm/src/Page/Debug.elm
+++ ruby_event_store-browser/elm/src/Page/Debug.elm
@@ -1,45 +1,37 @@
 module Page.Debug exposing (..)
 
-import Api exposing (Stats, getStats)
-import Flags exposing (Flags)
 import Html exposing (..)
+import Flags exposing (Flags)
+import Api exposing (getStats, Stats)
 import Http
 
-
-type alias Model =
-    { resVersion : String, repositoryAdapter : String, eventsInTotal : String }
-
+type alias Model = { resVersion : String, repositoryAdapter : String, eventsInTotal : String }
 
 type Msg
-    = GotStats (Result Http.Error Stats)
-
+  = GotStats (Result Http.Error Stats)
 
 init : Flags -> Model
-init flags =
+init flags  =
     { resVersion = flags.resVersion, repositoryAdapter = flags.repositoryAdapter, eventsInTotal = "" }
-
 
 initCmd : Flags -> Cmd Msg
 initCmd flags =
     getStats GotStats flags
 
-
-view : Model -> Html a
+view: Model -> Html a
 view model =
-    div []
-        [ p [] [ text ("RubyEventStore version: " ++ model.resVersion) ]
-        , p [] [ text ("RubyEventStore adapter: " ++ model.repositoryAdapter) ]
-        , p [] [ text ("Events in total: " ++ model.eventsInTotal) ]
-        ]
+    div [] [
+        p [] [text ("RubyEventStore version: " ++ model.resVersion)],
+        p [] [text ("RubyEventStore adapter: " ++ model.repositoryAdapter)],
+        p [] [text ("Events in total: " ++ model.eventsInTotal)]
+    ]
 
-
-update : Msg -> Model -> ( Model, Cmd Msg )
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-    case msg of
-        GotStats result ->
-            case result of
-                Ok stats ->
-                    ( { model | eventsInTotal = String.fromInt stats.eventsInTotal }, Cmd.none )
-
-                Err _ ->
-                    ( model, Cmd.none )
+  case msg of
+    GotStats result ->
+      case result of
+        Ok stats ->
+          ({ model | eventsInTotal = String.fromInt stats.eventsInTotal }, Cmd.none)
+        Err _ ->
+          (model, Cmd.none)
