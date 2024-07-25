77cd26b8c1f1f844d338c613a5733f1452bb6b7d
Łukasz Reszke <lukaszreszke93@gmail.com>
2024-07-25 09:29:44 +0200

    Revert "Debug module with its own model"
    This reverts commit 8a61cd9ca379d6d8d9495714ccd29597fab43ec1.

--- ruby_event_store-browser/elm/src/Page/Debug.elm
+++ ruby_event_store-browser/elm/src/Page/Debug.elm
@@ -2,12 +2,6 @@ module Page.Debug exposing (..)
 
 import Html exposing (..)
 
-type alias Model = { resVersion : String }
-
-init : String -> Model
-init resVersion =
-  { resVersion = resVersion }
-
-view: Model -> Html a
-view model =
-    text ("RubyEventStore version: " ++ model.resVersion)
+view: Html a
+view =
+    text "to be filled with some helpful content"
