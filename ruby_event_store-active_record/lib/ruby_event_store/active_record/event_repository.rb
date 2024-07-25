c653326b867d730485c6d5966737817d1b936c56
Łukasz Reszke <lukaszreszke93@gmail.com>
2024-07-25 09:37:15 +0200

    Revert "Show real repository adapter on Debug page"
    This reverts commit 89d0275dbaf2fb60cac430a5ef8e6d5b74c75714.

--- ruby_event_store-active_record/lib/ruby_event_store/active_record/event_repository.rb
+++ ruby_event_store-active_record/lib/ruby_event_store/active_record/event_repository.rb
@@ -81,10 +81,6 @@ module RubyEventStore
         @repo_reader.event_in_stream?(event_id, stream)
       end
 
-      def specification
-        "#{self.class.name} with #{::ActiveRecord::Base.connection.adapter_name} db adapter"
-      end
-
       private
 
       attr_reader :serializer
