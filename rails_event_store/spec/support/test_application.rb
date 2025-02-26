47bec3e34d52ce165b641c02a6669594724ea878
Mirosław Pragłowski <m@praglowski.com>
2025-02-26 11:13:38 +0100

    Opt in for new preserve time zone behavior
    Handles:
    
    ```
    DEPRECATION WARNING: `to_time` will always preserve the receiver timezone
    rather than system local time in Rails 8.1. To opt in to the new behavior,
    set `config.active_support.to_time_preserves_timezone = :zone`.
    ```

--- rails_event_store/spec/support/test_application.rb
+++ rails_event_store/spec/support/test_application.rb
@@ -9,6 +9,7 @@ class TestApplication < Rails::Application
   config.eager_load = false
   config.secret_key_base = SecureRandom.hex(16)
   config.event_store = RailsEventStore::Client.new
+  config.active_support.to_time_preserves_timezone = :zone
 
   routes.append { mount RailsEventStore::Browser => "/res" }
   routes.default_url_options = { host: "example.org" }
