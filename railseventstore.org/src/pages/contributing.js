83d5775a3d845975e0ff45f547fd88ebdc604d59
Mirosław Pragłowski <m@praglowski.com>
2025-04-10 14:20:01 +0200

    Contributing guide styling

--- railseventstore.org/src/pages/contributing.js
+++ railseventstore.org/src/pages/contributing.js
@@ -21,8 +21,8 @@ export default function Contributing() {
           </div>
         </header>
 
-        <div className="max-w-5xl  text-balance px-4 mx-auto divide-y divide-gray-200  dark:divide-gray-800 lg:px-8 xl:px-12 divide-solid *:!border-x-0 *:py-12">
-          <div className="flex flex-wrap flex-col gap-4">
+        <div className="max-w-5xl  text-balance px-4 mx-auto divide-y divide-gray-200  dark:divide-gray-800 lg:px-8 xl:px-12 divide-solid *:!border-x-0 *:py-12 prose prose-lg dark:prose-invert">
+          <div className="flex flex-wrap flex-col">
             <div className="w-full text-2xl font-bold">
               <h2 className="mb-1.5">Contributing to RailsEventStore organization repositories</h2>
               <span className="block text-base font-bold text-gray-500 uppercase">
@@ -30,25 +30,25 @@ export default function Contributing() {
               </span>
             </div>
             <div className="w-full text-xl">
-              <h2 id="found-a-bug-have-a-question" className="font-semibold mb-2">Found a bug? Have a question?</h2>
+              <h2 id="found-a-bug-have-a-question" className="font-semibold">Found a bug? Have a question?</h2>
               <ul>
-                <li className="py-2 list-disc">
+                <li>
                   <a href="https://help.github.com/articles/creating-an-issue/">Create a new issue</a>
                   , assuming one does not already exist.
                 </li>
-                <li className="py-2 list-disc">Clearly describe the problem including steps to reproduce when it is a bug.</li>
-                <li className="py-2 list-disc">If possible provide a Pull Request with failing test case.</li>
+                <li>Clearly describe the problem including steps to reproduce when it is a bug.</li>
+                <li>If possible provide a Pull Request with failing test case.</li>
               </ul>
             </div>
             <div className="w-full text-xl ">
-              <h2 id="prepare-a-pull-request" className="font-semibold mb-2">Prepare a Pull Request</h2>
+              <h2 id="prepare-a-pull-request" className="font-semibold">Prepare a Pull Request</h2>
               <ul>
-                <li className="py-2 list-disc">
+                <li>
                   Fork the{" "}
                   <a href="https://github.com/RailsEventStore/rails_event_store">RailsEventStore monorepo</a>
                 </li>
               </ul>
-              <div className="highlight text-base py-2">
+              <div className="highlight text-base">
                 <pre className="syntax-highlight text-base plaintext">
                   <code>
                     {" "}
@@ -57,23 +57,23 @@ export default function Contributing() {
                 </pre>
               </div>
               <ul>
-                <li className="py-2 list-disc">Make sure you have all latest changes or rebase your forked repository master branch with RailsEventStore master branch</li>
+                <li>Make sure you have all latest changes or rebase your forked repository master branch with RailsEventStore master branch</li>
               </ul>
-              <div className="highlight text-base py-2">
+              <div className="highlight text-base">
                 <pre className="syntax-highlight text-base plaintext">
                   <code> cd rails_event_store make rebase</code>
                 </pre>
               </div>
               <ul>
-                <li className="py-2 list-disc">Create a pull request branch</li>
+                <li>Create a pull request branch</li>
               </ul>
-              <div className="highlight text-base py-2">
+              <div className="highlight text-base">
                 <pre className="syntax-highlight text-base plaintext">
                   <code> git checkout -b new_branch</code>
                 </pre>
               </div>
               <ul>
-                <li className="py-2 list-disc">
+                <li>
                   <p>
                     Implement your feature, don&#x27;t forget about tests &amp; documentation (to see how to work with documentation files check{" "}
                     <a href="https://github.com/RailsEventStore/rails_event_store/blob/master/railseventstore.org/README.md">
@@ -81,11 +81,11 @@ export default function Contributing() {
                     </a>
                   </p>
                 </li>
-                <li className="py-2 list-disc">
+                <li>
                   <p>Make sure your code pass all tests</p>
                 </li>
               </ul>
-              <div className="highlight text-base py-2">
+              <div className="highlight text-base">
                 <pre className="syntax-highlight text-base plaintext">
                   <code> make test</code>
                 </pre>
@@ -96,15 +96,15 @@ export default function Contributing() {
                 again) there.
               </p>
               <ul>
-                <li className="py-2 list-disc">Make sure your changes survive mutation testing</li>
+                <li>Make sure your changes survive mutation testing</li>
               </ul>
-              <div className="highlight text-base py-2">
+              <div className="highlight text-base">
                 <pre className="syntax-highlight text-base plaintext">
                   <code> make mutate</code>
                 </pre>
               </div>
               <p>Will run mutation tests for all projects. The same command executed in specific project&#x27;s folder will run mutation tests only for that project. Mutation tests might be time consuming, so you could try to limit the scope of mutations to some specific subjects:</p>
-              <div className="highlight text-base py-2">
+              <div className="highlight text-base">
                 <pre className="syntax-highlight text-base plaintext">
                   <code> make mutate SUBJECT=code_to_mutate</code>
                 </pre>
@@ -117,7 +117,7 @@ export default function Contributing() {
                 .
               </p>
               <ul>
-                <li className="py-2 list-disc">
+                <li>
                   Don&#x27;t forget to{" "}
                   <a href="https://help.github.com/articles/creating-a-pull-request-from-a-fork/">create a Pull Request</a>
                   . You could do it even if not everything is ready. The sooner you will share your changes the quicker feedback you will get.
@@ -125,7 +125,7 @@ export default function Contributing() {
               </ul>
             </div>
           </div>
-          <div className="flex flex-wrap flex-col gap-2">
+          <div className="flex flex-wrap flex-col">
             <div className="w-full text-2xl font-bold">
               <h2 className="mb-1.5">License</h2>
             </div>
