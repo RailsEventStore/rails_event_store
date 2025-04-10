1f1aaa97e0aa37a1ec13a06e7cd0ff0fa70442ce
Mirosław Pragłowski <m@praglowski.com>
2025-04-10 14:39:11 +0200

    Support page styling

--- railseventstore.org/src/pages/support.js
+++ railseventstore.org/src/pages/support.js
@@ -21,16 +21,16 @@ export default function Support() {
           </div>
         </header>
 
-        <div className="max-w-5xl  text-balance px-4 mx-auto divide-y divide-gray-200  dark:divide-gray-800 lg:px-8 xl:px-12 divide-solid *:!border-x-0 *:py-12">
+        <div className="max-w-5xl  text-balance px-4 mx-auto divide-y divide-gray-200  dark:divide-gray-800 lg:px-8 xl:px-12 divide-solid *:!border-x-0 *:py-12 prose prose-lg dark:prose-invert">
           <div className="flex flex-wrap ">
             <div className="w-full text-2xl font-bold sm:w-2/5">
-              <h2 className="mb-1.5">Community Help</h2>
+              <h2 className="my-1.5">Community Help</h2>
               <span className="block text-base font-bold text-gray-500 uppercase">
                 Free
               </span>
             </div>
             <div className="w-full text-xl sm:sm:w-3/5">
-              <p className="mb-8 ">
+              <p className="mt-1.5 mb-8">
                 Rails Event Store has an active{" "}
                 <a href="/community">community</a> of awesome contributors. They
                 report encountered issues, propose and discuss possible
@@ -48,13 +48,13 @@ export default function Support() {
           </div>
           <div className="flex flex-wrap">
             <div className="w-full text-2xl font-bold sm:w-2/5">
-              <h2 className="mb-1.5">Domain-Driven Rails</h2>
+              <h2 className="my-1.5">Domain-Driven Rails</h2>
               <span className="block text-base font-bold text-gray-500 uppercase">
                 Starting from $49
               </span>
             </div>
             <div className="w-full text-xl sm:sm:w-3/5">
-              <p className="mb-8">
+              <p className="mt-1.5 mb-8">
                 Struggling with complex Rails app and business domain?
                 Domain-Driven Rails will help you:
               </p>
@@ -78,18 +78,18 @@ export default function Support() {
           </div>
           <div className="flex flex-wrap">
             <div className="w-full text-2xl font-bold sm:w-2/5">
-              <h2 className="mb-1.5">Arkademy Subscription</h2>
+              <h2 className="my-1.5">Arkademy Subscription</h2>
               <span className="block text-base font-bold text-gray-500 uppercase">
                 $49 / month
               </span>
             </div>
             <div className="w-full text-xl sm:sm:w-3/5">
-              <p className="mb-8">
+              <p className="mt-1.5 mb-8">
                 Get access to Rails Architect Masterclass, Domain-Driven Rails
                 video course, Anti-IF course, TDD video class and more. Get in
                 touch with fellow students and instructors on Discord.
               </p>
-              <p className="mb-8">
+              <p className="mt-1.5 mb-8">
                 On Arkademy you will learn everything about DDD, Backend
                 Architecture, UI Architecture Styles and Testing.
               </p>
@@ -103,13 +103,13 @@ export default function Support() {
           </div>
           <div className="flex flex-wrap">
             <div className="w-full text-2xl font-bold sm:w-2/5">
-              <h2 className="mb-1.5">Rails Event Store Mentoring</h2>
+              <h2 className="my-1.5">Rails Event Store Mentoring</h2>
               <span className="block text-base font-bold text-gray-500 uppercase">
                 $2,000 / month
               </span>
             </div>
             <div className="w-full text-xl sm:sm:w-3/5">
-              <p className="mb-8">
+              <p className="mt-1.5 mb-8">
                 3–month mentoring focused on initial development — first year of
                 your project. Package includes code review and questions and
                 answers calls every two weeks.
@@ -124,13 +124,13 @@ export default function Support() {
           </div>
           <div className="flex flex-wrap">
             <div className="w-full text-2xl font-bold sm:w-2/5">
-              <h2 className="mb-1.5">Custom Feature Development</h2>
+              <h2 className="my-1.5">Custom Feature Development</h2>
               <span className="block text-base font-bold text-gray-500 uppercase">
                 Ask for quote
               </span>
             </div>
             <div className="w-full text-xl sm:sm:w-3/5">
-              <p className="mb-8">
+              <p className="mt-1.5 mb-8">
                 You ask for a missing Rails Event Store feature, we give you a
                 fixed-price offer.
               </p>
@@ -144,13 +144,13 @@ export default function Support() {
           </div>
           <div className="flex flex-wrap">
             <div className="w-full text-2xl font-bold sm:w-2/5">
-              <h2 className="mb-1.5">One-shot Help</h2>
+              <h2 className="my-1.5">One-shot Help</h2>
               <span className="block text-base font-bold text-gray-500 uppercase">
                 Ask for quote
               </span>
             </div>
             <div className="w-full text-xl sm:sm:w-3/5">
-              <p className="mb-8">
+              <p className="mt-1.5 mb-8">
                 You're stuck with Rails Event Store — we can help. Price depends
                 on the speed of our availability — we can fix something for you
                 in a week or month.
@@ -165,13 +165,13 @@ export default function Support() {
           </div>
           <div className="flex flex-wrap">
             <div className="w-full text-2xl font-bold sm:w-2/5">
-              <h2 className="mb-1.5">Legacy Rails/DDD Mentoring</h2>
+              <h2 className="my-1.5">Legacy Rails/DDD Mentoring</h2>
               <span className="block text-base font-bold text-gray-500 uppercase">
                 Ask for quote
               </span>
             </div>
             <div className="w-full text-xl sm:sm:w-3/5">
-              <p className="mb-8">
+              <p className="mt-1.5 mb-8">
                 6–month mentoring focused on existing apps — older than 2 years.
                 Package includes code review and questions and answers calls
                 every two weeks.
@@ -186,13 +186,13 @@ export default function Support() {
           </div>
           <div className="flex flex-wrap">
             <div className="w-full text-2xl font-bold sm:w-2/5">
-              <h2 className="leading-tight">From Event Storming to an App</h2>
+              <h2 className="leading-tight my-1.5">From Event Storming to an App</h2>
               <span className="block text-base font-bold text-gray-500 uppercase">
                 Ask for quote
               </span>
             </div>
             <div className="w-full text-xl sm:sm:w-3/5">
-              <p className="mb-8">
+              <p className="mt-1.5 mb-8">
                 2–days workshop in your office or in Wrocław, Poland. 1st day
                 consists on an Event Storming session on your domain and
                 bootstraping an application. 2nd day focused on{" "}
@@ -212,13 +212,13 @@ export default function Support() {
           </div>
           <div className="flex flex-wrap">
             <div className="w-full text-2xl font-bold sm:w-2/5">
-              <h2 className="mb-1.5">Rails DDD Workshop</h2>
+              <h2 className="my-1.5">Rails DDD Workshop</h2>
               <span className="block text-base font-bold text-gray-500 uppercase">
                 Ask for quote
               </span>
             </div>
             <div className="w-full text-xl sm:sm:w-3/5">
-              <p className="mb-8">
+              <p className="mt-1.5 mb-8">
                 2-days workshop focused on DDD foundations and building blocks.
                 Open for public or for your company only.
               </p>
@@ -232,13 +232,13 @@ export default function Support() {
           </div>
           <div className="flex flex-wrap">
             <div className="w-full text-2xl font-bold sm:w-2/5">
-              <h2 className="mb-1.5">Full time help from Arkency</h2>
+              <h2 className="my-1.5">Full time help from Arkency</h2>
               <span className="block text-base font-bold text-gray-500 uppercase">
                 Ask for quote
               </span>
             </div>
             <div className="w-full text-xl sm:sm:w-3/5">
-              <p className="mb-8">
+              <p className="mt-1.5 mb-8">
                 Minimum 2 experienced remote developers from Arkency on
                 short-term or long-term contract.
               </p>
