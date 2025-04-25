6db4df87cf2e9387b94698653dc11cf93bb68275
Mirosław Pragłowski <m@praglowski.com>
2025-04-25 12:06:21 +0200

    Rename event_types in subscriptions to topics

--- ruby_event_store/lib/ruby_event_store/broker.rb
+++ ruby_event_store/lib/ruby_event_store/broker.rb
@@ -12,9 +12,9 @@ module RubyEventStore
       subscribers.each { |subscriber| dispatcher.call(subscriber, event, record) }
     end
 
-    def add_subscription(subscriber, event_types)
+    def add_subscription(subscriber, topics)
       verify_subscription(subscriber)
-      subscriptions.add_subscription(subscriber, event_types)
+      subscriptions.add_subscription(subscriber, topics)
     end
 
     def add_global_subscription(subscriber)
@@ -22,9 +22,9 @@ module RubyEventStore
       subscriptions.add_global_subscription(subscriber)
     end
 
-    def add_thread_subscription(subscriber, event_types)
+    def add_thread_subscription(subscriber, topics)
       verify_subscription(subscriber)
-      subscriptions.add_thread_subscription(subscriber, event_types)
+      subscriptions.add_thread_subscription(subscriber, topics)
     end
 
     def add_thread_global_subscription(subscriber)
