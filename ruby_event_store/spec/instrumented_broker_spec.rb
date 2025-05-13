b53e56ff16548aaa4f1ed89189ffad0d070b87f2
Mirosław Pragłowski <m@praglowski.com>
2025-05-13 16:36:14 +0200

    Add instrumentation for message broker
    As message broker can be now passed as an argument it should be instrumented separatelly

---
+++ ruby_event_store/spec/instrumented_broker_spec.rb
@@ -0,0 +1,184 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "ruby_event_store/spec/broker_lint"
+require "active_support/core_ext/object/try"
+require "active_support/isolated_execution_state"
+require "active_support/notifications"
+
+module RubyEventStore
+  ::RSpec.describe InstrumentedBroker do
+    describe "#call" do
+      specify "wraps around original implementation" do
+        some_broker = spy
+        instrumented_broker = InstrumentedBroker.new(some_broker, ActiveSupport::Notifications)
+        event = Object.new
+        record = Object.new
+
+        instrumented_broker.call(event, record)
+
+        expect(some_broker).to have_received(:call).with(event, record)
+      end
+
+      specify "instruments" do
+        instrumented_broker = InstrumentedBroker.new(spy, ActiveSupport::Notifications)
+        subscribe_to("call.broker.rails_event_store") do |notification_calls|
+          event = Object.new
+          record = Object.new
+
+          instrumented_broker.call(event, record)
+
+          expect(notification_calls).to eq([{ event: event, record: record }])
+        end
+      end
+    end
+
+    describe "#add_subscription" do
+      specify "wraps around original implementation" do
+        some_broker = spy
+        instrumented_broker = InstrumentedBroker.new(some_broker, ActiveSupport::Notifications)
+        subscriber = -> {  }
+        event_types = []
+
+        instrumented_broker.add_subscription(subscriber, event_types)
+
+        expect(some_broker).to have_received(:add_subscription).with(subscriber, event_types)
+      end
+
+      specify "instruments" do
+        instrumented_broker = InstrumentedBroker.new(spy, ActiveSupport::Notifications)
+        subscribe_to("add_subscription.broker.rails_event_store") do |notification_calls|
+          subscriber = -> {  }
+          event_types = []
+
+          instrumented_broker.add_subscription(subscriber, event_types)
+
+          expect(notification_calls).to eq([{ subscriber: subscriber, event_types: event_types }])
+        end
+      end
+    end
+
+    describe "#add_global_subscription" do
+      specify "wraps around original implementation" do
+        some_broker = spy
+        instrumented_broker = InstrumentedBroker.new(some_broker, ActiveSupport::Notifications)
+        subscriber = -> {  }
+
+        instrumented_broker.add_global_subscription(subscriber)
+
+        expect(some_broker).to have_received(:add_global_subscription).with(subscriber)
+      end
+
+      specify "instruments" do
+        instrumented_broker = InstrumentedBroker.new(spy, ActiveSupport::Notifications)
+        subscribe_to("add_global_subscription.broker.rails_event_store") do |notification_calls|
+          subscriber = -> {  }
+
+          instrumented_broker.add_global_subscription(subscriber)
+
+          expect(notification_calls).to eq([{ subscriber: subscriber }])
+        end
+      end
+    end
+
+    describe "#add_thread_subscription" do
+      specify "wraps around original implementation" do
+        some_broker = spy
+        instrumented_broker = InstrumentedBroker.new(some_broker, ActiveSupport::Notifications)
+        subscriber = -> {  }
+        event_types = []
+
+        instrumented_broker.add_thread_subscription(subscriber, event_types)
+
+        expect(some_broker).to have_received(:add_thread_subscription).with(subscriber, event_types)
+      end
+
+      specify "instruments" do
+        instrumented_broker = InstrumentedBroker.new(spy, ActiveSupport::Notifications)
+        subscribe_to("add_thread_subscription.broker.rails_event_store") do |notification_calls|
+          subscriber = -> {  }
+          event_types = []
+
+          instrumented_broker.add_thread_subscription(subscriber, event_types)
+
+          expect(notification_calls).to eq([{ subscriber: subscriber, event_types: event_types }])
+        end
+      end
+    end
+
+    describe "#add_thread_global_subscription" do
+      specify "wraps around original implementation" do
+        some_broker = spy
+        instrumented_broker = InstrumentedBroker.new(some_broker, ActiveSupport::Notifications)
+        subscriber = -> {  }
+
+        instrumented_broker.add_thread_global_subscription(subscriber)
+
+        expect(some_broker).to have_received(:add_thread_global_subscription).with(subscriber)
+      end
+
+      specify "instruments" do
+        instrumented_broker = InstrumentedBroker.new(spy, ActiveSupport::Notifications)
+        subscribe_to("add_thread_global_subscription.broker.rails_event_store") do |notification_calls|
+          subscriber = -> {  }
+
+          instrumented_broker.add_thread_global_subscription(subscriber)
+
+          expect(notification_calls).to eq([{ subscriber: subscriber }])
+        end
+      end
+    end
+
+    describe "#all_subscriptions_for" do
+      specify "wraps around original implementation" do
+        some_broker = spy
+        instrumented_broker = InstrumentedBroker.new(some_broker, ActiveSupport::Notifications)
+        event_type = String.new
+
+        instrumented_broker.all_subscriptions_for(event_type)
+
+        expect(some_broker).to have_received(:all_subscriptions_for).with(event_type)
+      end
+
+      specify "instruments" do
+        instrumented_broker = InstrumentedBroker.new(spy, ActiveSupport::Notifications)
+        subscribe_to("all_subscriptions_for.broker.rails_event_store") do |notification_calls|
+          event_type = String.new
+
+          instrumented_broker.all_subscriptions_for(event_type)
+
+          expect(notification_calls).to eq([{ event_type: event_type }])
+        end
+      end
+    end
+
+    specify "method unknown by instrumentation but known by broker" do
+      some_broker = double("Some broker", custom_method: 42)
+      instrumented_broker = InstrumentedBroker.new(some_broker, ActiveSupport::Notifications)
+      block = -> { "block" }
+      instrumented_broker.custom_method("arg", keyword: "keyarg", &block)
+
+      expect(instrumented_broker).to respond_to(:custom_method)
+      expect(some_broker).to have_received(:custom_method).with("arg", keyword: "keyarg") do |&received_block|
+        expect(received_block).to be(block)
+      end
+    end
+
+    specify "method unknown by instrumentation and unknown by broker" do
+      some_broker = Broker.new(subscriptions: Object.new, dispatcher: Object.new)
+      instrumented_broker = InstrumentedBroker.new(some_broker, ActiveSupport::Notifications)
+
+      expect(instrumented_broker).not_to respond_to(:arbitrary_method_name)
+      expect { instrumented_broker.arbitrary_method_name }.to raise_error(
+        NoMethodError,
+        /undefined method.+arbitrary_method_name.+RubyEventStore::InstrumentedBroker/
+      )
+    end
+
+    def subscribe_to(name)
+      received_payloads = []
+      callback = ->(_name, _start, _finish, _id, payload) { received_payloads << payload }
+      ActiveSupport::Notifications.subscribed(callback, name) { yield received_payloads }
+    end
+  end
+end
