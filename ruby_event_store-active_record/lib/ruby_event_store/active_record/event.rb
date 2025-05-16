30c9a7afc27951bbd415ab55768f6c01f7641705
Paweł Pacana <pawel.pacana@gmail.com>
2025-05-16 22:37:31 +0200

    Use non-private and available API
    The previously-used, undocumented one has been removed:
    https://github.com/rails/rails/issues/52685

--- ruby_event_store-active_record/lib/ruby_event_store/active_record/event.rb
+++ ruby_event_store-active_record/lib/ruby_event_store/active_record/event.rb
@@ -8,13 +8,17 @@ module RubyEventStore
       self.primary_key = :id
       self.table_name = "event_store_events"
 
-      skip_json_serialization = ->(initial_column_type) do
-        %i[json jsonb].include?(initial_column_type.type) ? ActiveModel::Type::Value.new : initial_column_type
-      end
+      def self.type_for_attribute(name, &block)
+        initial_column_type = super
 
-      attribute :data, skip_json_serialization
-      attribute :metadata, skip_json_serialization
+        if %i[json jsonb].include?(initial_column_type.type)
+          ActiveModel::Type::Value.new
+        else
+          initial_column_type
+        end
+      end
     end
+
     private_constant :Event
 
     class EventInStream < ::ActiveRecord::Base
