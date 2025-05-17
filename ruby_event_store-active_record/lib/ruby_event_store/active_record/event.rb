e692c9b31fd5817f73a2831c221d66dcc67e3c14
Paweł Pacana <pawel.pacana@gmail.com>
2025-05-17 18:54:28 +0200

    Legit concern use

--- ruby_event_store-active_record/lib/ruby_event_store/active_record/event.rb
+++ ruby_event_store-active_record/lib/ruby_event_store/active_record/event.rb
@@ -8,22 +8,28 @@ module RubyEventStore
       self.primary_key = :id
       self.table_name = "event_store_events"
 
-      def self.skip_json_serialization(cast_type)
-        if %i[json jsonb].include?(cast_type.type)
-          ActiveModel::Type::Value.new
-        else
-          cast_type
-        end
-      end
+      include(
+        Module.new do
+          extend ActiveSupport::Concern
 
-      if ::ActiveRecord.version >= Gem::Version.new("7.2.0")
-        def self.hook_attribute_type(name, cast_type)
-          skip_json_serialization(cast_type)
-        end
-      else
-        attribute :data, &method(:skip_json_serialization)
-        attribute :metadata, &method(:skip_json_serialization)
-      end
+          skip_json_serialization = ->(cast_type) do
+            %i[json jsonb].include?(cast_type.type) ? ActiveModel::Type::Value.new : cast_type
+          end
+
+          if ::ActiveRecord.version >= Gem::Version.new("7.2.0")
+            class_methods do
+              define_method(:hook_attribute_type) do |name, cast_type|
+                skip_json_serialization[cast_type]
+              end
+            end
+          else
+            included do
+              attribute :data, skip_json_serialization
+              attribute :metadata, skip_json_serialization
+            end
+          end
+        end,
+      )
     end
     private_constant :Event
 
