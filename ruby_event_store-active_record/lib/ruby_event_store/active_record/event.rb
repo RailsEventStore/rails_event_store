007c0c70de722142ae4a289b63100da8bd88a590
Paweł Pacana <pawel.pacana@gmail.com>
2025-05-17 18:32:10 +0200

    Why not both?
    Use new hook_attribute_type API available since 7.2.0 and an older,
    private and already removed one for AR 7.1.0 (and below, which we don't
    support as of now).

--- ruby_event_store-active_record/lib/ruby_event_store/active_record/event.rb
+++ ruby_event_store-active_record/lib/ruby_event_store/active_record/event.rb
@@ -8,13 +8,21 @@ module RubyEventStore
       self.primary_key = :id
       self.table_name = "event_store_events"
 
-      def self.hook_attribute_type(name, cast_type)
-        case cast_type.type
-        when :json, :jsonb
+      def self.skip_json_serialization(cast_type)
+        if %i[json jsonb].include?(cast_type.type)
           ActiveModel::Type::Value.new
         else
-          super
+          cast_type
         end
+      end
+
+      if ::ActiveRecord.version >= Gem::Version.new("7.2.0")
+        def self.hook_attribute_type(name, cast_type)
+          skip_json_serialization(cast_type)
+        end
+      else
+        attribute :data, &method(:skip_json_serialization)
+        attribute :metadata, &method(:skip_json_serialization)
       end
     end
     private_constant :Event
