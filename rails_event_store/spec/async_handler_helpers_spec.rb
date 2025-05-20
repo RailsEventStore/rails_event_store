b07173c2d35a11855119785374f2f50a18b98607
Paweł Pacana <pawel.pacana@gmail.com>
2025-05-20 16:52:41 +0200

    DRY that makes warnings silent

--- rails_event_store/spec/async_handler_helpers_spec.rb
+++ rails_event_store/spec/async_handler_helpers_spec.rb
@@ -141,21 +141,17 @@ module RailsEventStore
     end
 
     specify "ActiveJob with CorrelatedHandler prepended" do
-      HandlerA = Class.new(MetadataHandler)
-      HandlerA.prepend RailsEventStore::CorrelatedHandler
-      HandlerA.prepend RailsEventStore::AsyncHandler
-      event_store.subscribe_to_all_events(HandlerA)
-      event_store.publish(ev = RubyEventStore::Event.new)
-      expect($queue.pop).to eq({ correlation_id: ev.correlation_id, causation_id: ev.event_id })
+      with_correlated_handler do
+        event_store.publish(ev = RubyEventStore::Event.new)
+        expect($queue.pop).to eq({ correlation_id: ev.correlation_id, causation_id: ev.event_id })
+      end
     end
 
     specify "ActiveJob with CorrelatedHandler prepended (2)" do
-      HandlerB = Class.new(MetadataHandler)
-      HandlerB.prepend RailsEventStore::CorrelatedHandler
-      HandlerB.prepend RailsEventStore::AsyncHandler
-      event_store.subscribe_to_all_events(HandlerB)
-      event_store.publish(ev = RubyEventStore::Event.new(metadata: { correlation_id: "COID", causation_id: "CAID" }))
-      expect($queue.pop).to eq({ correlation_id: "COID", causation_id: ev.event_id })
+      with_correlated_handler do
+        event_store.publish(ev = RubyEventStore::Event.new(metadata: { correlation_id: "COID", causation_id: "CAID" }))
+        expect($queue.pop).to eq({ correlation_id: "COID", causation_id: ev.event_id })
+      end
     end
 
     specify "ActiveJob with sidekiq adapter that requires serialization", mutant: false do
@@ -170,42 +166,59 @@ module RailsEventStore
     end
 
     specify "CorrelatedHandler with event not yet scheduled with correlation_id" do
-      HandlerB = Class.new(MetadataHandler)
-      HandlerB.prepend RailsEventStore::CorrelatedHandler
-      HandlerB.prepend RailsEventStore::AsyncHandler
-      event_store.append(ev = RubyEventStore::Event.new)
-      ev = event_store.read.event(ev.event_id)
-      HandlerB.perform_now(serialize_without_correlation_id(ev))
-      expect($queue.pop).to eq({ correlation_id: nil, causation_id: ev.event_id })
+      with_correlated_handler do
+        event_store.append(ev = RubyEventStore::Event.new)
+        ev = event_store.read.event(ev.event_id)
+
+        handler.perform_now(serialize_without_correlation_id(ev))
+        expect($queue.pop).to eq({ correlation_id: nil, causation_id: ev.event_id })
+      end
     end
 
     specify "ActiveJob with AsyncHandlerJobIdOnly prepended" do
-      HandlerD = Class.new(IdOnlyHandler)
-      HandlerD.prepend AsyncHandlerJobIdOnly
-      event_store.subscribe_to_all_events(HandlerD)
-      event_store.publish(ev = RubyEventStore::Event.new)
+      with_test_handler(IdOnlyHandler) do |handler|
+        handler.prepend AsyncHandlerJobIdOnly
 
-      expect($queue.pop).to eq(ev)
+        event_store.publish(ev = RubyEventStore::Event.new)
+        expect($queue.pop).to eq(ev)
+      end
     end
 
     specify "ActiveJob with AsyncHandlerJobIdOnly prepended with event store locator" do
-      HandlerE = Class.new(IdOnlyHandler)
-      HandlerE.prepend AsyncHandlerJobIdOnly.with(event_store: nil, event_store_locator: -> { event_store })
-      event_store.subscribe_to_all_events(HandlerE)
-      event_store.publish(ev = RubyEventStore::Event.new)
+      with_test_handler(IdOnlyHandler) do |handler|
+        handler.prepend AsyncHandlerJobIdOnly.with(event_store: nil, event_store_locator: -> { event_store })
 
-      expect($queue.pop).to eq(ev)
+        event_store.publish(ev = RubyEventStore::Event.new)
+        expect($queue.pop).to eq(ev)
+      end
     end
 
     specify "ActiveJob with AsyncHandlerJobIdOnly prepended to host class" do
-      YetAnotherIdOnlyHandler.prepend AsyncHandlerJobIdOnly
-      event_store.subscribe_to_all_events(YetAnotherIdOnlyHandler)
-      event_store.publish(ev = RubyEventStore::Event.new)
+      with_test_handler(YetAnotherIdOnlyHandler) do |handler|
+        handler.prepend AsyncHandlerJobIdOnly
 
-      expect($queue.pop).to eq(ev)
+        event_store.publish(ev = RubyEventStore::Event.new)
+        expect($queue.pop).to eq(ev)
+      end
     end
 
     private
+
+    def with_test_handler(base_class)
+      stub_const("TestHandler", Class.new(base_class)) do
+        event_store.subscribe_to_all_events(TestHandler)
+        yield TestHandler
+      end
+    end
+
+    def with_correlated_handler
+      with_test_handler(MetadataHandler) do |handler|
+        handler.prepend RailsEventStore::CorrelatedHandler
+        handler.prepend RailsEventStore::AsyncHandler
+
+        yield handler
+      end
+    end
 
     def serialize_without_correlation_id(ev)
       serialized = event_store.__send__(:mapper).event_to_record(ev).serialize(RubyEventStore::Serializers::YAML).to_h
