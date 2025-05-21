0b5ef1de8d63b6ba1c730fd640d7967cc3b3f749
Mirosław Pragłowski <m@praglowski.com>
2025-05-21 15:30:20 +0200

    No warning here, keep experimental feature silent
    The batching mappings will be released as an experimental feature.
    The API might change, or even the whole feature might be changed or
    removed. That's why we do not want to warn custom mapper's devs about
    potential deprecation.
    
    See https://railseventstore.org/docs/contributing/maintenance_policy#experimental-features

--- ruby_event_store/spec/specification_reader_spec.rb
+++ ruby_event_store/spec/specification_reader_spec.rb
@@ -1,4 +1,3 @@
-
 # frozen_string_literal: true
 
 require "spec_helper"
@@ -8,16 +7,16 @@ module RubyEventStore
   ::RSpec.describe SpecificationReader do
     mk_repository = -> { InMemoryRepository.new }
 
-    let(:repository) {
+    let(:repository) do
       mk_repository.call.tap do |repo|
         repo.append_to_stream(
           [SRecord.new(event_id: "fbce0b3d-40e3-4d1d-90a1-901f1ded5a4a")],
           Stream.new("other"),
-          ExpectedVersion.none
+          ExpectedVersion.none,
         )
       end
-    }
-    let(:mapper_klass) {
+    end
+    let(:mapper_klass) do
       Class.new(Mappers::Default) do
         attr_reader :invoked_map_records_to_events
 
@@ -26,13 +25,11 @@ module RubyEventStore
           super(...)
         end
       end
-    }
+    end
 
     specify "reading invokes the mapper in batches" do
       mapper = mapper_klass.new
-      specification = Specification.new(
-        SpecificationReader.new(repository, mapper)
-      )
+      specification = Specification.new(SpecificationReader.new(repository, mapper))
       specification.to_a
       expect(mapper.invoked_map_records_to_events).to eq(true)
     end
@@ -40,11 +37,8 @@ module RubyEventStore
     specify "specification can still handle non batch compatible mappers" do
       mapper_klass.undef_method(:map_records_to_events)
       mapper = mapper_klass.new
-      expect(Warning).to receive(:warn).with("Your custom mapper does not support batch reading. This behaviour will be deprecated in future releases. You can include RubyEventStore::Mappers::BatchMapping to your mapper to make it compatible.\n", { category: nil }).once
 
-      specification = Specification.new(
-        SpecificationReader.new(repository, mapper)
-      )
+      specification = Specification.new(SpecificationReader.new(repository, mapper))
       specification.to_a
       expect(mapper.invoked_map_records_to_events).to_not eq(true)
     end
