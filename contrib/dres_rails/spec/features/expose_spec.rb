72b8bc1298fac5e9aac76dd0b3c3d94eff168e28
Paweł Pacana <pawel.pacana@gmail.com>
2025-05-28 12:17:38 +0200

    Prefer JSON serialization over YAML
    With PreserveTypes from JSONClient this serialization is lossless.

--- contrib/dres_rails/spec/features/expose_spec.rb
+++ contrib/dres_rails/spec/features/expose_spec.rb
@@ -21,9 +21,9 @@ require "ruby_event_store"
     ::ActiveRecord::Base.connection.execute("DELETE FROM event_store_events")
   end
 
-  let(:repository) { RubyEventStore::ActiveRecord::PgLinearizedEventRepository.new(serializer: YAML) }
+  let(:repository) { RubyEventStore::ActiveRecord::PgLinearizedEventRepository.new(serializer: JSON) }
   let(:res) do
-    RailsEventStore::Client.new(
+    RailsEventStore::JSONClient.new(
       repository: repository,
       correlation_id_generator: -> { "15b861b5-5697-40ae-bfea-7f01329c3385" },
     )
@@ -62,16 +62,38 @@ require "ruby_event_store"
         "events" => [
           {
             "event_id" => "dfc7f58d-aae3-4d21-8f3a-957bfa765ef8",
-            "data" => "---\n:one: 1\n",
-            "metadata" => "---\n:correlation_id: 15b861b5-5697-40ae-bfea-7f01329c3385\n",
+            "data" => JSON.dump("one" => 1),
+            "metadata" =>
+              JSON.dump(
+                "correlation_id" => "15b861b5-5697-40ae-bfea-7f01329c3385",
+                "types" => {
+                  "data" => {
+                    "one" => %w[Symbol Integer],
+                  },
+                  "metadata" => {
+                    "correlation_id" => %w[Symbol String],
+                  },
+                },
+              ),
             "valid_at" => "2018-04-07T12:30:00.000Z",
             "timestamp" => "2018-04-07T12:30:00.000Z",
             "event_type" => "MyEvent",
           },
           {
             "event_id" => "b2f58e9c-0887-4fbf-99a8-0bb19cfebeef",
-            "data" => "---\n:two: 2\n",
-            "metadata" => "---\n:correlation_id: 15b861b5-5697-40ae-bfea-7f01329c3385\n",
+            "data" => JSON.dump("two" => 2),
+            "metadata" =>
+              JSON.dump(
+                "correlation_id" => "15b861b5-5697-40ae-bfea-7f01329c3385",
+                "types" => {
+                  "data" => {
+                    "two" => %w[Symbol Integer],
+                  },
+                  "metadata" => {
+                    "correlation_id" => %w[Symbol String],
+                  },
+                },
+              ),
             "valid_at" => "2018-04-07T12:30:00.000Z",
             "timestamp" => "2018-04-07T12:30:00.000Z",
             "event_type" => "MyEvent",
