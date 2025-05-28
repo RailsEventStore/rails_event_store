bf1cee8b923072cb08f008afebb4806c8af393fd
Paweł Pacana <pawel.pacana@gmail.com>
2025-05-28 10:47:27 +0200

    Help mutant with test discovery

--- ruby_event_store/spec/mappers/batch_mapping_spec.rb
+++ ruby_event_store/spec/mappers/batch_mapping_spec.rb
@@ -4,27 +4,30 @@ require "spec_helper"
 require "ruby_event_store/spec/event_repository_lint"
 
 module RubyEventStore
-  ::RSpec.describe BatchMapping do
-    mk_repository = -> { InMemoryRepository.new }
+  module Mappers
+    ::RSpec.describe BatchMapping do
+      mk_repository = -> { InMemoryRepository.new }
 
-    let(:records) { 3.times.map { SRecord.new(event_id: SecureRandom.uuid) } }
-    let(:repository) do
-      mk_repository.call.tap { |repo| repo.append_to_stream(records, Stream.new("other"), ExpectedVersion.none) }
-    end
-
-    class CustomMapper
-      include Mappers::BatchMapping
-
-      def record_to_event(record)
-        record
+      let(:records) { 3.times.map { SRecord.new(event_id: SecureRandom.uuid) } }
+      let(:repository) do
+        mk_repository.call.tap { |repo| repo.append_to_stream(records, Stream.new("other"), ExpectedVersion.none) }
       end
-    end
 
-    specify "use batch mapping with custom mapper" do
-      mapper = CustomMapper.new
-      specification = Specification.new(SpecificationReader.new(repository, mapper, mapping: BatchMapping))
-      records.each { |record| expect(mapper).to receive(:record_to_event).with(record).and_call_original }
-      expect(specification.to_a).to eq(records)
+      class CustomMapper
+        include BatchMapping
+
+        def record_to_event(record)
+          record
+        end
+      end
+
+      specify "use batch mapping with custom mapper" do
+        mapper = CustomMapper.new
+        specification =
+          Specification.new(SpecificationReader.new(repository, mapper, mapping: RubyEventStore::BatchMapping))
+        records.each { |record| expect(mapper).to receive(:record_to_event).with(record).and_call_original }
+        expect(specification.to_a).to eq(records)
+      end
     end
   end
 end
