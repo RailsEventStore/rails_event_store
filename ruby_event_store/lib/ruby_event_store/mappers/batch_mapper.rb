92b0bcaf2460e238d7bba3eff03f08a97105871f
Mirosław Pragłowski <m@praglowski.com>
2025-05-29 09:00:52 +0200

    Replace BatchMapping module with a BatchMapper
    The idea:
    * keep existing mappers as is
    * wrap them with BatchMapper
    * use batch mapper always white transforming events
    * detect which kind of mapper is used (not specified yet how)
      and skip wrapping if not needed
    * deprecate single items mapper execution

---
+++ ruby_event_store/lib/ruby_event_store/mappers/batch_mapper.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module RubyEventStore
+  module Mappers
+    class BatchMapper
+      def initialize(mapper)
+        @mapper = mapper
+      end
+
+      def events_to_records(events)
+        events.map { |event| @mapper.event_to_record(event) }
+      end
+
+      def records_to_events(records)
+        records.map { |record| @mapper.record_to_event(record) }
+      end
+    end
+  end
+end
