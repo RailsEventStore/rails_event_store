43a5835a7de1a03ccfd932c3ed88d52eae728eac
Mirosław Pragłowski <m@praglowski.com>
2025-05-30 09:38:57 +0200

    Add instrumentation for batch mappers

---
+++ ruby_event_store/lib/ruby_event_store/mappers/instrumented_batch_mapper.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module RubyEventStore
+  module Mappers
+    class InstrumentedBatchMapper
+      def initialize(mapper, instrumentation)
+        @mapper = mapper
+        @instrumentation = instrumentation
+      end
+
+      def events_to_records(events)
+        instrumentation.instrument("serialize.mapper.rails_event_store", domain_events: events) do
+          mapper.events_to_records(events)
+        end
+      end
+
+      def records_to_events(records)
+        instrumentation.instrument("deserialize.mapper.rails_event_store", records: records) do
+          mapper.records_to_events(records)
+        end
+      end
+
+      private
+
+      attr_reader :instrumentation, :mapper
+    end
+  end
+end
