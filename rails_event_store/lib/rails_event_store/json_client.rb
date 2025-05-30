015dcf1b1f8ca254ff18b8c090408cf26d27f108
Mirosław Pragłowski <m@praglowski.com>
2025-05-30 10:25:20 +0200

    Use batch mappers in RailsEventStore clients
    And wrap them in batch instrumentation.

--- rails_event_store/lib/rails_event_store/json_client.rb
+++ rails_event_store/lib/rails_event_store/json_client.rb
@@ -5,45 +5,47 @@ require "json"
 module RailsEventStore
   class JSONClient < Client
     def initialize(
-      mapper: RubyEventStore::Mappers::PipelineMapper.new(
-        RubyEventStore::Mappers::Pipeline.new(
-          {
-            Symbol => {
-              serializer: ->(v) { v.to_s },
-              deserializer: ->(v) { v.to_sym },
-            },
-            Time => {
-              serializer: ->(v) { v.iso8601(RubyEventStore::TIMESTAMP_PRECISION) },
-              deserializer: ->(v) { Time.iso8601(v) },
-            },
-            ActiveSupport::TimeWithZone => {
-              serializer: ->(v) { v.iso8601(RubyEventStore::TIMESTAMP_PRECISION) },
-              deserializer: ->(v) { Time.iso8601(v).in_time_zone },
-              stored_type: ->(*) { "ActiveSupport::TimeWithZone" },
-            },
-            Date => {
-              serializer: ->(v) { v.iso8601 },
-              deserializer: ->(v) { Date.iso8601(v) },
-            },
-            DateTime => {
-              serializer: ->(v) { v.iso8601 },
-              deserializer: ->(v) { DateTime.iso8601(v) },
-            },
-            BigDecimal => {
-              serializer: ->(v) { v.to_s },
-              deserializer: ->(v) { BigDecimal(v) },
-            },
-          }.merge(
-            if defined?(OpenStruct)
-              { OpenStruct => { serializer: ->(v) { v.to_h }, deserializer: ->(v) { OpenStruct.new(v) } } }
-            else
-              {}
-            end,
-          )
-            .reduce(RubyEventStore::Mappers::Transformation::PreserveTypes.new) do |preserve_types, (klass, options)|
-              preserve_types.register(klass, **options)
-            end,
-          RubyEventStore::Mappers::Transformation::SymbolizeMetadataKeys.new,
+      mapper: RubyEventStore::Mappers::BatchMapper.new(
+        RubyEventStore::Mappers::PipelineMapper.new(
+          RubyEventStore::Mappers::Pipeline.new(
+            {
+              Symbol => {
+                serializer: ->(v) { v.to_s },
+                deserializer: ->(v) { v.to_sym },
+              },
+              Time => {
+                serializer: ->(v) { v.iso8601(RubyEventStore::TIMESTAMP_PRECISION) },
+                deserializer: ->(v) { Time.iso8601(v) },
+              },
+              ActiveSupport::TimeWithZone => {
+                serializer: ->(v) { v.iso8601(RubyEventStore::TIMESTAMP_PRECISION) },
+                deserializer: ->(v) { Time.iso8601(v).in_time_zone },
+                stored_type: ->(*) { "ActiveSupport::TimeWithZone" },
+              },
+              Date => {
+                serializer: ->(v) { v.iso8601 },
+                deserializer: ->(v) { Date.iso8601(v) },
+              },
+              DateTime => {
+                serializer: ->(v) { v.iso8601 },
+                deserializer: ->(v) { DateTime.iso8601(v) },
+              },
+              BigDecimal => {
+                serializer: ->(v) { v.to_s },
+                deserializer: ->(v) { BigDecimal(v) },
+              },
+            }.merge(
+              if defined?(OpenStruct)
+                { OpenStruct => { serializer: ->(v) { v.to_h }, deserializer: ->(v) { OpenStruct.new(v) } } }
+              else
+                {}
+              end,
+            )
+              .reduce(RubyEventStore::Mappers::Transformation::PreserveTypes.new) do |preserve_types, (klass, options)|
+                preserve_types.register(klass, **options)
+              end,
+            RubyEventStore::Mappers::Transformation::SymbolizeMetadataKeys.new,
+          ),
         ),
       ),
       repository: RubyEventStore::ActiveRecord::EventRepository.new(serializer: JSON),
