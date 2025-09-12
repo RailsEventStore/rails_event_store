b5d82d40756bdcfd3287862128ddd8261d287c28
Mirosław Pragłowski <m@praglowski.com>
2025-09-12 12:31:29 +0200

    Restore reverted lazy-loaded AR
    This reverts commit c54cb7c688dfe9572f7913e7b7f009148c310d1f.

--- ruby_event_store-active_record/lib/ruby_event_store/active_record/railtie.rb
+++ ruby_event_store-active_record/lib/ruby_event_store/active_record/railtie.rb
@@ -3,6 +3,12 @@
 module RubyEventStore
   module ActiveRecord
     class Railtie < ::Rails::Railtie
+      initializer "ruby_event_store-active_record" do
+        ActiveSupport.on_load(:active_record) do
+          require_relative "../active_record/skip_json_serialization"
+          require_relative "../active_record/event"
+        end
+      end
     end
   end
 end
