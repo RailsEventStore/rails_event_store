12f8e18508c25847ba730b6a3c0dc1b005cbc370
Paweł Pacana <pawel.pacana@gmail.com>
2025-10-14 14:08:14 +0200

    Dead code

--- support/helpers/schema_helper.rb
+++ support/helpers/schema_helper.rb
@@ -36,67 +36,6 @@ module SchemaHelper
     schema.read
   end
 
-  def build_schema(gemfile, template_name: nil)
-    run_in_subprocess(<<~EOF, env: ENV.to_h.slice("DATABASE_URL", "VERBOSE"))
-      require 'bundler/inline'
-
-      gemfile do
-        #{gemfile}
-      end
-
-      require 'ruby_event_store/active_record'
-      require 'ruby_event_store'
-      require 'logger'
-      require '../support/helpers/migrator'
-
-      $verbose = ENV.has_key?('VERBOSE') ? true : false
-      ActiveRecord::Schema.verbose = $verbose
-      ActiveRecord::Base.logger    = Logger.new(STDOUT) if $verbose
-      ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
-
-      gem_path = $LOAD_PATH.find { |path| path.match(/ruby_event_store/active_record/) }
-      Migrator.new(File.expand_path('ruby_event_store/active_record/generators/templates', gem_path))
-        .run_migration('create_event_store_events', #{template_name ? "'#{template_name}'" : "nil"})
-    EOF
-  end
-
-  def run_code(code, gemfile:)
-    run_in_subprocess(<<~EOF, env: ENV.to_h.slice("DATABASE_URL", "VERBOSE"))
-      require 'bundler/inline'
-
-      gemfile do
-        #{gemfile}
-      end
-
-      require 'ruby_event_store/active_record'
-      require 'ruby_event_store'
-      require 'logger'
-
-      $verbose = ENV.has_key?('VERBOSE') ? true : false
-      ActiveRecord::Schema.verbose = $verbose
-      ActiveRecord::Base.logger    = Logger.new(STDOUT) if $verbose
-      ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'])
-
-      #{code}
-    EOF
-  end
-
-  def validate_migration(source_gemfile, target_gemfile, source_template_name: nil)
-    begin
-      build_schema(source_gemfile, template_name: source_template_name)
-      establish_database_connection
-      yield
-      actual_schema = dump_schema
-      drop_database
-      close_database_connection
-      build_schema(target_gemfile)
-      establish_database_connection
-      expect(actual_schema).to eq(dump_schema)
-    ensure
-      drop_database
-    end
-  end
-
   private
 
   def template_path
