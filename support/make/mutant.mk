6476740537f974ad208eb06d531670fe072151c0
Tomasz Patrzek <patrzek.tomasz@gmail.com>
2026-04-21 20:39:52 +0200

    Skip Redis-dependent tests during mutant runs in sidekiq_scheduler
    Sidekiq's process_one does not work correctly inside mutant's fork
    isolation, causing neutral failures and blocking mutation analysis.
    Sets MUTANT=1 env var in all mutate make targets so spec_helper can
    reliably detect and exclude :redis-tagged tests.

--- support/make/mutant.mk
+++ support/make/mutant.mk
@@ -2,20 +2,20 @@ SINCE_SHA ?= master
 
 mutate: ## Run mutation tests
 	@echo "Running mutation tests"
-	@bundle exec mutant run \
+	@MUTANT=1 bundle exec mutant run \
  		$(if $(MUTANT_JOBS), --jobs $(MUTANT_JOBS)) \
 		$(SUBJECT)
 
 mutate-fast: ## Run mutation tests with --fail-fast
 	@echo "Running mutation tests"
-	@bundle exec mutant run \
+	@MUTANT=1 bundle exec mutant run \
  		$(if $(MUTANT_JOBS), --jobs $(MUTANT_JOBS)) \
 		--fail-fast \
 		$(SUBJECT)
 
 mutate-changes: ## Run incremental mutation tests
 	@echo "Running mutation tests"
-	@bundle exec mutant run \
+	@MUTANT=1 bundle exec mutant run \
  		$(if $(MUTANT_JOBS), --jobs $(MUTANT_JOBS)) \
 		--since $(SINCE_SHA) \
 		$(SUBJECT)
