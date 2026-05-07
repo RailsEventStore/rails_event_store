f9fe48bfce86190b7bfe9cb4755d19482148fb0a
Szymon Fiedler <szymon.fiedler@gmail.com>
2026-05-07 17:15:31 +0200

    Revert "Show gem path while installing/updating dependencies"
    This reverts commit f6701f554067b491a19d633b6eb0841a7100efb7.

--- support/make/install.mk
+++ support/make/install.mk
@@ -1,13 +1,11 @@
-CURRENT_DIR = $(dir $(abspath $(firstword $(MAKEFILE_LIST))))
-
-install: ## Install gem dependencies for current Gemfile
-	@echo "Installing gem dependencies: $(CURRENT_DIR)"
+install: ## Install gem dependencies for current Gemfile 
+	@echo "Installing gem dependencies"
 	@bundle install
 
 install-all: ## Install gem dependencies from all Gemfiles
-	@echo "Installing gem dependencies: $(CURRENT_DIR)"
+	@echo "Installing gem dependencies"
 	@find . -name Gemfile\* -a ! -name \*.lock -exec bundle install --gemfile {} \;
 
 update-all: ## Update gem dependencies in all Gemfiles
-	@echo "Updating gem dependencies: $(CURRENT_DIR)"
+	@echo "Updating gem dependencies"
 	@find . -name Gemfile\* -a ! -name \*.lock -exec bundle update --gemfile {} \;
