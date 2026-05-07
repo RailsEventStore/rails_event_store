f6701f554067b491a19d633b6eb0841a7100efb7
Mirosław Pragłowski <m@praglowski.com>
2026-05-07 10:12:27 +0200

    Show gem path while installing/updating dependencies
    Knowing where to look makes live easier when something is wrong.

--- support/make/install.mk
+++ support/make/install.mk
@@ -1,11 +1,13 @@
-install: ## Install gem dependencies for current Gemfile 
-	@echo "Installing gem dependencies"
+CURRENT_DIR = $(dir $(abspath $(firstword $(MAKEFILE_LIST))))
+
+install: ## Install gem dependencies for current Gemfile
+	@echo "Installing gem dependencies: $(CURRENT_DIR)"
 	@bundle install
 
 install-all: ## Install gem dependencies from all Gemfiles
-	@echo "Installing gem dependencies"
+	@echo "Installing gem dependencies: $(CURRENT_DIR)"
 	@find . -name Gemfile\* -a ! -name \*.lock -exec bundle install --gemfile {} \;
 
 update-all: ## Update gem dependencies in all Gemfiles
-	@echo "Updating gem dependencies"
+	@echo "Updating gem dependencies: $(CURRENT_DIR)"
 	@find . -name Gemfile\* -a ! -name \*.lock -exec bundle update --gemfile {} \;
