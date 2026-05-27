89de642155ca73ecae40de0f0c4bd0fb72bb4602
Tomasz Patrzek <patrzek.tomasz@gmail.com>
2026-05-27 19:05:09 +0200

    Remove rails_event_store_active_record gem rename deprecation warning
    Remove the runtime warn from the compat shim. The old gem name no longer
    emits warnings; users should have migrated to ruby_event_store-active_record.
    
    Based on deprecation-warnings commit 6954e5487

--- ruby_event_store-active_record/lib/rails_event_store_active_record.rb
+++ ruby_event_store-active_record/lib/rails_event_store_active_record.rb
@@ -1,14 +1,3 @@
-# frozen_string_literal: true
-
 require "ruby_event_store/active_record"
-require "ruby_event_store/deprecations"
 
-RubyEventStore::Deprecations.register(
-  :rails_event_store_active_record_renamed,
-  "The 'rails_event_store_active_record' gem has been renamed.\n" \
-    "Please use 'ruby_event_store-active_record' in your Gemfile instead.",
-)
-
-RubyEventStore::Deprecations.warn(:rails_event_store_active_record_renamed)
-
-RailsEventStoreActiveRecord = RubyEventStore::ActiveRecord unless defined?(RailsEventStoreActiveRecord)
+RailsEventStoreActiveRecord = RubyEventStore::ActiveRecord
