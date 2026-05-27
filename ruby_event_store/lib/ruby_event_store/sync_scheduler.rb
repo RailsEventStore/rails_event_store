caba09b46e5282da76b21a843ee27f857dfe7f7e
Tomasz Patrzek <patrzek.tomasz@gmail.com>
2026-05-27 18:26:17 +0200

    Remove class-as-subscriber support from SyncScheduler
    SyncScheduler now only accepts instances and callables.
    Passing a class is no longer supported.
    
    Original commit: https://github.com/RailsEventStore/rails_event_store/commit/fed065b91

--- ruby_event_store/lib/ruby_event_store/sync_scheduler.rb
+++ ruby_event_store/lib/ruby_event_store/sync_scheduler.rb
@@ -3,24 +3,11 @@
 module RubyEventStore
   class SyncScheduler
     def call(subscriber, event, _)
-      if Class === subscriber
-        Deprecations.warn(:class_subscriber)
-        subscriber = subscriber.new
-      end
       subscriber.call(event)
     end
 
     def verify(subscriber)
-      if Class === subscriber
-        Deprecations.warn(:class_subscriber)
-        begin
-          subscriber.new.respond_to?(:call)
-        rescue ArgumentError
-          false
-        end
-      else
-        subscriber.respond_to?(:call)
-      end
+      subscriber.respond_to?(:call)
     end
   end
 end
