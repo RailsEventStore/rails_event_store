9680bdac4b2841dba563db819d91c24057087a36
Tomasz Patrzek <patrzek.tomasz@gmail.com>
2026-05-27 18:29:03 +0200

    Remove EventClassRemapper
    Replace with Upcast transformation. Remove events_class_remapping option
    from Default and JSONMapper mappers, and from protobuf mapper.
    
    Original commit: https://github.com/RailsEventStore/rails_event_store/commit/cabb98b16

--- ruby_event_store/lib/ruby_event_store/deprecated.rb
+++ ruby_event_store/lib/ruby_event_store/deprecated.rb
@@ -9,19 +9,6 @@ module RubyEventStore
   Deprecations.deprecate(Mappers::NullMapper, :initialize, key: :null_mapper)
 
   Deprecations.register(
-    :events_class_remapping_option,
-    "`events_class_remapping` option in `RubyEventStore::Mappers::Default` is deprecated and will be removed in the next major release.\n" \
-      "Use `RubyEventStore::Mappers::Transformation::Upcast` instead.",
-  )
-
-  Deprecations.register(
-    :event_class_remapper,
-    "`RubyEventStore::Mappers::Transformation::EventClassRemapper` is deprecated and will be removed in the next major release.\n" \
-      "Use `RubyEventStore::Mappers::Transformation::Upcast` instead.",
-  )
-  Deprecations.deprecate(Mappers::Transformation::EventClassRemapper, :initialize, key: :event_class_remapper)
-
-  Deprecations.register(
     :any_version_with_specific_position,
     "Mixing expected version :any and specific position (or :auto) is deprecated and will raise UnsupportedVersionAnyUsage in RubyEventStore 3.0.",
   )
--- ruby_event_store/lib/ruby_event_store/mappers/default.rb
+++ ruby_event_store/lib/ruby_event_store/mappers/default.rb
@@ -3,18 +3,8 @@
 module RubyEventStore
   module Mappers
     class Default < PipelineMapper
-      def initialize(events_class_remapping: {})
-        if events_class_remapping.any?
-          Deprecations.warn(:events_class_remapping_option)
-          super(
-            Pipeline.new(
-              Transformation::EventClassRemapper.new(events_class_remapping),
-              Transformation::SymbolizeMetadataKeys.new,
-            ),
-          )
-        else
-          super(Pipeline.new(Transformation::SymbolizeMetadataKeys.new))
-        end
+      def initialize
+        super(Pipeline.new(Transformation::SymbolizeMetadataKeys.new))
       end
     end
   end
