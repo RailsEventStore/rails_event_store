94d767f360ceb36a4c6ad747db8862ef72f0120c
Tomasz Patrzek <patrzek.tomasz@gmail.com>
2026-05-27 18:24:45 +0200

    Remove Dispatcher alias for SyncScheduler
    Replace all uses of RubyEventStore::Dispatcher with SyncScheduler.
    Update Broker default dispatcher and broker_lint shared examples.
    
    Original commit: https://github.com/RailsEventStore/rails_event_store/commit/c8bc3dae

--- ruby_event_store/lib/ruby_event_store.rb
+++ ruby_event_store/lib/ruby_event_store.rb
@@ -2,7 +2,6 @@
 
 require_relative "ruby_event_store/deprecations"
 require_relative "ruby_event_store/sync_scheduler"
-require_relative "ruby_event_store/dispatcher"
 require_relative "ruby_event_store/subscriptions"
 require_relative "ruby_event_store/broker"
 require_relative "ruby_event_store/in_memory_repository"
