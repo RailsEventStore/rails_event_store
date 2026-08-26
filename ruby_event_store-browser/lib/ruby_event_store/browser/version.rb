a988b71caa9bf20eb5b7c55cdea44ecf943ff414
Tomasz Patrzek <patrzek.tomasz@gmail.com>
2026-08-26 17:19:02 +0200

    Version v2.19.3

--- ruby_event_store-browser/lib/ruby_event_store/browser/version.rb
+++ ruby_event_store-browser/lib/ruby_event_store/browser/version.rb
@@ -2,6 +2,6 @@
 
 module RubyEventStore
   module Browser
-    VERSION = "2.19.2"
+    VERSION = "2.19.3"
   end
 end
--- ruby_event_store-rspec/lib/ruby_event_store/rspec/version.rb
+++ ruby_event_store-rspec/lib/ruby_event_store/rspec/version.rb
@@ -2,6 +2,6 @@
 
 module RubyEventStore
   module RSpec
-    VERSION = "2.19.2"
+    VERSION = "2.19.3"
   end
 end
